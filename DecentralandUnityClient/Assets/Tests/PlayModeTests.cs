56d6a8c058f826ca800334886cbec2439dca9345
Pravus <pravusjif@gmail.com>
2018-10-25 15:22:22 -0300

    chore: Added 'playMode' unit tests integration for CI and command line building script

---
+++ DecentralandUnityClient/Assets/Tests/PlayModeTests.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using System.Collections;
+
+/*
+ * Play Mode Testing Highlits:
+ * - All Monobehaviour methods are invoked
+ * - Tests run in a standalone window
+ * - Tests may run slower, depending on the build target
+ */
+
+public class PlayModeTests {
+    [Test]
+    public void PlayMode_EntityCreationTest() {
+        DecentralandEntity entityObject;
+
+        // We need to load the required objects as prefabs to test, as we can't skip a frame in a NUnit tests, so we can't get already-instantiated scene objects.
+        SceneController sceneController = Resources.Load<GameObject>("Prefabs/SceneController").GetComponent<SceneController>();
+
+        Assert.IsTrue(sceneController != null);
+
+        string entityJSON = "{\"entityIdParam\": \"1\"}";
+
+        sceneController.entities.Clear();
+        sceneController.CreateEntity(entityJSON);
+
+        sceneController.entities.TryGetValue("1", out entityObject);
+
+        Assert.IsTrue(entityObject != null);
+
+        Assert.AreEqual(entityObject.entityIdParam, "1");
+    }
+}
