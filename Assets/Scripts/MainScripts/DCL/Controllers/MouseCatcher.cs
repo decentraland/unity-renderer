93e2f3553beb8ea9cc0b1b6935bf3fbd518ed864
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2019-05-23 17:28:01 +0200

    fix: #313 on click bug in WebGL (#322)
    * OnClick fix for WebGL
    
    * Code Review and fix in character camera retrieval
    
    * LayerMask was being read as raycast max distance

--- Assets/Scripts/MainScripts/DCL/Controllers/MouseCatcher.cs
+++ Assets/Scripts/MainScripts/DCL/Controllers/MouseCatcher.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DCL.Components;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,11 +9,45 @@ namespace DCL
 {
     public class MouseCatcher : MonoBehaviour, IPointerDownHandler
     {
+        //Default OnClick
+        public LayerMask OnPointerDownTarget = 1 << 9;
+        private Camera characterCamera;
+
+        private void OnEnable()
+        {
+            RetrieveCharacterCamera();
+        }
+
+        private void RetrieveCharacterCamera()
+        {
+            if (DCLCharacterController.i == null)
+                return;
+
+            characterCamera = DCLCharacterController.i.GetComponentInChildren<Camera>();
+        }
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 UnlockCursor();
+            }
+
+            if (Cursor.lockState != CursorLockMode.None && Input.GetMouseButtonDown(0))
+            {
+                if (characterCamera == null)
+                {
+                    RetrieveCharacterCamera();
+                }
+
+                //Not sure if this is needed
+                if (characterCamera != null)
+                {
+                    if (Physics.Raycast(characterCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out var hitInfo, characterCamera.farClipPlane, OnPointerDownTarget))
+                    {
+                        hitInfo.rigidbody.gameObject.GetComponentInChildren<OnClickComponent>()?.OnPointerDown();
+                    }
+                }
             }
         }
 
