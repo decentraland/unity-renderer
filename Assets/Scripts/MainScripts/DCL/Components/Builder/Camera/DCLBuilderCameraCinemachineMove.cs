19f0b903e86aafd48e7c49f1e6cb646a1f5e82d4
Matias Jaureguiberry <matiasjaure@gmail.com>
2019-06-06 16:53:51 -0300

    feat: builder orbit camera  (#344)
    * BuilderScene
    
    * feat: builder orbit camera scene and scripts
    
    * feat: adding package manifest
    
    * feat: reset camera
    
    * feat: zoom in and out
    
    * fix: files removal and change requests
    
    * refactor: the Cinemachine camera rig now uses a platform-independent input layer. KB+M is mostly implemented already. Touch input is coded but not tested
    
    * feat: camera move with arrow keys + reset fix
    
    * fix: review fixes
    
    * fix: remove [expandable]
    
    * fix: buildscene missing reference
    
    * fix: null reference exception in camera move

---
+++ Assets/Scripts/MainScripts/DCL/Components/Builder/Camera/DCLBuilderCameraCinemachineMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SaveDuringPlay]
+[AddComponentMenu("")]
+public class DCLBuilderCameraCinemachineMove : CinemachineExtension
+{
+    Vector3 moveAccumulated;
+    Vector3 moveIncrement;
+    public void SetMoveAxis(Vector3 ma)
+    {
+        moveIncrement = ma;
+    }
+
+    public void Reset()
+    {
+        moveAccumulated = Vector3.zero;
+    }
+
+    protected override void PostPipelineStageCallback(
+       CinemachineVirtualCameraBase vcam,
+       CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        var pos = state.RawPosition;
+        moveAccumulated += vcam.transform.right * moveIncrement.x;
+        Vector3 rawForward = vcam.transform.forward;
+        rawForward.y = 0;
+        rawForward.Normalize();
+        moveAccumulated += rawForward * moveIncrement.z;
+        state.RawPosition = pos + moveAccumulated;
+        moveIncrement = Vector3.zero;
+
+    }
+}
---
+++ Assets/Scripts/MainScripts/DCL/Components/Builder/Camera/DCLBuilderKeyboardMouseControl.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DCLBuilderKeyboardMouseControl : MonoBehaviour
+{
+
+
+    public DCLBuilderCameraInput cameraInput;
+
+    [Header("Mouse modal mapping")]
+    public int mouseOrbitButton = 0;
+
+    public int mousePanButton = 1;
+
+    [Header("Axis mapping")]
+    public string orbitXAxisName = "Mouse X";
+
+    public string orbitYAxisName = "Mouse Y";
+
+    public string panXAxisName = "Mouse X";
+
+    public string panYAxisName = "Mouse Y";
+
+    public string zoomAxisName = "Mouse ScrollWheel";
+
+    [Header("Key mapping")]
+    public KeyCode[] resetKeys;
+
+    public KeyCode[] zoomInKeys;
+
+    public KeyCode[] zoomOutKeys;
+
+    public KeyCode[] moveForwardKeys;
+
+    public KeyCode[] moveBackwardsKeys;
+
+    public KeyCode[] moveRightKeys;
+
+    public KeyCode[] moveLeftKeys;
+
+
+    void Update()
+    {
+        // One timers with AnyKeyDown
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.RESET, GetAnyKeyDown(resetKeys));
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.ZOOM_IN, GetAnyKeyDown(zoomInKeys));
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.ZOOM_OUT, GetAnyKeyDown(zoomOutKeys));
+
+        // Continuous behaviour with AnyKey
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.MOVE_FORWARD, GetAnyKey(moveForwardKeys));
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.MOVE_BACKWARDS, GetAnyKey(moveBackwardsKeys));
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.MOVE_RIGHT, GetAnyKey(moveRightKeys));
+        cameraInput.SetButtonInput(DCLBuilderCameraInput.TButton.MOVE_LEFT, GetAnyKey(moveLeftKeys));
+
+        // Zoom is always enabled
+        cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ZOOM, Input.GetAxis(zoomAxisName));
+
+        if (Input.GetMouseButton(mouseOrbitButton)) // Orbit has priority over panning
+        {
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ORBIT_X, Input.GetAxis(orbitXAxisName));
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ORBIT_Y, Input.GetAxis(orbitYAxisName));
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.PAN_X, 0f);
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.PAN_Y, 0f);
+        }
+        else if (Input.GetMouseButton(mousePanButton)) // Panning stops orbit input
+        {
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ORBIT_X, 0f);
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ORBIT_Y, 0f);
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.PAN_X, Input.GetAxis(panXAxisName));
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.PAN_Y, Input.GetAxis(panYAxisName));
+        }
+        else // Both disabled
+        {
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ORBIT_X, 0f);
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.ORBIT_Y, 0f);
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.PAN_X, 0f);
+            cameraInput.SetAxisInput(DCLBuilderCameraInput.TAxis.PAN_Y, 0f);
+        }
+    }
+
+    private bool GetAnyKeyDown(IReadOnlyList<KeyCode> keys)
+    {
+        for (int i = 0; i < keys.Count; ++i)
+        {
+            if (Input.GetKeyDown(keys[i])) return true;
+        }
+
+        return false;
+    }
+
+    private bool GetAnyKey(IReadOnlyList<KeyCode> keys)
+    {
+        for (int i = 0; i < keys.Count; ++i)
+        {
+            if (Input.GetKey(keys[i])) return true;
+        }
+
+        return false;
+    }
+
+    private bool GetAnyKeyUp(IReadOnlyList<KeyCode> keys)
+    {
+        for (int i = 0; i < keys.Count; ++i)
+        {
+            if (Input.GetKeyUp(keys[i])) return true;
+        }
+
+        return false;
+    }
+}
