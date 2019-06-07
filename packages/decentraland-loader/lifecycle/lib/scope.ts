32f2c85c67ed8cc93fb7e80cdad5ae8d46f798d9
Esteban Ordano <esteban@decentraland.org>
2019-06-07 08:45:55 -0300

    fix: start listing parcels near the user (#229)
    * fix: start listing parcels near the user
    
    * rename var
    
    * split cache code
    
    * save some more cpu

--- packages/decentraland-loader/lifecycle/lib/scope.ts
+++ packages/decentraland-loader/lifecycle/lib/scope.ts
@@ -4,19 +4,34 @@ export interface ParcelConfigurationOptions {
   lineOfSightRadius: number
 }
 
-export function squareDiff(a: number, b: number) {
+export function squareAndSum(a: number, b: number) {
   return a * a + b * b
 }
 
+const cachedDeltas: Vector2Component[] = []
+
 export function parcelsInScope(config: ParcelConfigurationOptions, position: Vector2Component): string[] {
   const result: string[] = []
-  const squareRadius = config.lineOfSightRadius * config.lineOfSightRadius
-  for (let x = position.x - config.lineOfSightRadius; x <= position.x + config.lineOfSightRadius; x++) {
-    for (let y = position.y - config.lineOfSightRadius; y <= position.y + config.lineOfSightRadius; y++) {
-      if (squareDiff(x - position.x, y - position.y) <= squareRadius) {
-        result.push(`${x},${y}`)
+  let length = cachedDeltas.length
+  if (!length) {
+    calculateCachedDeltas(config)
+    length = cachedDeltas.length
+  }
+  for (let i = 0; i < length; i++) {
+    result.push(`${position.x + cachedDeltas[i].x},${position.y + cachedDeltas[i].y}`)
+  }
+  return result
+}
+
+function calculateCachedDeltas(config: ParcelConfigurationOptions) {
+  const limit = config.lineOfSightRadius
+  const squaredRadius = limit * limit
+  for (let x = -limit; x <= limit; x++) {
+    for (let y = -limit; y <= limit; y++) {
+      if (x * x + y * y <= squaredRadius) {
+        cachedDeltas.push({ x, y })
       }
     }
   }
-  return result
+  cachedDeltas.sort((a, b) => squareAndSum(a.x, a.y) - squareAndSum(b.x, b.y))
 }
