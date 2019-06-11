8b82c3885157a9a22db4a1f1739f4feff1107aca
Matias Jaureguiberry <matiasjaure@gmail.com>
2019-06-11 16:13:00 -0300

    fix: panning issues fix (#386)(#387) (#424)
    * Panning issue fix (#386)
    
    * fix: follow camera effect fix
    
    * fix: reset camera fix
    
    * Panning issue fix (#386)
    
    * fix: follow camera effect fix
    
    * fix: reset camera fix
    
    * fix: review fixes

--- Assets/Scripts/MainScripts/DCL/Components/Builder/Camera/DCLBuilderCameraCinemachineMove.cs
+++ Assets/Scripts/MainScripts/DCL/Components/Builder/Camera/DCLBuilderCameraCinemachineMove.cs
@@ -9,29 +9,75 @@ using UnityEngine;
 public class DCLBuilderCameraCinemachineMove : CinemachineExtension
 {
     Vector3 moveAccumulated;
+    Vector3 moveTargetAccumulated;
+    Vector3 panIncrementTarget;
+    Vector3 panAccumulated;
     Vector3 moveIncrement;
+    Vector3 panIncrement;
+
+    Vector3 panIncrementLast;
+
+
+    Vector3 lastPanIncrement = Vector3.zero;
+
+    Vector3 targetOriginalPosition;
+
+    Transform target;
+    public void Start()
+    {
+        targetOriginalPosition = Vector3.zero;
+        panIncrementLast = Vector3.zero;
+    }
     public void SetMoveAxis(Vector3 ma)
     {
         moveIncrement = ma;
     }
 
+    public void SetPanAxis(Vector3 pa)
+    {
+        panIncrement = pa;
+    }
+
     public void Reset()
     {
-        moveAccumulated = Vector3.zero;
+        moveTargetAccumulated = Vector3.zero;
+        panIncrementTarget = Vector3.zero;
+        panIncrementLast = Vector3.zero;
+
     }
 
     protected override void PostPipelineStageCallback(
        CinemachineVirtualCameraBase vcam,
        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
+        if (targetOriginalPosition == Vector3.zero)
+        {
+            target = vcam.LookAt.GetComponent<CinemachineTargetGroup>().m_Targets[0].target;
+            targetOriginalPosition = target.position;
+        }
+
         var pos = state.RawPosition;
-        moveAccumulated += vcam.transform.right * moveIncrement.x;
-        Vector3 rawForward = vcam.transform.forward;
-        rawForward.y = 0;
-        rawForward.Normalize();
-        moveAccumulated += rawForward * moveIncrement.z;
-        state.RawPosition = pos + moveAccumulated;
+
+        //Pan movement
+        Vector3 panInc = panIncrement - panIncrementLast;
+        panIncrementTarget += vcam.transform.right * panInc.x;
+
+        //Pan Movement Y
+        state.RawPosition = pos + new Vector3(0, panIncrement.y, 0);
+
+        //Move with arrow keys
+        moveTargetAccumulated += vcam.transform.right * moveIncrement.x;
+
+        Vector3 vCamForward = vcam.transform.forward;
+        vCamForward.y = 0;
+        vCamForward.Normalize();
+
+        moveTargetAccumulated += vCamForward * moveIncrement.z;
+        //Move target group
+        target.position = targetOriginalPosition + moveTargetAccumulated + panIncrementTarget;
+
         moveIncrement = Vector3.zero;
+        panIncrementLast = panIncrement;
 
     }
 }
