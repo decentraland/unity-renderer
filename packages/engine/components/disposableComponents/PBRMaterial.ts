914354788f67fd46437673ea6a63e7d3474061a4
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2019-06-13 16:16:31 -0300

    chore: added auto to transparency mode (#250)
    * Added auto to transparency mode and updated Babylon to behave properly
    
    * formatting 1
    
    * formatting 2
    
    * Removed optional in textureMode and changed AUTO to 4

--- packages/engine/components/disposableComponents/PBRMaterial.ts
+++ packages/engine/components/disposableComponents/PBRMaterial.ts
@@ -28,7 +28,7 @@ const defaults = {
   bumpTexture: '',
   refractionTexture: '',
   disableLighting: false,
-  transparencyMode: 0,
+  transparencyMode: 4,
   hasAlpha: false
 }
 
@@ -101,7 +101,11 @@ export class PBRMaterial extends DisposableComponent {
     }
 
     if ('transparencyMode' in data) {
-      m.transparencyMode = Math.min(3, Math.max(0, validators.int(data.transparencyMode, defaults.transparencyMode)))
+      if (data.transparencyMode === 4) {
+        m.transparencyMode = null
+      } else {
+        m.transparencyMode = Math.min(3, Math.max(0, validators.int(data.transparencyMode, defaults.transparencyMode)))
+      }
     }
 
     if ('emissiveIntensity' in data) {
