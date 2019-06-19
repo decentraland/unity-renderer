2b5d6baa624473b465feb2bfd92830164c13f95e
Pravus <pravus@decentraland.org>
2019-06-19 17:20:04 -0300

    chore: update chat scene (#262)
    * Removed unused code from chat window scene. Improved chat objects hierarchy.
    
    * Removed OnEnter event from UIShapes. Updated chat scene
    
    * Updated chat window maximize/minimize logic

--- packages/ui/avatar/chatWindow.ts
+++ packages/ui/avatar/chatWindow.ts
@@ -1,14 +1,12 @@
-import { DecentralandInterface, IEvents } from 'decentraland-ecs/src/decentraland/Types'
-import { Color4, Texture, OnEnter } from 'decentraland-ecs/src'
-import { OnTextSubmit, OnBlur, OnChanged, OnClick, OnFocus } from 'decentraland-ecs/src/decentraland/UIEvents'
+import { DecentralandInterface } from 'decentraland-ecs/src/decentraland/Types'
+import { Color4 } from 'decentraland-ecs/src'
+import { OnTextSubmit, OnBlur, OnFocus } from 'decentraland-ecs/src/decentraland/UIEvents'
 
 import {
-  UIImage,
   UIInputText,
   UIText,
   UIContainerStack,
   UIContainerRect,
-  UIFullScreen,
   UIShape,
   UIScrollRect
 } from 'decentraland-ecs/src/decentraland/UIShapes'
@@ -19,11 +17,8 @@ import { execute } from './rpc'
 import { screenSpaceUI } from './ui'
 
 declare var dcl: DecentralandInterface
-declare var require: any
 
-const UI_CHAT = require('../../../static/images/ui-chat.png')
-const uiChatTexture = new Texture(UI_CHAT)
-
+const INITIAL_INPUT_TEXT_COLOR = Color4.Gray()
 const PRIMARY_TEXT_COLOR = Color4.White()
 const COMMAND_COLOR = Color4.FromHexString('#ffd7a9ff')
 
@@ -38,76 +33,27 @@ dcl.onEvent(event => {
   }
 })
 
-function createCloseButton(parent: UIShape, click: (ev: IEvents['onClick']) => void) {
-  const component = new UIImage(parent, uiChatTexture)
-  component.name = 'close-icon'
-  component.width = '20px'
-  component.height = '20px'
-  component.sourceWidth = 35
-  component.sourceHeight = 35
-  component.sourceTop = 5
-  component.sourceLeft = 96
-  component.hAlign = 'right'
-  component.vAlign = 'bottom'
-  component.positionX = '-10px'
-  component.isPointerBlocker = true
-  component.visible = false
-  component.onClick = new OnClick(click)
-
-  return { component }
-}
-
-function createTextInput(
-  parent: UIShape,
-  onChanged: (value?: any, pointerId?: Number) => void,
-  onFocus: () => void,
-  onBlur: () => void
-) {
-  const component = new UIInputText(parent)
-  component.name = 'input'
-  component.autoStretchWidth = false
-  component.color = PRIMARY_TEXT_COLOR
-  component.background = Color4.Clear()
-  component.focusedBackground = Color4.Clear()
-  component.placeholder = 'Say something to nearby people...'
-  component.fontSize = 12
-  component.width = 380
-  component.height = 20
-  component.thickness = 0
-  component.vAlign = 'bottom'
-  component.hAlign = 'left'
-  component.vTextAlign = 'bottom'
-  component.hTextAlign = 'left'
-  component.positionX = '10px'
-  component.positionY = '15px'
-  component.value = ''
-  component.isPointerBlocker = true
-  component.onChanged = new OnChanged(onChanged)
-  component.onFocus = new OnFocus(onFocus)
-  component.onBlur = new OnBlur(onBlur)
-  component.onTextSubmit = new OnTextSubmit(onInputSubmit)
-
-  return { component }
-}
-
-function createMessage(parent: UIShape, props: { sender: string; message: string; isCommand?: boolean }) {
+function createLogMessage(parent: UIShape, props: { sender: string; message: string; isCommand?: boolean }) {
   const { sender, message, isCommand } = props
   const color = isCommand ? COMMAND_COLOR : PRIMARY_TEXT_COLOR
-  const component = new UIText(parent)
 
-  component.color = color
-  component.value = `<b>${sender}:</b> ${message}`
-  component.fontSize = 12
-  component.vTextAlign = 'top'
-  component.hTextAlign = 'left'
-  component.vAlign = 'top'
-  component.hAlign = 'left'
-  component.adaptWidth = false
-  component.adaptHeight = true
-  component.width = '300px'
-  component.textWrapping = true
+  const messageText = new UIText(parent)
+  messageText.color = color
+  messageText.value = `<b>${sender}:</b> ${message}`
+  messageText.fontSize = 15
+  messageText.vAlign = 'top'
+  messageText.hAlign = 'left'
+  messageText.vTextAlign = 'top'
+  messageText.hTextAlign = 'left'
+  messageText.width = '350px'
+  messageText.adaptWidth = false
+  messageText.adaptHeight = true
+  messageText.textWrapping = true
+  messageText.outlineColor = Color4.Black()
 
-  return { component: component }
+  messagesLogScrollContainer.valueY = 0
+
+  return { component: messageText }
 }
 
 // -------------------------------
@@ -120,58 +66,78 @@ const internalState = {
 
 let isMaximized: boolean = false
 
-const containerMinimized = initializeMinimizedChat(screenSpaceUI)
+const chatContainer = new UIContainerRect(screenSpaceUI)
+chatContainer.name = 'chat-container'
+chatContainer.color = Color4.Clear()
+chatContainer.vAlign = 'bottom'
+chatContainer.hAlign = 'left'
+chatContainer.width = '400px'
+chatContainer.height = '300px'
+chatContainer.positionX = 10
+chatContainer.positionY = 10
+chatContainer.thickness = 0
 
-const container = new UIContainerRect(screenSpaceUI)
-container.name = 'gui-container'
-container.color = new Color4(0, 0, 0, 0.2)
-container.vAlign = 'bottom'
-container.hAlign = 'left'
-container.width = 400
-container.height = 320
-container.positionX = 20
-container.positionY = 0
-container.thickness = 0
-container.visible = false
+const chatInnerTopContainer = new UIContainerRect(chatContainer)
+chatInnerTopContainer.color = Color4.Clear()
+chatInnerTopContainer.name = 'inner-top-container'
+chatInnerTopContainer.vAlign = 'top'
+chatInnerTopContainer.hAlign = 'center'
+chatInnerTopContainer.width = '100%'
+chatInnerTopContainer.height = '84%'
 
-const transparentContainer = new UIScrollRect(screenSpaceUI)
-transparentContainer.name = 'gui-transparent-container'
-transparentContainer.vAlign = 'bottom'
-transparentContainer.hAlign = 'left'
-transparentContainer.width = '400px'
-transparentContainer.height = '250px'
-transparentContainer.positionX = '20px'
-transparentContainer.positionY = '70px'
-transparentContainer.valueY = 1
-transparentContainer.isVertical = false
-transparentContainer.isHorizontal = false
-transparentContainer.visible = true
+const messagesLogScrollContainer = new UIScrollRect(chatInnerTopContainer)
+messagesLogScrollContainer.name = 'messages-log-scroll-container'
+messagesLogScrollContainer.vAlign = 'top'
+messagesLogScrollContainer.hAlign = 'center'
+messagesLogScrollContainer.width = '100%'
+messagesLogScrollContainer.height = '90%'
+messagesLogScrollContainer.positionY = '-10px'
+messagesLogScrollContainer.valueY = 1
+messagesLogScrollContainer.isVertical = false
+messagesLogScrollContainer.isHorizontal = false
+messagesLogScrollContainer.visible = true
 
-const transparentMessageContainer = new UIContainerStack(transparentContainer)
-transparentMessageContainer.vAlign = 'bottom'
-transparentMessageContainer.hAlign = 'left'
-transparentMessageContainer.width = '100%'
-transparentMessageContainer.height = '100%'
-transparentMessageContainer.positionX = '10px'
-transparentMessageContainer.spacing = 6
+const messagesLogStackContainer = new UIContainerStack(messagesLogScrollContainer)
+messagesLogStackContainer.name = 'messages-log-stack-container'
+messagesLogStackContainer.vAlign = 'bottom'
+messagesLogStackContainer.hAlign = 'center'
+messagesLogStackContainer.width = '90%'
+messagesLogStackContainer.height = '100%'
+messagesLogStackContainer.spacing = 10
+messagesLogStackContainer.positionX = '-10px'
 
-const footerContainer = new UIContainerRect(screenSpaceUI)
-footerContainer.adaptHeight = true
-footerContainer.adaptWidth = true
-footerContainer.vAlign = 'bottom'
-footerContainer.hAlign = 'left'
-footerContainer.width = 400
-footerContainer.height = 25
-footerContainer.positionX = 20
-footerContainer.positionY = 20
-footerContainer.isPointerBlocker = true
+const textInputContainer = new UIContainerRect(chatContainer)
+textInputContainer.color = Color4.Clear()
+textInputContainer.name = 'input-text-container'
+textInputContainer.vAlign = 'bottom'
+textInputContainer.hAlign = 'center'
+textInputContainer.width = '100%'
+textInputContainer.height = '15%'
 
-const textInput = createTextInput(footerContainer, onInputChanged, onInputFocus, onInputBlur)
+const textInput = new UIInputText(textInputContainer)
+textInput.name = 'input-text'
+textInput.autoStretchWidth = false
+textInput.color = INITIAL_INPUT_TEXT_COLOR
+textInput.background = Color4.Clear()
+textInput.focusedBackground = Color4.Clear()
+textInput.placeholder = 'Press enter and start talking...'
+textInput.fontSize = 15
+textInput.width = '90%'
+textInput.height = '100%'
+textInput.thickness = 0
+textInput.vAlign = 'center'
+textInput.hAlign = 'center'
+textInput.positionX = '-5px'
+textInput.vTextAlign = 'center'
+textInput.hTextAlign = 'left'
+textInput.value = ''
+textInput.textWrapping = true
+textInput.isPointerBlocker = true
+textInput.onFocus = new OnFocus(onInputFocus)
+textInput.onBlur = new OnBlur(onInputBlur)
+textInput.onTextSubmit = new OnTextSubmit(onInputSubmit)
 
-createCloseButton(container, toggleChat)
 setMaximized(isMaximized)
-
-// Initialize chat scene
 
 export async function initializeChat() {
   const chatCmds = await execute('ChatController', 'getChatCommands', [null])
@@ -182,19 +148,22 @@ export async function initializeChat() {
   }
 }
 
-function toggleChat() {
-  setMaximized(!isMaximized)
-}
+function setMaximized(newMaximizedValue: boolean) {
+  if (isMaximized == newMaximizedValue) return
 
-function setMaximized(maximized: boolean) {
-  container.visible = maximized
-  containerMinimized.visible = !maximized
-  transparentContainer.isVertical = maximized
-  isMaximized = maximized
+  if (newMaximizedValue && !isMaximized) {
+    textInput.value = ''
 
-  if (!maximized) {
-    textInput.component.value = ''
+    chatInnerTopContainer.color = new Color4(0, 0, 0, 0.2)
+    textInputContainer.color = new Color4(0, 0, 0, 0.2)
+  } else if (isMaximized) {
+    chatInnerTopContainer.color = Color4.Clear()
+    textInputContainer.color = Color4.Clear()
   }
+
+  isMaximized = newMaximizedValue
+
+  messagesLogScrollContainer.isVertical = isMaximized
 }
 
 function onInputFocus() {
@@ -203,10 +172,6 @@ function onInputFocus() {
 
 function onInputBlur() {
   setMaximized(false)
-}
-
-function onInputChanged(message: string) {
-  // noop
 }
 
 async function onInputSubmit(e: { text: string }) {
@@ -225,33 +190,5 @@ async function sendMsg(messageToSend: string) {
 
 function addMessage(messageEntry: MessageEntry): void {
   internalState.messages.push(messageEntry)
-  createMessage(transparentMessageContainer, messageEntry)
-}
-
-function initializeMinimizedChat(parent: UIFullScreen) {
-  const containerMinimized = new UIContainerRect(parent)
-  containerMinimized.name = 'gui-container-minimized'
-  containerMinimized.adaptHeight = true
-  containerMinimized.adaptWidth = true
-  containerMinimized.vAlign = 'bottom'
-  containerMinimized.hAlign = 'left'
-  containerMinimized.positionX = 20
-  containerMinimized.positionY = 15
-  containerMinimized.thickness = 0
-
-  const minimizedIcon = new UIImage(containerMinimized, uiChatTexture)
-  minimizedIcon.name = 'minimize-icon'
-  minimizedIcon.width = '230px'
-  minimizedIcon.height = '55px'
-  minimizedIcon.sourceWidth = 210
-  minimizedIcon.sourceHeight = 50
-  minimizedIcon.sourceTop = 50
-  minimizedIcon.sourceLeft = 0
-  minimizedIcon.hAlign = 'right'
-  minimizedIcon.vAlign = 'top'
-  minimizedIcon.isPointerBlocker = true
-  minimizedIcon.onClick = new OnClick(toggleChat)
-  minimizedIcon.onEnter = new OnEnter(() => setMaximized(true))
-
-  return containerMinimized
+  createLogMessage(messagesLogStackContainer, messageEntry)
 }
