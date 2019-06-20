7408f207c4cc5d43f59d886aaa014021aea070d9
Pravus <pravus@decentraland.org>
2019-06-20 19:49:06 -0300

    fix: disable FPS Panel gameobject by default (#487)
    * Disabled FPS Panel gameobject by default (it is enabled when on debug mode)
    
    * Updated build version to 0.4.7. 
    
    * Updated WSSController generated debugging URL

--- Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -2,7 +2,7 @@ namespace DCL.Configuration
 {
     public static class ApplicationSettings
     {
-        public static string version = "0.4.6";
+        public static string version = "0.4.7";
     }
 
     public static class Environment
