e77213ce60fbec9fbbb4f5c8a44a24f2db4ede5d
Pravus <pravus@decentraland.org>
2019-07-19 15:47:42 -0300

    chore: update unity build 0.5.9

--- Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class ApplicationSettings
     {
-        public static string version = "0.5.8";
+        public static string version = "0.5.9";
     }
 
     public static class Environment
