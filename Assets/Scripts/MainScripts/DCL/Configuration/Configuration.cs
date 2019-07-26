29fd8175c2e4224056b99d02d573a8e97f44b5d2
Pravus <pravus@decentraland.org>
2019-07-26 11:30:40 -0300

    chore: update build version to v 0.6.3

--- Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class ApplicationSettings
     {
-        public static string version = "0.6.2";
+        public static string version = "0.6.3";
     }
 
     public static class Environment
