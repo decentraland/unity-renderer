913a7647ed53bb2630623af0c2378e283836a13e
Pravus <pravus@decentraland.org>
2019-07-31 16:28:07 -0300

    fix: exposed 'visible' and 'withCollisions' shape properties (#375)

--- packages/decentraland-ecs/src/decentraland/Components.ts
+++ packages/decentraland-ecs/src/decentraland/Components.ts
@@ -148,14 +148,12 @@ export class Billboard extends ObservableComponent {
 export class Shape extends ObservableComponent {
   /**
    * Set to true to turn on the collider for the entity.
-   * @alpha
    */
   @ObservableComponent.field
   withCollisions: boolean = false
 
   /**
    * Defines if the entity and its children should be rendered
-   * @alpha
    */
   @ObservableComponent.field
   visible: boolean = true
