144b02b870be04b9280a48b7b78f57094563cd1b
Pravus <pravus@decentraland.org>
2019-08-02 14:07:43 -0300

    chore: update NFTShape test parcel (#376)

--- public/test-parcels/-103.102.nft/game.ts
+++ public/test-parcels/-103.102.nft/game.ts
@@ -1,11 +1,21 @@
 import { NFTShape, Entity, engine, Transform, Vector3, Color3 } from 'decentraland-ecs/src'
 
 const entity = new Entity()
-const shapeComponent = new NFTShape('ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536', Color3.Green())
+const shapeComponent = new NFTShape('ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536')
 entity.addComponent(shapeComponent)
 entity.addComponent(
   new Transform({
-    position: new Vector3(4, 1.5, 4)
+    position: new Vector3(3, 1.5, 4)
   })
 )
 engine.addEntity(entity)
+
+const entity2 = new Entity()
+const shapeComponent2 = new NFTShape('ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536', Color3.Green())
+entity2.addComponent(shapeComponent2)
+entity2.addComponent(
+  new Transform({
+    position: new Vector3(5, 1.5, 4)
+  })
+)
+engine.addEntity(entity2)
