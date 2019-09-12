084b3d74514e7b510cbb94bdd5c1e1571006a557
Brian Amadori <brian@decentraland.org>
2019-09-12 18:56:06 -0300

    chore: change version number 0.7.7 (#836)

--- Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class ApplicationSettings
     {
-        public static string version = "0.7.6";
+        public static string version = "0.7.7";
     }
 
     public static class Environment
