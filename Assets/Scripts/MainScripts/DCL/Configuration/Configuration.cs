6acca0c387968a0f3c74ee330e461073a5155896
Brian Amadori <brian@decentraland.org>
2019-09-19 19:25:06 -0300

    update version number to 0.7.10 (#890)

--- Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class ApplicationSettings
     {
-        public static string version = "0.7.91";
+        public static string version = "0.7.10";
     }
 
     public static class Environment
