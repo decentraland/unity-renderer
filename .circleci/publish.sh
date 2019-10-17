9b6a7cfee00e2f47bbe198c3c500e829225021a2
Esteban Ordano <esteban@decentraland.org>
2019-10-17 20:02:04 -0300

    feat: report version on generated npm package (#1027)
    * feat: report version on generated npm package
    
    * fix: module(s)
    
    * fix: export(s)
    
    * fix: saner version of build.sh
    
    * fix: drop test info
    
    * fix path on script
    
    * fix: path on script
    
    * test TAG
    
    * fix: tag name
    
    * fix: tag and `

---
+++ .circleci/publish.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+export MAJOR=`[ "$CIRCLE_BRANCH" == "release" ] && echo "3" \
+    || ([ "$CIRCLE_BRANCH" == "staging" ] && echo "2" \
+    || ([ "$CIRCLE_BRANCH" == "master" ] && echo "1" \
+    || echo "0"))`;
+export MINOR=`cat Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs | grep 'version = "[0-9.]\+' | grep -o '\.[0-9.]\+'`
+export TAG=`[ "$CIRCLE_BRANCH" == "master" ] && echo "" || echo "$CIRCLE_BRANCH" | sed -e 's/[^a-zA-Z0-9-]/-/g'`
+export BUILD_VERSIONING=${MAJOR}${MINOR}'.'${CIRCLE_BUILD_NUM}`[ "$TAG" ] && echo "-$TAG"`
+
+echo "Building package.json for $BUILD_VERSIONING"
+
+
+function publish() {
+    # Build package.json
+    cd $BUILD_PATH
+    echo "Build path is $BUILD_PATH and cwd is $PWD -- Tag is $TAG"
+    echo '{"name": "decentraland-renderer", "version": "'${BUILD_VERSIONING}'", "license": "Apache-2.0", "devDependencies": { "npm": "5.6.0" } }' > package.json
+
+    # Export the name of the package as a file
+    echo 'module.exports = "'${BUILD_VERSIONING}'";' > index.js
+
+    # Delete unnecessary files from the build
+    rm game.js index.html Build/UnityLoader.js Build/unity.json
+    # Move all `.unityweb` files into the root build folder
+    mv Build/* .
+    # Publish on npm
+    npx npm publish --tag `[ "$CIRCLE_BRANCH" == "master" ] && echo "latest" || echo $TAG`
+}
+
+publish
+
