6d74625802f948ac0a692726da32143944edd8ea
Esteban Ordano <esteban@decentraland.org>
2019-10-31 18:49:21 -0300

    fix: inventory notifications (#620)
    * fix: inventory notifications
    
    * fix: actually run tests
    
    * fix: dont send more than one notification for multiple new items
    
    * Update test/unit/passport.saga.test.tsx
    
    Co-Authored-By: Miguel Oliva <moliva@users.noreply.github.com>
    
    * Update packages/shared/passports/sagas.ts
    
    * Update packages/shared/passports/sagas.ts

--- test/unit/passport.saga.test.tsx
+++ test/unit/passport.saga.test.tsx
@@ -1,9 +1,13 @@
 import { expectSaga } from 'redux-saga-test-plan'
 import { call, select } from 'redux-saga/effects'
-import { profileServerRequest, handleFetchProfile } from 'shared/passports/sagas'
-import { getProfileDownloadServer } from 'shared/passports/selectors'
 import { getAccessToken } from 'shared/auth/selectors'
-import { passportRequest, passportSuccess } from 'shared/passports/actions'
+import { notifyNewInventoryItem, passportRequest, passportSuccess } from 'shared/passports/actions'
+import {
+  compareInventoriesAndTriggerNotification,
+  handleFetchProfile,
+  profileServerRequest
+} from 'shared/passports/sagas'
+import { getProfile, getProfileDownloadServer } from 'shared/passports/selectors'
 
 describe('fetchProfile behavior', () => {
   it('behaves normally', () => {
@@ -14,6 +18,33 @@ describe('fetchProfile behavior', () => {
         [call(profileServerRequest, 'server', 'userId', 'access-token'), { data: 'profile' }]
       ])
       .put(passportSuccess('userId', 'profile' as any))
+      .run()
+  })
+})
+
+describe('notifications behavior', () => {
+  const getReturnsNull = (_: any) => undefined
+  const getReturnsYes = (_: any) => 'notified'
+  const noopSave = (_: any, __: any) => undefined
+  const profile = {}
+  const userId = 'userId'
+  it('triggers on new item', () => {
+    expectSaga(compareInventoriesAndTriggerNotification, userId, [], ['newItem'], getReturnsNull, noopSave)
+      .provide([[select(getProfile, userId), profile]])
+      .put(notifyNewInventoryItem())
+      .run()
+  })
+  it('does not trigger if already sent', () => {
+    expectSaga(compareInventoriesAndTriggerNotification, userId, [], ['newItem'], getReturnsYes, noopSave)
+      .provide([[select(getProfile, userId), profile]])
+      .not.put(notifyNewInventoryItem())
+      .run()
+  })
+  it('does not trigger multiple notifications if more than one item', () => {
+    expectSaga(compareInventoriesAndTriggerNotification, userId, [], ['newItem', 'newItem2'], getReturnsYes, noopSave)
+      .provide([[select(getProfile, userId), profile]])
+      .put(notifyNewInventoryItem())
+      .not.put(notifyNewInventoryItem())
       .run()
   })
 })
