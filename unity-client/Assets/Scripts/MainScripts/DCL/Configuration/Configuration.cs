8c3d449869040101061a2716b6f2ffef347d4eac
Esteban Ordano <esteban@decentraland.org>
2019-11-12 03:56:26 -0300

    chore: add dcl-renderer builds (#45)

--- unity-client/Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class ApplicationSettings
     {
-        public static string version = "0.7";
+        public static string version = "0.8";
     }
 
     public static class Environment
