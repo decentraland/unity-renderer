a9caf3695564625580182a82fd8d6c856d58c7a9
Pablo de Haro <pablitar@gmail.com>
2020-02-03 18:12:31 -0300

    hotfix: Reverting content url to lambdas

--- kernel/packages/config/index.ts
+++ kernel/packages/config/index.ts
@@ -231,7 +231,7 @@ export function getServerConfigurations() {
     landApi: `https://api.decentraland.${TLDDefault}/v1`,
     content: AWS
       ? `https://content.decentraland.${TLDDefault === 'today' ? 'org' : TLDDefault}`
-      : `https://bot1-katalyst.decentraland.zone/lambdas/contentv2`,
+      : `https://katalyst-lambdas.decentraland.zone/contentv2`,
     contentUpdate: `https://katalyst-content.decentraland.zone`,
     contentAsBundle: `https://content-assets-as-bundle.decentraland.org`,
     worldInstanceUrl: `wss://world-comm.decentraland.${TLDDefault}/connect`,
