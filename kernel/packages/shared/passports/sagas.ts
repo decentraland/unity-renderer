79d6ffb19617b1ed213758bab131fbc1e2d6f11d
moliva <oliva.miguelh@gmail.com>
2020-02-04 12:36:31 -0300

    hotfix: lint fix

--- kernel/packages/shared/passports/sagas.ts
+++ kernel/packages/shared/passports/sagas.ts
@@ -55,7 +55,7 @@ import { AuthIdentity, Authenticator, AuthLink } from '../crypto/Authenticator'
 
 const CID = require('cids')
 import { sha3 } from 'web3x/utils'
-import { getDefaultTLD, getContentUrl } from '../../config/index'
+import { getContentUrl } from '../../config/index'
 const multihashing = require('multihashing-async')
 const toBuffer = require('blob-to-buffer')
 
