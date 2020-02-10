73c7dbed0c64e3ff1aa70e68a50f7088b964bd7f
Miguel Oliva <moliva@users.noreply.github.com>
2020-02-10 18:48:34 -0300

    chore: usign infura project id (#512)

--- kernel/packages/config/index.ts
+++ kernel/packages/config/index.ts
@@ -298,13 +298,13 @@ export async function setNetwork(net: ETHEREUM_NETWORK) {
 
 export namespace ethereumConfigurations {
   export const mainnet = {
-    wss: 'wss://mainnet.infura.io/ws',
-    http: 'https://mainnet.infura.io/',
+    wss: 'wss://mainnet.infura.io/ws/v3/074a68d50a7c4e6cb46aec204a50cbf0',
+    http: 'https://mainnet.infura.io/v3/074a68d50a7c4e6cb46aec204a50cbf0/',
     etherscan: 'https://etherscan.io'
   }
   export const ropsten = {
-    wss: 'wss://ropsten.infura.io/ws',
-    http: 'https://ropsten.infura.io/',
+    wss: 'wss://ropsten.infura.io/ws/v3/074a68d50a7c4e6cb46aec204a50cbf0',
+    http: 'https://ropsten.infura.io/v3/074a68d50a7c4e6cb46aec204a50cbf0/',
     etherscan: 'https://ropsten.etherscan.io'
   }
 }
