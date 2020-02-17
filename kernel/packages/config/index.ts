872b5e4ee06b293c952bf414efd44407454b7fad
Esteban Ordano <esteban@decentraland.org>
2020-02-16 23:20:25 -0300

    fix: http -> https

--- kernel/packages/config/index.ts
+++ kernel/packages/config/index.ts
@@ -252,7 +252,7 @@ export function getServerConfigurations() {
   return {
     contentAsBundle: `https://content-assets-as-bundle.decentraland.org`,
     wearablesApi: `https://wearable-api.decentraland.org/v2`,
-    explorerConfiguration: `http://explorer-config.decentraland.${
+    explorerConfiguration: `https://explorer-config.decentraland.${
       TLDDefault === 'today' ? 'org' : TLDDefault
     }.s3.amazonaws.com/configuration.json`,
     avatar: {
