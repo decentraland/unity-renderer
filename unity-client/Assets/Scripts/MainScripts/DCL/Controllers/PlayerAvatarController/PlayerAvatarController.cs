27fc11fcff073e975465d0019413694460f7f6da
Brian Amadori <brian@decentraland.org>
2020-02-19 14:53:28 -0300

    fix draft (#601)

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/PlayerAvatarController/PlayerAvatarController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/PlayerAvatarController/PlayerAvatarController.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 public class PlayerAvatarController : MonoBehaviour
 {
     public AvatarRenderer avatarRenderer;
+    public float cameraDistanceToDeactivate = 1.0f;
 
-    UserProfile userProfile => UserProfile.GetOwnUserProfile();
-    bool repositioningWorld => DCLCharacterController.i.characterPosition.RepositionedWorldLastFrame();
+    private UserProfile userProfile => UserProfile.GetOwnUserProfile();
+    private bool repositioningWorld => DCLCharacterController.i.characterPosition.RepositionedWorldLastFrame();
+
+    private bool enableCameraCheck = false;
+    private Camera mainCamera;
 
     private void Awake()
     {
         //NOTE(Brian): We must wait for loading to finish before deactivating the renderer, or the GLTF Loader won't finish.
-        avatarRenderer.OnSuccessCallback -= Init;
-        avatarRenderer.OnFailCallback -= Init;
-        avatarRenderer.OnSuccessCallback += Init;
-        avatarRenderer.OnFailCallback += Init;
+        avatarRenderer.OnSuccessEvent -= Init;
+        avatarRenderer.OnFailEvent -= Init;
+        avatarRenderer.OnSuccessEvent += Init;
+        avatarRenderer.OnFailEvent += Init;
+
+        mainCamera = Camera.main;
     }
 
     private void Init()
     {
-        avatarRenderer.SetVisibility(false);
-        avatarRenderer.OnSuccessCallback -= Init;
-        avatarRenderer.OnFailCallback -= Init;
+        enableCameraCheck = true;
+        avatarRenderer.OnSuccessEvent -= Init;
+        avatarRenderer.OnFailEvent -= Init;
+    }
+
+    private void Update()
+    {
+        if (!enableCameraCheck || repositioningWorld)
+            return;
+
+        bool shouldBeVisible = Vector3.Distance(mainCamera.transform.position, transform.position) > cameraDistanceToDeactivate;
+
+        if (shouldBeVisible != avatarRenderer.gameObject.activeSelf)
+            avatarRenderer.SetVisibility(shouldBeVisible);
     }
     private void OnEnable()
     {
         userProfile.OnUpdate += OnUserProfileOnUpdate;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!repositioningWorld && other.CompareTag("MainCamera"))
-            avatarRenderer.SetVisibility(false);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (!repositioningWorld && other.CompareTag("MainCamera"))
-            avatarRenderer.SetVisibility(true);
     }
 
     private void OnUserProfileOnUpdate(UserProfile profile)
