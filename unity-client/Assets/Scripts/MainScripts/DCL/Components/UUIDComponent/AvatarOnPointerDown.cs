b909711d5c874058cb6236cd371004ba73b72621
Pravus <pravus@decentraland.org>
2020-03-17 18:53:04 -0300

    chore: avatar cursor interaction feedback (#731)
    * implemented hover feedback on other players avatar
    
    * created AvatarOnPointerDown to have an event when the pointer down is reported and use that from the AvatarShape
    
    * Added BooleanVariable scriptable object. 
    
    * Added PlayerInfoCardVisibleState common scriptable object. 
    
    * Added logic to disable hover feedback on the selected avatar if his info card is open

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Components/UUIDComponent/AvatarOnPointerDown.cs
@@ -0,0 +1,43 @@
+using DCL.Interface;
+using UnityEngine;
+using DCL.Helpers;
+
+namespace DCL.Components
+{
+    public class AvatarOnPointerDown : OnPointerDown
+    {
+        public event System.Action OnPointerDownReport;
+
+        void Awake()
+        {
+            CommonScriptableObjects.playerInfoCardVisibleState.OnChange += ReEnableOnInfoCardClosed;
+        }
+
+        void OnDestroy()
+        {
+            CommonScriptableObjects.playerInfoCardVisibleState.OnChange -= ReEnableOnInfoCardClosed;
+        }
+
+        public override void Report(WebInterface.ACTION_BUTTON buttonId, Ray ray, HitInfo hit)
+        {
+            if (!enabled) return;
+
+            if (ShouldReportEvent(buttonId, hit))
+            {
+                base.Report(buttonId, ray, hit);
+
+                SetHoverState(false);
+                enabled = false;
+
+                OnPointerDownReport?.Invoke();
+            }
+        }
+
+        void ReEnableOnInfoCardClosed(bool newState, bool prevState)
+        {
+            if (enabled || newState) return;
+
+            enabled = true;
+        }
+    }
+}
