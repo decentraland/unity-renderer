66c6ab7a2c5221176ba081a18ecc7aa96b5f080a
pbosio <pato@decentraland.org>
2020-03-31 17:15:24 -0300

    feat: add raise-hand avatar expression (#777)
    * added raise hand avatar expression
    
    * UI icons replaced and raise hand button animations added.
    
    * All animations merge into a single animator. Pressed loop animation turned off.
    
    Co-authored-by: Romina Marchetti <romi@decentraland.org>
    Co-authored-by: RominaMarchetti <51088292+RominaMarchetti@users.noreply.github.com>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PlayerAvatarController/ExpressionsHotKeyController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PlayerAvatarController/ExpressionsHotKeyController.cs
@@ -13,10 +13,12 @@ public class ExpressionsHotKeyController : MonoBehaviour
     [SerializeField] private ExpressionInputActionToId waveExpression;
     [SerializeField] private ExpressionInputActionToId robotExpression;
     [SerializeField] private ExpressionInputActionToId fistpumpExpression;
+    [SerializeField] private ExpressionInputActionToId raiseHandExpression;
 
     private InputAction_Trigger.Triggered waveDelegate;
     private InputAction_Trigger.Triggered robotDelegate;
     private InputAction_Trigger.Triggered fistpumpDelegate;
+    private InputAction_Trigger.Triggered raiseHandDelegate;
 
     private UserProfile ownUserProfile => UserProfile.GetOwnUserProfile();
 
@@ -30,6 +32,9 @@ public class ExpressionsHotKeyController : MonoBehaviour
 
         fistpumpDelegate = (x) => ExpressionCalled(fistpumpExpression.id);
         fistpumpExpression.inputAction.OnTriggered += fistpumpDelegate;
+
+        raiseHandDelegate = (x) => ExpressionCalled(raiseHandExpression.id);
+        raiseHandExpression.inputAction.OnTriggered += raiseHandDelegate;
     }
 
     private void ExpressionCalled(string id)
