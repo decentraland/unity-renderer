ab733f3e9ae6400847b509f6188300f6992f4452
Pravus <pravus@decentraland.org>
2020-04-09 13:44:32 -0300

    chore: disable navmap in production (#799)

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NavMap/NavmapView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NavMap/NavmapView.cs
@@ -25,6 +25,9 @@ namespace DCL
         MinimapMetadata mapMetadata;
         bool cursorLockedBeforeOpening = true;
 
+        // TODO: remove this bool and its usage once the feature is ready to be shippped.
+        bool enableInProduction = false;
+
         public static bool isOpen
         {
             private set;
@@ -58,6 +61,10 @@ namespace DCL
         internal void ToggleNavMap()
         {
             if (MapRenderer.i == null) return;
+
+#if !UNITY_EDITOR
+            if (!enableInProduction) return;
+#endif
 
             scrollRect.StopMovement();
 
