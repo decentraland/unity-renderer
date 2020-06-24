2cccd422f8e68f08c30db7a3b347c9e61ac0a9a4
pbosio <pato@decentraland.org>
2020-06-24 14:35:40 -0300

    chore: remove old tutorial scene (#1058)

--- kernel/scripts/generateTutorialSceneContents.sh
+++
@@ -1,9 +0,0 @@
-content="["
-for dir in `find ../static/loader/tutorial-scene -type f`; do
-    if [[ $dir != *".DS_Store"*  && $dir != *"generate_contents"* && $dir != *"tutorialSceneContents.json"* ]]; then
-        path=${dir:32}
-        content+="{\"file\": \"$path\", \"hash\": \"$path\"},"
-    fi
-done
-content=${content%?}"]"
-echo $content > ../packages/decentraland-loader/lifecycle/tutorial/tutorialSceneContents.json
