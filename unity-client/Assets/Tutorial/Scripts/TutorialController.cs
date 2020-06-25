a804e3532af4a647195118486b1fdce9f2d7bda5
pbosio <pato@decentraland.org>
2020-06-25 19:57:18 -0300

    chore: remove old tutorial from unity (#1060)

--- unity-client/Assets/Tutorial/Scripts/TutorialController.cs
+++ unity-client/Assets/Tutorial/Scripts/TutorialController.cs
@@ -9,31 +9,12 @@ namespace DCL.Tutorial
     {
         public static TutorialController i { private set; get; }
 
-#if UNITY_EDITOR
-        [Header("Debugging")]
-        public bool debugRunTutorialOnStart = false;
-        public TutorialStep.Id debugFlagStartingValue;
-#endif
-
         public bool isTutorialEnabled { private set; get; } = false;
-
-        [Header("Stage Controller References")]
-        [SerializeField] List<TutorialStep> steps = new List<TutorialStep>();
-        [SerializeField] GameObject emailPromptHUDGameObject;
-
-        TutorialStep runningStep = null;
-        TutorialStep.Id currentTutorialStepId = TutorialStep.Id.NONE;
-        int currentTutorialStepIndex = 0;
-        bool initialized = false;
-        Canvas chatUIScreen = null;
 
         public void SetTutorialEnabled()
         {
             CommonScriptableObjects.rendererState.OnChange += OnRenderingStateChanged;
-
             isTutorialEnabled = true;
-
-            AirdroppingHUDController.OnAirdropFinished += TriggerEmailPrompt;
         }
 
         private void Awake()
@@ -41,81 +22,11 @@ namespace DCL.Tutorial
             i = this;
         }
 
-#if UNITY_EDITOR
-        private void Start()
-        {
-            if (!debugRunTutorialOnStart)
-                return;
-
-            isTutorialEnabled = true;
-
-            CommonScriptableObjects.rendererState.OnChange += OnRenderingStateChanged;
-        }
-#endif
 
         private void OnDestroy()
         {
-            AirdroppingHUDController.OnAirdropFinished -= TriggerEmailPrompt;
             CommonScriptableObjects.rendererState.OnChange -= OnRenderingStateChanged;
-
             i = null;
-        }
-
-        Coroutine executeStepsCoroutine;
-
-        private void StartTutorialFromStep(TutorialStep.Id stepId)
-        {
-            if (!initialized)
-                Initialize();
-
-            if (runningStep != null)
-            {
-                StopCoroutine(executeStepsCoroutine);
-
-                runningStep.OnStepFinished();
-                Destroy(runningStep.gameObject);
-
-                runningStep = null;
-            }
-
-            executeStepsCoroutine = StartCoroutine(ExecuteSteps(stepId));
-        }
-
-        public void SkipToNextStep()
-        {
-            int nextStepIndex = currentTutorialStepIndex + 1;
-
-            if (nextStepIndex == steps.Count)
-                return;
-
-            StartTutorialFromStep(steps[nextStepIndex].stepId);
-        }
-
-        private IEnumerator ExecuteSteps(TutorialStep.Id startingStep)
-        {
-            int startingStepIndex = GetStepIndexFromTutorialStepId(startingStep);
-
-            for (int i = startingStepIndex; i < steps.Count; i++)
-            {
-                var stepPrefab = steps[i];
-
-                runningStep = Instantiate(stepPrefab).GetComponent<TutorialStep>();
-
-                currentTutorialStepId = runningStep.stepId;
-                currentTutorialStepIndex = i;
-
-                UserProfile.GetOwnUserProfile().SetTutorialStepId((int)currentTutorialStepId);
-
-                runningStep.OnStepStart();
-                yield return runningStep.OnStepExecute();
-                runningStep.OnStepFinished();
-
-                Destroy(runningStep.gameObject);
-            }
-
-            currentTutorialStepId = TutorialStep.Id.FINISHED;
-            UserProfile.GetOwnUserProfile().SetTutorialStepId((int)currentTutorialStepId);
-            runningStep = null;
         }
 
         private int GetTutorialStepFromProfile()
@@ -126,67 +37,6 @@ namespace DCL.Tutorial
         private void OnRenderingStateChanged(bool renderingEnabled, bool prevState)
         {
             if (!isTutorialEnabled || !renderingEnabled) return;
-
-            currentTutorialStepId = (TutorialStep.Id)GetTutorialStepFromProfile();
-
-#if UNITY_EDITOR
-            if (debugFlagStartingValue != 0)
-            {
-                currentTutorialStepId = debugFlagStartingValue;
-            }
-#endif
-            if (currentTutorialStepId == TutorialStep.Id.FINISHED || runningStep != null)
-                return;
-
-            StartTutorialFromStep(currentTutorialStepId);
-        }
-
-        private void Initialize()
-        {
-            if (initialized)
-                return;
-
-            initialized = true;
-            CacheChatScreen();
-        }
-
-        private void CacheChatScreen()
-        {
-            if (chatUIScreen == null && DCL.SceneController.i)
-            {
-                ParcelScene uiScene = DCL.SceneController.i.loadedScenes[DCL.SceneController.i.globalSceneId];
-                chatUIScreen = uiScene.uiScreenSpace.canvas;
-            }
-        }
-
-        public void SetChatVisible(bool visible)
-        {
-            if (chatUIScreen != null)
-            {
-                chatUIScreen.enabled = visible;
-            }
-        }
-
-        private int GetStepIndexFromTutorialStepId(TutorialStep.Id step)
-        {
-            int result = 0;
-            int stepsCount = steps.Count;
-
-            for (int i = 0; i < stepsCount; i++)
-            {
-                if (steps[i].stepId == step)
-                {
-                    result = i;
-                    break;
-                }
-            }
-
-            return result;
-        }
-
-        public void TriggerEmailPrompt()
-        {
-            emailPromptHUDGameObject.SetActive(true);
         }
     }
 }
