dacea18fe7fb74070e103107696057c2d8652e53
Pravus <pravus@decentraland.org>
2020-07-06 16:11:17 -0300

    fix: performance drop on expression animation call (#1081)
    * Added check to avoid processing the whole profile catalogue when the profile is updated with the AvatarHUD hidden (as is the case when executen expression animations)
    
    * Updated avatar HUD tests
    
    * Added batchmode check to deal with the breaking tests

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDController.cs
@@ -14,6 +14,7 @@ public class AvatarEditorHUDController : IHUD
     [NonSerialized] public bool bypassUpdateAvatarPreview = false;
     private UserProfile userProfile;
     private WearableDictionary catalog;
+    bool renderingEnabled => CommonScriptableObjects.rendererState.Get();
     private readonly Dictionary<string, List<WearableItem>> wearablesByCategory = new Dictionary<string, List<WearableItem>>();
     protected readonly AvatarEditorHUDModel model = new AvatarEditorHUDModel();
 
@@ -40,7 +41,7 @@ public class AvatarEditorHUDController : IHUD
 
         SetCatalog(catalog);
 
-        LoadUserProfile(userProfile);
+        LoadUserProfile(userProfile, true);
         this.userProfile.OnUpdate += LoadUserProfile;
     }
 
@@ -61,7 +62,12 @@ public class AvatarEditorHUDController : IHUD
 
     public void LoadUserProfile(UserProfile userProfile)
     {
-        if (userProfile?.avatar == null || string.IsNullOrEmpty(userProfile.avatar.bodyShape)) return;
+        LoadUserProfile(userProfile, false);
+    }
+
+    public void LoadUserProfile(UserProfile userProfile, bool forceLoading)
+    {
+        if ((!Application.isBatchMode && !forceLoading && renderingEnabled && !view.isOpen) || userProfile?.avatar == null || string.IsNullOrEmpty(userProfile.avatar.bodyShape)) return;
 
         var bodyShape = CatalogController.wearableCatalog.Get(userProfile.avatar.bodyShape);
         if (bodyShape == null)
