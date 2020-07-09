3ad01cb49ad31fcd5dddbdec4be5ca30bb9dba5a
Brian Amadori <brian@decentraland.org>
2020-07-09 12:14:36 -0300

    fix: add hiccup metrics (#1093)

--- unity-renderer/Assets/Scripts/MainScripts/Debugging/FPSDisplay.cs
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/FPSDisplay.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Globalization;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace DCL
@@ -34,30 +36,72 @@ namespace DCL
             new FPSColor(Color.red, 0),
         };
 
+        private double totalHiccupCount;
+        private double hiccupsPerMinute;
+        private double hiccupsPerMinuteMax;
+
+        private double fastFramesCount;
+        private double totalFramesCount;
+
+        private double avgHiccupPeak;
+
+        private const double HICCUP_THRESHOLD = 0.034; //NOTE(Brian): A bit over 30 FPS
+
         void Update()
         {
-            if (label != null)
+            if (label == null)
+                return;
+
+            float dt = Time.unscaledDeltaTime;
+
+            if (dt > HICCUP_THRESHOLD)
             {
-                deltas[end++] = Time.deltaTime;
-                if (end == 1000) end = 0;
-                countBetween += Time.deltaTime;
-                while (countBetween > 1.0f)
-                {
-                    countBetween -= deltas[begin++];
-                    if (begin == 1000) begin = 0;
-                }
-                float fps = end > begin ? end - begin : 1000 + end - begin;
-                string fpsFormatted = fps.ToString("##");
-                string msFormatted = (Time.deltaTime * 1000).ToString("##");
-                string targetText = $"Current {msFormatted} ms (fps: {fpsFormatted})";
-
-                if (label.text != targetText)
-                {
-                    label.text = targetText;
-                }
-
-                DisplayColor(label, fps);
+                totalHiccupCount++;
+                avgHiccupPeak += dt;
             }
+
+            hiccupsPerMinute = totalHiccupCount / (Time.realtimeSinceStartup / 60.0f);
+            hiccupsPerMinuteMax = Math.Max(hiccupsPerMinuteMax, hiccupsPerMinute);
+
+            deltas[end++] = dt;
+            if (end == 1000) end = 0;
+            countBetween += dt;
+
+            while (countBetween > 1.0f)
+            {
+                countBetween -= deltas[begin++];
+                if (begin == 1000) begin = 0;
+            }
+
+            float fps = end > begin ? end - begin : 1000 + end - begin;
+
+            if (dt < 0.033f)
+            {
+                fastFramesCount++;
+            }
+
+            totalFramesCount++;
+
+            string fpsFormatted = fps.ToString("##");
+            string msFormatted = (dt * 1000).ToString("##");
+
+            string hpsMaxFormatted = hiccupsPerMinuteMax.ToString("##.00", NumberFormatInfo.InvariantInfo);
+            string fps30percentileFormatted = ((fastFramesCount / totalFramesCount) * 100).ToString("##");
+            string avgHiccupPeakFormatted = ((avgHiccupPeak / totalHiccupCount) * 1000).ToString("##");
+
+            string targetText = string.Empty;
+
+            targetText += $"Avg hiccup ms: {avgHiccupPeakFormatted} ms\n";
+            targetText += $"Max hiccup count p/m: {hpsMaxFormatted}\n";
+            targetText += $"30 fps percentile: {fps30percentileFormatted}%\n";
+            targetText += $"Current {msFormatted} ms (fps: {fpsFormatted})";
+
+            if (label.text != targetText)
+            {
+                label.text = targetText;
+            }
+
+            DisplayColor(label, fps);
         }
 
         void DisplayColor(Text label, float fps)
