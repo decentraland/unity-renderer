730c4a727cecb8b9302891bb8713a5b3b5d2c0ad
pbosio <pato@decentraland.org>
2020-07-08 22:15:48 -0300

    feat: chat message when friend goes online (#1089)

--- kernel/packages/shared/comms/chat.ts
+++ kernel/packages/shared/comms/chat.ts
@@ -1,9 +1,11 @@
-import { MessageEntry, ChatMessageType } from 'shared/types'
+import { MessageEntry, ChatMessageType, PresenceStatus, UpdateUserStatusMessage } from 'shared/types'
 import { uuid } from 'atomicHelpers/math'
 import { StoreContainer } from '../store/rootTypes'
 import { messageReceived } from '../chat/actions'
+import { getProfile } from 'shared/profiles/selectors'
 
 declare const globalThis: StoreContainer
+let friendStatus: Record<string, PresenceStatus> = {}
 
 export enum ChatEventType {
   MESSAGE_RECEIVED = 'MESSAGE_RECEIVED',
@@ -24,4 +26,36 @@ export function notifyStatusThroughChat(status: string) {
       body: status
     })
   )
+}
+
+export function notifyFriendOnlineStatusThroughChat(userStatus: UpdateUserStatusMessage) {
+  const friendName = getProfile(globalThis.globalStore.getState(), userStatus.userId)?.name
+
+  if (friendName === undefined) {
+    return
+  }
+
+  if (!friendStatus[friendName]) {
+    friendStatus[friendName] = userStatus.presence
+    return
+  }
+
+  if (!(userStatus.realm?.layer && userStatus.realm?.serverName)) {
+    if (userStatus.presence !== PresenceStatus.ONLINE) {
+      friendStatus[friendName] = userStatus.presence
+    }
+    return
+  }
+
+  if (userStatus.presence === PresenceStatus.ONLINE && friendStatus[friendName] === PresenceStatus.OFFLINE) {
+    let message = `${friendName} joined ${userStatus.realm?.serverName}-${userStatus.realm?.layer}`
+
+    if (userStatus.position) {
+      message += ` ${userStatus.position.x}, ${userStatus.position.y}`
+    }
+
+    notifyStatusThroughChat(message)
+  }
+
+  friendStatus[friendName] = userStatus.presence
 }
