f487597c02044db8ad84ea84657f4fd1fcadf665
pablitar <pablo@decentraland.org>
2020-07-17 10:09:37 -0300

    chore: Configured the bot to click on the login components (#1115)
    Added a couple of clicks to the bot script to ensure that it can start with the new lobby

--- kernel/scripts/runLoops.ts
+++ kernel/scripts/runLoops.ts
@@ -2,22 +2,26 @@ const puppeteer = require('puppeteer')
 
 const position = process.env.BOT_POSITION ? process.env.BOT_POSITION : '20,20'
 const host = process.env.HOST ? process.env.HOST : 'https://explorer.decentraland.zone'
+const env = process.env.EXPLORER_ENV ? process.env : 'zone'
 
 async function main() {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox']
-  });
+  })
   const page = await browser.newPage()
 
-  page.on('console', msg => {
+  page.on('console', (msg) => {
     for (let i = 0; i < msg.args().length; ++i) {
       console.info(`${i}: ${msg.args()[i]}`)
     }
   })
 
-  await page.goto(`${host}/?position=${position}&ws=ws%3A%2F%2Flocalhost%3A5001%2Floop&NO_TUTORIAL`, {
+  await page.goto(`${host}/?position=${position}&ws=ws%3A%2F%2Flocalhost%3A5001%2Floop&NO_TUTORIAL&ENV=${env}`, {
     waitUntil: 'networkidle2'
   })
+
+  await page.click('#agree-check')
+  await page.click('#eth-login-confirm-button')
 
   while (true) {
     // await page.screenshot({ path: `example-${i++}.png` })
