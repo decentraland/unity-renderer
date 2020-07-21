4b9c7f77defc4b11fc86a7ac635a45fe8e07ca4e
Esteban Ordano <esteban@decentraland.org>
2020-07-21 22:12:28 +0200

    chore: tweak hiccup metric (part 2) (#1119)
    * new metric draft
    
    * feat: new hiccup metrics
    
    * feat: report hiccups
    
    * fix: actually call hiccup report
    
    * fix merge issue
    
    Co-authored-by: Brian Amadori <brian@decentraland.org>

---
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/FPSDisplay/FPSDisplay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DCL.FPSDisplay
+{
+
+    public class FPSDisplay : MonoBehaviour
+    {
+        public Text label;
+        private readonly LinealBufferHiccupCounter counter = new LinealBufferHiccupCounter();
+
+        void Update()
+        {
+            if (label == null)
+                return;
+
+            float dt = Time.unscaledDeltaTime;
+
+            counter.AddDeltaTime(dt);
+            float fps = counter.CurrentFPSCount();
+
+            string fpsFormatted = fps.ToString("##");
+            string msFormatted = (dt * 1000).ToString("##");
+
+            string targetText = string.Empty;
+
+            string NO_DECIMALS = "##";
+            string TWO_DECIMALS = "##.00";
+            targetText += $"Hiccups in the last 1000 frames: {counter.CurrentHiccupCount()}\n";
+            targetText += $"Hiccup loss: {(100.0f * counter.GetHiccupSum() / counter.GetTotalSeconds()).ToString(TWO_DECIMALS)}% ({counter.GetHiccupSum().ToString(TWO_DECIMALS)} in {counter.GetTotalSeconds().ToString(TWO_DECIMALS)} secs)\n";
+            targetText += $"Bad Frames Percentile: {((counter.CurrentHiccupCount()) / 10.0f).ToString(NO_DECIMALS)}%\n";
+            targetText += $"Current {msFormatted} ms (fps: {fpsFormatted})";
+
+            if (label.text != targetText)
+            {
+                label.text = targetText;
+            }
+
+            FPSColoring.DisplayColor(label, fps);
+        }
+    }
+}
---
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/FPSDisplay/LinealBufferHiccupCounter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace DCL.FPSDisplay
+{
+    public class LinealBufferHiccupCounter
+    {
+        private readonly LinealBufferFPSCounter counter = new LinealBufferFPSCounter();
+        public const float HICCUP_THRESHOLD_IN_SECONDS = 1 / (float)FPSEvaluation.BAD;
+        private int hiccupsCountInBuffer = 0;
+        private float hiccupsSum = 0.0f;
+        private float bufferCount = 0.0f;
+
+        public int HiccupsCountInBuffer { get => hiccupsCountInBuffer; private set => hiccupsCountInBuffer = value; }
+        public float HiccupsSum { get => hiccupsSum; set => hiccupsSum = value; }
+
+        public float CurrentFPSCount()
+        {
+            return counter.CurrentFPSCount();
+        }
+
+        public float GetTotalSeconds()
+        {
+            return bufferCount;
+        }
+
+        public void AddDeltaTime(float valueInSeconds)
+        {
+            if (IsHiccup(counter.values[counter.end]))
+            {
+                hiccupsCountInBuffer -= 1;
+                hiccupsSum -= counter.values[counter.end];
+            }
+            if (IsHiccup(valueInSeconds))
+            {
+                hiccupsCountInBuffer += 1;
+                hiccupsSum += valueInSeconds;
+            }
+            bufferCount -= counter.values[counter.end];
+            bufferCount += valueInSeconds;
+
+            counter.AddDeltaTime(valueInSeconds);
+        }
+
+        public int CurrentHiccupCount()
+        {
+            return hiccupsCountInBuffer;
+        }
+
+        private bool IsHiccup(float value)
+        {
+            return value > HICCUP_THRESHOLD_IN_SECONDS;
+        }
+
+        public float GetHiccupSum()
+        {
+            return hiccupsSum;
+        }
+    }
+}
