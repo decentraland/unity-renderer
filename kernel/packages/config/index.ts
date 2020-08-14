83dd7d7a1646d5ab0aa4d645cf976f5001483033
Brian Amadori <brian@decentraland.org>
2020-08-14 16:56:11 -0300

    rename DEBUG_LOG to DEBUG_SCENE_LOG (#1183)

--- kernel/packages/config/index.ts
+++ kernel/packages/config/index.ts
@@ -112,12 +112,7 @@ export const AUTO_CHANGE_REALM = location.search.indexOf('AUTO_CHANGE_REALM') !=
 
 export const LOS = qs.LOS
 
-export const DEBUG =
-  location.search.indexOf('DEBUG_MODE') !== -1 ||
-  location.search.indexOf('DEBUG_LOG') !== -1 ||
-  !!(global as any).mocha ||
-  PREVIEW ||
-  EDITOR
+export const DEBUG = location.search.indexOf('DEBUG_MODE') !== -1 || !!(global as any).mocha || PREVIEW || EDITOR
 export const DEBUG_ANALYTICS = location.search.indexOf('DEBUG_ANALYTICS') !== -1
 export const DEBUG_MOBILE = location.search.indexOf('DEBUG_MOBILE') !== -1
 export const DEBUG_MESSAGES = location.search.indexOf('DEBUG_MESSAGES') !== -1
@@ -126,6 +121,7 @@ export const DEBUG_WS_MESSAGES = location.search.indexOf('DEBUG_WS_MESSAGES') !=
 export const DEBUG_REDUX = location.search.indexOf('DEBUG_REDUX') !== -1
 export const DEBUG_LOGIN = location.search.indexOf('DEBUG_LOGIN') !== -1
 export const DEBUG_PM = location.search.indexOf('DEBUG_PM') !== -1
+export const DEBUG_SCENE_LOG = location.search.indexOf('DEBUG_SCENE_LOG') !== -1
 
 export const INIT_PRE_LOAD = location.search.indexOf('INIT_PRE_LOAD') !== -1
 
