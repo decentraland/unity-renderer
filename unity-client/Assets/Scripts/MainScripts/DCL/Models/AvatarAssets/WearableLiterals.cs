c2d380c3c5034ebefc4dc7d50ca96292abe2f912
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2020-09-21 10:37:29 +0200

    Wearables now retry multiple times before failing. (#1332)

--- unity-client/Assets/Scripts/MainScripts/DCL/Models/AvatarAssets/WearableLiterals.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Models/AvatarAssets/WearableLiterals.cs
@@ -25,6 +25,18 @@ public static class WearableLiterals
         public const string MOUTH = "mouth";
         public const string FACIAL = "facial";
         public const string HAIR = "hair";
+
+        //TODO: Implement an IReadOnlyCollection for HashSet to make them immutable
+        public static readonly HashSet<string> ALL  = new HashSet<string>
+        {
+            UPPER_BODY,
+            LOWER_BODY,
+            EYEBROWS,
+            FACIAL,
+            MOUTH,
+            FEET,
+            EYES,
+        };
     }
 
     public static class BodyShapes
@@ -44,7 +56,7 @@ public static class WearableLiterals
 
     public static class DefaultWearables
     {
-        private static readonly Dictionary<(string, string), string> defaultWearables = new Dictionary<(string, string), string>()
+        private static readonly Dictionary<(string, string), string> defaultWearables = new Dictionary<(string, string), string>
         {
             { (BodyShapes.MALE, Categories.EYES), "dcl://base-avatars/eyes_00" },
             { (BodyShapes.MALE, Categories.EYEBROWS), "dcl://base-avatars/eyebrows_00" },
