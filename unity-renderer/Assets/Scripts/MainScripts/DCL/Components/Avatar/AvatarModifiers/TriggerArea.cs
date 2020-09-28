5841b1d342da682b032fe4e085fe239205cc66fb
Nicolas Chamo <nicolas@chamo.com.ar>
2020-09-28 14:18:29 -0300

    fix: some perf improvements to avatar modifier area

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarModifiers/TriggerArea.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarModifiers/TriggerArea.cs
@@ -15,7 +15,12 @@ public class BoxTriggerArea : TriggerArea
 
     public override HashSet<GameObject> DetectAvatars(Vector3 center, Quaternion rotation)
     {
-        Collider[] colliders = Physics.OverlapBox(center, box / 2, rotation, LayerMask.GetMask("AvatarTriggerDetection"), QueryTriggerInteraction.Collide);
+        Collider[] colliders = Physics.OverlapBox(center, box * 0.5f, rotation, LayerMask.GetMask("AvatarTriggerDetection"), QueryTriggerInteraction.Collide);
+        if (colliders.Length == 0)
+        {
+            return null;
+        }
+
         HashSet<GameObject> result = new HashSet<GameObject>();
         foreach (Collider collider in colliders)
         {
