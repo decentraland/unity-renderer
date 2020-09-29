fbeec544932a2a86d8e90622dcb560b11668c53a
Agustin Mendez <agustin.mz.92@gmail.com>
2020-09-29 10:10:30 -0300

    hotfix: deployment script is now aware of symbolic links (#1372)

--- kernel/scripts/_utils.ts
+++ kernel/scripts/_utils.ts
@@ -22,6 +22,14 @@ export function copyFile(from: string, to: string) {
     throw new Error(`${from} does not exist`)
   }
 
+  // if it is not a file, remove it to avoid conflict with symbolic links
+  if (fs.existsSync(to)) {
+    const type = fs.lstatSync(to)
+    if (!type.isFile()) {
+      fs.removeSync(to)
+    }
+  }
+
   fs.copySync(from, to)
 
   if (!fs.existsSync(to)) {
