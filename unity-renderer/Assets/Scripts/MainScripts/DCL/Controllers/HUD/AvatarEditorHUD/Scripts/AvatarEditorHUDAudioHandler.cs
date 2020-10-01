65cd0239b505e808cb74ea489ec067b9a8130ed2
Mordi <martin.lande@hotmail.com>
2020-10-01 18:13:47 +0200

    fix: avatareditor hud audio design (#1295)
    * fix: Replaced and renamed all voice sounds
    
    * fix: Implemented audio in the "collectibles" tab
    
    It was missing audio there before.
    
    * fix: Redesigned rarity sounds
    
    * fix: Small edits to wearable sounds
    
    * fix: Rarity sound playing on skin/eye color change
    
    * fix: Remade clothes sounds. Added functionality to AudioEvents
    
    * fix: Separated voice reactions for common and rare wearables
    
    * fix: Rerecorded female voice reaction
    
    Added two new ones.
    
    * fix: Added music, changed volume of eyewear-sounds
    
    * fix: Revised wearable sounds, tried to fix avatar appear sounds when unselecting wearable
    
    The old solution was better - will revert.
    
    * chore: Saved project
    
    * fix: Issue where the wearable sounds would play when exiting the Avatar Editor
    
    * fix: Re-added audio to the collectibles tab
    
    * fix: Issue where list-item-appear would keep pitching up when exiting/entering Avatar Editor several times in a row
    
    * fix: Change file format of avatar editor HUD music
    
    Going from .wav to .ogg saves about 8 MB per minute.
    
    * feat: Added new male reaction sounds and rarity sounds
    
    Co-authored-by: Brian Amadori <brian@decentraland.org>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDAudioHandler.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDAudioHandler.cs
@@ -10,31 +10,39 @@ public class AvatarEditorHUDAudioHandler : MonoBehaviour
     [SerializeField]
     AvatarEditorHUDView view;
     [SerializeField]
+    ItemSelector nftItemSelector;
+    [SerializeField]
     Button randomizeButton;
     [SerializeField]
-    AudioEvent eventRarity, eventAvatarAppear, eventReactionMale, eventReactionFemale, eventWearableClothing, eventWearableEyewear, eventWearableJewelry,
+    AudioEvent eventMusic, eventRarity, eventAvatarAppear, eventReactionMale, eventReactionFemale, eventWearableClothing, eventWearableEyewear, eventWearableJewelry,
         eventWearableFootwear, eventWearableHair, eventWearableHatMask, eventWearableRarity;
 
-    WearableItem lastClickedWearable = null;
+    WearableItem lastSelectedWearable = null;
+    bool hasClickedRandomize = false, wearableIsSameAsPrevious = false;
+
+    IEnumerator musicFadeOut;
 
     private void Start()
     {
         int nPairs = view.wearableGridPairs.Length;
         for (int i = 0; i < nPairs; i++)
         {
-            view.wearableGridPairs[i].selector.OnItemClicked += OnWearableClicked;
+            view.wearableGridPairs[i].selector.OnItemClicked += OnSelectWearable;
         }
 
-        view.OnAvatarAppear += OnAvatarAppear;
+        nftItemSelector.OnItemClicked += OnSelectWearable;
 
-        if (randomizeButton != null)
-            randomizeButton.onClick.AddListener(ResetLastClickedWearable);
+        view.OnSetVisibility += OnSetAvatarEditorVisibility;
+        view.OnRandomize += OnClickRandomize;
     }
 
-    void OnWearableClicked(string wearableId)
+    void OnSelectWearable(string wearableId)
     {
         var wearable = CatalogController.wearableCatalog.Get(wearableId);
-        lastClickedWearable = wearable;
+        wearableIsSameAsPrevious = (wearable == lastSelectedWearable);
+        if (wearableIsSameAsPrevious) return;
+
+        lastSelectedWearable = wearable;
         if (wearable == null) return;
 
         switch (wearable.category)
@@ -83,9 +91,30 @@ public class AvatarEditorHUDAudioHandler : MonoBehaviour
         }
     }
 
+    void OnEyeColorChanged(Color color)
+    {
+        ResetLastClickedWearable();
+    }
+
+    void OnSkinColorChanged(Color color)
+    {
+        ResetLastClickedWearable();
+    }
+
+    void OnHairColorChanged(Color color)
+    {
+        ResetLastClickedWearable();
+    }
+
+    void OnClickRandomize()
+    {
+        hasClickedRandomize = true;
+        ResetLastClickedWearable();
+    }
+
     void ResetLastClickedWearable()
     {
-        lastClickedWearable = null;
+        lastSelectedWearable = null;
     }
 
     void OnAvatarAppear(AvatarModel model)
@@ -93,55 +122,118 @@ public class AvatarEditorHUDAudioHandler : MonoBehaviour
         if (!view.isOpen) return;
 
         eventAvatarAppear.Play(true);
-        PlayRarity();
 
-        // Play voice reaction from the avatar
-        if (Random.Range(0f, 1f) > 0.25f)
+        if (!wearableIsSameAsPrevious)
         {
-            AudioEvent eventReaction = null;
-            if (model.bodyShape.Contains("Female"))
-                eventReaction = eventReactionFemale;
-            else
-                eventReaction = eventReactionMale;
+            if (lastSelectedWearable != null)
+                PlayRarity();
 
-            if (eventReaction != null)
+            if (lastSelectedWearable != null || hasClickedRandomize)
+                PlayVoiceReaction(model.bodyShape);
+        }
+
+        hasClickedRandomize = false;
+    }
+
+    void PlayRarity()
+    {
+        if (lastSelectedWearable.rarity == null)
+            return;
+
+        /*switch (lastClickedWearable.rarity)
+         {
+             case Rarity.RARE:
+                 eventRarity.SetIndex(0);
+                 break;
+             case Rarity.EPIC:
+                 eventRarity.SetIndex(1);
+                 break;
+             case Rarity.LEGENDARY:
+                 eventRarity.SetIndex(2);
+                 break;
+             case Rarity.MYTHIC:
+                 eventRarity.SetIndex(3);
+                 break;
+             case Rarity.UNIQUE:
+                 eventRarity.SetIndex(4);
+                 break;
+             default:
+                 eventRarity.SetIndex(0);
+                 break;
+         }*/
+
+        if (lastSelectedWearable.rarity == Rarity.UNIQUE)
+            eventRarity.SetIndex(1);
+        else
+            eventRarity.SetIndex(0);
+
+        eventRarity.Play(true);
+    }
+
+    void PlayVoiceReaction(string bodyShape)
+    {
+        float chanceToPlay = 0.75f;
+        AudioEvent eventReaction = null;
+
+        if (bodyShape.Contains("Female"))
+            eventReaction = eventReactionFemale;
+        else
+            eventReaction = eventReactionMale;
+
+        if (lastSelectedWearable != null)
+        {
+            if (lastSelectedWearable.rarity == Rarity.EPIC
+                || lastSelectedWearable.rarity == Rarity.LEGENDARY
+                || lastSelectedWearable.rarity == Rarity.MYTHIC
+                || lastSelectedWearable.rarity == Rarity.UNIQUE)
             {
-                if (!eventReaction.source.isPlaying)
-                    eventReaction.PlayScheduled(0.6f);
+                eventReaction.RandomizeIndex(14, 28);
+                chanceToPlay = 1f;
+            }
+            else
+            {
+                eventReaction.RandomizeIndex(0, 14);
+            }
+        }
+
+        if (eventReaction != null && Random.Range(0f, 1f) <= chanceToPlay)
+        {
+            if (!eventReaction.source.isPlaying)
+            {
+                eventReaction.PlayScheduled(0.6f);
             }
         }
     }
 
-    public void PlayRarity()
+    void OnSetAvatarEditorVisibility(bool visible)
     {
-        if (lastClickedWearable == null)
-            return;
+        AudioScriptableObjects.listItemAppear.ResetPitch();
 
-        if (lastClickedWearable.rarity == null)
-            return;
-
-        /*switch (lastClickedWearable.rarity)
+        if (visible)
         {
-            case Rarity.RARE:
-                eventRarity.SetIndex(0);
-                break;
-            case Rarity.EPIC:
-                eventRarity.SetIndex(1);
-                break;
-            case Rarity.LEGENDARY:
-                eventRarity.SetIndex(2);
-                break;
-            case Rarity.MYTHIC:
-                eventRarity.SetIndex(3);
-                break;
-            case Rarity.UNIQUE:
-                eventRarity.SetIndex(4);
-                break;
-            default:
-                eventRarity.SetIndex(0);
-                break;
-        }*/
+            if (musicFadeOut != null)
+            {
+                StopCoroutine(musicFadeOut);
+                StartCoroutine(eventMusic.FadeIn(1f));
+            }
 
-        eventRarity.Play(true);
+            if (!eventMusic.source.isPlaying)
+                eventMusic.Play();
+
+            view.eyeColorSelector.OnColorChanged += OnEyeColorChanged;
+            view.skinColorSelector.OnColorChanged += OnSkinColorChanged;
+            view.hairColorSelector.OnColorChanged += OnHairColorChanged;
+            view.OnAvatarAppear += OnAvatarAppear;
+        }
+        else
+        {
+            musicFadeOut = eventMusic.FadeOut(2f);
+            StartCoroutine(musicFadeOut);
+
+            view.eyeColorSelector.OnColorChanged -= OnEyeColorChanged;
+            view.skinColorSelector.OnColorChanged -= OnSkinColorChanged;
+            view.hairColorSelector.OnColorChanged -= OnHairColorChanged;
+            view.OnAvatarAppear -= OnAvatarAppear;
+        }
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ExploreHUD/ExploreHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ExploreHUD/ExploreHUDController.cs
@@ -71,7 +71,7 @@ public class ExploreHUDController : IHUD
         if (visible)
         {
             AudioScriptableObjects.dialogOpen.Play(true);
-            AudioScriptableObjects.listItemAppear.SetPitch(1f);
+            AudioScriptableObjects.listItemAppear.ResetPitch();
         }
         else
             AudioScriptableObjects.dialogClose.Play(true);
