02e5a399ebcc24dcdddc14e80454ff47fab55273
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2020-10-19 20:35:40 +0200

    feat: add mask for tinting mouths in Avatar (#1458)
    * Mask for tinting mouths in Avatar
    
    * Inverted mouth mask

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/Tests/VisualTests/FacialFeatureController_VisualTests.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using AvatarShape_Tests;
+using DCL;
+using DCL.Helpers;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using WaitUntil = DCL.WaitUntil;
+
+namespace Tests
+{
+    public class FacialFeatureController_VisualTests : VisualTestsBase
+    {
+        [UnityTest]
+        [VisualTest]
+        [Explicit]
+        [Category("Explicit")]
+        public IEnumerator MouthWithMask_Generate()
+        {
+            yield return VisualTestHelpers.GenerateBaselineForTest(MouthWithMask());
+        }
+
+        [UnityTest]
+        [VisualTest]
+        [Category("Explicit")]
+        [Explicit("Something is wrong with the avatar renderer material")]
+        public IEnumerator MouthWithMask()
+        {
+            yield return InitVisualTestsScene("AvatarShapeVisualTests_MouthWithMask");
+
+            AvatarTestHelpers.CreateTestCatalogLocal();
+            var model = AvatarTestHelpers.GetTestAvatarModel("Avatar #1", "TestAvatar_MaskMouth.json");
+            AvatarShape avatar = AvatarTestHelpers.CreateAvatarShape(scene, model);
+
+            Vector3 camPos = new Vector3(-0.75f, 2.0f, 2.25f);
+            Vector3 camTarget = avatar.transform.position + Vector3.up * 2.0f;
+
+            VisualTestHelpers.RepositionVisualTestsCamera(camPos, camTarget);
+
+            yield return new WaitUntil(() => avatar.everythingIsLoaded, 20);
+            yield return new WaitForSeconds(3f);
+
+            Debug.Break(); yield return  null;
+
+            yield return VisualTestHelpers.TakeSnapshot();
+        }
+    }
+}
