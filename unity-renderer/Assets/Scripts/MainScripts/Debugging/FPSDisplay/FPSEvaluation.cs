d31a84d2346b0d6bfa4dcbfb8669e91a0b961235
Esteban Ordano <esteban@decentraland.org>
2020-10-20 14:34:03 -0300

    fix: adjust hiccup threshold from 33ms to 50ms (20FPS) (#1468)
    Co-authored-by: Santi Andrade <sandrade@decentraland.org>

--- unity-renderer/Assets/Scripts/MainScripts/Debugging/FPSDisplay/FPSEvaluation.cs
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/FPSDisplay/FPSEvaluation.cs
@@ -2,6 +2,8 @@
 {
     public class FPSEvaluation
     {
+        public const float HICCUP_THRESHOLD_IN_SECONDS = 0.05f;
+
         public const int GREAT = 50;
         public const int GOOD = 40;
         public const int BAD = 30;
