5ef2544df0aad6b6e6909c5421e8f0f48e491d66
Ezequiel <ezequiel@decentraland.org>
2020-11-20 16:18:32 -0300

    fixing loading on wallet selector (#1679)

--- website/src/components/auth/EthLogin.tsx
+++ website/src/components/auth/EthLogin.tsx
@@ -17,20 +17,18 @@ export interface EthLoginProps {
 
 export const EthLogin: React.FC<EthLoginProps> = (props) => {
   const [showWallet, setShowWallet] = useState(props.showWallet || false);
-  const walletLoading = !props.loading && showWallet;
-  const isLoading = props.loading || showWallet
+  const isLoading = props.loading || showWallet;
 
-  function handlePlay(event: React.MouseEvent) {
+  function handlePlay() {
     if (props.provider) {
       return props.onLogin(props.provider);
     }
-
     setShowWallet(true);
   }
 
-  function handlePlayAsGuest(event: React.MouseEvent) {
+  function handlePlayAsGuest() {
     if (props.onGuest) {
-      props.onGuest()
+      props.onGuest();
     }
   }
 
@@ -41,20 +39,25 @@ export const EthLogin: React.FC<EthLoginProps> = (props) => {
       <div id="eth-login-confirmation-wrapper">
         {isLoading && <Spinner />}
         {!isLoading && (
-          <button className="eth-login-confirm-button" onClick={handlePlay}>
-            Play
-          </button>
-        )}
-        {!isLoading && !props.hasWallet && (
-          <button className="eth-login-guest-button" onClick={handlePlayAsGuest}>
-            Enter as Guest
-          </button>
+          <React.Fragment>
+            <button className="eth-login-confirm-button" onClick={handlePlay}>
+              Play
+            </button>
+            {!props.hasWallet && (
+              <button
+                className="eth-login-guest-button"
+                onClick={handlePlayAsGuest}
+              >
+                Enter as Guest
+              </button>
+            )}
+          </React.Fragment>
         )}
       </div>
       <WalletSelector
         show={showWallet}
         metamask={!!props.hasMetamask}
-        loading={walletLoading}
+        loading={props.loading}
         onClick={props.onLogin}
         onCancel={() => setShowWallet(false)}
       />
