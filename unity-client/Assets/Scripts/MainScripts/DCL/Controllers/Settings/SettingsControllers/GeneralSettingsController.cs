5ba810322ac256f39895f6332a213e11de64b2b5
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2020-11-30 16:02:08 +0100

    chore: adjust mouse sensitivity (#1616)
    * Now mouse sensitivity modify camera speed and not acceleration.
    
    * General settings tests no succeed
    
    * Tweaked mouse sensitivity values

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/GeneralSettingsController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/GeneralSettingsController.cs
@@ -5,6 +5,13 @@ namespace DCL.SettingsController
 {
     public class GeneralSettingsController : MonoBehaviour
     {
+        internal const float FIRST_PERSON_MIN_SPEED = 25f;
+        internal const float FIRST_PERSON_MAX_SPEED = 350f;
+        internal const float THIRD_PERSON_X_MIN_SPEED = 100f;
+        internal const float THIRD_PERSON_X_MAX_SPEED = 450f;
+        internal const float THIRD_PERSON_Y_MIN_SPEED = 0.5f;
+        internal const float THIRD_PERSON_Y_MAX_SPEED = 3f;
+
         public CinemachineFreeLook thirdPersonCamera;
         public CinemachineVirtualCamera firstPersonCamera;
 
@@ -32,10 +39,12 @@ namespace DCL.SettingsController
 
         void ApplyGeneralSettings(DCL.SettingsData.GeneralSettings settings)
         {
-            thirdPersonCamera.m_XAxis.m_AccelTime = settings.mouseSensitivity;
-            thirdPersonCamera.m_YAxis.m_AccelTime = settings.mouseSensitivity;
-            povCamera.m_HorizontalAxis.m_AccelTime = settings.mouseSensitivity;
-            povCamera.m_VerticalAxis.m_AccelTime = settings.mouseSensitivity;
+            var povSpeed = Mathf.Lerp(FIRST_PERSON_MIN_SPEED, FIRST_PERSON_MAX_SPEED, settings.mouseSensitivity);
+            povCamera.m_HorizontalAxis.m_MaxSpeed = povSpeed;
+            povCamera.m_VerticalAxis.m_MaxSpeed = povSpeed;
+            thirdPersonCamera.m_XAxis.m_MaxSpeed = Mathf.Lerp(THIRD_PERSON_X_MIN_SPEED, THIRD_PERSON_X_MAX_SPEED, settings.mouseSensitivity);
+            thirdPersonCamera.m_YAxis.m_MaxSpeed = Mathf.Lerp(THIRD_PERSON_Y_MIN_SPEED, THIRD_PERSON_Y_MAX_SPEED, settings.mouseSensitivity);
+
             AudioListener.volume = settings.sfxVolume;
             DCL.Interface.WebInterface.ApplySettings(settings.voiceChatVolume, (int)settings.voiceChatAllow);
         }
