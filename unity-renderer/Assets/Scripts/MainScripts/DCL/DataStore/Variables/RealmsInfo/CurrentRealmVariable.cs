81f1fb88be41aef74aa3c6a16382564e91997c20
pbosio <pato@decentraland.org>
2020-12-03 10:12:07 -0300

    feat: first implementation of DataStore (#1753)

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/DataStore/Variables/RealmsInfo/CurrentRealmVariable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variables.RealmsInfo
+{
+    public class CurrentRealmVariable : BaseVariable<CurrentRealmModel>
+    {
+        public override bool Equals(CurrentRealmModel other)
+        {
+            if (value == null)
+            {
+                return other == null;
+            }
+
+            return value.Equals(other);
+        }
+    }
+
+    [Serializable]
+    public class CurrentRealmModel
+    {
+        public string layer;
+        public string serverName;
+
+        public bool Equals(CurrentRealmModel other)
+        {
+            if (other == null) return false;
+            return Equals(other.serverName, other.layer);
+        }
+
+        public bool Equals(string serverName, string layer)
+        {
+            return this.serverName == serverName && this.layer == layer;
+        }
+    }
+}
