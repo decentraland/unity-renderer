2956678a2b4938518e7213bc6e0df2ed92be6db1
Ezequiel <ezequiel@decentraland.org>
2020-12-08 18:52:03 -0300

    fix: duplicate welcome message (#1787)
    * adds logger for saga errors
    
    * fixing react props
    
    * fixing duplicate welcome message
    
    * replaces rendererObservable for ensureRendererEnabled

--- kernel/packages/entryPoints/website.ts
+++ kernel/packages/entryPoints/website.ts
@@ -13,6 +13,7 @@ import {
   experienceStarted,
   FAILED_FETCHING_UNITY,
   NOT_INVITED,
+  setLoadingScreen,
   setLoadingWaitTutorial
 } from 'shared/loading/types'
 import { worldToGrid } from '../atomicHelpers/parcelScenePositions'
@@ -20,10 +21,11 @@ import { DEBUG_PM, HAS_INITIAL_POSITION_MARK, NO_MOTD, OPEN_AVATAR_EDITOR } from
 import { signalParcelLoadingStarted, signalRendererInitialized } from 'shared/renderer/actions'
 import { lastPlayerPosition, teleportObservable } from 'shared/world/positionThings'
 import { RootStore, StoreContainer } from 'shared/store/rootTypes'
-import { setLoadingScreenVisible, startUnitySceneWorkers } from '../unity-interface/dcl'
+import { startUnitySceneWorkers } from '../unity-interface/dcl'
 import { initializeUnity, InitializeUnityResult } from '../unity-interface/initializer'
 import { HUDElementID, RenderProfile } from 'shared/types'
 import {
+  ensureRendererEnabled,
   foregroundObservable,
   isForeground,
   renderStateObservable
@@ -129,19 +131,12 @@ namespace webApp {
         i.ConfigureHUDElement(HUDElementID.USERS_AROUND_LIST_HUD, { active: voiceChatEnabled, visible: false })
         i.ConfigureHUDElement(HUDElementID.FRIENDS, { active: identity.hasConnectedWeb3, visible: false })
 
-        const observer = renderStateObservable.add((isRendering) => {
-          if (isRendering) {
-            renderStateObservable.remove(observer)
-            globalThis.globalStore.dispatch(setLoadingWaitTutorial(false))
-
-            globalThis.globalStore.dispatch(experienceStarted())
-
-            setLoadingScreenVisible(false)
-
-            Html.switchGameContainer(true)
-
-            i.ConfigureHUDElement(HUDElementID.GRAPHIC_CARD_WARNING, { active: true, visible: true })
-          }
+        ensureRendererEnabled().then(() => {
+          globalThis.globalStore.dispatch(setLoadingWaitTutorial(false))
+          globalThis.globalStore.dispatch(experienceStarted())
+          globalThis.globalStore.dispatch(setLoadingScreen(false))
+          Html.switchGameContainer(true)
+          i.ConfigureHUDElement(HUDElementID.GRAPHIC_CARD_WARNING, { active: true, visible: true })
         })
 
         EnsureProfile(identity.address)
