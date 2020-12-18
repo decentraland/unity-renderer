d3b5c83c1b658cb995fae65e19abb5f4b38e3ba2
Suduck <adrian@suduck.com>
2020-12-18 11:21:03 +0100

    feat: land permission handle

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/BuilderInWorldController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/BuilderInWorldController.cs
@@ -698,13 +698,38 @@ public class BuilderInWorldController : MonoBehaviour
 
     public void StartEnterEditMode()
     {
+        UserProfile userProfile = UserProfile.GetOwnUserProfile();
+
         if (sceneToEditId != null) return;
 
         FindSceneToEdit();
-        sceneToEditId = sceneToEdit.sceneData.id;
-        Environment.i.sceneController.OnReadyScene += NewSceneReady;
 
-        builderInWorldBridge.StartKernelEditMode(sceneToEdit);
+        bool canOperateInLand = false;
+
+        foreach(UserProfileModel.ParcelsWithAccess parcel in userProfile.parcelsWithAccess)
+        {
+            foreach(Vector2Int pos in sceneToEdit.sceneData.parcels)
+            {
+                if (parcel.x == pos.x && parcel.y == pos.y)
+                {
+                    canOperateInLand = true;
+                    break;
+                }
+            }
+         
+        }
+        if (canOperateInLand)
+        {
+            sceneToEditId = sceneToEdit.sceneData.id;
+            Environment.i.sceneController.OnReadyScene += NewSceneReady;
+
+            builderInWorldBridge.StartKernelEditMode(sceneToEdit);
+        }
+        else
+        {
+            sceneToEdit = null;
+            //TODO: Implement feedback to the user that he don't have permissions
+        }
     }
 
     public void EnterEditMode()
