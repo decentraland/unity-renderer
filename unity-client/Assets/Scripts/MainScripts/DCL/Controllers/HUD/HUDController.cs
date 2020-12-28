b36d4809298268bdd18154caffdf7c40e0f036df
Pravus <pravus@decentraland.org>
2020-12-28 12:50:37 -0300

    refactored to use new DataStore instead of old commonscriptableobjects

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/HUDController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/HUDController.cs
@@ -1,3 +1,4 @@
+using DCL;
 using DCL.HelpAndSupportHUD;
 using DCL.SettingsHUD;
 using DCL.SettingsPanelHUD;
@@ -494,7 +495,7 @@ public class HUDController : MonoBehaviour
     {
         if (avatarEditorHud != null)
         {
-            CommonScriptableObjects.isSignUpFlow.Set(true);
+            DataStore.isSignUpFlow.Set(true);
             ShowAvatarEditor();
         }
     }
