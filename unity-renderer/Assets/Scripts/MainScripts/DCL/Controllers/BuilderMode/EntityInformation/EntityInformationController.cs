c5c712f788c30001b6af63c2fbb4f189f39cfe35
pbosio <pato@decentraland.org>
2021-01-18 19:58:00 -0300

    feat: biw entity information UI #1939
    Squashed commit of the following: (#1939)
    
    commit a7079738095be2127761b7ef86b1e42d50af12c7
    Author: Pravus <pravus@decentraland.org>
    Date:   Mon Jan 18 17:52:50 2021 -0300
    
        added meta files
    
    commit 824307eb918c15c864b12fa377a592382d8f92a5
    Merge: e3db46109 228e3ddb8
    Author: Pravus <pravus@decentraland.org>
    Date:   Mon Jan 18 17:24:03 2021 -0300
    
        finished merging master solving conflicts
    
    commit e3db461090632be8549ad899e51de09ec7464046
    Author: Pravus <pravus@decentraland.org>
    Date:   Mon Jan 18 13:45:33 2021 -0300
    
        merged master solving conflicts (except BuildModeHUD prefab, will be solved after updating master)
    
    commit ee06baf4af4cc820b2cb1970c14b1acb4f2a354f
    Author: Suduck <adrian@suduck.com>
    Date:   Tue Dec 22 12:18:44 2020 +0100
    
        refactor: attributes events
    
    commit c31f987c48a7d6b00e90fcbf58f8f98a3e4f55b4
    Merge: 8e7ec4321 c154c609a
    Author: Suduck <54064092+Suduck@users.noreply.github.com>
    Date:   Mon Dec 21 11:02:30 2020 +0100
    
        Merge branch 'master' into feat/biw-entity-information-ui
    
    commit 8e7ec4321f8ecdbb249ffb5241c3eeb999c762d3
    Author: Suduck <adrian@suduck.com>
    Date:   Fri Dec 18 14:52:49 2020 +0100
    
        style: fix code style
    
    commit 55f556fc6ce2b42535f7e667f7951eff849a0b8b
    Author: Suduck <adrian@suduck.com>
    Date:   Fri Dec 18 14:47:10 2020 +0100
    
        fix: back buttons action
    
    commit dc0d441f708703f34f010f4148a838dc8a38c6df
    Author: Suduck <adrian@suduck.com>
    Date:   Fri Dec 18 14:36:18 2020 +0100
    
        feat: UI full implemented
    
    commit a6d2621eeedfeb3c5662f037fbe8a3ac617d0dcf
    Author: Suduck <adrian@suduck.com>
    Date:   Fri Dec 18 10:39:13 2020 +0100
    
        feat: added entity UI

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/EntityInformation/EntityInformationController.cs
@@ -0,0 +1,204 @@
+using DCL;
+using DCL.Controllers;
+using DCL.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntityInformationController : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite openMenuSprite;
+    public Sprite closeMenuSprite;
+
+    [Header("Prefab references")]
+    public TextMeshProUGUI titleTxt;
+    public TMP_InputField nameIF;
+    public RawImage entitytTumbailImg;
+    public AttributeXYZ positionAttribute;
+    public AttributeXYZ rotationAttribute;
+    public AttributeXYZ scaleAttribute;
+    public GameObject detailsGO;
+    public GameObject basicsGO;
+    public Image detailsToggleBtn;
+    public Image basicToggleBtn;
+
+
+    public event Action<Vector3> OnPositionChange;
+    public event Action<Vector3> OnRotationChange;
+    public event Action<Vector3> OnScaleChange;
+
+    public event Action<DCLBuilderInWorldEntity> OnNameChange;
+
+    DCLBuilderInWorldEntity currentEntity;
+    ParcelScene parcelScene;
+
+    bool isEnable = false;
+    bool isChangingName = false;
+
+    int framesBetweenUpdate = 5;
+    int framesCount = 0;
+
+    string loadedThumbnailURL;
+
+    AssetPromise_Texture loadedThumbnailPromise;
+
+    private void Start()
+    {
+        positionAttribute.OnChanged += (x) => OnPositionChange?.Invoke(x);
+        rotationAttribute.OnChanged += (x) => OnRotationChange?.Invoke(x);
+        scaleAttribute.OnChanged += (x) => OnScaleChange?.Invoke(x);
+    }
+
+    private void LateUpdate()
+    {
+        if (!isEnable)
+            return;
+
+        if (currentEntity == null)
+            return;
+
+        if (framesCount >= framesBetweenUpdate)
+        {
+            UpdateInfo(currentEntity);
+            framesCount = 0;
+        }
+        else
+        {
+            framesCount++;
+        }
+    }
+
+    public void ToggleDetailsInfo()
+    {
+        detailsGO.SetActive(!detailsGO.activeSelf);
+
+        detailsToggleBtn.sprite = detailsGO.activeSelf ? openMenuSprite : closeMenuSprite;
+    }
+
+    public void ToggleBasicInfo()
+    {
+        basicsGO.SetActive(!basicsGO.activeSelf);
+
+        basicToggleBtn.sprite = basicsGO.activeSelf ? openMenuSprite : closeMenuSprite;
+    }
+
+    public void StartChangingName()
+    {
+        isChangingName = true;
+    }
+
+    public void EndChangingName()
+    {
+        isChangingName = false;
+    }
+
+    public void ChangeEntityName(string newName)
+    {
+        titleTxt.text = newName;
+        currentEntity.SetDescriptiveName(newName);
+        OnNameChange?.Invoke(currentEntity);
+    }
+
+    public void SetEntity(DCLBuilderInWorldEntity entity, ParcelScene currentScene)
+    {
+        if (currentEntity != null)
+            entity.onStatusUpdate -= UpdateEntityName;
+
+        currentEntity = entity;
+        currentEntity.onStatusUpdate += UpdateEntityName;
+
+        parcelScene = currentScene;
+
+
+        entitytTumbailImg.enabled = false;
+        GetThumbnail(entity.GetSceneObjectAssociated());
+
+        UpdateEntityName(currentEntity);
+        UpdateInfo(currentEntity);
+    }
+
+    public void Enable()
+    {
+        gameObject.SetActive(true);
+        isEnable = true;
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+        isEnable = false;
+    }
+
+    public void UpdateEntityName(DCLBuilderInWorldEntity entity)
+    {
+        string currentName = entity.GetDescriptiveName();
+        titleTxt.text = currentName;
+
+        if (!isChangingName)
+            nameIF.SetTextWithoutNotify(currentName);
+    }
+
+    public void UpdateInfo(DCLBuilderInWorldEntity entity)
+    {
+        if (entity.gameObject != null)
+        {
+            Vector3 positionConverted = DCL.Environment.i.world.state.ConvertUnityToScenePosition(entity.gameObject.transform.position, parcelScene);
+            Vector3 currentRotation = entity.gameObject.transform.rotation.eulerAngles;
+            Vector3 currentScale = entity.gameObject.transform.localScale;
+
+            var newEuler = currentRotation;
+
+            newEuler.x = RepeatWorking(newEuler.x - currentRotation.x + 180.0F, 360.0F) + currentRotation.x - 180.0F;
+            newEuler.y = RepeatWorking(newEuler.y - currentRotation.y + 180.0F, 360.0F) + currentRotation.y - 180.0F;
+            newEuler.z = RepeatWorking(newEuler.z - currentRotation.z + 180.0F, 360.0F) + currentRotation.z - 180.0F;
+
+            currentRotation = newEuler;
+
+            positionAttribute.SetValues(positionConverted);
+            rotationAttribute.SetValues(currentRotation);
+            scaleAttribute.SetValues(currentScale);
+
+        }
+    }
+
+    private float RepeatWorking(float t, float length)
+    {
+        return (t - (Mathf.Floor(t / length) * length));
+    }
+
+    private void GetThumbnail(SceneObject sceneObject)
+    {
+        var url = sceneObject?.GetComposedThumbnailUrl();
+
+        if (sceneObject == null || string.IsNullOrEmpty(url))
+            return;
+
+        string newLoadedThumbnailURL = url;
+        var newLoadedThumbnailPromise = new AssetPromise_Texture(url);
+
+
+        newLoadedThumbnailPromise.OnSuccessEvent += SetThumbnail;
+        newLoadedThumbnailPromise.OnFailEvent += x => { Debug.Log($"Error downloading: {url}"); };
+
+        AssetPromiseKeeper_Texture.i.Keep(newLoadedThumbnailPromise);
+
+
+        AssetPromiseKeeper_Texture.i.Forget(loadedThumbnailPromise);
+
+        loadedThumbnailPromise = newLoadedThumbnailPromise;
+        loadedThumbnailURL = newLoadedThumbnailURL;
+    }
+
+    public void SetThumbnail(Asset_Texture texture)
+    {
+        if (entitytTumbailImg != null)
+        {
+            entitytTumbailImg.enabled = true;
+            entitytTumbailImg.texture = texture.texture;
+        }
+    }
+}
