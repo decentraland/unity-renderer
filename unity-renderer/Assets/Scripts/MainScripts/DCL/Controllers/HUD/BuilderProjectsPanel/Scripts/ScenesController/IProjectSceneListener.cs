530c4295ec57e8d5b44bd880f6ca108d3372ee3c
pbosio <pato@decentraland.org>
2021-02-03 11:37:00 -0300

    feat: add builder-projects-panel scenes controller (#1978)

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuilderProjectsPanel/Scripts/ScenesController/IProjectSceneListener.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+internal interface IProjectSceneListener
+{
+    void OnSetScenes(Dictionary<string, SceneCardView> scenes);
+    void OnSceneAdded(SceneCardView scene);
+    void OnSceneRemoved(SceneCardView scene);
+}
