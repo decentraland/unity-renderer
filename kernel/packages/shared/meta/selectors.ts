e6ca81a3b1f89b350efbca624477a9be69c07bde
Nicolas Chamo <nicolas@chamo.com.ar>
2021-02-09 13:16:28 -0300

    chore: remove feature flags for voice chat (#2067)

--- kernel/packages/shared/meta/selectors.ts
+++ kernel/packages/shared/meta/selectors.ts
@@ -1,6 +1,6 @@
 import { CommsConfig, MessageOfTheDayConfig, RootMetaState } from './types'
 import { Vector2Component } from 'atomicHelpers/landHelpers'
-import { getCatalystNodesDefaultURL, VOICE_CHAT_DISABLED_FLAG, VOICE_CHAT_ENABLED_FLAG, WORLD_EXPLORER } from 'config'
+import { getCatalystNodesDefaultURL, VOICE_CHAT_DISABLED_FLAG, WORLD_EXPLORER } from 'config'
 
 export const getAddedServers = (store: RootMetaState): string[] => {
   const { config } = store.meta
@@ -33,16 +33,8 @@ export const isMOTDInitialized = (store: RootMetaState): boolean =>
 export const getMessageOfTheDay = (store: RootMetaState): MessageOfTheDayConfig | null =>
   store.meta.config.world ? store.meta.config.world.messageOfTheDay || null : null
 
-export const isVoiceChatEnabled = (store: RootMetaState): boolean => {
-  if (!WORLD_EXPLORER || VOICE_CHAT_DISABLED_FLAG) return false
-  return !!getCommsConfig(store).voiceChatEnabled || VOICE_CHAT_ENABLED_FLAG
-}
-
-export const getVoiceChatAllowlist = (store: RootMetaState): string[] => getCommsConfig(store).voiceChatAllowlist ?? []
-
 export const isVoiceChatEnabledFor = (store: RootMetaState, userId: string): boolean =>
-  isVoiceChatEnabled(store) ||
-  (getVoiceChatAllowlist(store).includes(userId) && !VOICE_CHAT_DISABLED_FLAG && WORLD_EXPLORER)
+  WORLD_EXPLORER && !VOICE_CHAT_DISABLED_FLAG
 
 export const getCatalystNodesEndpoint = (store: RootMetaState): string =>
   store.meta.config.servers?.catalystsNodesEndpoint ?? getCatalystNodesDefaultURL()
