58dc3d76251d1cdcc221dcd9160eebb662b968ad
pbosio <pato@decentraland.org>
2021-03-02 10:06:33 -0300

    feat: add builder-projects-panel project scenes menu section (#2107)

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuilderProjectsPanel/Scripts/SectionController/MenuSections/SectionProjectScenesController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+internal class SectionProjectScenesController : SectionBase, IProjectSceneListener
+{
+    public const string VIEW_PREFAB_PATH = "BuilderProjectsPanelMenuSections/SectionProjectScenesView";
+    public override ISectionSearchHandler searchHandler => sceneSearchHandler;
+
+    private readonly SectionProjectScenesView view;
+
+    private readonly SceneSearchHandler sceneSearchHandler = new SceneSearchHandler();
+    private Dictionary<string, SceneCardView> scenesViews;
+
+    public SectionProjectScenesController() : this(
+        Object.Instantiate(Resources.Load<SectionProjectScenesView>(VIEW_PREFAB_PATH))
+        )
+    {
+    }
+    
+    public SectionProjectScenesController(SectionProjectScenesView view)
+    {
+        this.view = view;
+
+        view.OnScrollRectValueChanged += RequestHideContextMenu;
+        sceneSearchHandler.OnResult += OnSearchResult;
+    }
+
+    public override void SetViewContainer(Transform viewContainer)
+    {
+        view.SetParent(viewContainer);
+    }
+
+    public override void Dispose()
+    {
+        view.OnScrollRectValueChanged -= RequestHideContextMenu;
+        Object.Destroy(view.gameObject);
+    }
+
+    protected override void OnShow()
+    {
+        view.SetActive(true);
+    }
+
+    protected override void OnHide()
+    {
+        view.SetActive(false);
+    }
+
+    void IProjectSceneListener.OnSetScenes(Dictionary<string, SceneCardView> scenes)
+    {
+        scenesViews = new Dictionary<string, SceneCardView>(scenes);
+        sceneSearchHandler.SetSearchableList(scenes.Values.Select(scene => scene.searchInfo).ToList());
+    }
+
+    void IProjectSceneListener.OnSceneAdded(SceneCardView scene)
+    {
+        scenesViews.Add(scene.sceneData.id, scene);
+        sceneSearchHandler.AddItem(scene.searchInfo);
+    }
+
+    void IProjectSceneListener.OnSceneRemoved(SceneCardView scene)
+    {
+        scenesViews.Remove(scene.sceneData.id);
+        scene.gameObject.SetActive(false);
+    }
+
+    private void OnSearchResult(List<SceneSearchInfo> searchInfoScenes)
+    {
+        if (scenesViews == null)
+            return;
+
+        using (var iterator = scenesViews.GetEnumerator())
+        {
+            while (iterator.MoveNext())
+            {
+                iterator.Current.Value.SetParent(view.scenesCardContainer);
+                iterator.Current.Value.gameObject.SetActive(false);
+            }
+        }
+
+        for (int i = 0; i < searchInfoScenes.Count; i++)
+        {
+            if (!scenesViews.TryGetValue(searchInfoScenes[i].id, out SceneCardView cardView))
+                continue;
+            
+            cardView.gameObject.SetActive(true);
+            cardView.transform.SetSiblingIndex(i);
+        }
+        view.ResetScrollRect();
+    }
+}
