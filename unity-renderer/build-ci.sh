ed954a64b1b08c88b86e41a53b2605c959f50a0f
Brian Amadori <brian@decentraland.org>
2021-03-08 14:39:19 -0300

    chore: unity 2019.4.19f1 update (#2096)
    * change docker image target
    
    * unity executable path fix
    
    * update with new license
    
    * remove AcceptExternalModificationsToPlayer as its not working anymore with this version
    
    * update loader issues
    
    * add JS_WebRequest_Create patch
    
    * update unity docker image
    
    * change key name for library caching
    
    * code cleanup
    
    * fix compiler error
    
    * test fixes
    
    * change unity version in README.md

--- unity-renderer/build-ci.sh
+++ unity-renderer/build-ci.sh
@@ -12,15 +12,14 @@ cd $BUILD_PATH
 export BUILD_PATH=$PWD
 cd ../..
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
-  -projectPath $(pwd) \
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity \
   -quit \
   -batchmode \
+  -projectPath $(pwd) \
   -buildTarget $BUILD_TARGET \
   -customBuildTarget $BUILD_TARGET \
   -customBuildName $BUILD_NAME \
   -customBuildPath $BUILD_PATH \
-  -customBuildOptions AcceptExternalModificationsToPlayer \
   -executeMethod BuildCommand.PerformBuild \
   -logFile /tmp/buildlog.txt
 
