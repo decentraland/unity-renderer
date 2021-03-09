b7e299199ea8fb530e0611e3f9b93543a21b1c8e
Brian Amadori <brian@decentraland.org>
2021-03-09 17:26:04 -0300

    chore: move unity project to folder (#2)
    * chore: move unity project to folder
    
    * CI fix
    
    * CI folder fixes
    
    * Updated config.yml
    
    * CI folder fixes
    
    * fix CI path
    
    * CI fix
    
    * CI fix

--- ci-playmode-test.sh
+++ ci-playmode-test.sh
@@ -5,7 +5,6 @@ source ci-setup.sh
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity } \
         -batchmode \
         -logFile "$PROJECT_PATH/playmode-logs.txt" \
-        -projectPath "$PROJECT_PATH" \
         -runTests \
         -testPlatform PlayMode \
         -testResults "$PROJECT_PATH/playmode-results.xml" \
@@ -22,6 +21,8 @@ cp playmode-results.xml test-results/playmode/results.xml || true
 
 cat "$PROJECT_PATH/playmode-results.xml"
 
+set +x 2> /dev/null
+
 # Display results
 if [ $UNITY_EXIT_CODE -eq 0 ]; then
   echo "Run succeeded, no failures occurred";
@@ -33,4 +34,5 @@ else
   echo "Unexpected exit code $UNITY_EXIT_CODE";
 fi
 
+set -x
 exit $UNITY_EXIT_CODE
