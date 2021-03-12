c917c33dfa372881b1417098595c1294dac9de11
Brian Amadori <black.pentagram@gmail.com>
2021-03-12 19:51:05 -0300

    CI fix

--- ci-setup.sh
+++ ci-setup.sh
@@ -13,20 +13,13 @@ ls -lah /root/.cache/unity3d
 
 echo "UNITY PATH is $UNITY_PATH"
 
-if [ -z "$UNITY_LICENSE_2019_4" ]; then
-  echo 'UNITY_LICENSE_2019_4 not present. License won''t be configured'
+if [ -z "$UNITY_LICENSE_CONTENT_2019_4_19_BASE64" ]; then
+  echo 'UNITY_LICENSE_CONTENT_2019_4_19_BASE64 not present. License won''t be configured'
 else
-  LICENSE=$(echo "${UNITY_LICENSE_2019_4}" | base64 -d | tr -d '\r')
+  LICENSE=$(echo "${UNITY_LICENSE_CONTENT_2019_4_19_BASE64}" | base64 -di | tr -d '\r')
 
   echo "Writing LICENSE to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf"
   echo "$LICENSE" > /root/.local/share/unity3d/Unity/Unity_lic.ulf
-
-  xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity \
-    -quit \
-    -nographics \
-    -logFile /dev/stdout \
-    -batchmode \
-    -manualLicenseFile /root/.local/share/unity3d/Unity/Unity_lic.ulf || true
 fi
 
 set -x
