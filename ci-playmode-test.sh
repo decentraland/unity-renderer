ebc1d86719f1547a5dd74d7b0836d6f793362ba8
Brian Amadori <black.pentagram@gmail.com>
2021-03-12 19:42:11 -0300

    CI fix

--- ci-playmode-test.sh
+++ ci-playmode-test.sh
@@ -1,4 +1,4 @@
-#!/usr/bash
+#!/usr/bin/env bash
 
 source ci-setup.sh
 
--- ci-setup.sh
+++ ci-setup.sh
@@ -11,6 +11,8 @@ set +x
 
 ls -lah /root/.cache/unity3d
 
+echo "UNITY PATH is $UNITY_PATH"
+
 if [ -z "$UNITY_LICENSE_2019_4" ]; then
   echo 'UNITY_LICENSE_2019_4 not present. License won''t be configured'
 else
@@ -19,7 +21,7 @@ else
   echo "Writing LICENSE to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf"
   echo "$LICENSE" > /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
-  ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity } \
+  xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity \
     -quit \
     -nographics \
     -logFile /dev/stdout \
