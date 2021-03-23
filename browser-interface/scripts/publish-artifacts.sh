7cc2f666407d3ce7808e4e5b5fb9ba720d9213aa
Agustin Mendez <github@menduz.com>
2021-03-23 13:13:46 -0300

    fix invalidation path

--- browser-interface/scripts/publish-artifacts.sh
+++ browser-interface/scripts/publish-artifacts.sh
@@ -5,10 +5,12 @@
 # and creates an invalidation in the cloudfront caché
 ######################################################
 
+set -u # no unbound variables
+
 # Upload artifacts
 aws s3 sync /tmp/workspace/unity-renderer/browser-interface/dist "s3://${S3_BUCKET}/branch/${CIRCLE_BRANCH}" --acl public-read
 
 # Invalidate cache
 aws configure set preview.cloudfront true
 aws configure set preview.create-invalidation true
-aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION}" --paths "branch/${CIRCLE_BRANCH}"
+aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION}" --paths "/branch/${CIRCLE_BRANCH}/*"
