ab94c119d86a6b076d8a16393c4f3af26e6f6eb1
Agustin Mendez <github@menduz.com>
2021-03-23 12:19:21 -0300

    invalidate cache

---
+++ browser-interface/scripts/publish-artifacts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+######################################################
+# This file deploys the ./dist folder to the S3 bucket
+# and creates an invalidation in the cloudfront caché
+######################################################
+
+# Upload artifacts
+aws s3 sync /tmp/workspace/unity-renderer/browser-interface/dist "s3://${S3_BUCKET}/branch/${CIRCLE_BRANCH}" --acl public-read
+
+# Invalidate cache
+aws configure set preview.cloudfront true
+aws configure set preview.create-invalidation true
+aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION}" --paths "branch/${CIRCLE_BRANCH}"
