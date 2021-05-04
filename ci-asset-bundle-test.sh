03e316fc84f13c990faff8868158c9fa7ca6964c
Agustin Mendez <github@menduz.com>
2021-05-04 18:53:53 -0300

    try to watch logs, act 4

--- ci-asset-bundle-test.sh
+++ ci-asset-bundle-test.sh
@@ -8,9 +8,11 @@ source ci-setup.sh
 SCENE_ID="$(curl -s "https://peer.decentraland.org/content/entities/scene?pointer=0,0" | jq -r '.[0].id')"
 echo "Main plaza deployment: $SCENE_ID"
 
+pwd
+
 # Parameters are passed as ENV vars
 export SCENE_ID
-export OUTPUT_DIR="$(pwd)/ab-output"
+export OUTPUT_DIR=ab-output
 export LOCAL_LOG_FILE="$(pwd)/ab-logs.txt"
 export CONTENT_URL="https://peer.decentraland.org/lambdas/contentv2/contents/"
 
