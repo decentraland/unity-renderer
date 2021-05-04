293c303737e863f38911a099ff2a5a3470887dec
Agustin Mendez <github@menduz.com>
2021-05-04 14:56:08 -0300

    try to watch logs

--- ci-asset-bundle-test.sh
+++ ci-asset-bundle-test.sh
@@ -11,13 +11,7 @@ echo "Main plaza deployment: $SCENE_ID"
 # Parameters are passed as ENV vars
 export SCENE_ID
 export OUTPUT_DIR=ab-output
-export LOCAL_LOG_FILE=ab-log.txt
+export LOCAL_LOG_FILE=ab-logs.txt
 export CONTENT_URL=https://peer.decentraland.org/lambdas/contentv2/contents/
 # call the conversor
 bash ./convert-asset-bundles.sh
-
-# print results to terminal
-find "$OUTPUT_DIR"
-cat "$LOCAL_LOG_FILE"
-
-exit $UNITY_EXIT_CODE
