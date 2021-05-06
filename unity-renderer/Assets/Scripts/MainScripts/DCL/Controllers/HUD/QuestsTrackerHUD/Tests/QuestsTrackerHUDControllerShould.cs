90c863bddc7cbe2a1c7550e1cadf9354c8d342d5
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2021-05-06 16:51:47 +0200

    chore: new tests for quests UI (#329)
    * New quests tests
    
    * New QuestsController tests
    
    * Assest remaned
    
    * Update config.yml
    
    * Update config.yml
    
    * Fix test assembly to avoid building it on tests
    
    Co-authored-by: Romina Marchetti <romi@decentraland.org>
    Co-authored-by: Agustin Mendez <github@menduz.com>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/QuestsTrackerHUD/Tests/QuestsTrackerHUDControllerShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/QuestsTrackerHUD/Tests/QuestsTrackerHUDControllerShould.cs
@@ -6,10 +6,6 @@ using NSubstitute;
 using NSubstitute.Extensions;
 using NUnit.Framework;
 using System;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.TestTools;
-using Object = UnityEngine.Object;
 
 namespace Tests.QuestsTrackerHUD
 {
@@ -18,6 +14,7 @@ namespace Tests.QuestsTrackerHUD
         private const string MOCK_QUEST_ID = "quest0";
         private const string MOCK_SECTION_ID = "section0";
         private const string MOCK_TASK_ID = "task0";
+        private const string MOCK_REWARD_ID = "reward0";
 
         private IQuestsController questsController;
         private QuestsTrackerHUDController hudController;
@@ -29,8 +26,10 @@ namespace Tests.QuestsTrackerHUD
             QuestModel questMock = new QuestModel { id = MOCK_QUEST_ID, status = QuestsLiterals.Status.ON_GOING };
             QuestSection sectionMock = new QuestSection { id = MOCK_SECTION_ID };
             QuestTask taskMock = new QuestTask { id = MOCK_TASK_ID };
+            QuestReward rewardMock = new QuestReward { id = MOCK_REWARD_ID };
             sectionMock.tasks = new [] { taskMock };
             questMock.sections = new [] { sectionMock };
+            questMock.rewards = new [] { rewardMock };
             DataStore.i.Quests.quests.Set(new []
             {
                 (questMock.id, questMock)
@@ -122,220 +121,18 @@ namespace Tests.QuestsTrackerHUD
             hudView.Received().PinQuest("newQuest2");
         }
 
+        [Test]
+        public void CallViewWhenRewardObtained()
+        {
+            hudController.Initialize(questsController);
+
+            questsController.OnRewardObtained += Raise.Event<RewardObtained>(MOCK_QUEST_ID, MOCK_REWARD_ID);
+
+            hudView.Received().AddReward(MOCK_QUEST_ID, DataStore.i.Quests.quests[MOCK_QUEST_ID].rewards[0]);
+        }
+
         [TearDown]
         public void TearDown() { DataStore.Clear(); }
     }
 
-    public class QuestsTrackerHUDViewShould
-    {
-        private const string MOCK_QUEST_ID = "quest0";
-        private const string MOCK_SECTION_ID = "section0";
-        private const string MOCK_TASK_ID = "task0";
-
-        private QuestsTrackerHUDView hudView;
-
-        [SetUp]
-        public void SetUp()
-        {
-            QuestModel questMock = new QuestModel
-            {
-                id = MOCK_QUEST_ID, name = "name", description = "description",
-            };
-            QuestSection sectionMock = new QuestSection
-            {
-                id = MOCK_SECTION_ID
-            };
-            QuestTask taskMock = new QuestTask
-            {
-                id = MOCK_TASK_ID,
-                type = "single",
-                payload = JsonUtility.ToJson(new TaskPayload_Single
-                {
-                    isDone = false
-                })
-            };
-            sectionMock.tasks = new []
-            {
-                taskMock
-            };
-            questMock.sections = new []
-            {
-                sectionMock
-            };
-            DataStore.i.Quests.quests.Set(new []
-            {
-                (questMock.id, questMock)
-            });
-
-            QuestsTrackerHUDView.ENTRIES_PER_FRAME = int.MaxValue;
-
-            hudView = Object.Instantiate(Resources.Load<GameObject>("QuestsTrackerHUD")).GetComponent<QuestsTrackerHUDView>();
-        }
-
-        [Test]
-        public void BeEmptyOnInitialize() { Assert.AreEqual(0, hudView.currentEntries.Count); }
-
-        [Test]
-        public void TrackQuestProgress()
-        {
-            hudView.UpdateQuest(MOCK_QUEST_ID, true);
-
-            Assert.AreEqual(1, hudView.currentEntries.Count);
-            Assert.IsTrue(hudView.currentEntries.ContainsKey(MOCK_QUEST_ID));
-        }
-
-        [Test]
-        public void AddPinnedQuestToTracker()
-        {
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, true);
-
-            Assert.AreEqual(1, hudView.questsContainer.childCount);
-            Assert.IsTrue(hudView.currentEntries.ContainsKey(MOCK_QUEST_ID));
-            Assert.IsTrue(hudView.currentEntries[MOCK_QUEST_ID].isPinned);
-        }
-
-        [Test]
-        public void AddNotPinnedQuestToTracker()
-        {
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, false);
-
-            Assert.AreEqual(1, hudView.questsContainer.childCount);
-            Assert.IsTrue(hudView.currentEntries.ContainsKey(MOCK_QUEST_ID));
-            Assert.IsFalse(hudView.currentEntries[MOCK_QUEST_ID].isPinned);
-        }
-
-        [Test]
-        public void PinUntrackedQuestProperly()
-        {
-            hudView.PinQuest(MOCK_QUEST_ID);
-            Assert.IsTrue(hudView.currentEntries.ContainsKey(MOCK_QUEST_ID));
-        }
-
-        [Test]
-        public void PinTrackedQuestProperly()
-        {
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, false);
-
-            hudView.PinQuest(MOCK_QUEST_ID);
-
-            Assert.IsTrue(hudView.currentEntries[MOCK_QUEST_ID].isPinned);
-        }
-
-        [Test]
-        public void UnpinTrackedQuestProperly()
-        {
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, true);
-
-            hudView.UnpinQuest(MOCK_QUEST_ID);
-
-            Assert.IsFalse(hudView.currentEntries[MOCK_QUEST_ID].isPinned);
-        }
-
-        [UnityTest]
-        public IEnumerator RemoveTrackedEntry()
-        {
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, false);
-
-            hudView.RemoveEntry(MOCK_QUEST_ID);
-
-            yield return null; //Let Unity destroy the entry properly
-
-            Assert.AreEqual(0, hudView.currentEntries.Count);
-        }
-
-        [UnityTest]
-        public IEnumerator PopulateQuestEntryProperly_SingleTask()
-        {
-            DataStore.i.Quests.quests[MOCK_QUEST_ID].name = "questName";
-            DataStore.i.Quests.quests[MOCK_QUEST_ID].sections = new QuestSection[2];
-            for (int i = 0; i < 2; i++)
-            {
-                DataStore.i.Quests.quests[MOCK_QUEST_ID].sections[i] = new QuestSection()
-                {
-                    id = $"section{i}",
-                    name = $"sectionName{i}",
-                    progress = 1 - i, //So the first section is completed and the second one not
-                    tasks = new []
-                    {
-                        new QuestTask
-                        {
-                            id = $"task{i}",
-                            name = $"task{i}",
-                            type = "single",
-                            payload = JsonUtility.ToJson(new TaskPayload_Single { isDone = false })
-                        }
-                    }
-                };
-            }
-
-            DataStore.i.Quests.quests[MOCK_QUEST_ID].name = "questName";
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, false);
-
-            yield return null; //wait for all the instantiation/destruction of items to be done by unity
-
-            Assert.AreEqual( "questName", hudView.currentEntries[MOCK_QUEST_ID].questTitle.text);
-            Assert.AreEqual( 0, hudView.currentEntries[MOCK_QUEST_ID].progress.fillAmount);
-            Assert.AreEqual( 2, hudView.currentEntries[MOCK_QUEST_ID].sectionContainer.childCount);
-            Assert.AreEqual( 1, hudView.currentEntries[MOCK_QUEST_ID].sectionEntries["section0"].taskEntries.Count);
-
-            var taskEntry = hudView.currentEntries[MOCK_QUEST_ID].sectionEntries["section0"].taskContainer.GetComponentInChildren<QuestsTrackerTask>();
-            Assert.NotNull(taskEntry);
-            Assert.AreEqual("task0" , taskEntry.taskTitle.text);
-            Assert.AreEqual("0/1" , taskEntry.progressText.text);
-
-            var taskEntry1 = hudView.currentEntries[MOCK_QUEST_ID].sectionEntries["section1"].taskContainer.GetComponentInChildren<QuestsTrackerTask>();
-            Assert.NotNull(taskEntry1);
-            Assert.AreEqual("task1" , taskEntry1.taskTitle.text);
-            Assert.AreEqual("0/1" , taskEntry1.progressText.text);
-        }
-
-        [UnityTest]
-        public IEnumerator PopulateQuestEntryProperly_NumericTask()
-        {
-            DataStore.i.Quests.quests[MOCK_QUEST_ID].name = "questName";
-            DataStore.i.Quests.quests[MOCK_QUEST_ID].sections = new QuestSection[2];
-            for (int i = 0; i < 2; i++)
-            {
-                DataStore.i.Quests.quests[MOCK_QUEST_ID].sections[i] = new QuestSection()
-                {
-                    id = $"section{i}",
-                    name = $"sectionName{i}",
-                    progress = 1 - i, //So the first section is completed and the second one not
-                    tasks = new []
-                    {
-                        new QuestTask
-                        {
-                            id = $"task{i}",
-                            name = $"task{i}",
-                            type = "numeric",
-                            payload = JsonUtility.ToJson(new TaskPayload_Numeric { start = 10, current = 15, end = 20 })
-                        }
-                    }
-                };
-            }
-
-            DataStore.i.Quests.quests[MOCK_QUEST_ID].name = "questName";
-            hudView.AddOrUpdateQuest(MOCK_QUEST_ID, false);
-
-            yield return null; //wait for all the instantiation/destruction of items to be done by unity
-
-            Assert.AreEqual( "questName", hudView.currentEntries[MOCK_QUEST_ID].questTitle.text);
-            Assert.AreEqual( 0, hudView.currentEntries[MOCK_QUEST_ID].progress.fillAmount);
-            Assert.AreEqual( 2, hudView.currentEntries[MOCK_QUEST_ID].sectionContainer.childCount);
-            Assert.AreEqual( 1, hudView.currentEntries[MOCK_QUEST_ID].sectionEntries["section0"].taskEntries.Count);
-
-            var taskEntry = hudView.currentEntries[MOCK_QUEST_ID].sectionEntries["section0"].taskContainer.GetComponentInChildren<QuestsTrackerTask>();
-            Assert.NotNull(taskEntry);
-            Assert.AreEqual("task0" , taskEntry.taskTitle.text);
-            Assert.AreEqual("15/20" , taskEntry.progressText.text);
-
-            var taskEntry1 = hudView.currentEntries[MOCK_QUEST_ID].sectionEntries["section1"].taskContainer.GetComponentInChildren<QuestsTrackerTask>();
-            Assert.NotNull(taskEntry1);
-            Assert.AreEqual("task1" , taskEntry1.taskTitle.text);
-            Assert.AreEqual("15/20" , taskEntry1.progressText.text);
-        }
-
-        [TearDown]
-        public void TearDown() { Object.Destroy(hudView.gameObject); }
-    }
 }
