560d0ff4e15ff981f4517bd261356d6c2ea55f75
Brian Amadori <black.pentagram@gmail.com>
2021-05-12 14:26:59 -0300

    refactor: move IdleChecker to its own assembly
    - moved both interface AND implementation

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/IdleChecker/Interfaces/IIdleChecker.cs
@@ -0,0 +1,16 @@
+﻿namespace DCL
+{
+    public interface IIdleChecker
+    {
+        void Initialize();
+        void SetMaxTime(int time);
+        int GetMaxTime();
+        bool isIdle();
+        void Update();
+
+        delegate void ChangeStatus(bool isIdle);
+
+        void Subscribe(ChangeStatus callback);
+        void Unsubscribe(ChangeStatus callback);
+    }
+}
