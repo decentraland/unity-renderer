4390a62860e111307c0ab37738ecd77104b5996c
Brian Amadori <black.pentagram@gmail.com>
2021-05-12 14:33:30 -0300

    refactor: move Clipboard interfaces to their own assembly

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Clipboard/Interfaces/IClipboard.cs
@@ -0,0 +1,16 @@
+﻿using DCL.Helpers;
+
+public interface IClipboard
+{
+    /// <summary>
+    /// Push a string value to the clipboard
+    /// </summary>
+    /// <param name="text">string to store</param>
+    void WriteText(string text);
+
+    /// <summary>
+    /// Request the string stored at the clipboard
+    /// </summary>
+    /// <returns>Promise of the string value stored at clipboard</returns>
+    Promise<string> ReadText();
+}
