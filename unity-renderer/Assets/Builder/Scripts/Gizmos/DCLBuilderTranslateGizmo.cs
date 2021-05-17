b73f2861056d67c7fe924313fb16427a004e3174
Santi Andrade <sandrade@decentraland.org>
2021-05-17 11:39:20 +0200

    Fix translate gizmo snapping (II)

--- unity-renderer/Assets/Builder/Scripts/Gizmos/DCLBuilderTranslateGizmo.cs
+++ unity-renderer/Assets/Builder/Scripts/Gizmos/DCLBuilderTranslateGizmo.cs
@@ -8,25 +8,26 @@ namespace Builder.Gizmos
 
         public override float TransformEntity(Transform entityTransform, DCLBuilderGizmoAxis axis, float axisValue)
         {
-            Vector3 move = activeAxis.transform.forward * axisValue;
-            Vector3 position = entityTransform.position + move;
-
+            Vector3 initialEntityPosition = entityTransform.position;
             if (snapFactor > 0)
-                position = GetPositionRoundedToSnapFactor(position);
+                initialEntityPosition = GetPositionRoundedToSnapFactor(entityTransform.position, axisValue);
+
+            Vector3 move = activeAxis.transform.forward * axisValue;
+            Vector3 position = initialEntityPosition + move;
 
             entityTransform.position = position;
 
             return axisValue;
         }
 
-        private Vector3 GetPositionRoundedToSnapFactor(Vector3 position)
+        private Vector3 GetPositionRoundedToSnapFactor(Vector3 position, float axisValue)
         {
             Vector3 activeAxisVector = GetActiveAxisVector();
 
             position = new Vector3(
-                activeAxisVector == Vector3.right ? Mathf.Round(position.x / snapFactor) * snapFactor : position.x,
-                activeAxisVector == Vector3.up ? Mathf.Round(position.y / snapFactor) * snapFactor : position.y,
-                activeAxisVector == Vector3.back ? Mathf.Round(position.z / snapFactor) * snapFactor : position.z);
+                activeAxisVector == Vector3.right ? (axisValue >= 0 ? Mathf.Floor(position.x / snapFactor) : Mathf.Ceil(position.x / snapFactor)) * snapFactor : position.x,
+                activeAxisVector == Vector3.up ? (axisValue >= 0 ? Mathf.Floor(position.y / snapFactor) : Mathf.Ceil(position.y / snapFactor)) * snapFactor : position.y,
+                activeAxisVector == Vector3.back ? (axisValue >= 0 ? Mathf.Floor(position.z / snapFactor) : Mathf.Ceil(position.z / snapFactor)) * snapFactor : position.z);
 
             return position;
         }
