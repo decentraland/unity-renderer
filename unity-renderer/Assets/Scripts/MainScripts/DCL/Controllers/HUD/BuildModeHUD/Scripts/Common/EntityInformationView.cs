6340da7ab98cfede6a76ee9d7e01320ee3531903
Santi Andrade <sandrade@decentraland.org>
2021-05-28 16:59:59 +0200

    fix: BiW Editor Improve entity name edition (from entity info view)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuildModeHUD/Scripts/Common/EntityInformationView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuildModeHUD/Scripts/Common/EntityInformationView.cs
@@ -54,6 +54,7 @@ public class EntityInformationView : MonoBehaviour, IEntityInformationView
     [SerializeField] internal TextMeshProUGUI entityLimitsMaterialsTxt;
     [SerializeField] internal TextMeshProUGUI entityLimitsTextureTxt;
     [SerializeField] internal TMP_InputField nameIF;
+    [SerializeField] internal Image nameIFTextBoxImage;
     [SerializeField] internal RawImage entitytTumbailImg;
     [SerializeField] internal AttributeXYZ positionAttribute;
     [SerializeField] internal AttributeXYZ rotationAttribute;
@@ -95,9 +96,19 @@ public class EntityInformationView : MonoBehaviour, IEntityInformationView
         backButton.onClick.AddListener(Disable);
         detailsToggleButton.onClick.AddListener(ToggleDetailsInfo);
         basicInfoTogglekButton.onClick.AddListener(ToggleBasicInfo);
-        nameIF.onEndEdit.AddListener((newName) => ChangeEntityName(newName));
-        nameIF.onSelect.AddListener((newName) => StartChangingName());
+        nameIF.onEndEdit.AddListener((newName) =>
+        {
+            ChangeEntityName(newName);
+            SetNameIFTextboxActive(false);
+        });
+        nameIF.onSelect.AddListener((newName) =>
+        {
+            SetNameIFTextboxActive(true);
+            StartChangingName();
+        });
         nameIF.onDeselect.AddListener((newName) => EndChangingName());
+
+        SetNameIFTextboxActive(false);
     }
 
     private void OnDestroy()
@@ -105,9 +116,9 @@ public class EntityInformationView : MonoBehaviour, IEntityInformationView
         backButton.onClick.RemoveListener(Disable);
         detailsToggleButton.onClick.RemoveListener(ToggleDetailsInfo);
         basicInfoTogglekButton.onClick.RemoveListener(ToggleBasicInfo);
-        nameIF.onEndEdit.RemoveListener((newName) => ChangeEntityName(newName));
-        nameIF.onSelect.RemoveListener((newName) => StartChangingName());
-        nameIF.onDeselect.RemoveListener((newName) => EndChangingName());
+        nameIF.onEndEdit.RemoveAllListeners();
+        nameIF.onSelect.RemoveAllListeners();
+        nameIF.onDeselect.RemoveAllListeners();
     }
 
     private void LateUpdate()
@@ -202,5 +213,13 @@ public class EntityInformationView : MonoBehaviour, IEntityInformationView
         canvasGroup.alpha = alphaValue;
         canvasGroup.blocksRaycasts = interactable;
         canvasGroup.interactable = interactable;
+    }
+
+    private void SetNameIFTextboxActive(bool isActive)
+    {
+        if (nameIFTextBoxImage == null)
+            return;
+
+        nameIFTextBoxImage.enabled = isActive;
     }
 }
