bcbb4f24d964a19fc09145019198933ea44d8e33
Suduck <adrian@suduck.com>
2021-06-28 10:23:24 +0200

    fix: unit test

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneBoundariesController/Tests/SceneBoundarieCheckerFeedbackStyleShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneBoundariesController/Tests/SceneBoundarieCheckerFeedbackStyleShould.cs
@@ -35,44 +35,4 @@ public class SceneBoundarieCheckerFeedbackStyleShould : IntegrationTestSuite
         //Assert
         Assert.AreSame( Environment.i.world.sceneBoundsChecker.GetFeedbackStyle(), redFlickerStyle );
     }
-
-    [UnityTest]
-    public IEnumerator RedFlickerStyleApply()
-    {
-        //Arrange
-        ParcelScene scene = (ParcelScene) Environment.i.world.sceneController.CreateTestScene();
-
-        Environment.i.world.sceneBoundsChecker.SetFeedbackStyle(new SceneBoundsFeedbackStyle_Simple());
-        var redFlickerStyle = new SceneBoundsFeedbackStyle_RedFlicker();
-        string entityId = "1";
-        TestHelpers.CreateSceneEntity(scene, entityId);
-
-        Assert.IsTrue(
-            scene.entities[entityId].gameObject.GetComponentInChildren<UnityGLTF.InstantiatedGLTFObject>() == null,
-            "Since the shape hasn't been updated yet, the 'GLTFScene' child object shouldn't exist");
-
-        TestHelpers.CreateAndSetShape(scene, entityId, DCL.Models.CLASS_ID.GLTF_SHAPE, JsonConvert.SerializeObject(
-            new
-            {
-                src = TestAssetsUtils.GetPath() + "/GLB/Trunk/Trunk.glb"
-            }));
-
-        LoadWrapper gltfShape = GLTFShape.GetLoaderForEntity(scene.entities[entityId]);
-        yield return new DCL.WaitUntil(() => gltfShape.alreadyLoaded);
-
-        //Act
-        scene.entities[entityId].gameObject.transform.position = new Vector3(100, 100, 100);
-
-        yield return new DCL.WaitUntil(() => !scene.entities[entityId].meshesInfo.renderers[0].enabled);
-        Environment.i.world.sceneBoundsChecker.SetFeedbackStyle(redFlickerStyle);
-
-        yield return null;
-
-        //Assert
-        foreach (var renderer in scene.entities[entityId].renderers)
-        {
-            Assert.IsTrue(renderer.enabled);
-        }
-        Assert.Greater( redFlickerStyle.GetInvalidMeshesCount(), 0);
-    }
 }
