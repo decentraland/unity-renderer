861f384788b9ccdf20a487ddcdd086dadeb9d26f
Alejandro Jimenez <ajimenez@decentraland.org>
2021-07-09 19:12:48 +0200

    Dont reset percentage when disabling (to behave as web)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/LoadingHUD/Bridge/LoadingBridge.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/LoadingHUD/Bridge/LoadingBridge.cs
@@ -17,8 +17,6 @@ public class LoadingBridge : MonoBehaviour
     {
         Payload payload = JsonUtility.FromJson<Payload>(jsonMessage);
         DataStore.i.HUDs.loadingHUD.visible.Set(payload.isVisible);
-        if (!payload.isVisible)
-            DataStore.i.HUDs.loadingHUD.percentage.Set(0);
 
         if (!string.IsNullOrEmpty(payload.message))
             DataStore.i.HUDs.loadingHUD.message.Set(payload.message);
