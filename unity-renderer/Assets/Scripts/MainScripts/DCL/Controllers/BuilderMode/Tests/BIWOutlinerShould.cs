315d3e25fec2cbac4afb113f252070343de07643
Suduck <adrian@suduck.com>
2021-07-14 12:20:12 +0200

    fix: unit test fixing

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/Tests/BIWOutlinerShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/Tests/BIWOutlinerShould.cs
@@ -63,10 +63,10 @@ public class BIWOutlinerShould : IntegrationTestSuite_Legacy
     public void OutlineLayer()
     {
         outlinerController.OutlineEntity(entity);
-        Assert.AreEqual(entity.rootEntity.meshesInfo.renderers[0].gameObject.layer, BIWSettings.SELECTION_LAYER_INDEX);
+        Assert.AreEqual(entity.rootEntity.meshesInfo.renderers[0].gameObject.layer, LayerMask.NameToLayer("Selection"));
 
         outlinerController.CancelEntityOutline(entity);
-        Assert.AreNotEqual(entity.rootEntity.meshesInfo.renderers[0].gameObject.layer, BIWSettings.SELECTION_LAYER_INDEX);
+        Assert.AreNotEqual(entity.rootEntity.meshesInfo.renderers[0].gameObject.layer, LayerMask.NameToLayer("Selection"));
     }
 
     [Test]
