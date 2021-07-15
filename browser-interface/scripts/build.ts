e00e6f6fe6f0c535d597a9195d9bcff901010338
Agustin Mendez <github@menduz.com>
2021-07-15 11:00:45 -0300

    fix: typing roots (#807)
    * fix typing roots
    
    * consistent naming
    
    * good, the test failed

--- browser-interface/scripts/build.ts
+++ browser-interface/scripts/build.ts
@@ -14,6 +14,8 @@ const PROD = !!process.env.CI
 
 console.log(`production: ${PROD}`)
 
+const typingsRoot = "./src"
+
 const plugins = [
   typescript({
     verbosity: 2,
@@ -98,14 +100,15 @@ async function buildRollup() {
 }
 
 async function createTypings() {
-  console.log("> writing typings.d.ts")
+  console.log("> writing index.d.ts")
   writeFileSync(
-    path.resolve(DIST_PATH, "typings.d.ts"),
+    path.resolve(DIST_PATH, "index.d.ts"),
     `
-import * as Renderer from './index'
+import * as Renderer from '${typingsRoot}/index'
 declare var DclRenderer: typeof Renderer
     `
   )
+  ensureFileExists(path.resolve(DIST_PATH, typingsRoot), "index.d.ts")
 }
 
 async function createPackageJson() {
@@ -116,7 +119,7 @@ async function createPackageJson() {
       {
         name: "@dcl/unity-renderer",
         main: "index.js",
-        typings: "typings.d.ts",
+        typings: "index.d.ts",
         version: `1.0.${process.env.CIRCLE_BUILD_NUM || "0-development"}`,
         tag: process.env.CIRCLE_TAG,
         commit: process.env.CIRCLE_SHA1,
