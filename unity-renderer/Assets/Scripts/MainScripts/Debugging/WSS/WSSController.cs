514b29c7f6454e8fcb7769d80c5113795acae593
Agustin Mendez <github@menduz.com>
2021-07-15 20:19:39 -0300

    removing support of handling automatic login from unity since it is part of website logic now

--- unity-renderer/Assets/Scripts/MainScripts/Debugging/WSS/WSSController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/WSS/WSSController.cs
@@ -11,7 +11,7 @@ namespace DCL
 {
     public class DCLWebSocketService : WebSocketBehavior
     {
-        public static bool enterAsAGuest = false; // TODO(Mateo): Refactor https://github.com/decentraland/unity-renderer/issues/585
+        // TODO(Mateo): Refactor https://github.com/decentraland/unity-renderer/issues/585
         static bool VERBOSE = false;
 
         private void SendMessageToWeb(string type, string message)
@@ -61,8 +61,6 @@ namespace DCL
             base.OnOpen();
             WebInterface.OnMessageFromEngine += SendMessageToWeb;
             Send("{\"welcome\": true}");
-            if (enterAsAGuest)
-                WebInterface.SendAuthentication(WebInterface.RendererAuthenticationType.Guest);
         }
     }
 
