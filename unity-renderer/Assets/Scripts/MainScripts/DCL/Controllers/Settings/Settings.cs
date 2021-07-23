1e7397b80d0f1f91e55cb3c199fb51b669f7b97b
Martin <martin.lande@hotmail.com>
2021-07-23 13:31:37 +0200

    Added AudioSettings struct to Settings.cs

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/Settings.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/Settings.cs
@@ -9,14 +9,17 @@ namespace DCL
     {
         const string QUALITY_SETTINGS_KEY = "Settings.Quality";
         const string GENERAL_SETTINGS_KEY = "Settings.General";
+        const string AUDIO_SETTINGS_KEY = "Settings.Audio";
 
         public event Action<SettingsData.QualitySettings> OnQualitySettingsChanged;
         public event Action<SettingsData.GeneralSettings> OnGeneralSettingsChanged;
+        public event Action<SettingsData.AudioSettings> OnAudioSettingsChanged;
         public event Action OnResetAllSettings;
 
         public SettingsData.QualitySettings qualitySettings { get { return currentQualitySettings; } }
         public SettingsData.QualitySettingsData qualitySettingsPresets { get { return qualitySettingsPreset; } }
         public SettingsData.GeneralSettings generalSettings { get { return currentGeneralSettings; } }
+        public SettingsData.AudioSettings audioSettings { get { return currentAudioSettings; } }
 
         private static SettingsData.QualitySettingsData qualitySettingsPreset = null;
 
@@ -27,6 +30,7 @@ namespace DCL
 
         public SettingsData.QualitySettings currentQualitySettings { private set; get; }
         private SettingsData.GeneralSettings currentGeneralSettings;
+        public SettingsData.AudioSettings currentAudioSettings { private set; get; }
 
         public Settings()
         {
@@ -46,6 +50,7 @@ namespace DCL
 
             LoadQualitySettings();
             LoadGeneralSettings();
+            LoadAudioSettings();
         }
 
         private void LoadQualitySettings()
@@ -89,14 +94,34 @@ namespace DCL
             }
         }
 
+        private void LoadAudioSettings() {
+            currentAudioSettings = GetDefaultAudioSettings();
+
+            if (PlayerPrefsUtils.HasKey(AUDIO_SETTINGS_KEY))
+            {
+                try
+                {
+                    object currentSetting = currentAudioSettings;
+                    JsonUtility.FromJsonOverwrite(PlayerPrefsUtils.GetString(AUDIO_SETTINGS_KEY), currentSetting);
+                    currentAudioSettings = (SettingsData.AudioSettings)currentSetting;
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
+            }
+        }
+
         public void LoadDefaultSettings()
         {
             autosettingsEnabled.Set(false);
             currentQualitySettings = qualitySettingsPreset.defaultPreset;
             currentGeneralSettings = GetDefaultGeneralSettings();
+            currentAudioSettings = GetDefaultAudioSettings();
 
             ApplyQualitySettings(currentQualitySettings);
             ApplyGeneralSettings(currentGeneralSettings);
+            ApplyAudioSettings(currentAudioSettings);
         }
 
         public void ResetAllSettings()
@@ -118,6 +143,20 @@ namespace DCL
                 voiceChatVolume = 1,
                 voiceChatAllow = SettingsData.GeneralSettings.VoiceChatAllow.ALL_USERS,
                 autoqualityOn = false
+            };
+        }
+
+        private SettingsData.AudioSettings GetDefaultAudioSettings()
+        {
+            return new SettingsData.AudioSettings()
+            {
+                masterVolume = 1f,
+                voiceChatVolume = 1f,
+                avatarSFXVolume = 1f,
+                uiSFXVolume = 1f,
+                sceneSFXVolume = 1f,
+                musicVolume = 1f,
+                chatSFXEnabled = true
             };
         }
 
@@ -159,6 +198,15 @@ namespace DCL
             autosettingsEnabled.Set(settings.autoqualityOn);
         }
 
+        public void ApplyAudioSettings(SettingsData.AudioSettings settings)
+        {
+            if (settings.Equals(currentAudioSettings))
+                return;
+
+            currentAudioSettings = settings;
+            OnAudioSettingsChanged?.Invoke(settings);
+        }
+
         public void SaveSettings()
         {
             PlayerPrefsUtils.SetString(GENERAL_SETTINGS_KEY, JsonUtility.ToJson(currentGeneralSettings));
@@ -194,6 +242,28 @@ namespace DCL.SettingsData
                    && voiceChatVolume == settings.voiceChatVolume
                    && voiceChatAllow == settings.voiceChatAllow
                    && autoqualityOn == settings.autoqualityOn;
+        }
+    }
+
+    [Serializable]
+    public struct AudioSettings
+    {
+        public float masterVolume;
+        public float voiceChatVolume;
+        public float avatarSFXVolume;
+        public float uiSFXVolume;
+        public float sceneSFXVolume;
+        public float musicVolume;
+        public bool chatSFXEnabled;
+
+        public bool Equals(AudioSettings settings) {
+            return masterVolume == settings.masterVolume
+                && voiceChatVolume == settings.voiceChatVolume
+                && avatarSFXVolume == settings.avatarSFXVolume
+                && uiSFXVolume == settings.uiSFXVolume
+                && sceneSFXVolume == settings.sceneSFXVolume
+                && musicVolume == settings.musicVolume
+                && chatSFXEnabled == settings.chatSFXEnabled;
         }
     }
 }
