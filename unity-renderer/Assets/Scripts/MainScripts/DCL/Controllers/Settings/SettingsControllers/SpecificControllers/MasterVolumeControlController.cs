86a000cc7e392bb072085584239fa74f9e1ebaff
Martin <martin.lande@hotmail.com>
2021-07-23 14:55:57 +0200

    Implemented master volume (partially) and UI SFX volume

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/MasterVolumeControlController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/MasterVolumeControlController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using DCL.Helpers;
 
 namespace DCL.SettingsControls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Master Volume", fileName = "MasterVolumeControlController")]
     public class MasterVolumeControlController : SliderSettingsControlController
     {
+        [SerializeField]
+        AudioMixer audioMixer;
+
         public override object GetStoredValue() { return currentAudioSettings.masterVolume * 100; }
 
         public override void UpdateSetting(object newValue) {
             currentAudioSettings.masterVolume = (float)newValue * 0.01f;
-            // TODO: Apply volume change
+            audioMixer.SetFloat("AllBusVolume", Utils.ToAudioMixerGroupVolume(currentAudioSettings.masterVolume));
         }
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/UISFXVolumeControlController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/UISFXVolumeControlController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using DCL.Helpers;
 
 namespace DCL.SettingsControls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/UI SFX Volume", fileName = "UISFXVolumeControlController")]
     public class UISFXVolumeControlController : SliderSettingsControlController
     {
+        [SerializeField]
+        AudioMixer audioMixer;
+
         public override object GetStoredValue() { return currentAudioSettings.uiSFXVolume * 100; }
 
         public override void UpdateSetting(object newValue) {
             currentAudioSettings.uiSFXVolume = (float)newValue * 0.01f;
-            // TODO: Apply volume change
+            audioMixer.SetFloat("UIBusVolume", Utils.ToAudioMixerGroupVolume(currentAudioSettings.uiSFXVolume));
         }
     }
 }
