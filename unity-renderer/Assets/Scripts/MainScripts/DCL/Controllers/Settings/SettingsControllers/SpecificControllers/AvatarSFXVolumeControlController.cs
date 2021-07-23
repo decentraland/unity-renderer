25b7368c3a82c14f5df6f41dc72d86884b41cb64
Martin <martin.lande@hotmail.com>
2021-07-23 14:47:36 +0200

    Implemented music volume and avatar SFX volume

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/AvatarSFXVolumeControlController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/AvatarSFXVolumeControlController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using DCL.Helpers;
 
 namespace DCL.SettingsControls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Avatar SFX Volume", fileName = "AvatarSFXVolumeControlController")]
     public class AvatarSFXVolumeControlController : SliderSettingsControlController
     {
+        [SerializeField]
+        AudioMixer audioMixer;
+
         public override object GetStoredValue() { return currentAudioSettings.avatarSFXVolume * 100; }
 
         public override void UpdateSetting(object newValue) {
             currentAudioSettings.avatarSFXVolume = (float)newValue * 0.01f;
-            // TODO: Apply volume change
+            audioMixer.SetFloat("AvatarSFXBusVolume", Utils.ToAudioMixerGroupVolume(currentAudioSettings.avatarSFXVolume));
         }
     }
 }
