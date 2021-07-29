42fd07145741eb7e31defe01e0739f65eb5ef525
Brian Amadori <black.pentagram@gmail.com>
2021-07-29 16:10:06 -0300

    fix: -1 textureIndex path not being taken correctly in SampleTexture.hlsl

--- unity-renderer/Assets/Rendering/Shaders/Toon/ShaderGraph/Includes/SampleTexture.hlsl
+++ unity-renderer/Assets/Rendering/Shaders/Toon/ShaderGraph/Includes/SampleTexture.hlsl
@@ -1,35 +1,37 @@
 void SampleTexture_float(float4 defaultColor, float textureIndex, float2 uv, out float4 Color)
 {
-    float4 result;
+    if (textureIndex < 0)
+    {
+        Color = defaultColor;
+        return;
+    }
 
     textureIndex = abs(textureIndex);
 
     if (textureIndex < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap1), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap1), uv);
     else if (abs(1 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap2), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap2), uv);
     else if (abs(2 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap3), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap3), uv);
     else if (abs(3 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap4), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap4), uv);
     else if (abs(4 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap5), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap5), uv);
     else if (abs(5 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap6), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap6), uv);
     else if (abs(6 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap7), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap7), uv);
     else if (abs(7 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap8), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap8), uv);
     else if (abs(8 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap9), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap9), uv);
     else if (abs(9 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap10), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap10), uv);
     else if (abs(10 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap11), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap11), uv);
     else if (abs(11 - textureIndex) < 0.01)
-        result = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap12), uv);
+        Color = tex2D(UnityBuildTexture2DStructNoScale(_AvatarMap12), uv);
     else
-        result = defaultColor;
-
-    Color = result;
+        Color = defaultColor;
 }
