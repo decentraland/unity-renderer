82183b5e83f943f89d6a1d53a737786b084fc8ab
Pato <pato@decentraland.org>
2021-08-04 17:04:28 -0300

    fix: AssetPromiseKeeper singleton

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/GLTF/AssetPromiseKeeper_GLTF.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/GLTF/AssetPromiseKeeper_GLTF.cs
@@ -2,6 +2,9 @@ namespace DCL
 {
     public class AssetPromiseKeeper_GLTF : AssetPromiseKeeper<Asset_GLTF, AssetLibrary_GLTF, AssetPromise_GLTF>
     {
+        private static AssetPromiseKeeper_GLTF instance;
+        public static AssetPromiseKeeper_GLTF i { get { return instance ??= new AssetPromiseKeeper_GLTF(); } }
+        
         public AssetPromiseKeeper_GLTF() : base(new AssetLibrary_GLTF()) { }
 
         protected override void OnSilentForget(AssetPromise_GLTF promise) { promise.asset.Hide(); }
