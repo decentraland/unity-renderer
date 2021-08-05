c92ac38e4315056a7d0818f3dc9c5ebf06b33be1
pbosio <pato@decentraland.org>
2021-08-05 18:24:58 -0300

    fix: AssetPromiseKeeper singleton (#916)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundles/AB_GameObject/AssetPromiseKeeper_AB_GameObject.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundles/AB_GameObject/AssetPromiseKeeper_AB_GameObject.cs
@@ -1,17 +1,16 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("AssetPromiseKeeper_AssetBundleModelTests")]
+
 namespace DCL
 {
     public class AssetPromiseKeeper_AB_GameObject : AssetPromiseKeeper<Asset_AB_GameObject, AssetLibrary_AB_GameObject, AssetPromise_AB_GameObject>
     {
-        public AssetPromiseKeeper_AB_GameObject() : base(new AssetLibrary_AB_GameObject())
-        {
-        }
-        protected override void OnSilentForget(AssetPromise_AB_GameObject promise)
-        {
-            promise.asset.Hide();
-        }
+        private static AssetPromiseKeeper_AB_GameObject instance;
+        public static AssetPromiseKeeper_AB_GameObject i { get { return instance ??= new AssetPromiseKeeper_AB_GameObject(); } }
+
+        public AssetPromiseKeeper_AB_GameObject() : base(new AssetLibrary_AB_GameObject()) { }
+        protected override void OnSilentForget(AssetPromise_AB_GameObject promise) { promise.asset.Hide(); }
 
     }
 
