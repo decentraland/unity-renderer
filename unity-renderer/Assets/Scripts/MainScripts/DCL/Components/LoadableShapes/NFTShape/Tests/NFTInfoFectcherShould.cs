5e45366a7027c6ff0d72d89e223ce1bb121493f7
Suduck <54064092+Suduck@users.noreply.github.com>
2021-08-10 13:13:47 +0200

    fix: scene boundaries unit test (#874)
    * feat: NFT test loaded from local
    
    * feat: removed explicit from scene boundaries test
    
    * feat: add unit test for NFTInfoFetcher and renamed FileLoader
    
    * style: remove local
    
    * fix: unit test
    
    * style: better code style
    
    * style: better naming
    
    * fix: correct nft info to fetch

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/LoadableShapes/NFTShape/Tests/NFTInfoFectcherShould.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class NFTInfoFectcherShould
+{
+    private NFTInfoFetcher fetcher;
+
+    [SetUp]
+    protected void SetUp() {  fetcher = new NFTInfoFetcher(); }
+
+    [Test]
+    public void NFTInfoFetcherFail()
+    {
+        //Act - Assert
+        fetcher.FetchNFTImage("testAddress", "testId", info =>  Assert.Fail(), Assert.Pass );
+    }
+
+    [Test]
+    public void NFTInfoFetcherDipose()
+    {
+        //Arrange
+        fetcher.FetchNFTImage("testAddress", "testId", null, null );
+
+        //Act
+        fetcher.Dispose();
+
+        //Assert
+        Assert.IsNull(fetcher.fetchCoroutine);
+    }
+
+    [TearDown]
+    protected void TearDown() { fetcher.Dispose(); }
+}
