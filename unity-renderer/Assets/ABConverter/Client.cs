58ce5c85b651989690ad448399273459fd0bb9a9
Brian Amadori <black.pentagram@gmail.com>
2021-08-20 13:05:18 -0300

    fix: add filtering to BuildAllWearableCollectionsUrl method

--- unity-renderer/Assets/ABConverter/Client.cs
+++ unity-renderer/Assets/ABConverter/Client.cs
@@ -76,7 +76,7 @@ namespace DCL.ABConverter
             return env;
         }
 
-        public static string BuildAllWearableCollectionsURL()
+        public static string BuildAllWearableCollectionsURL(Func<string, bool> collectionFilter = null)
         {
             UnityWebRequest w = UnityWebRequest.Get(COLLECTIONS_FETCH_URL);
             w.SendWebRequest();
@@ -92,10 +92,23 @@ namespace DCL.ABConverter
             var collectionsApiData = JsonUtility.FromJson<WearableCollectionsAPIData>(w.downloadHandler.text);
 
             string finalUrl = WEARABLES_FETCH_URL;
-            finalUrl += "collectionId=" + collectionsApiData.collections[0].id;
-            for (int i = 1; i < collectionsApiData.collections.Length; i++)
+            bool firstCollection = true;
+
+            for (int i = 0; i < collectionsApiData.collections.Length; i++)
             {
-                finalUrl += "&collectionId=" + collectionsApiData.collections[i].id;
+                string collectionId = collectionsApiData.collections[i].id;
+
+                if ( collectionFilter != null )
+                {
+                    if ( !collectionFilter.Invoke(collectionId) )
+                        continue;
+                }
+
+                if ( !firstCollection )
+                    finalUrl += "&";
+
+                finalUrl += "collectionId=" + collectionId;
+                firstCollection = false;
             }
 
             return finalUrl;
@@ -361,8 +374,8 @@ namespace DCL.ABConverter
             EnsureEnvironment();
 
             List<WearableItem> avatarItemList = GetWearableItems(BuildAllWearableCollectionsURL())
-                                                .Where(x => x.data.category == WearableLiterals.Categories.BODY_SHAPE)
-                                                .ToList();
+                .Where(x => x.data.category == WearableLiterals.Categories.BODY_SHAPE)
+                .ToList();
 
             Queue<WearableItem> itemQueue = new Queue<WearableItem>(avatarItemList);
             var settings = new Settings();
@@ -385,8 +398,8 @@ namespace DCL.ABConverter
 
             // For debugging purposes we can intercept this item list with LinQ for specific wearables
             List<WearableItem> avatarItemList = GetWearableItems(BuildAllWearableCollectionsURL())
-                                                .Where(x => x.data.category != WearableLiterals.Categories.BODY_SHAPE)
-                                                .ToList();
+                .Where(x => x.data.category != WearableLiterals.Categories.BODY_SHAPE)
+                .ToList();
 
             Queue<WearableItem> itemQueue = new Queue<WearableItem>(avatarItemList);
             var settings = new Settings();
