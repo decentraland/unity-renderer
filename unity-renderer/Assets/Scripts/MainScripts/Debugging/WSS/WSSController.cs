050cac7e34fab7a0c4b3c40d34118e11bdc46a95
Agustin Mendez <github@menduz.com>
2021-08-21 11:48:30 -0300

    fix: make unity editor work again with race condition fixes for .zone [MERGE ASAP TO UNBLOCK TEAM] (#1037)
    * fix: use desambiguated content server
    
    * fix race condition
    
    * fix to work with latest version of kernel

--- unity-renderer/Assets/Scripts/MainScripts/Debugging/WSS/WSSController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/WSS/WSSController.cs
@@ -60,7 +60,16 @@ namespace DCL
         {
             base.OnOpen();
             WebInterface.OnMessageFromEngine += SendMessageToWeb;
-            Send("{\"welcome\": true}");
+
+            // we now need to send the "The engine is ready to handle messages" signal.
+            // that signal is triggered by the SystemInfoReport message in kernel
+            SendMessageToWeb("SystemInfoReport", "{}");
+
+            // TODO(menduz): For some reason beyond my knowledge, the following line
+            //               _does not_ send any message to the kernel, but it should.
+            //               The line avobe is "hacking" it to make it work. 
+            // WebInterface.SendSystemInfoReport();
+
             if (enterAsAGuest)
                 WebInterface.SendAuthentication(WebInterface.RendererAuthenticationType.Guest);
         }
