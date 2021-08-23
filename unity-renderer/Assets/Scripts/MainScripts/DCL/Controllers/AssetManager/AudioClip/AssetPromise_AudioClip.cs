e236a6cb1e7321d1d24a4214c7fbe819e951733c
Pato <pato@decentraland.org>
2021-08-23 17:18:51 -0300

    feat: add AudioClip promise keeper

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AudioClip/AssetPromise_AudioClip.cs
@@ -0,0 +1,104 @@
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace DCL
+{
+    public class AssetPromise_AudioClip : AssetPromise<Asset_AudioClip>
+    {
+        private readonly string url;
+        private readonly string id;
+        private readonly AudioType audioType;
+        private readonly IWebRequestController webRequestController;
+
+        private WebRequestAsyncOperation webRequestAsyncOperation;
+
+        public AssetPromise_AudioClip(string clipPath, ContentProvider provider) : this(clipPath, provider, Environment.i.platform.webRequest) { }
+
+        public AssetPromise_AudioClip(string clipPath, ContentProvider provider, IWebRequestController webRequestController)
+        {
+            this.url = provider.GetContentsUrl(clipPath);
+            this.id = this.url != null ? this.url.Substring(this.url.LastIndexOf('/') + 1) : $"{GetHashCode()}";
+            this.audioType = GetAudioTypeFromUrlName(clipPath);
+            this.webRequestController = webRequestController;
+        }
+
+        protected override void OnLoad(Action OnSuccess, Action OnFail)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                OnFail?.Invoke();
+                return;
+            }
+
+            webRequestAsyncOperation = webRequestController.GetAudioClip(url, audioType,
+                request =>
+                {
+                    bool supported = true;
+#if UNITY_EDITOR
+                    supported = audioType != AudioType.MPEG;
+#endif
+                    asset.audioClip = null;
+
+                    if (supported)
+                    {
+                        asset.audioClip = DownloadHandlerAudioClip.GetContent(request);
+                    }
+
+                    OnSuccess?.Invoke();
+                },
+                request =>
+                {
+                    OnFail?.Invoke();
+                });
+        }
+
+        protected override void OnCancelLoading()
+        {
+            webRequestAsyncOperation?.Dispose();
+            webRequestAsyncOperation = null;
+        }
+
+        protected override bool AddToLibrary()
+        {
+            if (!library.Add(asset))
+            {
+                return false;
+            }
+            asset = library.Get(asset.id);
+            return true;
+        }
+
+        protected override void OnBeforeLoadOrReuse() { }
+
+        protected override void OnAfterLoadOrReuse() { }
+
+        public override object GetId()
+        {
+            return id;
+        }
+
+        private static AudioType GetAudioTypeFromUrlName(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                Debug.LogError("GetAudioTypeFromUrlName >>> Null url!");
+                return AudioType.UNKNOWN;
+            }
+
+            string ext = url.Substring(url.Length - 3).ToLower();
+
+            switch (ext)
+            {
+                case "mp3":
+                    return AudioType.MPEG;
+                case "wav":
+                    return AudioType.WAV;
+                case "ogg":
+                    return AudioType.OGGVORBIS;
+                default:
+                    return AudioType.UNKNOWN;
+            }
+        }
+    }
+}
