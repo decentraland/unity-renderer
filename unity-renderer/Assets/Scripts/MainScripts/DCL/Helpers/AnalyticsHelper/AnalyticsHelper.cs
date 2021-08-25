2d51a9f3ffb592c4eca29bedee926b7870cbde58
Adrian <54064092+Suduck@users.noreply.github.com>
2021-08-25 10:49:59 +0200

    feat: implemented analytic for external link, NFTs and voicechat (#1026)
    * feat: implemented analytic
    
    * feat: implemented analytic for voice chat
    
    * feat: correct information send
    
    * style: unified code to get the scene where the player is

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Helpers/AnalyticsHelper/AnalyticsHelper.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using DCL;
+using DCL.Controllers;
+using UnityEngine;
+
+public static class AnalyticsHelper
+{
+    public static void AddSceneNameAndBasePositionToDictionary(Dictionary<string, string> analyticDict)
+    {
+        string sceneId = Environment.i.world.state.currentSceneId;
+        if (!string.IsNullOrEmpty(sceneId))
+        {
+            analyticDict.Add("parcel", Environment.i.world.state.loadedScenes[sceneId].sceneData.basePosition.x + "," + Environment.i.world.state.loadedScenes[sceneId].sceneData.basePosition.y );
+            analyticDict.Add("scene", sceneId);
+        }
+    }
+
+    public static void SendVoiceChatStartedAnalytic()
+    {
+        Dictionary<string, string> eventToSend = new Dictionary<string, string>();
+        AddSceneNameAndBasePositionToDictionary(eventToSend);
+        Analytics.i.SendAnalytic("voice_chat_start_recording", eventToSend);
+    }
+
+    public static void SendExternalLinkAnalytic(string url, string nftToken)
+    {
+        Dictionary<string, string> eventToSend = new Dictionary<string, string>();
+        eventToSend.Add("url", url);
+        if (nftToken != null)
+            eventToSend.Add("nft_token_id", nftToken);
+        AddSceneNameAndBasePositionToDictionary(eventToSend);
+        Analytics.i.SendAnalytic("external_link_open", eventToSend);
+    }
+
+    public static void SendExternalLinkAnalytic(string url) { SendExternalLinkAnalytic(url, null); }
+}
