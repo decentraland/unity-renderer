3799596dfeda49aa041e1f10b3439564180f6f87
pbosio <pato@decentraland.org>
2021-08-27 10:27:43 -0300

    chore: send analytic event when gltf fail (#1081)

--- unity-renderer/Assets/UnityGLTF/Scripts/Loader/WebRequestLoader.cs
+++ unity-renderer/Assets/UnityGLTF/Scripts/Loader/WebRequestLoader.cs
@@ -1,9 +1,11 @@
-using DCL;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using DCL;
 using UnityEngine;
 using UnityEngine.Networking;
+using Environment = DCL.Environment;
 
 #if WINDOWS_UWP
 using System.Threading.Tasks;
@@ -53,7 +55,10 @@ namespace UnityGLTF.Loader
             return uri;
         }
 
-        public void LoadStreamSync(string jsonFilePath) { throw new NotImplementedException(); }
+        public void LoadStreamSync(string jsonFilePath)
+        {
+            throw new NotImplementedException();
+        }
 
         private IEnumerator CreateHTTPRequest(string rootUri, string httpRequestPath)
         {
@@ -73,16 +78,19 @@ namespace UnityGLTF.Loader
             yield return asyncOp;
 
             bool error = false;
+            string errorMessage = null;
 
             if (!asyncOp.isSucceded)
             {
                 Debug.LogError($"{asyncOp.webRequest.error} - {finalUrl}");
+                errorMessage = $"{asyncOp.webRequest.error} {asyncOp.webRequest.downloadHandler.text}";
                 error = true;
             }
 
             if (!error && asyncOp.webRequest.downloadedBytes > int.MaxValue)
             {
                 Debug.LogError("Stream is too big for a byte array");
+                errorMessage = "Stream is too big for a byte array";
                 error = true;
             }
 
@@ -94,7 +102,24 @@ namespace UnityGLTF.Loader
                 byte[] data = asyncOp.webRequest.downloadHandler.data;
 
                 if (data != null)
+                {
                     LoadedStream = new MemoryStream(data, 0, data.Length, true, true);
+                }
+                else
+                {
+                    error = true;
+                    errorMessage = "Downloaded data is null";
+                }
+            }
+
+            if (error && Environment.i != null)
+            {
+                Environment.i.platform.serviceProviders.analytics.SendAnalytic("gltf_fail_download",
+                    new Dictionary<string, string>()
+                    {
+                        { "url", finalUrl },
+                        { "message", errorMessage }
+                    });
             }
 
             asyncOp.Dispose();
