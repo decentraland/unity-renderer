47bc2cc9992adc3e10c6167d2d054c426a4e99df
Brian Amadori <brian@decentraland.org>
2021-09-06 15:17:28 -0300

    fix: gpu skinning normal issues (#1130)
    * fix: gpu skinning normal issues
    
    * apply PR feedback

--- unity-renderer/Assets/Rendering/Shaders/Toon/Compiled/DepthNormalsOnlyPass.hlsl
+++ unity-renderer/Assets/Rendering/Shaders/Toon/Compiled/DepthNormalsOnlyPass.hlsl
@@ -9,6 +9,11 @@ PackedVaryings vert(Attributes input)
     ApplyGPUSkinning(input, input.tangentOS, input.uv1);
     #endif
     output = BuildVaryings(input);
+    // TODO(Brian): ApplyGPUSkinning sets the final world space normal and this ends up
+    // stored to normalOS. The BuildVaryings method recalculates the normal world position
+    // assuming the normalOS is still in object space, so we have to force it to world space
+    // again here. We should improve this code in the future to avoid this workaround.
+    output.normalWS = input.normalOS;
     PackedVaryings packedOutput = (PackedVaryings)0;
     packedOutput = PackVaryings(output);
     return packedOutput;
@@ -24,9 +29,9 @@ half4 frag(PackedVaryings packedInput) : SV_TARGET
     SurfaceDescription surfaceDescription = SurfaceDescriptionFunction(surfaceDescriptionInputs);
 
     #if _AlphaClip
-        clip(surfaceDescription.Alpha - surfaceDescription.AlphaClipThreshold);
+    clip(surfaceDescription.Alpha - surfaceDescription.AlphaClipThreshold);
     #endif
-    
+
     return float4(PackNormalOctRectEncode(TransformWorldToViewDir(unpacked.normalWS, true)), 0.0, 0.0);
 }
 
--- unity-renderer/Assets/Rendering/Shaders/Toon/Compiled/GPUSkinning.hlsl
+++ unity-renderer/Assets/Rendering/Shaders/Toon/Compiled/GPUSkinning.hlsl
@@ -39,31 +39,25 @@ float4x4 inverse(float4x4 input)
 
 void ApplyGPUSkinning(inout Attributes input, float4 boneWeights01, float4 boneWeights23)
 {
-    
-    float4x4 localBoneMatrix0_weigthed = mul(mul(_Matrices[boneWeights01.x], _BindPoses[boneWeights01.x]), boneWeights01.y);
-    float4x4 localBoneMatrix1_weigthed = mul(mul(_Matrices[boneWeights01.z], _BindPoses[boneWeights01.z]), boneWeights01.w);
-    float4x4 localBoneMatrix2_weighted = mul(mul(_Matrices[boneWeights23.x], _BindPoses[boneWeights23.x]), boneWeights23.y);
-    float4x4 localBoneMatrix3_weighted = mul(mul(_Matrices[boneWeights23.z], _BindPoses[boneWeights23.z]), boneWeights23.w);
-
-    float4x4 finalPose = localBoneMatrix0_weigthed +
-        localBoneMatrix1_weigthed +
-        localBoneMatrix2_weighted +
-        localBoneMatrix3_weighted;
+    const float4x4 boneMatrix0_OS =
+        mul(mul(_WorldInverse, _Matrices[boneWeights01.x]), _BindPoses[boneWeights01.x]);
+    const float4x4 boneMatrix1_OS =
+        mul(mul(_WorldInverse, _Matrices[boneWeights01.z]), _BindPoses[boneWeights01.z]);
+    const float4x4 boneMatrix2_OS =
+        mul(mul(_WorldInverse, _Matrices[boneWeights23.x]), _BindPoses[boneWeights23.x]);
+    const float4x4 boneMatrix3_OS =
+        mul(mul(_WorldInverse, _Matrices[boneWeights23.z]), _BindPoses[boneWeights23.z]);
 
     // Skin with 4 weights per vertex
-    float4 pos =
-        mul(localBoneMatrix0_weigthed, float4(input.positionOS, 1))
-        +
-        mul(localBoneMatrix1_weigthed, float4(input.positionOS, 1))
-        +
-        mul(localBoneMatrix2_weighted, float4(input.positionOS, 1))
-        +
-        mul(localBoneMatrix3_weighted, float4(input.positionOS, 1));
+    const float4x4 skinningMatrixOS =
+        mul(boneMatrix0_OS, boneWeights01.y) + mul(boneMatrix1_OS, boneWeights01.w)
+        + mul(boneMatrix2_OS, boneWeights23.y) + mul(boneMatrix3_OS, boneWeights23.w);
 
-    // Substract the renderer position, as we want to only have bone positions into account.
-    // If we do not do this, when moving the outer parents the world position will be computed twice.
-    // (one for the bones, one for the renderer).
-    input.positionOS = mul(_WorldInverse, pos).xyz; //There's probably a way to reuse `finalPose` but I cannot find it   
-    input.normalOS = mul(transpose(inverse(finalPose)), input.normalOS);
+    const float4 finalVertexPositionOS = mul(skinningMatrixOS, float4(input.positionOS.xyz, 1.0));
+    const float3x3 transposedFinalPose = transpose(inverse(skinningMatrixOS));
+    const float3 normalOS = normalize(mul(transposedFinalPose, input.normalOS));
+
+    input.positionOS = finalVertexPositionOS.xyz;
+    input.normalOS = mul(normalOS, _WorldInverse);
 }
 #endif
