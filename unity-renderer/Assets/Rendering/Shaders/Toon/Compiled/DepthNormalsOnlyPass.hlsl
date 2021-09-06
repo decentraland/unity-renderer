290a460f5fe4f41898784063add8074365f0b014
Brian Amadori <black.pentagram@gmail.com>
2021-09-06 14:47:19 -0300

    apply PR feedback

--- unity-renderer/Assets/Rendering/Shaders/Toon/Compiled/DepthNormalsOnlyPass.hlsl
+++ unity-renderer/Assets/Rendering/Shaders/Toon/Compiled/DepthNormalsOnlyPass.hlsl
@@ -9,6 +9,10 @@ PackedVaryings vert(Attributes input)
     ApplyGPUSkinning(input, input.tangentOS, input.uv1);
     #endif
     output = BuildVaryings(input);
+    // TODO(Brian): ApplyGPUSkinning sets the final world space normal and this ends up
+    // stored to normalOS. The BuildVaryings method recalculates the normal world position
+    // assuming the normalOS is still in object space, so we have to force it to world space
+    // again here. We should improve this code in the future to avoid this workaround.
     output.normalWS = input.normalOS;
     PackedVaryings packedOutput = (PackedVaryings)0;
     packedOutput = PackVaryings(output);
