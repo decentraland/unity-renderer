0600a01fad7599696b3e47652ef7b4034acfaa08
Kinerius <kinerius@gmail.com>
2021-09-10 14:37:14 -0300

    fix: video texture not reporting pause status after being paused (#1167)
    * fix: video texture not reporting pause status after being paused
    
    * fix: hardcoding the stop state
    
    * fix: videoState not being defined on that scope, hardcoding value
    
    * fix: video events being reported twice after pausing

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Video/Plugins/VideoState.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Video/Plugins/VideoState.cs
@@ -7,6 +7,8 @@ namespace DCL.Components.Video.Plugin
         LOADING = 2,
         READY = 3,
         PLAYING = 4,
-        BUFFERING = 5
+        BUFFERING = 5,
+        SEEKING = 6,
+        PAUSED = 7,
     };
 }
