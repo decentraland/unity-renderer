381a1f055c2a988b2986e300b0a5db7747d711d7
Pravus <pravusjif@gmail.com>
2021-09-10 20:30:46 -0300

    Removed alpha channel usage when tinting impostors

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarRendererHelpers/AvatarRendererHelpers.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarRendererHelpers/AvatarRendererHelpers.cs
@@ -4,7 +4,6 @@ namespace DCL
 {
     public static class AvatarRendererHelpers
     {
-        
         private static readonly byte[] questionMarkPNG = { 137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 8, 0, 0, 0, 8, 8, 2, 0, 0, 0, 75, 109, 41, 220, 0, 0, 0, 65, 73, 68, 65, 84, 8, 29, 85, 142, 81, 10, 0, 48, 8, 66, 107, 236, 254, 87, 110, 106, 35, 172, 143, 74, 243, 65, 89, 85, 129, 202, 100, 239, 146, 115, 184, 183, 11, 109, 33, 29, 126, 114, 141, 75, 213, 65, 44, 131, 70, 24, 97, 46, 50, 34, 72, 25, 39, 181, 9, 251, 205, 14, 10, 78, 123, 43, 35, 17, 17, 228, 109, 164, 219, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130, };
         private static readonly int IMPOSTOR_TEXTURE_PROPERTY = Shader.PropertyToID("_BaseMap");
         private static readonly int IMPOSTOR_TEXTURE_COLOR_PROPERTY = Shader.PropertyToID("_BaseColor");
@@ -15,8 +14,6 @@ namespace DCL
         private const float IMPOSTOR_TINT_MAX_DISTANCE = 81.33f;
         private const float IMPOSTOR_TINT_NEAREST_BLACKNESS = 0f;
         private const float IMPOSTOR_TINT_FAREST_BLACKNESS = 0.74f;
-        private const float IMPOSTOR_ALPHA_NEAREST_VALUE = 1f;
-        private const float IMPOSTOR_ALPHA_FAREST_VALUE = 1f;
 
         // 2048x2048 atlas with 8 512x1024 snapshot-sprites
         private const int GENERIC_IMPOSTORS_ATLAS_COLUMNS = 4;
@@ -53,7 +50,7 @@ namespace DCL
 
         public static void SetImpostorTexture(Texture2D impostorTexture, Mesh impostorMesh, Material impostorMaterial)
         {
-            if (impostorTexture == null || IsQuestionMarkPNG(impostorTexture))
+            if (IsQuestionMarkPNG(impostorTexture))
                 return;
 
             ResetImpostorMeshUVs(impostorMesh);
@@ -61,11 +58,12 @@ namespace DCL
             impostorMaterial.SetTexture(IMPOSTOR_TEXTURE_PROPERTY, impostorTexture);
         }
 
+        /// <summary>
+        /// Sets impostor texture tint color ignoring the alpha channel
+        /// </summary>
         public static void SetImpostorTintColor(Material impostorMaterial, Color newColor)
         {
-            if (impostorMaterial == null)
-                return;
-
+            newColor.a = impostorMaterial.GetColor(IMPOSTOR_TEXTURE_COLOR_PROPERTY).a;
             impostorMaterial.SetColor(IMPOSTOR_TEXTURE_COLOR_PROPERTY, newColor);
         }
 
@@ -75,7 +73,6 @@ namespace DCL
             float tintStep = Mathf.InverseLerp(IMPOSTOR_TINT_MIN_DISTANCE, IMPOSTOR_TINT_MAX_DISTANCE, initialStep);
             float tintValue = Mathf.Lerp(IMPOSTOR_TINT_NEAREST_BLACKNESS, IMPOSTOR_TINT_FAREST_BLACKNESS, tintStep);
             Color newColor = Color.Lerp(Color.white, Color.black, tintValue);
-            newColor.a = Mathf.Lerp(IMPOSTOR_ALPHA_NEAREST_VALUE, IMPOSTOR_ALPHA_FAREST_VALUE, tintStep);
 
             return newColor;
         }
