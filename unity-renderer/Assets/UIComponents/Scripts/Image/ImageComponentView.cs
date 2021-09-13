05dc03f6a1c359d914a6371c4cd2ef1bc5dd179d
Santi Andrade <sandrade@decentraland.org>
2021-09-13 14:18:58 +0200

    New UI component implemented: Image

---
+++ unity-renderer/Assets/UIComponents/Scripts/Image/ImageComponentView.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface IImageComponentView
+{
+    /// <summary>
+    /// Fill the model and updates the image with this data.
+    /// </summary>
+    /// <param name="model">Data to configure the image.</param>
+    void Configure(ImageComponentModel model);
+
+    /// <summary>
+    /// Set an image.
+    /// </summary>
+    /// <param name="sprite">A sprite.</param>
+    void SetImage(Sprite sprite);
+
+    /// <summary>
+    /// Active or deactive the loading indicator.
+    /// </summary>
+    /// <param name="isVisible">True for showing the loading indicator and hidding the image.</param>
+    void SetLoadingIndicatorVisible(bool isVisible);
+}
+
+public class ImageComponentView : BaseComponentView, IImageComponentView
+{
+    [Header("Prefab References")]
+    [SerializeField] private Image image;
+    [SerializeField] private GameObject loadingIndicator;
+
+    [Header("Configuration")]
+    [SerializeField] protected ImageComponentModel model;
+
+    public void Configure(ImageComponentModel model)
+    {
+        this.model = model;
+        RefreshControl();
+    }
+
+    public override void RefreshControl() { SetImage(model.sprite); }
+
+    public void SetImage(Sprite sprite)
+    {
+        model.sprite = sprite;
+
+        if (image == null)
+            return;
+
+        image.sprite = sprite;
+    }
+
+    public void SetLoadingIndicatorVisible(bool isVisible)
+    {
+        image.enabled = !isVisible;
+        loadingIndicator.SetActive(isVisible);
+    }
+}
