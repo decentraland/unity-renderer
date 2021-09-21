a9209b8aecc18265426450ad259485befa17e1f6
Brian Amadori <brian@decentraland.org>
2021-09-21 18:00:28 -0300

    fix: combined avatar mesh leak (#1230)
    * fix: combined avatar mesh leak
    
    * fix: unhandled issue when the face wearables fail to fetch
    
    * fix empty if

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarRenderer.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarRenderer.cs
@@ -381,6 +381,15 @@ namespace DCL
                 }
             }
 
+            if ( eyesController == null && !unusedCategories.Contains(Categories.EYES))
+                unusedCategories.Add(Categories.EYES);
+
+            if ( mouthController == null && !unusedCategories.Contains(Categories.MOUTH))
+                unusedCategories.Add(Categories.MOUTH);
+
+            if ( eyebrowsController == null && !unusedCategories.Contains(Categories.EYEBROWS))
+                unusedCategories.Add(Categories.EYEBROWS);
+
             foreach (var category in unusedCategories)
             {
                 switch (category)
@@ -600,7 +609,7 @@ namespace DCL
 
             mainMeshRenderer.enabled = newVisibility;
         }
-        
+
         public void SetImpostorVisibility(bool impostorVisibility) { lodRenderer.gameObject.SetActive(impostorVisibility); }
 
         public void SetImpostorForward(Vector3 newForward) { lodRenderer.transform.forward = newForward; }
