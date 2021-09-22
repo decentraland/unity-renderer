b4102b8ba457afcf0b931fe162733ab79ede4060
Nishant Kaushik <90669979+nishant-kaush@users.noreply.github.com>
2021-09-22 14:03:50 +0530

    feat: cubemap feature added in tool refactor: All skybox code is under namespace DCL.Skybox refactor: Include DCL.Skybox namespace in Plugin class

--- unity-renderer/Assets/Rendering/ProceduralSkybox/ToolProceduralSkybox/Scripts/SkyboxController.cs
+++ unity-renderer/Assets/Rendering/ProceduralSkybox/ToolProceduralSkybox/Scripts/SkyboxController.cs
@@ -1,123 +1,126 @@
 using System.Linq;
 using UnityEngine;
 
-/// <summary>
-/// This class will handle runtime execution of skybox cycle. 
-/// Load and assign material to the Skybox.
-/// This will mostly increment the time cycle and apply values from configuration to the material.
-/// </summary>
-public class SkyboxController : PluginFeature
+namespace DCL.Skybox
 {
-    public static SkyboxController i { get; private set; }
-
-    public const string DEFAULT_SKYBOX_ID = "Generic Skybox";
-    public float timeOfTheDay;                                      // (Nishant.K) Time will be provided from outside, So remove this variable
-
-    private Light directionalLight;
-    private SkyboxConfiguration configuration;
-    private Material selectedMat;
-    private bool overrideDefaultSkybox;
-    private string overrideSkyboxID;
-    private bool isPaused;
-
-    public override void Initialize()
+    /// <summary>
+    /// This class will handle runtime execution of skybox cycle. 
+    /// Load and assign material to the Skybox.
+    /// This will mostly increment the time cycle and apply values from configuration to the material.
+    /// </summary>
+    public class SkyboxController : PluginFeature
     {
-        base.Initialize();
+        public static SkyboxController i { get; private set; }
 
-        i = this;
+        public const string DEFAULT_SKYBOX_ID = "Generic Skybox";
+        public float timeOfTheDay;                                      // (Nishant.K) Time will be provided from outside, So remove this variable
 
-        SelectSkyboxConfiguration();
+        private Light directionalLight;
+        private SkyboxConfiguration configuration;
+        private Material selectedMat;
+        private bool overrideDefaultSkybox;
+        private string overrideSkyboxID;
+        private bool isPaused;
 
-        // Enable/Disable or Create new Directional Light Object
-        directionalLight = GameObject.FindObjectsOfType<Light>().Where(s => s.type == LightType.Directional).FirstOrDefault();
-
-        if (directionalLight == null)
+        public override void Initialize()
         {
-            GameObject temp = new GameObject("The Sun");
-            // Add the light component
-            directionalLight = temp.AddComponent<Light>();
-            directionalLight.type = LightType.Directional;
+            base.Initialize();
+
+            i = this;
+
+            SelectSkyboxConfiguration();
+
+            // Enable/Disable or Create new Directional Light Object
+            directionalLight = GameObject.FindObjectsOfType<Light>().Where(s => s.type == LightType.Directional).FirstOrDefault();
+
+            if (directionalLight == null)
+            {
+                GameObject temp = new GameObject("The Sun");
+                // Add the light component
+                directionalLight = temp.AddComponent<Light>();
+                directionalLight.type = LightType.Directional;
+            }
+            DCL.DataStore.i.isProceduralSkyboxInUse.Set(true);
         }
-        DCL.DataStore.i.isProceduralSkyboxInUse.Set(true);
-    }
 
-    private void SelectSkyboxConfiguration()
-    {
-        string configToLoad = DEFAULT_SKYBOX_ID;
-
-        if (overrideDefaultSkybox)
+        private void SelectSkyboxConfiguration()
         {
-            configToLoad = overrideSkyboxID;
-        }
-        configuration = Resources.Load<SkyboxConfiguration>("Skybox Configurations/" + configToLoad);
+            string configToLoad = DEFAULT_SKYBOX_ID;
 
-        if (configuration == null)
-        {
+            if (overrideDefaultSkybox)
+            {
+                configToLoad = overrideSkyboxID;
+            }
+            configuration = Resources.Load<SkyboxConfiguration>("Skybox Configurations/" + configToLoad);
+
+            if (configuration == null)
+            {
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
-            Debug.LogError("No configuration found in Resources. (Default path through tool is Assets/Scripts/Resources/Skybox Configurations)");
+                Debug.LogError("No configuration found in Resources. (Default path through tool is Assets/Scripts/Resources/Skybox Configurations)");
 #endif
-            return;
+                return;
+            }
+
+            // Apply material as per number of Layers.
+            //TODO: Change shader on same materialinstead of having multiple material.
+            MaterialReferenceContainer.Mat_Layer matLayer = MaterialReferenceContainer.i.GetMat_LayerForLayers(configuration.textureLayers.Count);
+            if (matLayer == null)
+            {
+                matLayer = MaterialReferenceContainer.i.materials[0];
+            }
+
+            configuration.ResetMaterial(matLayer.material, matLayer.maxLayer);
+            selectedMat = matLayer.material;
+            RenderSettings.skybox = selectedMat;
         }
 
-        // Apply material as per number of Layers.
-        //TODO: Change shader on same materialinstead of having multiple material.
-        MaterialReferenceContainer.Mat_Layer matLayer = MaterialReferenceContainer.i.GetMat_LayerForLayers(configuration.textureLayers.Count);
-        if (matLayer == null)
+        // Update is called once per frame
+        public override void Update()
         {
-            matLayer = MaterialReferenceContainer.i.materials[0];
+            if (configuration == null || isPaused)
+            {
+                return;
+            }
+
+            timeOfTheDay += Time.deltaTime;
+            timeOfTheDay = Mathf.Clamp(timeOfTheDay, 0.01f, 24);
+
+            configuration.ApplyOnMaterial(selectedMat, timeOfTheDay, GetNormalizedDayTime(), directionalLight);
+
+            if (timeOfTheDay >= 24)
+            {
+                timeOfTheDay = 0.01f;
+            }
         }
 
-        configuration.ResetMaterial(matLayer.material, matLayer.maxLayer);
-        selectedMat = matLayer.material;
-        RenderSettings.skybox = selectedMat;
-    }
-
-    // Update is called once per frame
-    public override void Update()
-    {
-        if (configuration == null || isPaused)
+        public override void Dispose()
         {
-            return;
+            base.Dispose();
+            DCL.DataStore.i.isProceduralSkyboxInUse.Set(false);
         }
 
-        timeOfTheDay += Time.deltaTime;
-        timeOfTheDay = Mathf.Clamp(timeOfTheDay, 0.01f, 24);
+        public void PauseTime() { isPaused = true; }
 
-        configuration.ApplyOnMaterial(selectedMat, timeOfTheDay, GetNormalizedDayTime(), directionalLight);
-
-        if (timeOfTheDay >= 24)
+        public void ResumeTime(bool overrideTime = false, float newTime = 0)
         {
-            timeOfTheDay = 0.01f;
+            isPaused = false;
+            if (overrideTime)
+            {
+                timeOfTheDay = newTime;
+            }
         }
-    }
 
-    public override void Dispose()
-    {
-        base.Dispose();
-        DCL.DataStore.i.isProceduralSkyboxInUse.Set(false);
-    }
+        public bool IsPaused() { return isPaused; }
 
-    public void PauseTime() { isPaused = true; }
-
-    public void ResumeTime(bool overrideTime = false, float newTime = 0)
-    {
-        isPaused = false;
-        if (overrideTime)
+        private float GetNormalizedDayTime()
         {
-            timeOfTheDay = newTime;
+            float tTime = 0;
+
+            tTime = timeOfTheDay / 24;
+
+            tTime = Mathf.Clamp(tTime, 0, 1);
+
+            return tTime;
         }
-    }
-
-    public bool IsPaused() { return isPaused; }
-
-    private float GetNormalizedDayTime()
-    {
-        float tTime = 0;
-
-        tTime = timeOfTheDay / 24;
-
-        tTime = Mathf.Clamp(tTime, 0, 1);
-
-        return tTime;
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FeatureController/PluginSystem.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FeatureController/PluginSystem.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DCL;
 using DCL.Tutorial;
+using DCL.Skybox;
 using UnityEngine;
 
 /// <summary>
