897edc58a3ddf9a40feb7ecd366b448c248741a5
Patricio Guerra <kinerius@gmail.com>
2021-09-28 13:48:04 -0300

    Finally fixed dropdown box size

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/DropdownControlView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/DropdownControlView.cs
@@ -42,8 +42,6 @@ namespace DCL.SettingsPanelHUD.Controls
                 return;
 
             dropdown.options = labels.Select(l => new TMP_Dropdown.OptionData(l)).ToList();
-            dropdown.template.ForceUpdateRectTransforms();
-            dropdown.template.SetSizeWithCurrentAnchors( RectTransform.Axis.Vertical,dropdown.template.rect.height + 5);
         }
 
         protected override void OnDestroy()
