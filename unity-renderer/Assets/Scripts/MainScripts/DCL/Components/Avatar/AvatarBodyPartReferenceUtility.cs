7858b8dabb0d627bcfb4d50b113fd84321a603f8
Martin <martin.lande@hotmail.com>
2021-10-06 20:44:03 +0200

    fix: Code cleanup in AvatarBodyPartReferenceUtility

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarBodyPartReferenceUtility.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarBodyPartReferenceUtility.cs
@@ -6,23 +6,28 @@ namespace DCL
 {
     public static class AvatarBodyPartReferenceUtility
     {
-        public static Transform GetLeftHand(Transform[] bodyParts) {
+        public static Transform GetLeftHand(Transform[] bodyParts)
+        {
             return GetBodyPart(bodyParts, "Avatar_LeftHand");
         }
 
-        public static Transform GetRightHand(Transform[] bodyParts) {
+        public static Transform GetRightHand(Transform[] bodyParts)
+        {
             return GetBodyPart(bodyParts, "Avatar_RightHand");
         }
 
-        public static Transform GetLeftToe(Transform[] bodyParts) {
+        public static Transform GetLeftToe(Transform[] bodyParts)
+        {
             return GetBodyPart(bodyParts, "Avatar_LeftToeBase");
         }
 
-        public static Transform GetRightToe(Transform[] bodyParts) {
+        public static Transform GetRightToe(Transform[] bodyParts)
+        {
             return GetBodyPart(bodyParts, "Avatar_RightToeBase");
         }
 
-        private static Transform GetBodyPart(Transform[] bodyParts, string name) {
+        private static Transform GetBodyPart(Transform[] bodyParts, string name)
+        {
             for (int i = 0; i < bodyParts.Length; i++) {
                 if (bodyParts[i].name == name)
                     return bodyParts[i];
