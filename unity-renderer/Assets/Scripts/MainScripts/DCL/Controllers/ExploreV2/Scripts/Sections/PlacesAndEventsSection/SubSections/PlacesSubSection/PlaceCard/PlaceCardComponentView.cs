e9aa7470f497a03af60c8bc385c454ce94a6bdca
Santi Andrade <sandrade@decentraland.org>
2021-10-06 19:49:00 +0200

    First commit

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExploreV2/Scripts/Sections/PlacesAndEventsSection/SubSections/PlacesSubSection/PlaceCard/PlaceCardComponentView.cs
@@ -0,0 +1,8 @@
+public interface IPlaceCardComponentView { }
+
+public class PlaceCardComponentView : BaseComponentView, IPlaceCardComponentView
+{
+    public override void PostInitialization() { }
+
+    public override void RefreshControl() { }
+}
