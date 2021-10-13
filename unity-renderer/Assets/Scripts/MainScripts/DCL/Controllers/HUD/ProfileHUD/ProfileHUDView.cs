546e50fdf56732eb784d2d3c0a4873bf657c203c
Nicolas Lorusso <nalorusso@gmail.com>
2021-10-13 16:29:54 -0300

    style: logout size correction, name edition icon tooltip does not appears when name is claimed, bio char limit container adjusts to text size, name edition size adjustements

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ProfileHUD/ProfileHUDView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ProfileHUD/ProfileHUDView.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Environment = DCL.Environment;
 
