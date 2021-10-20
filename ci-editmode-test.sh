11f335c1b0433f2a0ec3ce714c5db77da5319e6d
Suduck <adrian@suduck.com>
2021-10-20 12:03:35 +0200

    fix: editor test

--- ci-editmode-test.sh
+++ ci-editmode-test.sh
@@ -17,12 +17,12 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Edito
 # Catch exit code
 UNITY_EXIT_CODE=$?
 
-mkdir -p "$PROJECT_PATH/test-results/editormode"
-cp "$PROJECT_PATH/editmode-results.xml" "$PROJECT_PATH/test-results/editormode/results.xml" || true
+#mkdir -p "$PROJECT_PATH/test-results/editormode"
+#cp "$PROJECT_PATH/editmode-results.xml" "$PROJECT_PATH/test-results/editormode/results.xml" || true
 
 # Print unity log output
 cat "$PROJECT_PATH/editmode-results.xml"
-cat "$PROJECT_PATH/editmode-results.xml" | grep test-run | grep Passed
+#cat "$PROJECT_PATH/editmode-results.xml" | grep test-run | grep Passed
 
 # Display results
 if [ $UNITY_EXIT_CODE -eq 0 ]; then
