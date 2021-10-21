a40d331250219056e121ce1f6bf1f54c5af17a6c
Alejandro Jimenez <ajimenez@decentraland.org>
2021-10-21 18:59:19 +0200

    refined metaverse calendar

--- unity-renderer/Assets/Scripts/TemporaryForMetaverse/MetaverseController.cs
+++ unity-renderer/Assets/Scripts/TemporaryForMetaverse/MetaverseController.cs
@@ -1,4 +1,6 @@
+using System;
 using DCL.Interface;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +9,8 @@ public class MetaverseController : MonoBehaviour
     [SerializeField] private Button open;
     [SerializeField] private Button close;
     [SerializeField] private GameObject container;
+    [SerializeField] private Button overlay;
+    [SerializeField] private TextMeshProUGUI clock;
 
     [Header("Jump Ins")]
     [SerializeField] private Button smash;
@@ -14,28 +18,29 @@ public class MetaverseController : MonoBehaviour
     [SerializeField] private Button techno;
     [SerializeField] private Button world;
     [SerializeField] private Button evolution;
-    [SerializeField] private Button hex;
 
     private static readonly Vector2Int smashCoords = new Vector2Int(-67, 84);
     private static readonly Vector2Int acousticCoords = new Vector2Int(-66, 81);
     private static readonly Vector2Int technoCoords = new Vector2Int(-62, 81);
     private static readonly Vector2Int worldCoords = new Vector2Int(-61, 85);
     private static readonly Vector2Int evolutionCoords = new Vector2Int(-64, 92);
-    private static readonly Vector2Int hexCoords = new Vector2Int(-69, 95);
 
     void Start()
     {
         container.SetActive(false);
         open.onClick.AddListener(OnOpen);
         close.onClick.AddListener(OnClose);
+        overlay.onClick.AddListener(OnClose);
 
         smash.onClick.AddListener(() => OnJumpIn(smashCoords));
         acoustic.onClick.AddListener(() => OnJumpIn(acousticCoords));
         techno.onClick.AddListener(() => OnJumpIn(technoCoords));
         world.onClick.AddListener(() => OnJumpIn(worldCoords));
         evolution.onClick.AddListener(() => OnJumpIn(evolutionCoords));
-        hex.onClick.AddListener(() => OnJumpIn(hexCoords));
     }
+
+    private void Update() { clock.text = DateTime.UtcNow.ToString("HH:mm:ss"); }
+
     private void OnJumpIn(Vector2Int coords)
     {
         OnClose();
