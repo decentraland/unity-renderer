f4516cc03a8046a3f9ecccfea7634ab23c24027b
Santi Andrade <sandrade@decentraland.org>
2021-11-09 10:43:37 +0100

    fix: Set transparent background in NFT prompts (#1460)
    * Set transparent background in NFT prompt
    
    * Set transparent background in NFT prompt (II)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NFTPromptHUD/NFTPromptHUDView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NFTPromptHUD/NFTPromptHUDView.cs
@@ -147,7 +147,7 @@ internal class NFTPromptHUDView : MonoBehaviour, INFTPromptHUDView
         if (fetchNFTImageRoutine != null)
             StopCoroutine(fetchNFTImageRoutine);
 
-        imageNftBackground.color = Color.white;
+        SetTransparentBackground();
 
         imageNft.gameObject.SetActive(false);
         textNftName.gameObject.SetActive(false);
@@ -178,7 +178,7 @@ internal class NFTPromptHUDView : MonoBehaviour, INFTPromptHUDView
         nftContent.SetActive(true);
 
         nftTokenId = info.tokenId;
-        imageNftBackground.color = Color.white;
+        SetTransparentBackground();
         backgroundColorSet = info.backgroundColor != null;
         if (backgroundColorSet)
         {
@@ -309,7 +309,7 @@ internal class NFTPromptHUDView : MonoBehaviour, INFTPromptHUDView
             }
             else if (!backgroundColorSet)
             {
-                SetSmartBackgroundColor(texture);
+                SetTransparentBackground();
             }
 
             SetNFTImageSize(texture);
@@ -365,11 +365,13 @@ internal class NFTPromptHUDView : MonoBehaviour, INFTPromptHUDView
         return ret;
     }
 
-    private void SetSmartBackgroundColor(Texture2D texture)
+    private void SetTransparentBackground()
     {
-        //Note: we can't set the color like that because the texture is no readable even if it has been created from script
-        //imageNftBackground.color = texture.GetPixel(0, 0);
-        imageNftBackground.color = Color.white;
+        imageNftBackground.color = new Color(
+            imageNftBackground.color.r,
+            imageNftBackground.color.g,
+            imageNftBackground.color.b,
+            0f);
     }
 
     private void SetTokenSymbol(TextMeshProUGUI textToken, string symbol)
