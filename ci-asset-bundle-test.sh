44eae791e0e029045ffd941290129937f55da082
Agustin Mendez <github@menduz.com>
2021-11-11 13:41:03 -0300

    chore: test AB conversion of big scenes

--- ci-asset-bundle-test.sh
+++ ci-asset-bundle-test.sh
@@ -5,8 +5,8 @@ set -u  # fail if any env var is not set
 
 source ci-setup.sh
 
-SCENE_ID="$(curl -s "https://peer.decentraland.org/content/entities/scene?pointer=-110,-110" | jq -r '.[0].id')"
-echo "Testing scene at -110,-110: $SCENE_ID"
+SCENE_ID="$(curl -s "https://peer-lb.decentraland.org/content/entities/scene?pointer=$POSITION" | jq -r '.[0].id')"
+echo "Testing scene at $POSITION: $SCENE_ID"
 
 pwd
 
@@ -14,7 +14,7 @@ pwd
 export SCENE_ID
 export OUTPUT_DIR=ab-output
 export LOCAL_LOG_FILE="$(pwd)/ab-logs.txt"
-export CONTENT_URL="https://peer.decentraland.org/lambdas/contentv2/contents/"
+export CONTENT_URL="https://peer-lb.decentraland.org/lambdas/contentv2/contents/"
 
 # call the conversor
 bash ./convert-asset-bundles.sh
