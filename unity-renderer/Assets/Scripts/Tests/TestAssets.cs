059b7b510e817132b0ec962042b5fca5827eb01a
Patricio Guerra <kinerius@gmail.com>
2021-11-11 17:20:56 -0300

    removed useless debug logs

--- unity-renderer/Assets/Scripts/Tests/TestAssets.cs
+++ unity-renderer/Assets/Scripts/Tests/TestAssets.cs
@@ -15,20 +15,15 @@ namespace Tests
         {
             if (_assetSearchCache.ContainsKey(assetName))
             {
-                Debug.Log($"Reuising reference for {assetName}");
-
                 return (T)_assetSearchCache[assetName];
             }
 
-            Debug.Log($"Searching for {assetName}");
-            
             foreach (var path in AssetDatabase.GetAllAssetPaths())
             {
                 string fullPath = Path.GetFullPath(path);
                 if (fullPath.Contains(assetName))
                 {
                     var relativePath = Regex.Match(fullPath, "Assets.*").Value;
-                    Debug.Log("Found path: " + relativePath);
                     var asset = AssetDatabase.LoadAssetAtPath<T>(relativePath);
                     _assetSearchCache.Add(assetName, asset);
                     return asset;
