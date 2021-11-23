dc9da13a2479fd2acc6f6efad3ad1d7eeb6e97f3
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2021-11-23 11:50:17 -0300

    refactor: settings persistence & fix of quality preset setting (#1555)

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/ProxySettingsRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DCL.SettingsCommon
+{
+    public class ProxySettingsRepository<T> : ISettingsRepository<T> where T : struct
+    {
+        private readonly ISettingsRepository<T> latestRepository;
+        private readonly ISettingsRepository<T> recoveryRepository;
+
+        public event Action<T> OnChanged
+        {
+            add => latestRepository.OnChanged += value;
+            remove => latestRepository.OnChanged -= value;
+        }
+
+        public T Data
+        {
+            get
+            {
+                if (!latestRepository.HasAnyData() && recoveryRepository.HasAnyData())
+                    latestRepository.Apply(recoveryRepository.Data);
+                return latestRepository.Data;
+            }
+        }
+
+        public ProxySettingsRepository(ISettingsRepository<T> latestRepository,
+            ISettingsRepository<T> recoveryRepository)
+        {
+            this.latestRepository = latestRepository;
+            this.recoveryRepository = recoveryRepository;
+        }
+
+        public void Apply(T settings) => latestRepository.Apply(settings);
+
+        public void Reset() => latestRepository.Reset();
+
+        public void Save() => latestRepository.Save();
+
+        public bool HasAnyData() => latestRepository.HasAnyData();
+    }
+}
