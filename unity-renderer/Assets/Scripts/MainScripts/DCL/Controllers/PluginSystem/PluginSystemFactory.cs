44205633c258de1608ec1fa4bb6c028c415e7e01
Nishant Kaushik <90669979+nishant-kaush@users.noreply.github.com>
2021-12-03 16:47:07 +0530

    chore: flag name changed. Currently the feature is open by default

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
@@ -14,7 +14,7 @@ namespace DCL
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new ExploreV2Feature(), "explorev2");
-            //pluginSystem.RegisterWithFlag(() => new SkyboxController(), "procedural_skybox");
+            //pluginSystem.RegisterWithFlag(() => new SkyboxController(), "ENABLE_PROCEDURAL_SKYBOX");
             pluginSystem.Register(() => new SkyboxController());
 
             pluginSystem.SetFeatureFlagsData(DataStore.i.featureFlags.flags);
