a90f9e8f9c0d036c883b30d993483953682cf2eb
Brian Amadori <brian@decentraland.org>
2021-12-03 08:50:48 -0300

    remove commented code

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/BlockerController/Interfaces/IWorldBlockersController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/BlockerController/Interfaces/IWorldBlockersController.cs
@@ -5,8 +5,6 @@ namespace DCL.Controllers
 {
     public interface IWorldBlockersController : IService
     {
-        //void Initialize(ISceneHandler sceneHandler, IBlockerInstanceHandler blockerInstanceHandler);
-        //void InitializeWithDefaultDependencies(ISceneHandler sceneHandler, ICullingController cullingController);
         void SetupWorldBlockers();
         void SetEnabled(bool targetValue);
     }
