04461506fcc27b230cfdf463fd4db6a37b0fabc1
Patricio Guerra <kinerius@gmail.com>
2021-12-09 18:28:23 -0300

    chore: added ProfileHUDController and view more extensibility for desktop

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ProfileHUD/ProfileHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ProfileHUD/ProfileHUDController.cs
@@ -25,7 +25,7 @@ public class ProfileHUDController : IHUD
     private const string URL_PRIVACY_POLICY = "https://decentraland.org/privacy";
     private const float FETCH_MANA_INTERVAL = 60;
 
-    internal ProfileHUDView view;
+    public readonly ProfileHUDView view;
     internal AvatarEditorHUDController avatarEditorHud;
 
     private UserProfile ownUserProfile => UserProfile.GetOwnUserProfile();
@@ -44,7 +44,7 @@ public class ProfileHUDController : IHUD
         mouseCatcher = SceneReferences.i?.mouseCatcher;
 
 
-        view = UnityEngine.Object.Instantiate(Resources.Load<GameObject>("ProfileHUD")).GetComponent<ProfileHUDView>();
+        view = UnityEngine.Object.Instantiate(GetViewPrefab()).GetComponent<ProfileHUDView>();
         view.name = "_ProfileHUD";
 
         CommonScriptableObjects.builderInWorldNotNecessaryUIVisibilityStatus.OnChange += ChangeVisibilityForBuilderInWorld;
@@ -92,6 +92,10 @@ public class ProfileHUDController : IHUD
             KernelConfig.i.EnsureConfigInitialized().Then(config => OnKernelConfigChanged(config, null));
             KernelConfig.i.OnChange += OnKernelConfigChanged;
         }
+    }
+    protected virtual GameObject GetViewPrefab()
+    {
+        return Resources.Load<GameObject>("ProfileHUD");
     }
 
     public void ChangeVisibilityForBuilderInWorld(bool current, bool previus) { view.gameObject.SetActive(current); }
