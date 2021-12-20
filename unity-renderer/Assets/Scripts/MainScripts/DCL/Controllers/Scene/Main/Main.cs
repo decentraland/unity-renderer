0c6dbc427f83f730103374623b9f6cbf95a62027
Brian Amadori <brian@decentraland.org>
2021-12-20 12:52:49 -0300

    rename `SetupEnvironment` to `SetupServices`

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Main/Main.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Main/Main.cs
@@ -92,7 +92,7 @@ namespace DCL
             pluginSystem = PluginSystemFactory.Create();
         }
 
-        protected virtual void SetupEnvironment()
+        protected virtual void SetupServices()
         {
             Environment.Setup(ServiceLocatorFactory.CreateDefault());
         }
