d0b55eb1a1544ed96c17d8f0ada664a23a1d2887
Pato <pato@decentraland.org>
2021-12-22 17:50:15 -0300

    move `AvatarOnPointerDown` out of UUIDComponents folder

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarOnPointerDown/AvatarOnPointerDown.cs
@@ -0,0 +1,144 @@
+using System;
+using DCL;
+using DCL.Components;
+using DCL.Helpers;
+using DCL.Interface;
+using DCL.Models;
+using UnityEngine;
+
+public class AvatarOnPointerDown : MonoBehaviour, IPointerEvent, IPoolLifecycleHandler, IAvatarOnPointerDownCollider
+{
+    public new Collider collider;
+    private OnPointerEvent.Model model;
+    private OnPointerEventHandler eventHandler;
+    public IDCLEntity entity { get; private set; }
+    public event Action OnPointerDownReport;
+    public event Action OnPointerEnterReport;
+    public event Action OnPointerExitReport;
+    private bool isHovering = false;
+
+    public WebInterface.ACTION_BUTTON GetActionButton()
+    {
+        return model.GetActionButton();
+    }
+
+    public void SetHoverState(bool state)
+    {
+        bool isHoveringDirty = state != isHovering;
+        isHovering = state;
+        eventHandler.SetFeedbackState(model.showFeedback, state, model.button, model.hoverText);
+        if (!isHoveringDirty)
+            return;
+        if (isHovering)
+            OnPointerEnterReport?.Invoke();
+        else
+            OnPointerExitReport?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        if (!isHovering)
+            return;
+        isHovering = false;
+        OnPointerExitReport?.Invoke();
+    }
+
+    void Awake()
+    {
+        CommonScriptableObjects.playerInfoCardVisibleState.OnChange += ReEnableOnInfoCardClosed;
+    }
+
+    void OnDestroy()
+    {
+        CommonScriptableObjects.playerInfoCardVisibleState.OnChange -= ReEnableOnInfoCardClosed;
+        eventHandler?.Dispose();
+        CollidersManager.i.RemoveEntityCollider(entity, collider);
+    }
+
+    public void Initialize(OnPointerEvent.Model model, IDCLEntity entity)
+    {
+        this.model = model;
+        this.entity = entity;
+
+        if (eventHandler == null)
+            eventHandler = new OnPointerEventHandler();
+
+        eventHandler?.SetColliders(entity);
+        CollidersManager.i.AddOrUpdateEntityCollider(entity, collider);
+    }
+
+    public bool IsAtHoverDistance(float distance)
+    {
+        return distance <= model.distance;
+    }
+
+    public bool IsVisible()
+    {
+        return true;
+    }
+
+    public bool ShouldReportEvent(WebInterface.ACTION_BUTTON buttonId, HitInfo hit)
+    {
+        return IsAtHoverDistance(hit.distance) &&
+               (model.button == "ANY" || buttonId.ToString() == model.button);
+    }
+
+    public void Report(WebInterface.ACTION_BUTTON buttonId, Ray ray, HitInfo hit)
+    {
+        if (!enabled)
+            return;
+
+        if (ShouldReportEvent(buttonId, hit))
+        {
+            string meshName = null;
+
+            if (hit.collider != null)
+                meshName = eventHandler.GetMeshName(hit.collider);
+
+            // WebInterface.ReportOnPointerDownEvent(
+            //     buttonId,
+            //     entity.scene.sceneData.id,
+            //     model.uuid,
+            //     entity.entityId,
+            //     meshName,
+            //     ray,
+            //     hit.point,
+            //     hit.normal,
+            //     hit.distance);
+
+            eventHandler.SetFeedbackState(model.showFeedback, false, model.button, model.hoverText);
+            enabled = false;
+
+            OnPointerDownReport?.Invoke();
+        }
+    }
+
+    public PointerEventType GetEventType()
+    {
+        return PointerEventType.DOWN;
+    }
+
+    void ReEnableOnInfoCardClosed(bool newState, bool prevState)
+    {
+        if (enabled || newState)
+            return;
+
+        enabled = true;
+    }
+    public void SetColliderEnabled(bool newEnabledState)
+    {
+        collider.enabled = newEnabledState;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+
+    public void OnPoolRelease()
+    {
+        eventHandler.Dispose();
+    }
+
+    public void OnPoolGet() { }
+}
