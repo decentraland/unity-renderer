f020b28f08b7fb69542bb3f0179c17d71806c5f0
Nicolas Lorusso <nalorusso@gmail.com>
2021-12-22 16:42:01 -0300

    chore: added cursor locking deactivation comments

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/EmotesHUD/EmotesHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/EmotesHUD/EmotesHUDController.cs
@@ -40,6 +40,8 @@ public class EmotesHUDController : IHUD
         }
         else
         {
+            // Avoid locking cursor. It produces undesired mouse locking the first time you gain focus in the window
+            // during loading screen or first avatar creation
             //DCL.Helpers.Utils.LockCursor();
             ownUserProfile.snapshotObserver.RemoveListener(view.UpdateAvatarSprite);
         }
