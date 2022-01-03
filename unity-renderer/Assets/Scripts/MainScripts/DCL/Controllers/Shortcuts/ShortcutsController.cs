0fad21bcaf3731e5d16fb900a67c93f0918560cb
Nicolas Lorusso <nalorusso@gmail.com>
2022-01-03 20:13:21 -0300

    fixed unable to open start menu during tutorial

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Shortcuts/ShortcutsController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Shortcuts/ShortcutsController.cs
@@ -66,9 +66,7 @@ public class ShortcutsController : IDisposable
     private void ToggleStartMenuTriggered(DCLAction_Trigger action)
     {
         bool value = !DataStore.i.exploreV2.isOpen.Get();
-        var isOnTutorialFlow = DataStore.i.common.isTutorialRunning.Get()
-                               | DataStore.i.common.isSignUpFlow.Get();
-        if (isOnTutorialFlow) return;
+        if (DataStore.i.common.isSignUpFlow.Get()) return;
 
         if (value)
         {
