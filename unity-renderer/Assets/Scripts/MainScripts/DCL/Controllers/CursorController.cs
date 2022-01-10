4d2dc36fc4f390763abe37b4346589e816a51721
Nicolas Lorusso <nalorusso@gmail.com>
2022-01-10 11:48:47 -0300

    added alpha interpolation when showing or hiding the cursor

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/CursorController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/CursorController.cs
@@ -1,26 +1,38 @@
-﻿using DCL;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CursorController : MonoBehaviour
 {
+    private const float ALPHA_INTERPOLATION_DURATION = 0.1f;
+    
     public static CursorController i { get; private set; }
     public Image cursorImage;
     public Sprite normalCursor;
     public Sprite hoverCursor;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    private Coroutine alphaRoutine;
 
     void Awake() { i = this; }
 
     public void Show()
     {
         if (cursorImage == null) return;
+        if (cursorImage.gameObject.activeSelf) return;
         cursorImage.gameObject.SetActive(true);
+        if (alphaRoutine != null) StopCoroutine(alphaRoutine);
+        alphaRoutine = StartCoroutine(SetAlpha(0f, 1f, ALPHA_INTERPOLATION_DURATION));
     }
 
     public void Hide()
     {
         if (cursorImage == null) return;
-        cursorImage.gameObject.SetActive(false);
+        if (!cursorImage.gameObject.activeSelf) return;
+        if (alphaRoutine != null) StopCoroutine(alphaRoutine);
+        alphaRoutine = StartCoroutine(SetAlpha(1f, 0f, ALPHA_INTERPOLATION_DURATION,
+            () => cursorImage.gameObject.SetActive(false)));
     }
 
     public void SetNormalCursor()
@@ -33,5 +45,20 @@ public class CursorController : MonoBehaviour
     {
         cursorImage.sprite = hoverCursor;
         cursorImage.SetNativeSize();
+    }
+
+    private IEnumerator SetAlpha(float from, float to, float duration, Action callback = null)
+    {
+        var time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(from, to, time / duration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = to;
+        callback?.Invoke();
     }
 }
