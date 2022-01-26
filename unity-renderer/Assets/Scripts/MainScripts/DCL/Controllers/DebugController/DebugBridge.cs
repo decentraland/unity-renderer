25e9ea13e8ef167ad3f8c57b06d03eed5fd3fc6b
Pato <pato@decentraland.org>
2022-01-26 17:10:48 -0300

    message payload should be a string...

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/DebugController/DebugBridge.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/DebugController/DebugBridge.cs
@@ -132,10 +132,12 @@ namespace DCL
             DataStore.i.debugConfig.showSceneBoundingBoxes.AddOrSet(data.sceneId, data.enabled);
         }
 
-        public void TogglePreviewMenu(int booleanValue)
+        public void TogglePreviewMenu(string booleanValue)
         {
-            bool isEnabled = booleanValue != 0;
-            DataStore.i.debugConfig.isPreviewMenuActive.Set(isEnabled);
+            if (bool.TryParse(booleanValue, out bool isEnabled))
+            {
+                DataStore.i.debugConfig.isPreviewMenuActive.Set(isEnabled);
+            }
         }
 
 #if UNITY_EDITOR
