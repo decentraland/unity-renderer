99fb0f2c5024af0ede7d1817e0c0d8db38885d09
Santi Andrade <sandrade@decentraland.org>
2022-02-07 12:23:00 +0100

    Add Feature Flag (for ALL PortableExperiences flow)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
@@ -14,11 +14,11 @@ namespace DCL
             pluginSystem.Register(() => new ExploreV2Feature());
             pluginSystem.Register(() => new DebugShapesBoundingBoxDisplayer());
             pluginSystem.Register(() => new TransactionFeature());
-            pluginSystem.Register(() => new ExperiencesViewerFeature());
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
             pluginSystem.RegisterWithFlag(() => new SkyboxController(), "procedural_skybox");
+            pluginSystem.RegisterWithFlag(() => new ExperiencesViewerFeature(), "portable_experiences");
 
             pluginSystem.SetFeatureFlagsData(DataStore.i.featureFlags.flags);
 
