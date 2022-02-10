eb090d8408fb022a41068705f915f5db9759abee
Patricio Guerra <kinerius@gmail.com>
2022-02-10 14:22:29 -0300

    Rollback undesired changes

--- unity-renderer/Assets/Scripts/MainScripts/Debugging/DebugParameters/DebugConfigComponent.cs
+++ unity-renderer/Assets/Scripts/MainScripts/Debugging/DebugParameters/DebugConfigComponent.cs
@@ -79,7 +79,6 @@ namespace DCL
         public bool builderInWorld = false;
         public bool soloScene = true;
         public bool multithreaded = false;
-        public bool disableTextureCompress = false;
         public DebugPanel debugPanelMode = DebugPanel.Off;
 
         private void Awake()
@@ -92,7 +91,6 @@ namespace DCL
             DataStore.i.debugConfig.ignoreGlobalScenes = debugConfig.ignoreGlobalScenes;
             DataStore.i.debugConfig.msgStepByStep = debugConfig.msgStepByStep;
             DataStore.i.performance.multithreading.Set(multithreaded);
-            DataStore.i.performance.compressTextures.Set(!disableTextureCompress);
             Texture.allowThreadedTextureCreation = multithreaded;
         }
 
