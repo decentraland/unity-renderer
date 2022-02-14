3b1fda468c5f97480a742371e15be331e2cf9480
pbosio <pato@decentraland.org>
2022-02-14 15:20:19 -0300

    feat: add preview mode menu (#1845)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
@@ -14,6 +14,7 @@ namespace DCL
             pluginSystem.Register(() => new ExploreV2Feature());
             pluginSystem.Register(() => new DebugShapesBoundingBoxDisplayer());
             pluginSystem.Register(() => new TransactionFeature());
+            pluginSystem.Register(() => new PreviewMenuPlugin());
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
