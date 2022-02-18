b915eebed4d08abace9587f7f7cb5ec2bc6b7af1
Santi Andrade <sandrade@decentraland.org>
2022-02-18 21:36:19 +0100

    Remove the use of the "portable_experiences" FF temporally

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
@@ -17,11 +17,11 @@ namespace DCL
             pluginSystem.Register(() => new TransactionFeature());
             pluginSystem.Register(() => new PreviewMenuPlugin());
             pluginSystem.Register(() => new SkyboxController());
+            pluginSystem.Register(() => new ExperiencesViewerFeature());
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
             pluginSystem.RegisterWithFlag(() => new SkyboxController(), "procedural_skybox");
-            pluginSystem.RegisterWithFlag(() => new ExperiencesViewerFeature(), "portable_experiences");
 
             pluginSystem.SetFeatureFlagsData(DataStore.i.featureFlags.flags);
 
