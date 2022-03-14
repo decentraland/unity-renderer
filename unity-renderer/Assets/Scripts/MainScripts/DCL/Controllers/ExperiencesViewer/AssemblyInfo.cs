7970eeb10441e8ca003c4caef0ca01aa5d470236
Santi Andrade <sandrade@decentraland.org>
2022-03-14 19:26:10 +0100

    feat: Experiences Viewer implementation (#1873)
    * Initial commit
    
    * Fix tests
    
    * Initialize and instantiate ExperiencesViewer in the correct place
    
    * ExperienceRow UI Component implemented
    
    * ExperienceViewer UI Component implemented
    
    * Load PEX in the viewer (I)
    
    * Start implementing the business logic
    
    * Hide show/hide UI buttons when the PEX is not playing
    
    * Fix bug with the UnlockCursor when the StartMenu open
    
    * Apply final UI assets
    
    * Make the PEX list independent of the wearables
    
    * Show an info toast when a PEX UI is hidden
    
    * Implement Start/Stop PEX
    
    * Only keep an experience paused in the list if our avatar has the related wearable equipped
    
    * Remove PEX behaviour in the taskbar
    
    * Add Feature Flag (for ALL PortableExperiences flow)
    
    * Use SetDisabledPortableExperiences to start/stop PEX
    
    * Update SceneController.cs
    
    * Update ExperiencesViewerComponentController.cs
    
    * Implement ExperienceRowComponentViewTests
    
    * Implement ExperiencesViewerComponentViewTests
    
    * Implement ExperiencesViewerComponentControllerTests
    
    * Add feature namespace
    
    * Update ExperiencesViewerComponentController.cs
    
    * Update Taskbar.prefab
    
    * Use a Toggle_Capsule into ExperienceRow prefab
    
    * Update ExperiencesViewerComponentController.cs
    
    * Apply feedback
    
    * Apply feedback
    
    * Apply feedback
    
    * Apply feedback
    
    * Update Taskbar.prefab
    
    * Update ExperiencesViewerComponentController.cs
    
    * Update TexturesUI.spriteatlas
    
    * Remove the use of the "portable_experiences" FF temporally
    
    * Update ExperiencesViewerComponentController.cs
    
    * Use KillPortableExperience for PEX non-associated to smart wearables
    
    * Update visuals
    
    * Apply feedback
    
    * Apply visual feedback
    
    * Prefabs polished
    
    * Textures settings updated
    
    * Update Taskbar.prefab
    
    * Update TexturesUI.spriteatlas
    
    Co-authored-by: Romina Marchetti <rma.marchetti@gmail.com>

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExperiencesViewer/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ExperiencesViewerTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] //Needed for nsubstitute
