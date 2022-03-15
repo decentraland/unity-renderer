e177f677601e3efb0068cefed3b819e5d890de00
davidejensen <davidejensen@live.it>
2022-03-15 10:03:36 +0100

    Feat: clickable coordinates in chat (#1965)
    * initial changes for goto button when coordinates are present
    
    * different changes for clickable coordinates in chat and private chat
    
    * general improvements
    
    * removed log
    
    * reworked panel for chat tp
    
    * minor improvement to goto panel
    
    * minor cleanup of codebase
    
    * minor code cleanup
    
    * additional code cleanup
    
    * applied UI changes as requested by Romi
    
    * Reworked goto panel
    
    * code cleanup and prefab improvs
    
    * various improvements
    
    * various improvements and fixes
    
    * applied requested changes and init tests
    
    * Fonts material fixed. Toast container corner radius adjusted.
    
    * applied various changes as suggested
    
    * minor fix
    
    * fixed worng prefab reference
    
    * completed refactoring
    
    * code improvs
    
    Co-authored-by: Romina Marchetti <rma.marchetti@gmail.com>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
@@ -18,6 +18,7 @@ namespace DCL
             pluginSystem.Register(() => new TransactionFeature());
             pluginSystem.Register(() => new PreviewMenuPlugin());
             pluginSystem.Register(() => new SkyboxController());
+            pluginSystem.Register(() => new GotoPanelPlugin());
             pluginSystem.Register(() => new ExperiencesViewerFeature());
             pluginSystem.Register(() => new EmoteAnimationsPlugin(DataStore.i.emotes));
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
