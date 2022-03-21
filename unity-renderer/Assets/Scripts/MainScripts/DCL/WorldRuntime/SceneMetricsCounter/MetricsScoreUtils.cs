0c259259391e1a10ee5daec73e37cfdeb1dcc1fc
Brian Amadori <brian@decentraland.org>
2022-03-21 12:46:16 -0300

    fix null ref

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneMetricsCounter/MetricsScoreUtils.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/SceneMetricsCounter/MetricsScoreUtils.cs
@@ -6,6 +6,9 @@ namespace DCL
     {
         public static long ComputeAudioClipScore(AudioClip audioClip)
         {
+            if (audioClip == null)
+                return 0;
+            
             long baseOverhead = 3000; // Measured 2708 profiling AudioClip with 1 sample and 2 channels.
             // Rounding up just in case.
 
@@ -16,14 +19,13 @@ namespace DCL
 
         public static long ComputeTextureScore(Texture2D texture)
         {
-            // https://forum.unity.com/threads/does-unity-always-use-double-memory-for-texture-in-runtime.198270/
-            const double ARBITRARY_UNITY_MEMORY_MULTIPLIER = 1;
+            if (texture == null)
+                return 0;
 
             // The mipmap memory increase should be actually 1.33 according to many sources
             const double MIPMAP_FACTOR = 1.4f; 
             const double BYTES_PER_PIXEL = 4;
-            return (long) (texture.width * texture.height * BYTES_PER_PIXEL * MIPMAP_FACTOR *
-                           ARBITRARY_UNITY_MEMORY_MULTIPLIER);
+            return (long) (texture.width * texture.height * BYTES_PER_PIXEL * MIPMAP_FACTOR);
         }
     }
 }
