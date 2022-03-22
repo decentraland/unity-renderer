5dde6eb953a01d43f2a90c2be089eae29209d311
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2022-03-22 19:36:40 +0100

    feat: emotes from contentserver (#2025)
    * feat: emotes from the content server
    
    * chore: emote animations plugin tests
    
    * fix: issue in a equipper test
    
    * chore: code review

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/EmoteAnimationsPlugin/EmoteAnimationLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using AvatarSystem;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace DCL.Emotes
+{
+    public class EmoteAnimationLoader : IEmoteAnimationLoader
+    {
+        private readonly IWearableRetriever retriever;
+        public AnimationClip animation { get; internal set; }
+
+        public EmoteAnimationLoader(IWearableRetriever retriever) { this.retriever = retriever; }
+
+        public async UniTask LoadEmote(GameObject container, WearableItem emote, string bodyShapeId, CancellationToken ct = default)
+        {
+            if (container == null)
+                throw new NullReferenceException("Container cannot be null");
+
+            if (emote == null)
+                throw new NullReferenceException("Emote cannot be null");
+
+            if (string.IsNullOrEmpty(bodyShapeId))
+                throw new NullReferenceException("bodyShapeId cannot be null or empty");
+
+            ct.ThrowIfCancellationRequested();
+
+            WearableItem.Representation representation = emote.GetRepresentation(bodyShapeId);
+            if (representation == null)
+            {
+                throw new Exception($"No representation for {bodyShapeId} of emote: {emote.id}");
+            }
+
+            Rendereable rendereable = await retriever.Retrieve(container, emote.GetContentProvider(bodyShapeId), emote.baseUrlBundles, representation.mainFile, ct);
+
+            animation = rendereable.container.GetComponentInChildren<Animation>()?.clip;
+        }
+
+        public void Dispose() { retriever?.Dispose(); }
+    }
+}
---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/EmoteAnimationsPlugin/Tests/EmoteAnimationsPluginShould.cs
@@ -0,0 +1,94 @@
+using System.Threading;
+using AvatarSystem;
+using Cysharp.Threading.Tasks;
+using DCL.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace DCL.Emotes
+{
+    public class EmoteAnimationsPluginShould
+    {
+        private EmoteAnimationsPlugin emoteAnimationsPlugin;
+        private DataStore_Emotes dataStore;
+        private EmoteAnimationLoaderFactory loaderFactory;
+        private IWearableItemResolver resolver;
+        private CatalogController catalogController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            catalogController = TestUtils.CreateComponentWithGameObject<CatalogController>("CatalogController");
+            dataStore = new DataStore_Emotes();
+            loaderFactory = Substitute.ForPartsOf<EmoteAnimationLoaderFactory>();
+            loaderFactory.Get().Returns(Substitute.For<IEmoteAnimationLoader>());
+            resolver = Substitute.For<IWearableItemResolver>();
+            emoteAnimationsPlugin = new EmoteAnimationsPlugin(dataStore, loaderFactory, resolver);
+        }
+
+        [TearDown]
+        public void TearDown() { Object.Destroy(catalogController.gameObject); }
+
+        [Test]
+        public void InitializeEmbeddedEmotesOnConstructor()
+        {
+            EmbeddedEmotesSO embeddedEmotes = Resources.Load<EmbeddedEmotesSO>("EmbeddedEmotes");
+            foreach (EmbeddedEmote emote in embeddedEmotes.emotes)
+            {
+                Assert.AreEqual(dataStore.animations[(WearableLiterals.BodyShapes.FEMALE, emote.id)], emote.femaleAnimation);
+                Assert.AreEqual(dataStore.animations[(WearableLiterals.BodyShapes.MALE, emote.id)], emote.maleAnimation);
+                Assert.IsTrue(emoteAnimationsPlugin.loaders.ContainsKey((WearableLiterals.BodyShapes.MALE, emote.id)));
+                Assert.AreEqual(CatalogController.wearableCatalog[emote.id], emote);
+            }
+        }
+
+        [Test]
+        public void ReactToEquipEmotesIncreasingReference()
+        {
+            string bodyShapeId = WearableLiterals.BodyShapes.FEMALE;
+
+            AnimationClip tikAnim = Resources.Load<AnimationClip>("tik");
+            WearableItem emote = new WearableItem { id = "emote0" };
+            resolver.Resolve("emote0", Arg.Any<CancellationToken>()).Returns(new UniTask<WearableItem>(emote));
+            IEmoteAnimationLoader loader = Substitute.For<IEmoteAnimationLoader>();
+            loader.animation.Returns(tikAnim);
+            loaderFactory.Get().Returns(loader);
+
+            dataStore.emotesOnUse.IncreaseRefCount((bodyShapeId, "emote0"));
+
+            loaderFactory.Received().Get();
+            resolver.Received().Resolve("emote0", Arg.Any<CancellationToken>());
+            loader.Received().LoadEmote(emoteAnimationsPlugin.animationsModelsContainer, emote, bodyShapeId, Arg.Any<CancellationToken>());
+            Assert.AreEqual(tikAnim, dataStore.animations[(bodyShapeId, "emote0")]);
+        }
+
+        [Test]
+        public void ReactToEquipEmotesIncreasingReferenceWithExistentLoader()
+        {
+            loaderFactory.ClearReceivedCalls();
+            emoteAnimationsPlugin.loaders.Add((WearableLiterals.BodyShapes.FEMALE, "emote0"), Substitute.For<IEmoteAnimationLoader>());
+
+            dataStore.emotesOnUse.IncreaseRefCount((WearableLiterals.BodyShapes.FEMALE, "emote0"));
+
+            resolver.DidNotReceive().Resolve("emote0", Arg.Any<CancellationToken>());
+            loaderFactory.DidNotReceive().Get();
+        }
+
+        [Test]
+        public void ReactToEquipEmotesReferenceSetTo0()
+        {
+            string bodyshapeId = WearableLiterals.BodyShapes.FEMALE;
+            var tikAnim = Resources.Load<AnimationClip>("tik");
+            dataStore.animations.Add((bodyshapeId, "emote0"), tikAnim);
+            IEmoteAnimationLoader loader = Substitute.For<IEmoteAnimationLoader>();
+            emoteAnimationsPlugin.loaders.Add((bodyshapeId, "emote0"), loader);
+
+            dataStore.emotesOnUse.SetRefCount((bodyshapeId, "emote0"), 0);
+
+            Assert.IsFalse(emoteAnimationsPlugin.loaders.ContainsKey((bodyshapeId, "emote0")));
+            loader.Received().Dispose();
+            Assert.IsFalse(dataStore.animations.ContainsKey((bodyshapeId, "emote0")));
+        }
+    }
+}
