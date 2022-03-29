15a2ade253b254cecf3f609cc483746e90b5bc38
Alejandro Jimenez <50231608+AjimenezDCL@users.noreply.github.com>
2022-03-29 12:36:24 +0200

    refactor: implement humble pattern in EmoteAnimations plugin (#2079)
    * refactor: implement humble pattern in EmoteAnimations plugin
    
    * chore: added some context on plugin construction

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystem/PluginSystemFactory.cs
@@ -13,6 +13,9 @@ namespace DCL
         {
             var pluginSystem = new PluginSystem();
 
+            // Ideally the Plugin class itself should be a really small entry point with a parameterless constructor
+            // the heavy lifting should be done by another class (following the Humble Object Pattern)
+
             pluginSystem.Register(() => new DebugPluginFeature());
             pluginSystem.Register(() => new ShortcutsFeature());
             pluginSystem.Register(() => new ExploreV2Feature());
@@ -22,7 +25,7 @@ namespace DCL
             pluginSystem.Register(() => new SkyboxController());
             pluginSystem.Register(() => new GotoPanelPlugin());
             pluginSystem.Register(() => new ExperiencesViewerFeature());
-            pluginSystem.Register(() => new EmoteAnimationsPlugin(DataStore.i.emotes, new EmoteAnimationLoaderFactory(), new WearableItemResolver()));
+            pluginSystem.Register(() => new EmoteAnimationsPlugin());
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
