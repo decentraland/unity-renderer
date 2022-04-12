af9c6909431a4b2cc110566f783b434e3cdfce71
Nicolas Lorusso <nalorusso@gmail.com>
2022-04-12 12:04:12 -0300

    hud tests uses its own HUDController instance instead of referencing from Environment

--- unity-renderer/Assets/Scripts/Tests/IntegrationTestSuite_Legacy.cs
+++ unity-renderer/Assets/Scripts/Tests/IntegrationTestSuite_Legacy.cs
@@ -52,7 +52,6 @@ public class IntegrationTestSuite_Legacy
         result.Register<IMemoryManager>(() => Substitute.For<IMemoryManager>());
         result.Register<IParcelScenesCleaner>(() => Substitute.For<IParcelScenesCleaner>());
         result.Register<ICullingController>(() => Substitute.For<ICullingController>());
-        result.Register<IWebRequestController>(() => Substitute.For<IWebRequestController>());
 
         result.Register<IServiceProviders>(
             () =>
