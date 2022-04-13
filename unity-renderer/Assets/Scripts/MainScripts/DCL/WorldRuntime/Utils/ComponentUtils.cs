68a32085c4b8296ab08c51f1b16102529635c1ad
Brian Amadori <brian@decentraland.org>
2022-04-13 17:38:18 -0300

    apply PR feedback

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/Utils/ComponentUtils.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/Utils/ComponentUtils.cs
@@ -10,20 +10,20 @@ namespace DCL.Controllers
         {
             float dist = int.MaxValue;
 
-            if (disposableComponent.GetAttachedEntities().Count > 0)
-            {
-                using (var iterator = disposableComponent.GetAttachedEntities().GetEnumerator())
-                {
-                    while (iterator.MoveNext())
-                    {
-                        IDCLEntity entity = iterator.Current;
+            if (disposableComponent.GetAttachedEntities().Count <= 0)
+                return dist;
 
-                        if (IsEntityVisible(iterator.Current))
-                        {
-                            var entityDist = (entity.meshRootGameObject.transform.position - fromPosition).sqrMagnitude;
-                            if (entityDist < dist)
-                                dist = entityDist;
-                        }
+            using (var iterator = disposableComponent.GetAttachedEntities().GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    IDCLEntity entity = iterator.Current;
+
+                    if (IsEntityVisible(iterator.Current))
+                    {
+                        var entityDist = (entity.meshRootGameObject.transform.position - fromPosition).sqrMagnitude;
+                        if (entityDist < dist)
+                            dist = entityDist;
                     }
                 }
             }
@@ -33,17 +33,17 @@ namespace DCL.Controllers
 
         public static bool IsComponentVisible(ISharedComponent disposableComponent)
         {
-            if (disposableComponent.GetAttachedEntities().Count > 0)
-            {
-                using (var iterator = disposableComponent.GetAttachedEntities().GetEnumerator())
-                {
-                    while (iterator.MoveNext())
-                    {
-                        bool isEntityVisible = IsEntityVisible(iterator.Current);
+            if (disposableComponent.GetAttachedEntities().Count <= 0)
+                return false;
 
-                        if (isEntityVisible)
-                            return true;
-                    }
+            using (var iterator = disposableComponent.GetAttachedEntities().GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    bool isEntityVisible = IsEntityVisible(iterator.Current);
+
+                    if (isEntityVisible)
+                        return true;
                 }
             }
 
@@ -54,39 +54,41 @@ namespace DCL.Controllers
         {
             if (entity.meshesInfo == null)
                 return false;
+            
             if (entity.meshesInfo.currentShape == null)
                 return false;
+            
             return entity.meshesInfo.currentShape.IsVisible();
         }
 
         public static void UnsubscribeToEntityShapeUpdate(ISharedComponent component,
             System.Action<IDCLEntity> OnUpdate)
         {
-            if (component.GetAttachedEntities().Count > 0)
+            if (component.GetAttachedEntities().Count <= 0)
+                return;
+
+            using (var iterator = component.GetAttachedEntities().GetEnumerator())
             {
-                using (var iterator = component.GetAttachedEntities().GetEnumerator())
+                while (iterator.MoveNext())
                 {
-                    while (iterator.MoveNext())
-                    {
-                        var entity = iterator.Current;
-                        entity.OnShapeUpdated -= OnUpdate;
-                    }
+                    var entity = iterator.Current;
+                    entity.OnShapeUpdated -= OnUpdate;
                 }
             }
         }
 
         public static void SubscribeToEntityUpdates(ISharedComponent component, System.Action<IDCLEntity> OnUpdate)
         {
-            if (component.GetAttachedEntities().Count > 0)
+            if (component.GetAttachedEntities().Count <= 0)
+                return;
+
+            using (var iterator = component.GetAttachedEntities().GetEnumerator())
             {
-                using (var iterator = component.GetAttachedEntities().GetEnumerator())
+                while (iterator.MoveNext())
                 {
-                    while (iterator.MoveNext())
-                    {
-                        var entity = iterator.Current;
-                        entity.OnShapeUpdated -= OnUpdate;
-                        entity.OnShapeUpdated += OnUpdate;
-                    }
+                    var entity = iterator.Current;
+                    entity.OnShapeUpdated -= OnUpdate;
+                    entity.OnShapeUpdated += OnUpdate;
                 }
             }
         }
