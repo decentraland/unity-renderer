cd3b29870148dd8119c3b33328a87f7933fae356
Santi Andrade <sandrade@decentraland.org>
2022-04-15 15:45:54 +0200

    Implement “no results found” for Dropdown UI Component

--- unity-renderer/Assets/UIComponents/Scripts/Components/Dropdown/DropdownComponentModel.cs
+++ unity-renderer/Assets/UIComponents/Scripts/Components/Dropdown/DropdownComponentModel.cs
@@ -6,9 +6,11 @@ public class DropdownComponentModel : BaseComponentModel
 {
     public string title;
     public string searchPlaceHolderText;
+    public string searchNotFoundText;
     public bool isMultiselect = true;
     public bool showSelectAllOption = true;
     public bool isOptionsPanelHeightDynamic = false;
     public float maxValueForDynamicHeight = 500f;
+    public string emptyContentText;
     public List<ToggleComponentModel> options;
 }
