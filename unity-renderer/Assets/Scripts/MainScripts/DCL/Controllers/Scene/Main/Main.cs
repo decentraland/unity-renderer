a0166db64d75d8c7491cc4fe8df80f79bac321fc
Mateo Miccino <mateomiccino@gmail.com>
2022-04-25 09:54:07 -0300

    refactor: plugin system register with the type as key instead of the … (#2083)
    * refactor: plugin system register with the type as key instead of the pluginbuilder
    
    * add adr reference
    
    * add initialization to the PluginSystem
    
    * wip
    
    * fix unregister flags

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Main/Main.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Main/Main.cs
@@ -91,6 +91,7 @@ namespace DCL
         protected virtual void SetupPlugins()
         {
             pluginSystem = PluginSystemFactory.Create();
+            pluginSystem.Initialize();
         }
 
         protected virtual void SetupServices()
