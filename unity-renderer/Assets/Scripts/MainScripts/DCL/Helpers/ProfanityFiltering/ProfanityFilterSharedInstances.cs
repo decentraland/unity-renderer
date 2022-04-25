04a4463ddbbde0535cad74f460a4cd4d11101b6a
Kinerius <kinerius@gmail.com>
2022-04-25 18:22:16 -0300

    Added comment

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Helpers/ProfanityFiltering/ProfanityFilterSharedInstances.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Helpers/ProfanityFiltering/ProfanityFilterSharedInstances.cs
@@ -1,5 +1,8 @@
 ﻿public static class ProfanityFilterSharedInstances
 {
+    // Check https://github.com/decentraland/unity-renderer/issues/2201 for more info about this number
+    private const int PARTITION_SIZE = 20;
+    
     private static IProfanityFilter regexFilterInstance;
 
     public static IProfanityFilter regexFilter
@@ -7,7 +10,7 @@
         get
         {
             return regexFilterInstance ??= new ThrottledRegexProfanityFilter(
-                new ProfanityWordProviderFromResourcesJson("Profanity/badwords"), 20);
+                new ProfanityWordProviderFromResourcesJson("Profanity/badwords"), PARTITION_SIZE);
         }
     }
 }
