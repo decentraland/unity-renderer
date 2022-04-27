142511fe2b0eba1946947933bc7549494848269d
Suduck <adrian@suduck.com>
2022-04-27 09:59:17 +0200

    feat: remove dclresources

--- unity-renderer/Assets/Scripts/MainScripts/DCL/DCLResources/DCLResourcesInterfaces/IDCLTextureResource.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDCLTextureResource 
-{
-    Texture2D texture2D { get; }
-}
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Environment/Factories/ServiceLocatorFactory/ServiceLocatorFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Environment/Factories/ServiceLocatorFactory/ServiceLocatorFactory.cs
@@ -29,7 +29,6 @@ namespace DCL
             result.Register<ISceneBoundsChecker>(() => new SceneBoundsChecker());
             result.Register<IWorldBlockersController>(() => new WorldBlockersController());
             result.Register<IRuntimeComponentFactory>(() => new RuntimeComponentFactory());
-            result.Register<IResourceManagerService>(() => new ResourceManagerService());
 
             result.Register<IMessagingControllersManager>(() => new MessagingControllersManager());
 
