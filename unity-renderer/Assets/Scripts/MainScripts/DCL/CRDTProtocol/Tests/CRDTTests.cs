98a0b61042c65e28f301dd3efa754ef56839d44d
Pato <pato@decentraland.org>
2022-04-28 17:50:00 -0300

    refactor: test

--- unity-renderer/Assets/Scripts/MainScripts/DCL/CRDTProtocol/Tests/CRDTTests.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/CRDTProtocol/Tests/CRDTTests.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
 using DCL;
-using Newtonsoft.Json;
 using NUnit.Framework;
-using UnityEditor;
-using UnityEngine;
 
 namespace Tests
 {
@@ -14,82 +9,33 @@ namespace Tests
         [Test]
         public void MessagesProcessedCorrectly()
         {
-            var g = AssetDatabase.FindAssets($"t:Script {nameof(CRDTTests)}");
-            var filePath = AssetDatabase.GUIDToAssetPath(g[0]);
-            var testDir = filePath.Substring(filePath.IndexOf('/') + 1).Replace($"{nameof(CRDTTests)}.cs", string.Empty);
-            var searchDir = $"{Application.dataPath}/{testDir}";
-            string[] filesPath = Directory.GetFiles(searchDir, "*.test");
+            string[] filesPath = CRDTTestsUtils.GetTestFilesAtThisDirectory();
 
             for (int i = 0; i < filesPath.Length; i++)
             {
-                ProcessFile(filesPath[i]);
+                ParsedCRDTTestFile parsedFile = CRDTTestsUtils.ParseTestFile(filesPath[i]);
+                AssertTestFile(parsedFile);
             }
         }
 
-        private void ProcessFile(string filePath)
+        private void AssertTestFile(ParsedCRDTTestFile parsedFile)
         {
             CRDTProtocol crdt = new CRDTProtocol();
-            string testSpecName = null;
-            bool nextLineIsState = false;
-            int lineNumber = 0;
-
-            void ResetCRDT()
+            for (int i = 0; i < parsedFile.fileInstructions.Count; i++)
             {
-                crdt = new CRDTProtocol();
-                testSpecName = null;
-                nextLineIsState = false;
-            }
-
-            foreach (string line in File.ReadLines(filePath))
-            {
-                lineNumber++;
-
-                if (line == "#")
-                    continue;
-
-                if (line.StartsWith("#"))
+                ParsedCRDTTestFile.TestFileInstruction instruction = parsedFile.fileInstructions[i];
+                if (instruction.instructionType == ParsedCRDTTestFile.InstructionType.MESSAGE)
                 {
-                    testSpecName ??= line;
+                    CRDTMessage msg = ParsedCRDTTestFile.InstructionToMessage(instruction);
+                    crdt.ProcessMessage(msg);
                 }
-
-                if (line == "# Final CRDT State")
+                else if (instruction.instructionType == ParsedCRDTTestFile.InstructionType.FINAL_STATE)
                 {
-                    nextLineIsState = true;
-                    continue;
-                }
+                    var finalState = ParsedCRDTTestFile.InstructionToFinalState(instruction);
 
-                if (line.StartsWith("{") && line.EndsWith("}"))
-                {
-                    if (nextLineIsState)
-                    {
-                        Dictionary<string, CRDTMessage> inputDictionary = null;
-                        try
-                        {
-                            inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, CRDTMessage>>(line);
-                        }
-                        catch (Exception e)
-                        {
-                            Debug.LogError($"Error parsing line for state (ln: {lineNumber}) {line} for file {filePath}, {e}");
-                        }
-
-                        if (inputDictionary != null)
-                        {
-                            Assert.IsTrue(AreStatesEqual(crdt, inputDictionary), $"Final state mismatch in line:{lineNumber} for file {filePath}");
-                        }
-                        ResetCRDT();
-                    }
-                    else
-                    {
-                        try
-                        {
-                            CRDTMessage msg = JsonConvert.DeserializeObject<CRDTMessage>(line);
-                            crdt.ProcessMessage(msg);
-                        }
-                        catch (Exception e)
-                        {
-                            Debug.LogError($"Error parsing line for msg (ln: {lineNumber}) {line} for file {filePath}, {e}");
-                        }
-                    }
+                    Assert.IsTrue(AreStatesEqual(crdt, finalState), $"Final state mismatch {instruction.testSpect} " +
+                                                                    $"in line:{instruction.lineNumber} for file {instruction.fileName}");
+                    crdt = new CRDTProtocol();
                 }
             }
         }
