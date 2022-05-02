a3bcfd2478bfef4b8c611eaaebb53dc714775961
nishant kaushik <98018257+dcl-glb@users.noreply.github.com>
2022-05-02 16:30:11 +0530

    chore: skybox camera is added to Cameracontroller prefab. * skybox camera removed from player controller * state of skybox camera is set with the Main camera

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Camera/CameraController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Camera/CameraController.cs
@@ -14,10 +14,13 @@ namespace DCL.Camera
         [SerializeField]
         internal new UnityEngine.Camera camera;
 
+        [SerializeField]
+        internal new UnityEngine.Camera skyboxCamera;
+
         private Transform cameraTransform;
 
         [SerializeField]
-        internal CinemachineFreeLook thirdPersonCamera; 
+        internal CinemachineFreeLook thirdPersonCamera;
 
         [Header("Virtual Cameras")]
         [SerializeField]
@@ -102,13 +105,10 @@ namespace DCL.Camera
             if (visibleState == prevVisibleState)
                 return;
 
-            camera.enabled = !visibleState && CommonScriptableObjects.rendererState.Get();
+            SetCameraEnabledState(!visibleState && CommonScriptableObjects.rendererState.Get());
         }
 
-        void OnOutputTextureChange(RenderTexture current, RenderTexture previous)
-        {
-            camera.targetTexture = current;
-        }
+        void OnOutputTextureChange(RenderTexture current, RenderTexture previous) { camera.targetTexture = current; }
 
         public bool TryGetCameraStateByType<T>(out CameraStateBase searchedCameraState)
         {
@@ -125,7 +125,7 @@ namespace DCL.Camera
             return false;
         }
 
-        private void OnRenderingStateChanged(bool enabled, bool prevState) { camera.enabled = enabled && !CommonScriptableObjects.isFullscreenHUDOpen; }
+        private void OnRenderingStateChanged(bool enabled, bool prevState) { SetCameraEnabledState(enabled && !CommonScriptableObjects.isFullscreenHUDOpen); }
 
         private void CameraBlocked_OnChange(bool current, bool previous)
         {
@@ -137,11 +137,13 @@ namespace DCL.Camera
 
         private void OnMouseWheelChangeValue(DCLAction_Measurable action, float value)
         {
-            if ((value > -mouseWheelThreshold && value < mouseWheelThreshold) || CommonScriptableObjects.cameraModeInputLocked.Get()) return;
-            if (Utils.IsPointerOverUIElement()) return;
+            if ((value > -mouseWheelThreshold && value < mouseWheelThreshold) || CommonScriptableObjects.cameraModeInputLocked.Get())
+                return;
+            if (Utils.IsPointerOverUIElement())
+                return;
 
             if (CommonScriptableObjects.cameraMode == CameraMode.ModeId.FirstPerson && value < -mouseWheelThreshold)
-                SetCameraMode(CameraMode.ModeId.ThirdPerson);   
+                SetCameraMode(CameraMode.ModeId.ThirdPerson);
 
             if (CommonScriptableObjects.cameraMode == CameraMode.ModeId.ThirdPerson && value > mouseWheelThreshold)
                 SetCameraMode(CameraMode.ModeId.FirstPerson);
@@ -225,10 +227,9 @@ namespace DCL.Camera
 
         public UnityEngine.Camera GetCamera() { return camera; }
 
-        private void SetInvertYAxis(bool current, bool previous)
-        {
-            thirdPersonCamera.m_YAxis.m_InvertInput = !current;
-        }
+        private void SetInvertYAxis(bool current, bool previous) { thirdPersonCamera.m_YAxis.m_InvertInput = !current; }
+
+        private void SetCameraEnabledState(bool enabled) { camera.enabled = skyboxCamera.enabled = enabled; }
 
         private void OnDestroy()
         {
