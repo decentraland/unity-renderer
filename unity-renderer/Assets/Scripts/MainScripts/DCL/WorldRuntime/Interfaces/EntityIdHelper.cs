5d7865f70fccc946028f01f6f116cc85e31af207
Adrian <54064092+Suduck@users.noreply.github.com>
2022-05-04 17:57:26 +0200

    fix: entity error (#2252)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/Interfaces/EntityIdHelper.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/Interfaces/EntityIdHelper.cs
@@ -51,25 +51,25 @@ namespace DCL
         {
             long entityIdLong = 0;
             var regex = new Regex("^*[A-Z]");
-            entityId = entityId.Substring(1);
+            string entityIdExtracted = entityId.Substring(1);
             
             // It is not base 36, so we assign a negative number for it
-            if (regex.IsMatch(entityId))
+            if (regex.IsMatch(entityIdExtracted))
             {
-                if (negativeValues.ContainsKey(entityId))
+                if (negativeValues.ContainsKey(entityIdExtracted))
                 {
-                    entityIdLong = negativeValues[entityId];
+                    entityIdLong = negativeValues[entityIdExtracted];
                 }
                 else
                 {
                     entityIdLong = negativeCounter;
-                    negativeValues.Add(entityId,negativeCounter);
+                    negativeValues.Add(entityIdExtracted,negativeCounter);
                     negativeCounter--;
                 }
             }
             else
             {
-                entityIdLong = DecodeBase36(entityId) << 9;
+                entityIdLong = DecodeBase36(entityIdExtracted) << 9;
             }
 
             if (!entityIdToLegacyId.ContainsKey(entityIdLong))
