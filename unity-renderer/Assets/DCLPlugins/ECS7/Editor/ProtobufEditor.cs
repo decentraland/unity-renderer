bd425e91a8bf2137029a8f04712322bd9607c7f9
Suduck <adrian@suduck.com>
2022-05-04 12:52:03 +0200

    feat: advance

--- unity-renderer/Assets/DCLPlugins/ECS7/Editor/ProtobufEditor.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/Editor/ProtobufEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,64 +11,51 @@ namespace DCL.Protobuf
 {
     public static class ProtobufEditor
     {
+        private const string PATH_TO_COMPONENTS = "/DCLPlugins/ECS7/ECSComponents/";
+        private const string PATH_TO_PROTO = "/DCLPlugins/ECS7/Editor/";
+        private const string PROTO_FILENAME = "protoc";
+
         [MenuItem("Decentraland/Protobuf/Regenerate models")]
         public static void UpdateModels()
         {
             Debug.Log("Starting update");
-            bool compile =CompileProtobufSystemPath("D:\\UnityProjects\\unity-renderer\\unity-renderer\\Assets\\DCLPlugins\\ECS7\\Editor\\BoxShape.proto");
-            Debug.Log("Models Updated " + compile);
+            string outputPath = Application.dataPath + PATH_TO_COMPONENTS + "BoxShape/Data";
+            outputPath = "D:/Borrar/bin/";
+            bool compile = CompileProtobufSystemPath(outputPath,"BoxShape");
+            Debug.Log("Models has been updated: " + compile);
         }
 
-        private static bool CompileProtobufSystemPath(string protoFileSystemPath)
+        private static bool CompileProtobufSystemPath(string outputPath , string protoFileName)
         {
-            //Do not compile changes coming from UPM package.
-            if (protoFileSystemPath.Contains("Packages/com.e7.protobuf-unity"))
-                return false;
+            string filePath = Application.dataPath + PATH_TO_PROTO;
+            string proto_path = filePath + PROTO_FILENAME;
+            string finalArguments = $"protoc --csharp_out=\"{outputPath}\" --proto_path=\"{filePath}\" {protoFileName}.proto";
+            UnityEngine.Debug.Log("Protobuf Unity : Final arguments :\n" + finalArguments);
 
-            if (Path.GetExtension(protoFileSystemPath) == ".proto")
+            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = proto_path, Arguments = finalArguments };
+
+            Process proc = new Process() { StartInfo = startInfo };
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+
+            string output = proc.StandardOutput.ReadToEnd();
+            string error = proc.StandardError.ReadToEnd();
+            proc.WaitForExit();
+
+            if (output != "")
             {
-                string outputPath = Path.GetDirectoryName(protoFileSystemPath);
-
-                string options = " --csharp_out \"{0}\" ";
-                // foreach (string s in includePaths)
-                // {
-                //     options += string.Format(" --proto_path \"{0}\" ", s);
-                // }
-                
-                //options += $" --grpc_out={outputPath} --plugin=protoc-gen-grpc={ProtoPrefs.grpcPath}";
-                //string combinedPath = string.Join(" ", optionFiles.Concat(new string[] { protoFileSystemPath }));
-
-                string finalArguments = string.Format("\"{0}\"", protoFileSystemPath) + string.Format(options, outputPath);
-
-
-                UnityEngine.Debug.Log("Protobuf Unity : Final arguments :\n" + finalArguments);
-
-
-                ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = Application.dataPath, Arguments = finalArguments };
-
-                Process proc = new Process() { StartInfo = startInfo };
-                proc.StartInfo.UseShellExecute = false;
-                proc.StartInfo.RedirectStandardOutput = true;
-                proc.StartInfo.RedirectStandardError = true;
-                proc.Start();
-
-                string output = proc.StandardOutput.ReadToEnd();
-                string error = proc.StandardError.ReadToEnd();
-                proc.WaitForExit();
-
-                if (output != "")
-                {
-                    UnityEngine.Debug.Log("Protobuf Unity : " + output);
-                }
-                UnityEngine.Debug.Log("Protobuf Unity : Compiled " + Path.GetFileName(protoFileSystemPath));
-
-                if (error != "")
-                {
-                    UnityEngine.Debug.LogError("Protobuf Unity : " + error);
-                }
-                return true;
+                UnityEngine.Debug.Log("Protobuf Unity : " + output);
             }
-            return false;
+            UnityEngine.Debug.Log("Protobuf Unity : Compiled " + protoFileName);
+
+            if (error != "")
+            {
+                UnityEngine.Debug.LogError("Protobuf Unity : " + error);
+                return false;
+            }
+            return true;
         }
     }
 }
