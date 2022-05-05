6db84b63749ffff326537b48a9117bbc3a0e1f49
dcl-glb <98018257+dcl-glb@users.noreply.github.com>
2022-05-05 15:35:02 +0530

    feat: skybox domes shader (#2226)
    * Improvements to blur shading and controllers.
    
    * Skybox: Added dome shader and script to set the scales of the domes automatically.
    
    * feat: new scripts for 3D elements
    
    * chore: Material data reference changed
    
    * chore: Editor tool changes for dome implementation WIP
    
    * chore: Dome instantiation WIP
    
    * chore: Dome instantiation and working done with editor tool.
    
    * Removed horizon plane from dome shader.
    
    * chore: dome integration at game time
    - Play mode dome integration
    - Play mode editor tool working
    
    * chore: config changed for testing
    
    * chore: config changed for testing
    
    * chore: second camera added
    
    * fix: second camera issue and Editor toolUndo option
    
    * chore: default skybox with dome
    
    * chore: texture layer copy functionality (WIP)
    
    * feat: copy/paste feature added
    
    * Removed wrong dome shader. Optimization and cleanup of the correct dome shader
    
    * chore: BG Layer added to the dome
    
    * feat: dome addition in between.
    * code refactoring (WIP)
    
    * Bug fixing for the blur controller script
    
    * refactor: code modularity
    
    * chore: editor tool changes for dome
    * single layer in domes
    * editor tool UI changes for same
    * Copy/paste functionality change for domes
    
    * fix: bug fixes after merge
    
    * chore 3D layers in new UI. WIP
    
    * Blur feature ready for implementation.
    
    * fix: bug fixes
    * null reference when selected layer or dome deleted
    * movement of layers or dome affects the render of right panel
    
    * feat: add/remove option inside context menu for both domes and layers
    
    * feat: copy/paste functionality
    
    * feat: copy/paste functionality for texture layer
    
    * feat: copy/paste of 3D domes
    
    * Reverted ForwardRenderer file to it's previous state
    
    * chore: secondary camera added and new layer related changes
    
    * refactor: deep copy code uses memberwise clone for value types
    
    * Revert "Merge pull request #8 from dcl-glb/feat/UI-Blur"
    
    This reverts commit bb6382838286d8129322048ec2deaf36242ebf66, reversing
    changes made to 4a0434b3c8891e6b0639134a4a4b44e902d51f99.
    
    Revert merge of UI-Blur
    
    * chore: reverted skybox configuration to default values
    
    * refactor: all debugs and commented code removed
    
    * chore: skybox camera is added to Cameracontroller prefab.
    * skybox camera removed from player controller
    * state of skybox camera is set with the Main camera
    
    * refactor: extra scripts removed
    
    * refactor: reduced code
    * constant declartion in seperate static class
    * changed naming as per convention of const variable
    
    * refactor: skybox camera dependency on prefab is removed.
    * new skybox camera script is made to initialize skybox cam
    
    * refactor: player and camera controller prefab reset to old state
    
    * test: test dome layer
    
    * chore: test configuration removed.
    * commented code removed
    * initial yPos of domes reduced
    
    * chore: Main camera converted to overlay and added in stack of skybox camera
    
    Co-authored-by: Juan Martin Salice <juan.salice@globant.com>
    Co-authored-by: Juan Martin Salice <dcl.glb.TA@gmail.com>
    Co-authored-by: nishant-kaush <nishant.kaushik@globant.com>
    Co-authored-by: Glob-TA <98959229+Glob-TA@users.noreply.github.com>

---
+++ unity-renderer/Assets/Rendering/ProceduralSkybox/ToolProceduralSkybox/Scripts/SkyboxElements.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DCL.Skybox
+{
+    public class DomeReferences
+    {
+        public GameObject domeGO;
+        public Material domeMat;
+        public bool domeInUse;
+    }
+
+    public class SkyboxElements
+    {
+        private const float DOME_DEFAULT_SIZE = 50;
+        private const string DOME_RESOURCES_PATH = "SkyboxPrefabs/Dome";
+
+        private GameObject skyboxElements;
+        private GameObject domeElements;
+        private GameObject domePrefab;
+
+        Queue<DomeReferences> domeObjects = new Queue<DomeReferences>();
+        Queue<DomeReferences> activeDomeObjects = new Queue<DomeReferences>();
+
+        public void Initialize3DObjects(SkyboxConfiguration configuration)
+        {
+            if (skyboxElements == null)
+            {
+                skyboxElements = GameObject.Find("Skybox Elements");
+
+                // If Skybox element doesn't exsist make new object else find dome objects 
+                if (skyboxElements == null)
+                {
+                    skyboxElements = new GameObject("Skybox Elements");
+                    skyboxElements.layer = LayerMask.NameToLayer("Skybox");
+                    domeElements = new GameObject("Dome Elements");
+                    domeElements.layer = LayerMask.NameToLayer("Skybox");
+                    domeElements.transform.parent = skyboxElements.transform;
+                }
+                else
+                {
+                    domeElements = skyboxElements.transform.Find("Dome Elements").gameObject;
+                    for (int i = 0; i < domeElements.transform.childCount; i++)
+                    {
+                        DomeReferences dome = new DomeReferences();
+                        dome.domeGO = domeElements.transform.GetChild(i).gameObject;
+                        dome.domeGO.GetComponent<Renderer>().material = Object.Instantiate(MaterialReferenceContainer.i.domeMat);
+                        dome.domeMat = dome.domeGO.GetComponent<Renderer>().sharedMaterial;
+                        dome.domeGO.SetActive(false);
+                        domeObjects.Enqueue(dome);
+                    }
+                }
+                skyboxElements.transform.position = Vector3.zero;
+            }
+        }
+
+        public void ResetObjects()
+        {
+            while (activeDomeObjects.Count > 0)
+            {
+                DomeReferences dome = activeDomeObjects.Dequeue();
+                dome.domeGO.SetActive(false);
+                domeObjects.Enqueue(dome);
+            }
+        }
+
+        #region 3D Domes
+
+        private DomeReferences GetDomeReference()
+        {
+            DomeReferences dome = null;
+            if (domeObjects.Count <= 0)
+            {
+                dome = InstantiateNewDome();
+            }
+            else
+            {
+                dome = domeObjects.Dequeue();
+            }
+
+            dome.domeGO.SetActive(true);
+
+            // Add to active objects
+            activeDomeObjects.Enqueue(dome);
+            return dome;
+        }
+
+        DomeReferences InstantiateNewDome()
+        {
+            if (domePrefab == null)
+            {
+                domePrefab = Resources.Load<GameObject>(DOME_RESOURCES_PATH);
+            }
+
+            GameObject obj = GameObject.Instantiate<GameObject>(domePrefab);
+            obj.layer = LayerMask.NameToLayer("Skybox");
+            obj.name = "Dome";
+            obj.transform.parent = domeElements.transform;
+            obj.transform.localPosition = new Vector3(0, -0.7f, 0);
+            obj.GetComponent<Renderer>().material = Object.Instantiate<Material>(MaterialReferenceContainer.i.domeMat);
+
+            DomeReferences dome = new DomeReferences();
+            dome.domeGO = obj;
+            dome.domeMat = obj.GetComponent<Renderer>().sharedMaterial;
+
+            return dome;
+        }
+
+        public List<DomeReferences> GetOrderedGameobjectList(List<Config3DDome> configList)
+        {
+            ResetObjects();
+            List<DomeReferences> orderedList = new List<DomeReferences>();
+            // make a list of domes for each active dome config
+
+            for (int i = 0; i < configList.Count; i++)
+            {
+                if (!configList[i].enabled)
+                {
+                    continue;
+                }
+
+                DomeReferences dome = GetDomeReference();
+
+                // resize
+                dome.domeGO.transform.localScale = DOME_DEFAULT_SIZE * Vector3.one + Vector3.one * i;
+                orderedList.Insert(0, dome);
+            }
+
+            return orderedList;
+        }
+
+        #endregion
+
+    }
+}
