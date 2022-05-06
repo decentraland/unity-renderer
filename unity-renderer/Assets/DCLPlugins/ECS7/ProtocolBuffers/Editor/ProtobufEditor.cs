586dfd20b71bf6ec3bd56017d4b3c8f8b2107f51
Leandro Mendoza <leandro@decentraland.org>
2022-05-06 16:55:45 -0300

    download @dcl/ecs package and copy component definitions

--- unity-renderer/Assets/DCLPlugins/ECS7/ProtocolBuffers/Editor/ProtobufEditor.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ProtocolBuffers/Editor/ProtobufEditor.cs
@@ -3,18 +3,97 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+
+using Newtonsoft.Json;
 
 namespace DCL.Protobuf
 {
     public static class ProtobufEditor
     {
+        private const string PATH_TO_COMPONENTS_DEFINITIONS = "/DCLPlugins/ECS7/ECSComponents/Definitions";
         private const string PATH_TO_COMPONENTS = "/DCLPlugins/ECS7/ECSComponents/";
         private const string PATH_TO_PROTO = "/DCLPlugins/ECS7/ProtocolBuffers/Editor/";
         private const string PATH_TO_FILES = "/DCLPlugins/ECS7/ProtocolBuffers";
         private const string PROTO_FILENAME = "protoc";
+        
+        [MenuItem("Decentraland/Protobuf/Download proto definitionsssss")]
+        public static void DownloadProtos()
+        {
+            WebClient client = new WebClient();
+            Stream data;
+            StreamReader reader;
+            string jsonString;
+            Dictionary<string, object> dictA, dictB;
+
+            // Download the data of @dcl/ecs
+            client = new WebClient();
+            data = client.OpenRead(@"https://registry.npmjs.org/@dcl/ecs");
+            reader = new StreamReader(data);
+            jsonString = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            
+            // Process the response
+			dictA = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+            dictB = JsonConvert.DeserializeObject<Dictionary<string, object>>(dictA["dist-tags"].ToString());
+            
+            string nextVersion = dictB["next"].ToString();
+            UnityEngine.Debug.Log("@dcl/ecs next version: " + nextVersion);
+            
+            // Download the "package.json" of @dcl/ecs@next
+            client = new WebClient();
+            data = client.OpenRead(@"https://registry.npmjs.org/@dcl/ecs/" + nextVersion);
+            reader = new StreamReader(data);
+            jsonString = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+
+            // Process the response
+            dictA = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+            dictB = JsonConvert.DeserializeObject<Dictionary<string, object>>(dictA["dist"].ToString());
+
+            string tgzUrl = dictB["tarball"].ToString();
+            UnityEngine.Debug.Log("@dcl/ecs@next url: " + tgzUrl);
+            
+            // Download package
+            client = new WebClient();
+            client.DownloadFile(tgzUrl, "dcl-ecs-next.tgz");
+            UnityEngine.Debug.Log("File downloaded dcl-ecs-next.tgz");
+
+            string destPackage = "dcl-ecs-" + nextVersion;
+            if (Directory.Exists(destPackage))
+            {
+                Directory.Delete(destPackage, true);
+            }
+            Directory.CreateDirectory(destPackage);
+            
+            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "tar", Arguments = "-xvzf dcl-ecs-next.tgz -C " + destPackage };
+            Process proc = new Process() { StartInfo = startInfo };
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+
+            string output = proc.StandardOutput.ReadToEnd();
+            string error = proc.StandardError.ReadToEnd();
+            proc.WaitForExit();
+            
+            UnityEngine.Debug.Log("Unzipped dcl-ecs-next.tgz");
+
+            string componentDefinitionPath = Application.dataPath + PATH_TO_COMPONENTS_DEFINITIONS;
+                
+            if (Directory.Exists(componentDefinitionPath))
+                Directory.Delete(componentDefinitionPath, true);
+            
+            Directory.Move(destPackage + "/package/dist/components/definitions", componentDefinitionPath);
+            Directory.Delete(destPackage, true);
+            if (File.Exists("dcl-ecs-next.tgz"))
+                File.Delete("dcl-ecs-next.tgz");
+        }
 
         [MenuItem("Decentraland/Protobuf/Regenerate models")]
         public static void UpdateModels()
