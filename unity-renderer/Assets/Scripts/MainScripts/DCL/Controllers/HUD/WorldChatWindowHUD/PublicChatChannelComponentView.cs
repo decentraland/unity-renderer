410c110f18ce9106cac990ce5b74417f457f161c
Santi Andrade <sandrade@decentraland.org>
2022-05-09 12:49:04 +0200

    Apply feedback: Retrieve channel name from model

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/WorldChatWindowHUD/PublicChatChannelComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/WorldChatWindowHUD/PublicChatChannelComponentView.cs
@@ -19,7 +19,7 @@ public class PublicChatChannelComponentView : BaseComponentView, IChannelChatWin
     public bool IsActive => gameObject.activeInHierarchy;
     public IChatHUDComponentView ChatHUD => chatView;
     public RectTransform Transform => (RectTransform) transform;
-    public string Channel => nameLabel.text;
+    public string Channel => model.name;
 
     public static PublicChatChannelComponentView Create()
     {
