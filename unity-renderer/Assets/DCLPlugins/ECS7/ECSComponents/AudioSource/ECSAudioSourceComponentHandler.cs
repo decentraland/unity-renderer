40e171a508cddd1676b5bba2a4a54a6b2878e5f8
Suduck <adrian@suduck.com>
2022-05-16 09:27:31 +0200

    feat: audiosource

---
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/AudioSource/ECSAudioSourceComponentHandler.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DCL.Controllers;
+using DCL.ECSRuntime;
+using DCL.Helpers;
+using DCL.Models;
+using DCL.SettingsCommon;
+using UnityEngine;
+using AudioSettings = DCL.SettingsCommon.AudioSettings;
+
+namespace DCL.ECSComponents
+{
+    public class ECSAudioSourceComponentHandler : IECSComponentHandler<ECSAudioSource>, IOutOfSceneBoundariesHandler
+    {
+        public float playTime => audioSource.time;
+        internal AudioSource audioSource;
+        internal AssetPromise_AudioClip promiseAudioClip;
+        
+        private bool isDestroyed = false;
+        public long playedAtTimestamp = 0;
+        
+        // TODO: We should figure out how to change this value
+        private bool isOutOfBoundaries = false;
+        
+        private ECSAudioSource model;
+        private IParcelScene scene;
+        private AudioClip audioClip;
+
+        public void OnComponentCreated(IParcelScene scene, IDCLEntity entity)
+        {
+           
+            audioSource = entity.gameObject.AddComponent<AudioSource>();
+
+            if (Settings.i != null)
+                Settings.i.audioSettings.OnChanged += OnAudioSettingsChanged;
+    
+            DataStore.i.virtualAudioMixer.sceneSFXVolume.OnChange += OnVirtualAudioMixerChangedValue;
+        }
+
+        public void OnComponentRemoved(IParcelScene scene, IDCLEntity entity) { Dispose(); }
+
+        private void Dispose()
+        {
+            if (isDestroyed)
+                return;
+            isDestroyed = true;
+            
+            AssetPromiseKeeper_AudioClip.i.Forget(promiseAudioClip);
+            
+            CommonScriptableObjects.sceneID.OnChange -= OnCurrentSceneChanged;
+
+            if (Settings.i != null)
+                Settings.i.audioSettings.OnChanged -= OnAudioSettingsChanged;
+            
+            DataStore.i.virtualAudioMixer.sceneSFXVolume.OnChange -= OnVirtualAudioMixerChangedValue;
+            GameObject.Destroy(audioSource);
+        }
+        
+        public void OnComponentModelUpdated(IParcelScene scene, IDCLEntity entity, ECSAudioSource model)
+        {
+            this.scene = scene;
+            this.model = model;
+            
+            ApplyCurrentModel();
+            
+            promiseAudioClip = new AssetPromise_AudioClip(model.audioClipUrl, scene.contentProvider);
+            promiseAudioClip.OnSuccessEvent += OnComplete;
+            promiseAudioClip.OnFailEvent += OnFail;
+
+            AssetPromiseKeeper_AudioClip.i.Keep(promiseAudioClip);
+            
+            CommonScriptableObjects.sceneID.OnChange -= OnCurrentSceneChanged;
+            CommonScriptableObjects.sceneID.OnChange += OnCurrentSceneChanged;
+        }
+        
+        private void ApplyCurrentModel()
+        {
+            if (audioSource == null)
+            {
+                Debug.LogWarning("AudioSource is null!.");
+                return;
+            }
+            
+            UpdateAudioSourceVolume();
+            audioSource.loop = model.loop;
+            audioSource.pitch = model.pitch;
+            audioSource.spatialBlend = 1;
+            audioSource.dopplerLevel = 0.1f;
+
+            if (!model.playing)
+            {
+                if (audioSource.isPlaying)
+                {
+                    audioSource.Stop();
+                }
+            }
+        }
+        
+        private void ApplyLoadedAudioClip(AudioClip clip)
+        {
+            if (audioSource.clip != clip)
+                audioSource.clip = clip;
+            
+            bool shouldPlay = playedAtTimestamp != model.playedAtTimestamp ||
+                              (model.playing && !audioSource.isPlaying);
+
+            if (audioSource.enabled && model.playing && shouldPlay)
+            {
+                //To remove a pesky and quite unlikely warning when the audiosource is out of scenebounds
+                audioSource.Play();
+            }
+
+            playedAtTimestamp = model.playedAtTimestamp;
+        }
+        
+        public void UpdateOutOfBoundariesState(bool isInsideBoundaries)
+        {
+            if (scene.isPersistent)
+                isInsideBoundaries = true;
+
+            isOutOfBoundaries = !isInsideBoundaries;
+            UpdateAudioSourceVolume();
+        }
+
+        private void OnComplete(Asset_AudioClip assetAudioClip)
+        {
+            if (assetAudioClip.audioClip == null)
+                return;
+
+            if (audioClip != null)
+                DataStore.i.sceneWorldObjects.RemoveAudioClip(scene.sceneData.id, audioClip);
+
+            audioClip = assetAudioClip.audioClip;
+
+            DataStore.i.sceneWorldObjects.AddAudioClip(scene.sceneData.id, audioClip);
+            
+            ApplyLoadedAudioClip(assetAudioClip.audioClip);
+        }
+
+        private void OnFail(Asset_AudioClip assetAudioClip, Exception exception)
+        {
+            Debug.LogError("Audio clip couldn't be loaded. Url: " +model.audioClipUrl + "     error: " + exception.Message);
+            AssetPromiseKeeper_AudioClip.i.Forget(promiseAudioClip);
+        }
+
+        private void OnAudioSettingsChanged(AudioSettings settings)
+        {
+            UpdateAudioSourceVolume();
+        }
+
+        private void OnVirtualAudioMixerChangedValue(float currentValue, float previousValue)
+        {
+            UpdateAudioSourceVolume();
+        }
+
+        private void UpdateAudioSourceVolume()
+        {
+            float newVolume = 0;
+            
+            // isOutOfBoundaries will always be false for global scenes.
+            if (!isOutOfBoundaries)
+            {
+                AudioSettings audioSettingsData =
+                    Settings.i != null ? Settings.i.audioSettings.Data : new AudioSettings();
+                newVolume = model.volume * Utils.ToVolumeCurve(
+                    DataStore.i.virtualAudioMixer.sceneSFXVolume.Get() * audioSettingsData.sceneSFXVolume *
+                    audioSettingsData.masterVolume);
+            }
+
+            bool isCurrentScene = scene.isPersistent || scene.sceneData.id == CommonScriptableObjects.sceneID.Get();
+
+            audioSource.volume = isCurrentScene ? newVolume : 0f;
+        }
+        
+        private void OnCurrentSceneChanged(string currentSceneId, string previousSceneId)
+        {
+            if (audioSource == null)
+                return;
+            
+            float volume = 0;
+
+            if (scene.isPersistent || scene.sceneData.id == currentSceneId)
+            {
+                volume = model.volume;
+            }
+
+            audioSource.volume = volume;
+        }
+    }
+}
---
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/AudioSource/Tests/ECSAudioSourceShould.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DCL.Controllers;
+using DCL.ECSComponents;
+using DCL.ECSRuntime;
+using DCL.Helpers;
+using DCL.Models;
+using NSubstitute;
+using NSubstitute.Extensions;
+using NUnit.Framework;
+using Tests;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace DCL.ECSComponents.Test
+{
+    public class ECSAudioSourceShould : IntegrationTestSuite
+    {
+        private IDCLEntity entity;
+        private IParcelScene scene;
+        private ECSAudioSourceComponentHandler audioSourceComponentHandler;
+        private GameObject gameObject;
+
+        protected override void InitializeServices(ServiceLocator serviceLocator)
+        {
+            base.InitializeServices(serviceLocator);
+            serviceLocator.Register<IWebRequestController>(WebRequestController.Create);
+        }
+
+        [UnitySetUp]
+        protected override IEnumerator SetUp()
+        {
+            yield return base.SetUp();
+            gameObject = new GameObject();
+            entity = Substitute.For<IDCLEntity>();
+            scene = Substitute.For<IParcelScene>();
+            audioSourceComponentHandler = new ECSAudioSourceComponentHandler();
+
+            entity.entityId.Returns(1);
+            entity.gameObject.Returns(gameObject);
+            LoadParcelScenesMessage.UnityParcelScene sceneData = new LoadParcelScenesMessage.UnityParcelScene();
+            sceneData.id = "1";
+
+            ContentProvider_Dummy providerDummy = new ContentProvider_Dummy();
+
+            scene.sceneData.Configure().Returns(sceneData);
+            scene.Configure().contentProvider.Returns(providerDummy);
+
+            audioSourceComponentHandler.OnComponentCreated(scene, entity);
+        }
+
+        [UnityTearDown]
+        protected override IEnumerator TearDown()
+        {
+            yield return base.TearDown();
+            audioSourceComponentHandler.OnComponentRemoved(scene, entity);
+            GameObject.Destroy(gameObject);
+        }
+        
+        [UnityTest]
+        public IEnumerator UpdatePlayingModelComponentCorrectly()
+        {
+            // Arrange
+            ECSAudioSource model = new ECSAudioSource();
+            model.playing = true;
+            model.loop = false;
+            model.audioClipUrl = TestAssetsUtils.GetPath() + "/Audio/short_effect.ogg";
+
+            // Act
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model);
+
+            yield return new WaitUntil( () => audioSourceComponentHandler.promiseAudioClip.state == AssetPromiseState.FINISHED);
+
+            // Assert
+            Assert.AreEqual(audioSourceComponentHandler.audioSource.isPlaying, true);
+        }
+
+        [Test]
+        public void UpdateLoopModelComponentCorrectly()
+        {
+            // Arrange
+            ECSAudioSource model = new ECSAudioSource();
+            model.audioClipUrl = TestAssetsUtils.GetPath() + "/Audio/short_effect.ogg";
+            model.loop = false;
+            
+            ECSAudioSource model2 = new ECSAudioSource();
+            model2.audioClipUrl = TestAssetsUtils.GetPath() + "/Audio/short_effect.ogg";
+            model2.loop = true;
+            
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model);
+            
+            // Act
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model2);
+
+            // Assert
+            Assert.AreEqual(audioSourceComponentHandler.audioSource.loop, model2.loop);
+        }
+        
+        [Test]
+        public void UpdatePitchModelComponentCorrectly()
+        {
+            // Arrange
+            ECSAudioSource model = new ECSAudioSource();
+            model.pitch = 0f;
+            
+            ECSAudioSource model2 = new ECSAudioSource();
+            model2.pitch = 1f;
+            
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model);
+            
+            // Act
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model2);
+
+            // Assert
+            Assert.AreEqual(audioSourceComponentHandler.audioSource.pitch, model2.pitch);
+        }
+        
+        [Test]
+        public void UpdateVolumeModelComponentCorrectly()
+        {
+            // Arrange
+            ECSAudioSource model = new ECSAudioSource();
+            model.volume = 0f;
+            
+            ECSAudioSource model2 = new ECSAudioSource();
+            model2.volume = 1f;
+            
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model);
+            
+            // Act
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model2);
+
+            // Assert
+            Assert.AreEqual(audioSourceComponentHandler.audioSource.volume, model2.volume);
+        }
+
+        [Test]
+        public void DisposeComponentCorrectly()
+        {
+            // Arrange
+            ECSAudioSource model = new ECSAudioSource();
+            audioSourceComponentHandler.OnComponentModelUpdated(scene, entity, model);
+
+            // Act
+            audioSourceComponentHandler.OnComponentRemoved(scene, entity);
+
+            // Assert
+            Assert.IsNull(audioSourceComponentHandler.audioSource);
+            Assert.IsTrue(audioSourceComponentHandler.promiseAudioClip.isForgotten);
+        }
+    }
+}
