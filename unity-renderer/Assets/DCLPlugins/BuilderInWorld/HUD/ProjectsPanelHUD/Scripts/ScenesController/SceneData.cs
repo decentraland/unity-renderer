79709cd3c3bc2358cee843c8fc3fe4c7de4115fb
Suduck <adrian@suduck.com>
2022-05-16 10:59:48 +0200

    fix: fix biw publish

--- unity-renderer/Assets/DCLPlugins/BuilderInWorld/HUD/ProjectsPanelHUD/Scripts/ScenesController/SceneData.cs
+++ unity-renderer/Assets/DCLPlugins/BuilderInWorld/HUD/ProjectsPanelHUD/Scripts/ScenesController/SceneData.cs
@@ -102,8 +102,8 @@ namespace DCL.Builder
             isContributor = false;
             isDeployed = true;
             authorName = scene.author;
-            requiredPermissions = scene.requiredPermissions;
-            bannedUsers = scene.bannedUsers;
+            // requiredPermissions = scene.requiredPermissions;
+            // bannedUsers = scene.bannedUsers;
             isEditable = scene.source != Scene.Source.SDK;
             parcels = scene.parcels;
             projectId = scene.projectId;
@@ -111,11 +111,11 @@ namespace DCL.Builder
             source = scene.source;
 
             isMatureContent = false;
-            if (!string.IsNullOrEmpty(scene.contentRating))
-            {
-                isMatureContent = scene.contentRating.Equals(CONTENT_MATURE_SYMBOL, StringComparison.OrdinalIgnoreCase)
-                                  || scene.contentRating.Equals(CONTENT_ADULTS_ONLY_SYMBOL, StringComparison.OrdinalIgnoreCase);
-            }
+            // if (!string.IsNullOrEmpty(scene.contentRating))
+            // {
+            //     isMatureContent = scene.contentRating.Equals(CONTENT_MATURE_SYMBOL, StringComparison.OrdinalIgnoreCase)
+            //                       || scene.contentRating.Equals(CONTENT_ADULTS_ONLY_SYMBOL, StringComparison.OrdinalIgnoreCase);
+            // }
 
             if (scene.parcels.Length < 2)
             {
