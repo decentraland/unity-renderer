2bd6c2a83e42ed111507009034fbcb99238c9488
Juan M. Salice <104371686+JuanSalice@users.noreply.github.com>
2022-05-17 18:04:14 -0300

    Optimization of the ghost shader. Improvements to the reveal script. Removed unnecessary files.

--- unity-renderer/Assets/Rendering/LoadingAvatar/CrossSection/AvatarReveal.cs
+++ unity-renderer/Assets/Rendering/LoadingAvatar/CrossSection/AvatarReveal.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class AvatarReveal : MonoBehaviour
 {
     public bool avatarLoaded;
@@ -12,30 +11,64 @@ public class AvatarReveal : MonoBehaviour
 
     public float revealSpeed;
 
+    public float fadeInSpeed;
+    public Renderer ghostDummy;
+    Material _ghostMaterial;
+    public List<Renderer> targets = new List<Renderer>();
+    List<Material> materials = new List<Material>();
+
+    private void Start()
+    {
+        _ghostMaterial = ghostDummy.material;
+
+        foreach (Renderer r in targets)
+        {
+            materials.Add(r.material);
+        }
+    }
     private void Update()
     {
+        UpdateMaterials();
+
         if(avatarLoaded)
         {
-            if(revealer.transform.position.y < finalPosition.position.y)
-            {
-                RevealAvatar();
-            }
-            else if(particles.isPlaying)
-            {
-                particles.Stop();
-                revealParticles.Stop();
-            }
+            RevealAvatar();
         }
     }
 
     void RevealAvatar()
     {
-        revealer.transform.position += revealer.transform.up * revealSpeed * Time.deltaTime;
-        
-        if(!particles.isPlaying)
+        if (revealer.transform.position.y < finalPosition.position.y)
         {
-            particles.Play();
-            revealParticles.Play();
+            revealer.transform.position += revealer.transform.up * revealSpeed * Time.deltaTime;
+
+            if (!particles.isPlaying)
+            {
+                particles.Play();
+                revealParticles.Play();
+            }
+        }
+        else if (particles.isPlaying)
+        {
+            particles.Stop();
+            revealParticles.Stop();
+        }
+    }
+
+    void UpdateMaterials()
+    {
+        if(_ghostMaterial.GetColor("_Color").a < 0.9f)
+        {
+            Color gColor = _ghostMaterial.GetColor("_Color");
+            Color tempColor = new Color(gColor.r, gColor.g, gColor.b, gColor.a + Time.deltaTime * fadeInSpeed);
+            _ghostMaterial.SetColor("_Color", tempColor);
+        }        
+
+        _ghostMaterial.SetVector("_RevealPosition", revealer.transform.position);
+
+        foreach (Material m in materials)
+        {
+            m.SetVector("_RevealPosition", revealer.transform.position);
         }
     }
 }
