bfd54947c525a31c50cc0823f54df6f995f61436
Pato <pato@decentraland.org>
2022-05-18 12:44:39 -0300

    add headers length constants

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/BinaryMessage/KernelBinaryMessageSerializer.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/BinaryMessage/KernelBinaryMessageSerializer.cs
@@ -7,7 +7,7 @@ namespace KernelCommunication
         public static void Serialize(BinaryWriter binaryWriter, CRDTMessage message)
         {
             //sizeof(messageHeader) + sizeof(componentHeader) + dataLength
-            int messageLength = 32 + (((byte[])message.data)?.Length ?? 0);
+            int messageLength = BinaryMessageConstants.CRDT_MESSAGE_BASE_HEADER_LENGTH + (((byte[])message.data)?.Length ?? 0);
             var type = message.data != null ? KernelBinaryMessageType.PUT_COMPONENT : KernelBinaryMessageType.DELETE_COMPONENT;
 
             binaryWriter.Write(messageLength);
