41800b9deeda22da8f7125b14d87eb61ec676a4f
Pato <pato@decentraland.org>
2022-05-18 15:36:14 -0300

    Squashed branch `feat/serialize-crdtmessages`

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/BinaryMessage/KernelBinaryMessageSerializer.cs
@@ -0,0 +1,19 @@
+using DCL.CRDT;
+
+namespace KernelCommunication
+{
+    public static class KernelBinaryMessageSerializer
+    {
+        public static void Serialize(BinaryWriter binaryWriter, CRDTMessage message)
+        {
+            //sizeof(messageHeader) + sizeof(componentHeader) + dataLength
+            int messageLength = BinaryMessageConstants.CRDT_MESSAGE_BASE_HEADER_LENGTH + (((byte[])message.data)?.Length ?? 0);
+            var type = message.data != null ? KernelBinaryMessageType.PUT_COMPONENT : KernelBinaryMessageType.DELETE_COMPONENT;
+
+            binaryWriter.Write(messageLength);
+            binaryWriter.Write((int)type);
+
+            CRDTSerializer.Serialize(binaryWriter, message);
+        }
+    }
+}
