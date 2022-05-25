0b97a4d15475eb214b2495c0ac67f730294d6c8f
Pato <pato@decentraland.org>
2022-05-25 19:37:26 -0300

    add methods

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/ECSRuntime/ComponentWriter/Interfaces/IECSComponentWriter.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/ECSRuntime/ComponentWriter/Interfaces/IECSComponentWriter.cs
@@ -7,7 +7,10 @@ namespace DCL.ECSRuntime
     public interface IECSComponentWriter : IDisposable
     {
         void AddOrReplaceComponentSerializer<T>(int componentId, Func<T, byte[]> serializer);
+        void RemoveComponentSerializer(int componentId);
         void PutComponent<T>(IParcelScene scene, IDCLEntity entity, int componentId, T model);
+        void PutComponent<T>(string sceneId, long entityId, int componentId, T model);
         void RemoveComponent(IParcelScene scene, IDCLEntity entity, int componentId);
+        void RemoveComponent(string sceneId, long entityId, int componentId);
     }
 }
