36a51e00fdc506e1ee1cfab7a961deeb64c1cc34
Suduck <adrian@suduck.com>
2022-06-06 10:09:36 +0200

    feat: merge master

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/ECS7ComponentsPlugin.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/ECS7ComponentsPlugin.cs
@@ -16,31 +16,32 @@ public class ECS7ComponentsPlugin : IDisposable
     {
         this.componentsFactory = componentsFactory;
         transformComponent = new ECSTransformComponent(ComponentID.TRANSFORM, componentsFactory, componentsWriter);
-        
+
         // Box Shape
         componentsFactory.AddOrReplaceComponent(ComponentID.BOX_SHAPE,
             data => PBBoxShape.Parser.ParseFrom((byte[])data),
             () =>  new ECSBoxShapeComponentHandler(DataStore.i.ecs7));
-        
+
         // Plane Shape
         componentsFactory.AddOrReplaceComponent(ComponentID.PLANE_SHAPE,
             data => PBPlaneShape.Parser.ParseFrom((byte[])data),
             () =>  new ECSPlaneShapeComponentHandler());
-        
+
         // Sphere Shape
         componentsFactory.AddOrReplaceComponent(ComponentID.SPHERE_SHAPE,
             data => PBSphereShape.Parser.ParseFrom((byte[])data),
             () =>  new ECSSphereShapeComponentHandler());
-                    // Cylinder Shape
-                    componentsFactory.AddOrReplaceComponent(ComponentID.CYLINDER_SHAPE,
-                        data => PBCylinderShape.Parser.ParseFrom((byte[])data),
-                        () =>  new ECSCylinderShapeComponentHandler());
         
+        // Cylinder Shape
+        componentsFactory.AddOrReplaceComponent(ComponentID.CYLINDER_SHAPE,
+            data => PBCylinderShape.Parser.ParseFrom((byte[])data),
+            () =>  new ECSCylinderShapeComponentHandler());
+
         // Audio Source
         componentsFactory.AddOrReplaceComponent(ComponentID.AUDIO_SOURCE_SHAPE,
             data => PBAudioSource.Parser.ParseFrom((byte[])data),
             () =>  new ECSAudioSourceComponentHandler(DataStore.i, Settings.i, AssetPromiseKeeper_AudioClip.i, CommonScriptableObjects.sceneID));
-        
+
         // Audio Stream
         componentsFactory.AddOrReplaceComponent(ComponentID.AUDIO_STREAM_SHAPE,
             data => PBAudioStream.Parser.ParseFrom((byte[])data),
@@ -51,10 +52,10 @@ public class ECS7ComponentsPlugin : IDisposable
     {
         transformComponent.Dispose();
         componentsFactory.RemoveComponent(ComponentID.BOX_SHAPE);
-        componentsFactory.RemoveComponent(ComponentID.CYLINDER_SHAPE);
         componentsFactory.RemoveComponent(ComponentID.BOX_SHAPE);
         componentsFactory.RemoveComponent(ComponentID.PLANE_SHAPE);
         componentsFactory.RemoveComponent(ComponentID.SPHERE_SHAPE);
+        componentsFactory.RemoveComponent(ComponentID.CYLINDER_SHAPE);
         componentsFactory.RemoveComponent(ComponentID.AUDIO_SOURCE_SHAPE);
         componentsFactory.RemoveComponent(ComponentID.AUDIO_STREAM_SHAPE);
     }
