70208539560b8818faefac9d090285eebd08f1cf
Adrian <54064092+Suduck@users.noreply.github.com>
2022-06-06 10:31:36 +0200

    feat: add cylinder shape to the new ECSComponent (#2377)
    * feat: add cylinder shape
    
    * refactor: better approach
    
    * fix: unit testing
    
    * Revert "refactor: better approach"
    
    This reverts commit bfa97123dd155d8b029c8335c516106e87db363d.
    
    * Revert "fix: unit testing"
    
    This reverts commit 6f2bc426d0c419d1f6d52afced4e26a450d8b20c.
    
    * fix: correct comparation
    
    * style: better style

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/ComponentID.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/ComponentID.cs
@@ -6,6 +6,7 @@ namespace DCL.ECS7
         public const int BOX_SHAPE = 1013;
         public const int PLANE_SHAPE = 1014;
         public const int SPHERE_SHAPE = 1015;
+        public const int CYLINDER_SHAPE = 1016;
         public const int AUDIO_SOURCE_SHAPE = 1020;
         public const int AUDIO_STREAM_SHAPE = 1021;
     }
