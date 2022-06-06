7d89b9b218ad4e5f63bd3225904cc725a04d699f
Leandro Mendoza <leandro@decentraland.org>
2022-06-06 18:37:38 -0300

    fix: update the source of the components protocol-buffer definitions which is `decentraland-ecs`

--- unity-renderer/Assets/DCLPlugins/ECS7/ProtocolBuffers/Editor/ProtobufEditor.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ProtocolBuffers/Editor/ProtobufEditor.cs
@@ -36,9 +36,9 @@ namespace DCL.Protobuf
             string libraryJsonString;
             Dictionary<string, object> libraryContent, libraryInfo;
 
-            // Download the data of @dcl/ecs
+            // Download the data of decentraland-ecs
             client = new WebClient();
-            data = client.OpenRead(@"https://registry.npmjs.org/@dcl/ecs");
+            data = client.OpenRead(@"https://registry.npmjs.org/decentraland-ecs");
             reader = new StreamReader(data);
             libraryJsonString = reader.ReadToEnd();
             data.Close();
@@ -49,11 +49,11 @@ namespace DCL.Protobuf
             libraryInfo = JsonConvert.DeserializeObject<Dictionary<string, object>>(libraryContent["dist-tags"].ToString());
             
             string nextVersion = libraryInfo["next"].ToString();
-            UnityEngine.Debug.Log("@dcl/ecs next version: " + nextVersion);
+            UnityEngine.Debug.Log("decentraland-ecs next version: " + nextVersion);
             
-            // Download the "package.json" of @dcl/ecs@next
+            // Download the "package.json" of decentraland-ecs
             client = new WebClient();
-            data = client.OpenRead(@"https://registry.npmjs.org/@dcl/ecs/" + nextVersion);
+            data = client.OpenRead(@"https://registry.npmjs.org/decentraland-ecs/" + nextVersion);
             reader = new StreamReader(data);
             libraryJsonString = reader.ReadToEnd();
             data.Close();
@@ -64,14 +64,14 @@ namespace DCL.Protobuf
             libraryInfo = JsonConvert.DeserializeObject<Dictionary<string, object>>(libraryContent["dist"].ToString());
 
             string tgzUrl = libraryInfo["tarball"].ToString();
-            UnityEngine.Debug.Log("@dcl/ecs@next url: " + tgzUrl);
+            UnityEngine.Debug.Log("decentraland-ecs@next url: " + tgzUrl);
             
             // Download package
             client = new WebClient();
-            client.DownloadFile(tgzUrl, "dcl-ecs-next.tgz");
-            UnityEngine.Debug.Log("File downloaded dcl-ecs-next.tgz");
+            client.DownloadFile(tgzUrl, "decentraland-ecs-next.tgz");
+            UnityEngine.Debug.Log("File downloaded decentraland-ecs-next.tgz");
 
-            string destPackage = "dcl-ecs-" + nextVersion;
+            string destPackage = "decentraland-ecs-" + nextVersion;
             if (Directory.Exists(destPackage))
                 Directory.Delete(destPackage, true);
             
@@ -80,13 +80,13 @@ namespace DCL.Protobuf
                 Directory.CreateDirectory(destPackage);
 
                 // We unzip the library
-                ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "tar", Arguments = "-xvzf dcl-ecs-next.tgz -C " + destPackage, CreateNoWindow = true};
+                ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "tar", Arguments = "-xvzf decentraland-ecs-next.tgz -C " + destPackage, CreateNoWindow = true};
                 Process proc = new Process() { StartInfo = startInfo };
                 proc.Start();
 
                 proc.WaitForExit(5 * 1000);
 
-                UnityEngine.Debug.Log("Unzipped dcl-ecs-next.tgz");
+                UnityEngine.Debug.Log("Unzipped decentraland-ecs-next.tgz");
 
                 string componentDefinitionPath = Application.dataPath + PATH_TO_COMPONENTS_DEFINITIONS;
 
@@ -94,7 +94,7 @@ namespace DCL.Protobuf
                     Directory.Delete(componentDefinitionPath, true);
 
                 // We move the definitions to their correct path
-                Directory.Move(destPackage + "/package/dist/components/definitions", componentDefinitionPath);
+                Directory.Move(destPackage + "/package/dist/ecs7/proto-definitions", componentDefinitionPath);
                 UnityEngine.Debug.Log("Success copying definitions in " + componentDefinitionPath);
             }
             catch (Exception e)
@@ -104,8 +104,8 @@ namespace DCL.Protobuf
             finally // We delete the downloaded package
             {
                 Directory.Delete(destPackage, true);
-                if (File.Exists("dcl-ecs-next.tgz"))
-                    File.Delete("dcl-ecs-next.tgz");
+                if (File.Exists("decentraland-ecs-next.tgz"))
+                    File.Delete("decentraland-ecs-next.tgz");
             }
         }
 
