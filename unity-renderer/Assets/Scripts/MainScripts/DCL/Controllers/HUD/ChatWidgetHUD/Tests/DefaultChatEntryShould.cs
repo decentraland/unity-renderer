217ba330d20ac69b62fa9f927a738d2e558e4cf8
Nicolas Lorusso <nalorusso@gmail.com>
2022-06-09 18:50:04 -0300

    added test coverage for DefaultChatEntry & DateSeparatorEntryShould

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ChatWidgetHUD/Tests/DefaultChatEntryShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ChatWidgetHUD/Tests/DefaultChatEntryShould.cs
@@ -155,6 +155,85 @@ public class DefaultChatEntryShould
         Assert.AreEqual("test message", entry.body.text);
     }
 
+    [TestCase("im at 100,100", "100,100", "im at ")]
+    [TestCase("nah we should go to 0,-122", "0,-122", "nah we should go to ")]
+    public void AddParcelCoordinates(string body, string coordinates, string bodyWithoutCoordinates)
+    {
+        GivenEntryChat("PublicChatEntryReceivedWhisper");
+        
+        var message = new ChatEntryModel
+        {
+            messageType = ChatMessage.Type.PRIVATE,
+            senderName = "user-test",
+            recipientName = "receiver-test",
+            bodyText = body,
+            subType = ChatEntryModel.SubType.SENT
+        };
+        
+        entry.Populate(message);
+        
+        Assert.AreEqual($"<b>To receiver-test:</b> {bodyWithoutCoordinates}</noparse><link={coordinates}><color=#4886E3><u>{coordinates}</u></color></link><noparse>", entry.body.text);
+    }
+
+    [Test]
+    public void AddManyParcelCoordinates()
+    {
+        GivenEntryChat("PublicChatEntryReceivedWhisper");
+        
+        var message = new ChatEntryModel
+        {
+            messageType = ChatMessage.Type.PRIVATE,
+            senderName = "user-test",
+            recipientName = "receiver-test",
+            bodyText = "perhaps 73,94 then -5,42 and after -36,72",
+            subType = ChatEntryModel.SubType.SENT
+        };
+        
+        entry.Populate(message);
+        
+        Assert.AreEqual("<b>To receiver-test:</b> perhaps </noparse><link=73,94><color=#4886E3><u>73,94</u></color></link><noparse> then </noparse><link=-5,42><color=#4886E3><u>-5,42</u></color></link><noparse> and after </noparse><link=-36,72><color=#4886E3><u>-36,72</u></color></link><noparse>", entry.body.text);
+    }
+
+    [Test]
+    public void DoNotShowUserName()
+    {
+        GivenEntryChat("PrivateChatEntryReceived");
+        entry.showUserName = false;
+        
+        var message = new ChatEntryModel
+        {
+            messageType = ChatMessage.Type.PRIVATE,
+            senderName = "user-test",
+            recipientName = "receiver-test",
+            bodyText = "test message",
+            subType = ChatEntryModel.SubType.RECEIVED
+        };
+        
+        entry.Populate(message);
+        
+        Assert.AreEqual("test message", entry.body.text);
+    }
+    
+    [Test]
+    public void ShowUserName()
+    {
+        GivenEntryChat("PrivateChatEntryReceived");
+        entry.showUserName = true;
+        
+        var message = new ChatEntryModel
+        {
+            messageType = ChatMessage.Type.PRIVATE,
+            senderName = "user-test",
+            recipientName = "receiver-test",
+            bodyText = "test message",
+            subType = ChatEntryModel.SubType.SENT
+        };
+        
+        entry.Populate(message);
+        
+        Assert.AreEqual("<b>To receiver-test:</b> test message", entry.body.text);
+    }
+
     private void GivenEntryChat(string prefabName)
     {
         entry = Object.Instantiate(Resources.Load<DefaultChatEntry>($"SocialBarV1/{prefabName}"),
