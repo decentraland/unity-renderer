400c4b5d9de6c240b644c36ce613b611b0c7379f
Kinerius <kinerius@gmail.com>
2022-06-29 13:29:31 -0300

    Added shadervariants to inlcude GLTFast default shadergraphs

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/GLTFast/AssetPromise_GLTFast.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/GLTFast/AssetPromise_GLTFast.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using GLTFast;
+using GLTFast.Logging;
+using GLTFast.Materials;
 using UnityEngine;
 
 // Disable async call not being awaited warning
@@ -101,6 +103,29 @@ namespace DCL
             }
         }
         
+    }
+
+    internal class GLTFastMaterialGenerator : IMaterialGenerator
+    {
+        private readonly Shader defaultShader;
+        private Material defaultMaterial;
+        public GLTFastMaterialGenerator()
+        {
+            defaultShader = Shader.Find("DCL/Universal Render Pipeline/Lit");
+            defaultMaterial = new Material(defaultShader);
+        }
+        public Material GetDefaultMaterial()
+        {
+             return defaultMaterial;
+        }
+        public Material GenerateMaterial(GLTFast.Schema.Material gltfMaterial, IGltfReadable gltf)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetLogger(ICodeLogger logger)
+        {
+            
+        }
     }
 
 }
