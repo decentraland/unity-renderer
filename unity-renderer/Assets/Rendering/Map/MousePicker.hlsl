852588daad6d463dab6a6111019af53a129d63d8
Juan M. Salice <104371686+JuanSalice@users.noreply.github.com>
2022-06-30 18:02:07 -0300

    Advancements in the map overlay functionality.

--- unity-renderer/Assets/Rendering/Map/MousePicker.hlsl
+++ unity-renderer/Assets/Rendering/Map/MousePicker.hlsl
@@ -1,19 +1,47 @@
+float2 OverlayClampToTexture(float2 uv, float2 size)
+{
+    float2 clampUv = ceil(uv * size) / size;
+    float2 pixel = float2(1.0, 1.0) / size;
+
+    return clampUv - pixel * float2(0.5, 0.5);
+}
+
 SAMPLER(custom_point_clamp_sampler);
-void MousePicker_float(UnityTexture2D Input, float2 Resolution, float2 UV, float2 MousePos, out float Out)
+void MousePicker_float(UnityTexture2D Input, float2 TextureSize, float2 Resolution, float Zoom, float2 UV, float2 MousePos, out float Out)
 {
     SamplerState ss = custom_point_clamp_sampler;
 
-    float2 mouseUV = MousePos / Resolution;
+    float2 zoom;
 
-    float4 mouseData = SAMPLE_TEXTURE2D(Input, ss, mouseUV);
-    float4 rawData = SAMPLE_TEXTURE2D(Input, ss, UV);
-
-    if (mouseData.b == rawData.b)
+    if (Resolution.x > Resolution.y)
     {
-        Out = 1;
+        UV -= 0.5;
+        zoom = TextureSize.x / Resolution.y * Zoom;
     }
     else
     {
-        Out = 0;
+        UV -= 0.5;
+        zoom = TextureSize.y / Resolution.x * Zoom;
+    }
+
+    float2 mouseUV = OverlayClampToTexture(MousePos/zoom, TextureSize);
+
+
+    float2 tileOfInterest = UV / zoom + float2(0.5, 0.5);
+    float2 uv = OverlayClampToTexture(tileOfInterest, TextureSize);
+
+    float4 mouseData = SAMPLE_TEXTURE2D(Input, ss, mouseUV);
+    float4 rawData = SAMPLE_TEXTURE2D(Input, ss, uv);
+
+    if (mouseData.b + mouseData.g + mouseData.r != 0)
+    {
+        if (mouseData.b == rawData.b && mouseData.g == rawData.g && mouseData.r == rawData.r)
+        {
+            Out = 1;
+        }
+        else
+        {
+            Out = 0;
+        }
     }
 }
