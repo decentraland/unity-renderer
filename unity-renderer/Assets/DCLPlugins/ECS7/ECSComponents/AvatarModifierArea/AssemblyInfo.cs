38d3c9d3f0f610b74753e5b180daa9d8f583ded9
Suduck <adrian@suduck.com>
2022-06-30 09:56:52 +0200

    fix: assembly info

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/AvatarModifierArea/AssemblyInfo.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/AvatarModifierArea/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("DCL.ECSComponents.AvatarModifierAreaShould")]
+[assembly: InternalsVisibleTo("DCL.ECSComponents.AvatarModifierArea.Tests")]
