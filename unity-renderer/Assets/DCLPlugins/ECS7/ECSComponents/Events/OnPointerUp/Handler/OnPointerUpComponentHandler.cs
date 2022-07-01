7131ebc3396e2660150f7879f537e71ecce23fe3
Adrian <54064092+Suduck@users.noreply.github.com>
2022-07-01 15:51:16 +0200

    feat: change events for the new ECS (#2570)
    feat: change the way it works

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/Events/OnPointerUp/Handler/OnPointerUpComponentHandler.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/Events/OnPointerUp/Handler/OnPointerUpComponentHandler.cs
@@ -40,7 +40,7 @@ namespace DCL.ECSComponents.OnPointerUp
 
         public void OnComponentModelUpdated(IParcelScene scene, IDCLEntity entity, PBOnPointerUp model)
         {
-            representantion.SetData(scene, entity, model.ShowFeedback, model.Button, model.Distance, model.Identifier, model.HoverText);
+            representantion.SetData(scene, entity, model.ShowFeedback, model.Button, model.Distance, model.HoverText);
             if (!isAdded)
             {
                 isAdded = true;
