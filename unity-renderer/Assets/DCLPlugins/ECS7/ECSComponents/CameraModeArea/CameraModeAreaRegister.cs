146056f27e57479008f8d59d77e6bd568ea882b0
Adrian <54064092+Suduck@users.noreply.github.com>
2022-07-04 12:30:14 +0200

    feat: add camera mode component (#2566)
    * feat: advance
    
    * feat: advance
    
    * feat: finished component
    
    * style: better style
    
    * fix: area mode
    
    * fix: compile isses
    
    * fix: unit test

---
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/CameraModeArea/CameraModeAreaRegister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DCL.ECSRuntime;
+
+namespace DCL.ECSComponents
+{
+    public class CameraModeAreaRegister : IDisposable
+    {
+        private readonly ECSComponentsFactory factory;
+        private readonly IECSComponentWriter componentWriter;
+        private readonly int componentId;
+
+        public CameraModeAreaRegister(int componentId, ECSComponentsFactory factory, IECSComponentWriter componentWriter)
+        {
+            factory.AddOrReplaceComponent(componentId, CameraModeAreaSerializer.Deserialize, () => new CameraModeAreaComponentHandler(Environment.i.platform.updateEventHandler, DataStore.i.player));
+            componentWriter.AddOrReplaceComponentSerializer<PBCameraModeArea>(componentId, CameraModeAreaSerializer.Serialize);
+
+            this.factory = factory;
+            this.componentWriter = componentWriter;
+            this.componentId = componentId;
+        }
+
+        public void Dispose()
+        {
+            factory.RemoveComponent(componentId);
+            componentWriter.RemoveComponentSerializer(componentId);
+        }
+    }
+}
--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/Utils/ProtoConvertUtils.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/Utils/ProtoConvertUtils.cs
@@ -50,5 +50,18 @@ namespace DCL.ECSComponents
             vector.z = original.Z;
             return vector;
         }
+        
+        public static CameraMode.ModeId PBCameraEnumToUnityEnum(PBCameraModeArea.Types.CameraMode mode)
+        {
+            switch (mode)
+            {
+                case PBCameraModeArea.Types.CameraMode.FirstPerson:
+                    return CameraMode.ModeId.FirstPerson;
+                case PBCameraModeArea.Types.CameraMode.ThirdPerson:
+                    return CameraMode.ModeId.ThirdPerson;
+                default:
+                    return CommonScriptableObjects.cameraMode.Get();
+            }
+        }
     }
 }
