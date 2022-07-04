f866567010b21460d381d20ab7cbd88ded71019b
davidejensen <davidejensen@live.it>
2022-07-04 14:07:39 +0200

    removed debug log

--- unity-renderer/Assets/Scripts/MainScripts/DCL/AvatarSystem/AvatarWithHologram.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/AvatarSystem/AvatarWithHologram.cs
@@ -66,7 +66,6 @@ namespace AvatarSystem
                 List<WearableItem> emotes = null;
 
                 baseAvatar.Initialize();
-                Debug.Log($"base avatar armature container {baseAvatar.GetArmatureContainer() == null} main renderer {baseAvatar.GetMainRenderer() == null}");
                 animator.Prepare(settings.bodyshapeId, baseAvatar.GetArmatureContainer());
                 (bodyshape, eyes, eyebrows, mouth, wearables, emotes) = await avatarCurator.Curate(settings, wearablesIds, linkedCt);
                 if (!loader.IsValidForBodyShape(bodyshape, eyes, eyebrows, mouth))
