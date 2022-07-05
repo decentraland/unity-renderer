9e53196027ab98966156e313cf0783f64e364b04
Juan Molteni <juanignaciomolteni@gmail.com>
2022-07-05 15:05:25 -0300

    Adding MaterialTransitionController statically in AssetPromise_AB_GameObject

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/MaterialTransitionController/MaterialTransitionController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/MaterialTransitionController/MaterialTransitionController.cs
@@ -290,6 +290,9 @@ public class MaterialTransitionController : MonoBehaviour
 
             if (r.gameObject.GetComponent<MaterialTransitionController>() != null)
                 continue;
+            
+            if (r.transform.parent.parent.name.Equals("Armature"))
+                continue;
 
             MaterialTransitionController transition = r.gameObject.AddComponent<MaterialTransitionController>();
             Material finalMaterial = r.sharedMaterial;
