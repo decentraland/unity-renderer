b5bcbede64983a4b4061923e51cc7562b4bb6b29
davidejensen <davidejensen@live.it>
2022-07-05 09:25:21 +0200

    Enabled hologram avatar

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Main/Main.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Main/Main.cs
@@ -66,7 +66,7 @@ namespace DCL
         {
             DataStore.i.textureConfig.gltfMaxSize.Set(512);
             DataStore.i.textureConfig.generalMaxSize.Set(2048);
-            DataStore.i.avatarConfig.useHologramAvatar.Set(false);
+            DataStore.i.avatarConfig.useHologramAvatar.Set(true);
         }
 
         protected virtual void InitializeCommunication()
