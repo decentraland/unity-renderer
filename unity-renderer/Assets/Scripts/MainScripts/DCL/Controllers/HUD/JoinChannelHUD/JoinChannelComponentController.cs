226b17895eeb71f4dcf927f2050b55b53a07b35e
Santi Andrade <sandrade@decentraland.org>
2022-07-27 16:00:20 +0200

    Update JoinChannelComponentController.cs

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/JoinChannelHUD/JoinChannelComponentController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/JoinChannelHUD/JoinChannelComponentController.cs
@@ -19,6 +19,8 @@ public class JoinChannelComponentController : IDisposable
     public void Dispose()
     {
         channelsDataStore.currentJoinChannelModal.OnChange -= OnChannelToJoinChanged;
+        joinChannelView.OnCancelJoin -= OnCancelJoin;
+        joinChannelView.OnConfirmJoin -= OnConfirmJoin;
     }
 
     private void OnChannelToJoinChanged(string currentChannelId, string previousChannelId)
