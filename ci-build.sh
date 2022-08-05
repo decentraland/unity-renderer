ad6bd3932db701d4236931f83df27d20fd53e3a5
Kinerius <kinerius@gmail.com>
2022-08-05 13:53:13 -0300

    Added deep profiling and auto profiler connect to the build

--- ci-build.sh
+++ ci-build.sh
@@ -16,6 +16,8 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Edito
   -customBuildTarget "$BUILD_TARGET" \
   -customBuildName "$BUILD_NAME" \
   -customBuildPath "$BUILD_PATH" \
+  -deepprofiling \
+  -profiler-enable \
   -executeMethod BuildCommand.PerformBuild
 
 UNITY_EXIT_CODE=$?
