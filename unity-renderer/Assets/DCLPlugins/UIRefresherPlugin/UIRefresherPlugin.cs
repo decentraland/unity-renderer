052141b4f09a7017a133f4ff4b3052468d991909
Patricio Guerra <kinerius@gmail.com>
2022-08-09 11:14:43 -0300

    re-revert: uishape polling, with extra fix (#2786)
    * fix: ui shape refresh polling is now a task
    
    * letting UIShape enqueue multiple refreshes
    
    * Changed the approach, now it uses a controlled static late update and a queue instead of a task, also optimized when a UIText decides to dirty itself
    
    * Refactored the code a bit, now the queue is processed on a temporal static coroutine
    
    * Added batch mode check back again
    
    * Removed old debug code
    
    * Refactored the UIRefresh coroutine into UIRefresherPlugin
    
    * Fixed UI Visual tests having no UIRefresherPlugin
    
    * Requested changes
    
    * fix: uishape performance regression (#2788)
    
    fix: uishape refresh performance regression

---
+++ unity-renderer/Assets/DCLPlugins/UIRefresherPlugin/UIRefresherPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using DCL;
+using DCL.Components.Interfaces;
+using Environment = DCL.Environment;
+
+namespace DCLPlugins.UIRefresherPlugin
+{
+    public class UIRefresherPlugin : IPlugin
+    {
+        private readonly UIRefresherController controller;
+        
+        public UIRefresherPlugin()
+        {
+            controller = new UIRefresherController(Environment.i.platform.updateEventHandler, 
+                CommonScriptableObjects.sceneID,
+                DataStore.i.HUDs.dirtyShapes);
+        }
+        
+        public void Dispose()
+        {
+            DataStore.i.HUDs.dirtyShapes.Set(new Dictionary<string, Queue<IUIRefreshable>>());
+            controller.Dispose();
+        }
+    }
+
+}
