5d54405ca8b437c71a62ffd33f6b069b9b1c28be
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2022-08-17 13:20:58 -0300

    feat: add toast for avatar modifier (#2603)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/DataStore/DataStore_HUDs.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/DataStore/DataStore_HUDs.cs
@@ -22,8 +22,8 @@ namespace DCL
         public readonly BaseVariable<bool> gotoPanelVisible = new BaseVariable<bool>(false);
         public readonly BaseVariable<ParcelCoordinates> gotoPanelCoordinates = new BaseVariable<ParcelCoordinates>(new ParcelCoordinates(0,0));
         public readonly BaseVariable<bool> isSceneUIEnabled = new BaseVariable<bool>(true);
+        public readonly BaseRefCounter<AvatarAreaWarningID> avatarAreaWarnings = new BaseRefCounter<AvatarAreaWarningID>();
         public readonly BaseVariable<Dictionary<string, Queue<IUIRefreshable>>> dirtyShapes = new BaseVariable<Dictionary<string, Queue<IUIRefreshable>>>(new Dictionary<string, Queue<IUIRefreshable>>());
-        
         public readonly LoadingHUD loadingHUD = new LoadingHUD();
 
         public class LoadingHUD
