971e98ea8c71a3297718cdda09ba8fad8e72391f
Pato <pato@decentraland.org>
2022-08-23 14:15:01 -0300

    fix plane mesh upside down

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/PrimitiveMesh/PrimitiveMeshFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/PrimitiveMesh/PrimitiveMeshFactory.cs
@@ -20,14 +20,14 @@ internal class PrimitiveMeshFactory
                 mesh = PrimitiveMeshBuilder.BuildSphere(1f);
                 break;
             case AssetPromise_PrimitiveMesh_Model.PrimitiveType.Plane:
-                mesh = PrimitiveMeshBuilder.BuildPlane(1f);
+                mesh = PrimitiveMeshBuilder.BuildPlaneV2(1f);
                 if (meshModelModel.uvs != null && meshModelModel.uvs.Count > 0)
                 {
                     mesh.uv = Utils.FloatArrayToV2List(meshModelModel.uvs);
                 }
                 break;
             case AssetPromise_PrimitiveMesh_Model.PrimitiveType.Cylinder:
-                mesh = PrimitiveMeshBuilder.BuildCylinder(50, meshModelModel.radiusTop, meshModelModel.radiusBottom, 
+                mesh = PrimitiveMeshBuilder.BuildCylinder(50, meshModelModel.radiusTop, meshModelModel.radiusBottom,
                     2f, 0f, true, false);
                 break;
         }
