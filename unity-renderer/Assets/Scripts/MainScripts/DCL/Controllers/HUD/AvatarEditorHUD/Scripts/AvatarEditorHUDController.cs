1380a4715aa25df4044b789e8dc89b9bc6167471
antivortex <anton.vikharev@decentraland.org>
2022-08-23 14:20:08 +0400

    remove unused namespace JetBrains.Annotations
    Signed-off-by: antivortex <anton.vikharev@decentraland.org>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDController.cs
@@ -6,7 +6,6 @@ using DCL.NotificationModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
