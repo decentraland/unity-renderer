2ed4a643aa5fefd71dc7eac4cf8c6738840ba61c
Santi Andrade <sandrade@decentraland.org>
2022-08-23 17:23:55 +0200

    Fix

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/VoiceChatHUD/VoiceChatWindowComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/VoiceChatHUD/VoiceChatWindowComponentView.cs
@@ -56,13 +56,6 @@ public class VoiceChatWindowComponentView : BaseComponentView, IVoiceChatWindowC
         ConfigureAllowUsersFilter();
     }
 
-    public override void Start()
-    {
-        base.Start();
-        
-        AllowUsersOptionChanged(true, VoiceChatAllow.ALL_USERS.ToString(), ALLOW_USERS_TITLE_ALL);
-    }
-
     public void Configure(VoiceChatWindowComponentModel newModel)
     {
         model = newModel;
