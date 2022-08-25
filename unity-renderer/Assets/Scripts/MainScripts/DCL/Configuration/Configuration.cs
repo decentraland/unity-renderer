ca7d3bf2a14b1f02924d705fb8121c9f413a45a8
antivortex <anton.vikharev@decentraland.org>
2022-08-25 19:26:09 +0400

    configuration typo fix

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -132,7 +132,7 @@ namespace DCL.Configuration
         public const int GLTF_TEX_MAX_SIZE_WEB = 512;
         public const int GENERAL_TEX_MAX_SIZE_WEB = 2048;
         public const int GLTF_TEX_MAX_SIZE_DESKTOP = 1024;
-        public const int GENERAL_TEXT_MAX_SIZE_DESKTOP = 4096;
+        public const int GENERAL_TEX_MAX_SIZE_DESKTOP = 4096;
     }
 
     public static class ApplicationSettings
