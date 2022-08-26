33335bdd18abc6db118041c7d701f1da32d39fd8
antivortex <anton.vikharev@decentraland.org>
2022-08-26 12:51:21 +0400

    remove desktop constants to move then to desktop repo

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Configuration/Configuration.cs
@@ -131,8 +131,6 @@ namespace DCL.Configuration
     {
         public const int GLTF_TEX_MAX_SIZE_WEB = 512;
         public const int GENERAL_TEX_MAX_SIZE_WEB = 2048;
-        public const int GLTF_TEX_MAX_SIZE_DESKTOP = 1024;
-        public const int GENERAL_TEX_MAX_SIZE_DESKTOP = 4096;
     }
 
     public static class ApplicationSettings
