10db341e16af8e6f86f38e29ff691af4b4a7ea6f
Alejandro Jimenez <ajimenez@decentraland.org>
2022-08-26 12:44:41 +0200

    chore: disable parcel highlight by default

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/MapAtlas.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/MapAtlas.cs
@@ -1,5 +1,4 @@
 using DCL.Helpers;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +13,8 @@ namespace DCL
         public RectTransform chunksParent;
         public GameObject overlayLayerGameobject;
         [SerializeField] private Image mapRenderer;
+
+        private void Awake() { SetHighlightedScene(null); }
 
         public void Cleanup() { }
 
