44affb0cfb595376488678a0ba5aa82d5329089f
davidejensen <davidejensen@live.it>
2022-08-26 10:13:05 +0200

    additional minor fix for tests

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Minimap/MinimapHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Minimap/MinimapHUDController.cs
@@ -26,7 +26,8 @@ public class MinimapHUDController : IHUD
         minimapZoom.Set(1f);
         UpdateData(model);
         metadataController = MinimapMetadataController.i;
-        metadataController?.OnHomeChanged += SetNewHome;
+        if(metadataController != null)
+            metadataController.OnHomeChanged += SetNewHome;
     }
 
     protected internal virtual MinimapHUDView CreateView() { return MinimapHUDView.Create(this); }
@@ -45,7 +46,9 @@ public class MinimapHUDController : IHUD
         CommonScriptableObjects.playerCoords.OnChange -= OnPlayerCoordsChange;
         CommonScriptableObjects.builderInWorldNotNecessaryUIVisibilityStatus.OnChange -= ChangeVisibilityForBuilderInWorld;
         MinimapMetadata.GetMetadata().OnSceneInfoUpdated -= OnOnSceneInfoUpdated;
-        metadataController?.OnHomeChanged -= SetNewHome;
+        
+        if (metadataController != null)
+            metadataController.OnHomeChanged -= SetNewHome;
     }
 
     private void OnPlayerCoordsChange(Vector2Int current, Vector2Int previous)
