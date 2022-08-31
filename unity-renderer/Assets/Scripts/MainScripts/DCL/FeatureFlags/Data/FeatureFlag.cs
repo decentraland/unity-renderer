ea3a8ae726eede2d554d552e335c33065b58a3f1
Agnieszka Besz <aga@besz.me>
2022-08-31 17:17:01 +0300

    fix: add support for variants in AB test

--- unity-renderer/Assets/Scripts/MainScripts/DCL/FeatureFlags/Data/FeatureFlag.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/FeatureFlags/Data/FeatureFlag.cs
@@ -1,22 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 
 public class FeatureFlag
 {
     public readonly Dictionary<string, bool> flags  = new Dictionary<string, bool>();
     public readonly Dictionary<string, FeatureFlagVariant> variants  = new Dictionary<string, FeatureFlagVariant>();
     
-
     public bool IsFeatureEnabled(string featureName)
     {
+        if (variants.TryGetValue(featureName, out var variant) && variant.enabled)
+        {
+            //We use name property, to determine what A/B test variant is used
+            //Currently for simplicity we supposed enabled/disabled name 
+            return variant.name == "enabled";
+        }
+        
         if (flags.ContainsKey(featureName))
             return flags[featureName];
 
         return false;
     }
-
+   
     public string ToString()
     {
         string result = "";
@@ -32,13 +35,13 @@ public class FeatureFlag
 
 public class FeatureFlagVariant
 {
-    private string name;
-    private bool enable;
-    private Payload payload;
+    public string name;
+    public bool enabled;
+    public FeatureFlagVariantPayload payload;
+}
 
-    struct Payload
-    {
-        private string type;
-        private string value;
-    }
+public class FeatureFlagVariantPayload
+{
+    public string type;
+    public string value;
 }
