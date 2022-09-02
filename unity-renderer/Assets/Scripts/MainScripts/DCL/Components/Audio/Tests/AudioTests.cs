b9038ed35b0868dcf0a030086fee1a12db1c6a83
Pravus <pravusjif@gmail.com>
2022-09-02 02:05:45 -0300

    removed unneeded scene id change that broke a couple of scene boundaries checker audio tests

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Audio/Tests/AudioTests.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Audio/Tests/AudioTests.cs
@@ -195,9 +195,6 @@ namespace Tests
             // We disable SceneController monobehaviour to avoid its current scene id update
             sceneController.enabled = false;
 
-            // Set current scene with this scene's id
-            CommonScriptableObjects.sceneID.Set(scene.sceneData.id);
-
             var entity = TestUtils.CreateSceneEntity(scene);
             yield return null;
 
