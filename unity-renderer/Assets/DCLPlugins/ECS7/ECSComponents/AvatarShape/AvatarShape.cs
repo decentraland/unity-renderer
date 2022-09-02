34e7d6d28499594ca726a46cf3bd6c0100bbdf54
Suduck <adrian@suduck.com>
2022-09-02 12:47:51 +0200

    feat: update proto

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/AvatarShape/AvatarShape.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/AvatarShape/AvatarShape.cs
@@ -192,9 +192,7 @@ namespace DCL.ECSComponents
                 }
             }
             
-            // If the model contains a value for expressionTriggerId then we try it, if value doesn't exist, we skip
-            if(model.HasExpressionTriggerId)
-                avatar.PlayEmote(model.ExpressionTriggerId, model.GetExpressionTriggerTimestamp());
+            avatar.PlayEmote(model.ExpressionTriggerId, model.GetExpressionTriggerTimestamp());
 
             UpdatePlayerStatus(entity, model);
 
