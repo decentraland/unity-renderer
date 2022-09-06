87bb3fae08f02611312f8d190fe5a344be9e36f7
Nicolas Lorusso <nalorusso@gmail.com>
2022-09-06 15:44:15 -0300

    fixed merge issues

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NotificationMessagesHUD/ChatNotificationController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NotificationMessagesHUD/ChatNotificationController.cs
@@ -58,10 +58,6 @@ public class ChatNotificationController : IHUD
         }
 
         var peerId = ExtractPeerId(message);
-
-        var channel = chatController.GetAllocatedChannel(peerId);
-        if (channel is {Muted: true}) return;
-
         var peerProfile = userProfileBridge.Get(peerId);
         var peerName = peerProfile?.userName ?? peerId;
         var peerProfilePicture = peerProfile?.face256SnapshotURL;
