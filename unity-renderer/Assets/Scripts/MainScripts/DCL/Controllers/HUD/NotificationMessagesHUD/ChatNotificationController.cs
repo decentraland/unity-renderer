f6f922fcc1ca29af3703c05667eadf014e297279
Nicolas Lorusso <nalorusso@gmail.com>
2022-09-06 15:32:50 -0300

    fixed cases when the profile does not exist

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NotificationMessagesHUD/ChatNotificationController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NotificationMessagesHUD/ChatNotificationController.cs
@@ -12,19 +12,20 @@ public class ChatNotificationController : IHUD
 {
     private const int FADEOUT_DELAY = 8000;
 
-    private DataStore dataStore;
-    private IChatController chatController;
-    private IMainChatNotificationsComponentView mainChatNotificationView;
-    private ITopNotificationsComponentView topNotificationView;
-    private IUserProfileBridge userProfileBridge;
+    private readonly DataStore dataStore;
+    private readonly IChatController chatController;
+    private readonly IMainChatNotificationsComponentView mainChatNotificationView;
+    private readonly ITopNotificationsComponentView topNotificationView;
+    private readonly IUserProfileBridge userProfileBridge;
     private BaseVariable<Transform> notificationPanelTransform => dataStore.HUDs.notificationPanelTransform;
     private BaseVariable<Transform> topNotificationPanelTransform => dataStore.HUDs.topNotificationPanelTransform;
     private BaseVariable<HashSet<string>> visibleTaskbarPanels => dataStore.HUDs.visibleTaskbarPanels;
     private CancellationTokenSource fadeOutCT = new CancellationTokenSource();
-
     private UserProfile ownUserProfile;
 
-    public ChatNotificationController(DataStore dataStore, IMainChatNotificationsComponentView mainChatNotificationView, ITopNotificationsComponentView topNotificationView, IChatController chatController, IUserProfileBridge userProfileBridge)
+    public ChatNotificationController(DataStore dataStore, IMainChatNotificationsComponentView mainChatNotificationView,
+        ITopNotificationsComponentView topNotificationView, IChatController chatController,
+        IUserProfileBridge userProfileBridge)
     {
         this.dataStore = dataStore;
         this.chatController = chatController;
@@ -33,7 +34,6 @@ public class ChatNotificationController : IHUD
         this.topNotificationView = topNotificationView;
         mainChatNotificationView.OnResetFade += ResetFadeOut;
         mainChatNotificationView.OnPanelFocus += TogglePanelBackground;
-        ownUserProfile = userProfileBridge.GetOwn();
         chatController.OnAddMessage += HandleMessageAdded;
         notificationPanelTransform.Set(mainChatNotificationView.GetPanelTransform());
         topNotificationPanelTransform.Set(topNotificationView.GetPanelTransform());
@@ -48,22 +48,17 @@ public class ChatNotificationController : IHUD
     private void HandleMessageAdded(ChatMessage message)
     {
         if (message.messageType != ChatMessage.Type.PRIVATE && message.messageType != ChatMessage.Type.PUBLIC) return;
+        ownUserProfile ??= userProfileBridge.GetOwn();
         if (message.sender == ownUserProfile.userId) return;
-
-        if (message.sender != null)
-        {
-            var profile = ExtractRecipient(message);
-            if (profile == null) return;
-            mainChatNotificationView.AddNewChatNotification(message, profile.userName, profile.face256SnapshotURL);
-            if(topNotificationPanelTransform.Get().gameObject.activeInHierarchy)
-                topNotificationView.AddNewChatNotification(message, profile.userName, profile.face256SnapshotURL);
-        }
-        else
-        {
-            mainChatNotificationView.AddNewChatNotification(message);
-            if (topNotificationPanelTransform.Get().gameObject.activeInHierarchy)
-                topNotificationView.AddNewChatNotification(message);
-        }
+        
+        var peerId = ExtractPeerId(message);
+        var peerProfile = userProfileBridge.Get(peerId);
+        var peerName = peerProfile?.userName ?? peerId;
+        var peerProfilePicture = peerProfile?.face256SnapshotURL;
+            
+        mainChatNotificationView.AddNewChatNotification(message, peerName, peerProfilePicture);
+        if (topNotificationPanelTransform.Get().gameObject.activeInHierarchy)
+            topNotificationView.AddNewChatNotification(message, peerName, peerProfilePicture);
     }
 
     public void ResetFadeOut(bool fadeOutAfterDelay = false)
@@ -72,7 +67,7 @@ public class ChatNotificationController : IHUD
         fadeOutCT.Cancel();
         fadeOutCT = new CancellationTokenSource();
 
-        if(fadeOutAfterDelay)
+        if (fadeOutAfterDelay)
             WaitThenFadeOutNotifications(fadeOutCT.Token).Forget();
     }
 
@@ -93,9 +88,9 @@ public class ChatNotificationController : IHUD
 
         mainChatNotificationView.HideNotifications();
     }
-
-    private UserProfile ExtractRecipient(ChatMessage message) =>
-        userProfileBridge.Get(message.sender != ownUserProfile.userId ? message.sender : message.recipient);
+    
+    private string ExtractPeerId(ChatMessage message) =>
+        message.sender != ownUserProfile.userId ? message.sender : message.recipient;
 
     public void SetVisibility(bool visible)
     {
@@ -116,7 +111,7 @@ public class ChatNotificationController : IHUD
     public void Dispose()
     {
         chatController.OnAddMessage -= HandleMessageAdded;
-        visibleTaskbarPanels.OnChange -= VisiblePanelsChanged; 
+        visibleTaskbarPanels.OnChange -= VisiblePanelsChanged;
         mainChatNotificationView.OnResetFade -= ResetFadeOut;
     }
 }
