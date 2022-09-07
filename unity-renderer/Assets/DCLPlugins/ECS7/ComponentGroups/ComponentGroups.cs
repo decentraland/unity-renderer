43016382cd55db0db3b123dd373f1eaf03aff0f2
pbosio <pato@decentraland.org>
2022-09-07 13:11:07 -0300

    fix: ECS7 pointer events for MeshColliders (#2952)

--- unity-renderer/Assets/DCLPlugins/ECS7/ComponentGroups/ComponentGroups.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ComponentGroups/ComponentGroups.cs
@@ -1,13 +1,23 @@
+using DCL.ECS7;
 using DCL.ECS7.InternalComponents;
+using DCL.ECSComponents;
 using DCL.ECSRuntime;
 
 public readonly struct ComponentGroups : IComponentGroups
 {
-    public readonly IECSReadOnlyComponentsGroup<InternalMaterial, InternalTexturizable> texturizableGroup { get; }
+    public IECSReadOnlyComponentsGroup<InternalMaterial, InternalTexturizable> texturizableGroup { get; }
+    public IECSReadOnlyComponentsGroup<InternalColliders, PBOnPointerDown> pointerDownGroup { get; }
+    public IECSReadOnlyComponentsGroup<InternalColliders, PBOnPointerUp> pointerUpGroup { get; }
 
     public ComponentGroups(ECSComponentsManager componentsManager)
     {
         texturizableGroup = componentsManager.CreateComponentGroup<InternalMaterial, InternalTexturizable>
             ((int)InternalECSComponentsId.MATERIAL, (int)InternalECSComponentsId.TEXTURIZABLE);
+
+        pointerDownGroup = componentsManager.CreateComponentGroup<InternalColliders, PBOnPointerDown>
+            ((int)InternalECSComponentsId.COLLIDER_POINTER, ComponentID.ON_POINTER_DOWN);
+
+        pointerUpGroup = componentsManager.CreateComponentGroup<InternalColliders, PBOnPointerUp>
+            ((int)InternalECSComponentsId.COLLIDER_POINTER, ComponentID.ON_POINTER_UP);
     }
 }
