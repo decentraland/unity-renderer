7d27c857c2da7faad0b959a05611842acbb8590b
MichaelBitsakis <infogdrs@yahoo.com>
2022-09-09 13:37:55 +0300

    Blur Shader Structure as signed

---
+++ unity-renderer/Assets/Rendering/Shaders/BlurRT/Scripts/HelperScripts/RuntimeProfilerScripts/FileManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public static class FileManager
+{
+    public static bool WriteToFile(string a_FileName, string a_FileContents)
+    {
+        var fullPath = Path.Combine(Application.persistentDataPath, a_FileName);
+        try
+        {
+            File.WriteAllText(fullPath, a_FileContents);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to write to {fullPath} with exception {e}");
+            return false;
+        }
+    }
+
+    public static bool LoadFromFile(string a_FileName, out string result)
+    {
+        var fullPath = Path.Combine(Application.persistentDataPath, a_FileName);
+
+        try
+        {
+            result = File.ReadAllText(fullPath);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to read from {fullPath} with exception {e}");
+            result = "";
+            return false;
+        }
+    }
+}
