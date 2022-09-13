d9698c97ebf04bba4bcd3f0380b1d0c468429238
MichaelBitsakis <infogdrs@yahoo.com>
2022-09-13 14:25:00 +0300

    Blur UI Only UI Grabber Setup C1

--- unity-renderer/Assets/Rendering/Shaders/BlurRT/Graph/Blur_v4_WebGL_Compatible/Scripts/UIGrabber/UIGrabber.cs
+++ unity-renderer/Assets/Rendering/Shaders/BlurRT/Graph/Blur_v4_WebGL_Compatible/Scripts/UIGrabber/UIGrabber.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class UIGrabber : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("UI - Cam - RenderTexture Setup")]
+    [SerializeField] private GameObject _ui;
+    [SerializeField] private Camera _uiCamera;
+
+    [Header("Render Texture")]
+    [SerializeField] private RenderTexture _renderTexture;
+
+    // list for ui elements
+    [SerializeField] private List<CanvasRenderer> _uiElements = new List<CanvasRenderer>();
+    
+
+
+    private void Awake()
     {
-        
+        SetRenderTexture(_renderTexture);
     }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetCanvasChildren();
+    }
+    
+    
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // set render texture to ui camera
+    public void SetRenderTexture(RenderTexture renderTexture)
+    {
+        _uiCamera.targetTexture = renderTexture;
+    }
+
+    // set canvas children to list
+    public void SetCanvasChildren()
+    {
+        _uiElements.Clear();
+        foreach (Transform child in _ui.transform)
+        {
+            _uiElements.Add(child.GetComponent<CanvasRenderer>());
+        }
     }
 }
