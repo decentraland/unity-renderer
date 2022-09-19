f8589ec8f6f3b1b3682f6eff72d006bb0ad185da
Kinerius <kinerius@gmail.com>
2022-09-19 13:30:48 -0300

    rollback

--- unity-renderer/Assets/Scripts/MainScripts/DCL/AvatarSystem/Loader/Loader.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/AvatarSystem/Loader/Loader.cs
@@ -191,7 +191,7 @@ namespace AvatarSystem
             // AttachExternalCancellation is needed because cancellation will take a wait to trigger
             await UniTask.WaitForEndOfFrame(ct).AttachExternalCancellation(ct);
             var featureFlags = DataStore.i.featureFlags.flags.Get();
-            avatarMeshCombiner.useCullOpaqueHeuristic = false;
+            avatarMeshCombiner.useCullOpaqueHeuristic = true;
             avatarMeshCombiner.enableCombinedMesh = false;
             bool success = avatarMeshCombiner.Combine(bonesContainer, allRenderers.ToArray());
             if (!success)
