49c97f44cd05ce1d4294e1dc48e32d08528acb09
Santi Andrade <sandrade@decentraland.org>
2022-10-05 10:31:15 -0300

    fix: Lazy Loading fixes (#3120)
    * Fix online friends and jumpIn button
    
    * Restrict the request of more entries to a timer
    
    * Fix loading more spinner visibility
    
    * Fix conversation list loading
    
    * Fix tests
    
    * Fix tests (II)
    
    * refactor of friends with direct messages pagination behaviour
    
    * fixed tests
    
    * refactor of WorldChatWindow search behaviour
    
    * refactor of friend search behaviour
    
    * fixed white images on conversation list searching entries
    
    * fixed avatar images on friend list
    
    * fixed counter label for search modes
    
    * fixed extra entries when searching friends
    
    * Fix tests
    
    * fix for opening chat windows from search entries in the conversation list
    
    * fixed online friends being added after removing them
    
    * avoid loading more friends when changing tabs or initializing friends
    
    * higher time threshold to load more friends after clearing
    
    * fix for loading friends 2nd page out of nowhere: trigger user status update only when changes due kernel issues
    
    Co-authored-by: Nicolas Lorusso <nalorusso@gmail.com>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/UserStatus.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/UserStatus.cs
@@ -9,6 +9,15 @@ public class UserStatus
     {
         public string serverName;
         public string layer;
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj) return true;
+            if (!(obj is Realm other)) return false;
+            if (serverName != other.serverName) return false;
+            if (layer != other.layer) return false;
+            return true;
+        }
     }
 
     public Realm realm;
@@ -17,4 +26,18 @@ public class UserStatus
     public FriendshipStatus friendshipStatus;
     public PresenceStatus presence;
     [NonSerialized] public DateTime friendshipStartedTime;
+
+    public override bool Equals(object obj)
+    {
+        if (this == obj) return true;
+        if (!(obj is UserStatus other)) return false;
+        if (realm != null && other.realm == null) return false;
+        if (realm == null && other.realm != null) return false;
+        if (realm != null && !realm.Equals(other.realm)) return false;
+        if (position != other.position) return false;
+        if (userId != other.userId) return false;
+        if (friendshipStatus != other.friendshipStatus) return false;
+        if (friendshipStartedTime != other.friendshipStartedTime) return false;
+        return true;
+    }
 }
