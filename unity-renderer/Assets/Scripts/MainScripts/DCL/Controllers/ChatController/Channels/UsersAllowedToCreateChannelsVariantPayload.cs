3c18b8bd3e9a55c35761b5d5df3bd1bb8db11d02
Santi Andrade <sandrade@decentraland.org>
2022-10-10 17:43:20 -0300

    Apply feedback: Use of the DCL.Chat.Channels namespace

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ChatController/Channels/UsersAllowedToCreateChannelsVariantPayload.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ChatController/Channels/UsersAllowedToCreateChannelsVariantPayload.cs
@@ -1,16 +1,19 @@
 using System;
 
-[Serializable]
-public enum AllowChannelsCreationMode
+namespace DCL.Chat.Channels
 {
-    ALLOWLIST = 0,
-    NAMES = 1,
-    WALLET = 2
-}
+    [Serializable]
+    public enum AllowChannelsCreationMode
+    {
+        ALLOWLIST = 0,
+        NAMES = 1,
+        WALLET = 2
+    }
 
-[Serializable]
-public class UsersAllowedToCreateChannelsVariantPayload
-{
-    public AllowChannelsCreationMode mode;
-    public string[] allowList;
+    [Serializable]
+    public class UsersAllowedToCreateChannelsVariantPayload
+    {
+        public AllowChannelsCreationMode mode;
+        public string[] allowList;
+    }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ChatController/ChannelsUtils.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ChatController/ChannelsUtils.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DCL.Chat
+namespace DCL.Chat.Channels
 {
     public class ChannelsUtils : IChannelsUtils
     {
