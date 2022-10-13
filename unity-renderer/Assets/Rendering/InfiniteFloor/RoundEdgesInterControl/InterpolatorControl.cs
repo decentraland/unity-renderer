032ce67dca80d4aaca501bd706f3148b5874e550
MichaelBitsakis <infogdrs@yahoo.com>
2022-10-13 18:58:21 +0300

    Infinite Floor Round Edges Implementation

--- unity-renderer/Assets/Rendering/InfiniteFloor/RoundEdgesInterControl/InterpolatorControl.cs
+++ unity-renderer/Assets/Rendering/InfiniteFloor/RoundEdgesInterControl/InterpolatorControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InterplatorControl : MonoBehaviour
+public class InterpolatorControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
