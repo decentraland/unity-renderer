5b84a40161cf65005512efdc48605f62970d483b
Juan Molteni <juanignaciomolteni@gmail.com>
2022-10-20 14:37:23 -0300

    Using AVPro helper function

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Video/AvPro/AvProVideoPlayer.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Video/AvPro/AvProVideoPlayer.cs
@@ -83,55 +83,11 @@ public class AvProVideoPlayer : IVideoPlayer, IDisposable
         if (resizeDone)
         {
             avProTexture = avProMediaPlayer.TextureProducer.GetTexture(0);
-            if (SystemInfo.copyTextureSupport == UnityEngine.Rendering.CopyTextureSupport.None)
-            {
-                //High GC allocs here
-                videoTexture.SetPixels(((Texture2D)avProTexture).GetPixels()));
-                videoTexture.Apply();
-            } else {
-                Graphics.CopyTexture(avProTexture, videoTexture);
-            }
+            //Graphics.CopyTexture(avProTexture, videoTexture);
         
-            
-            // Create a temporary RenderTexture of the same size as the texture
-            RenderTexture tmp = RenderTexture.GetTemporary( 
-                texture.width,
-                texture.height,
-                0,
-                RenderTextureFormat.Default,
-                RenderTextureReadWrite.Linear);
+            //Helper.GetReadableTexture( avProTexture, avProMediaPlayer.TextureProducer.RequiresVerticalFlip(), Helper.GetOrientation(avProMediaPlayer.Info.GetTextureTransform()), videoTexture);
+            Helper.GetReadableTexture( avProTexture, false, Helper.GetOrientation(avProMediaPlayer.Info.GetTextureTransform()), videoTexture);
 
-
-// Blit the pixels on texture to the RenderTexture
-            Graphics.Blit(texture, tmp);
-
-
-// Backup the currently set RenderTexture
-            RenderTexture previous = RenderTexture.active;
-
-
-// Set the current RenderTexture to the temporary one we created
-            RenderTexture.active = tmp;
-
-
-// Create a new readable Texture2D to copy the pixels to it
-            Texture2D myTexture2D = new Texture2D(texture.width, texture.height);
-
-
-// Copy the pixels from the RenderTexture to the new Texture
-            myTexture2D.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
-            myTexture2D.Apply();
-
-
-// Reset the active RenderTexture
-            RenderTexture.active = previous;
-
-
-// Release the temporary RenderTexture
-            RenderTexture.ReleaseTemporary(tmp);
-
-
-// "myTexture2D" now has the same pixels from "texture" and it's re
         }
     }
 
