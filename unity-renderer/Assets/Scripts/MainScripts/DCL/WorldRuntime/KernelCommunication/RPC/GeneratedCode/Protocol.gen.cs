cc261429b19ed33d20be0b7ffa1931c10fe03efa
Mateo Miccino <mateomiccino@gmail.com>
2022-10-27 12:13:14 -0300

    chore: update @dcl/rpc-csharp (#3333)
    * chore: update @dcl/rpc
    
    * change code generation to comply with the conventions (example: ICRDTService to CRDTServiceBase)
    
    * fixes
    
    * delete deprecated crdt stream request

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/GeneratedCode/Protocol.gen.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/GeneratedCode/Protocol.gen.cs
@@ -25,19 +25,16 @@ public static partial class ProtocolReflection {
           "Cg5wcm90b2NvbC5wcm90byJLChBDUkRUTWFueU1lc3NhZ2VzEhAKCHNjZW5l",
           "X2lkGAEgASgJEg8KB3BheWxvYWQYAiABKAwSFAoMc2NlbmVfbnVtYmVyGAMg",
           "ASgFIg4KDENSRFRSZXNwb25zZSI5Cg9QdWxsQ1JEVFJlcXVlc3QSEAoIc2Nl",
-          "bmVfaWQYASABKAkSFAoMc2NlbmVfbnVtYmVyGAMgASgFIhcKEUNSRFRTdHJl",
-          "YW1SZXF1ZXN0OgIYATK1AQoLQ1JEVFNlcnZpY2USLgoIU2VuZENyZHQSES5D",
-          "UkRUTWFueU1lc3NhZ2VzGg0uQ1JEVFJlc3BvbnNlIgASMQoIUHVsbENyZHQS",
-          "EC5QdWxsQ1JEVFJlcXVlc3QaES5DUkRUTWFueU1lc3NhZ2VzIgASQwoWQ3Jk",
-          "dE5vdGlmaWNhdGlvblN0cmVhbRISLkNSRFRTdHJlYW1SZXF1ZXN0GhEuQ1JE",
-          "VE1hbnlNZXNzYWdlcyIAMAFiBnByb3RvMw=="));
+          "bmVfaWQYASABKAkSFAoMc2NlbmVfbnVtYmVyGAMgASgFMnAKC0NSRFRTZXJ2",
+          "aWNlEi4KCFNlbmRDcmR0EhEuQ1JEVE1hbnlNZXNzYWdlcxoNLkNSRFRSZXNw",
+          "b25zZSIAEjEKCFB1bGxDcmR0EhAuUHVsbENSRFRSZXF1ZXN0GhEuQ1JEVE1h",
+          "bnlNZXNzYWdlcyIAYgZwcm90bzM="));
     descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
         new pbr::FileDescriptor[] { },
         new pbr::GeneratedClrTypeInfo(null, null, new pbr::GeneratedClrTypeInfo[] {
           new pbr::GeneratedClrTypeInfo(typeof(global::CRDTManyMessages), global::CRDTManyMessages.Parser, new[]{ "SceneId", "Payload", "SceneNumber" }, null, null, null, null),
           new pbr::GeneratedClrTypeInfo(typeof(global::CRDTResponse), global::CRDTResponse.Parser, null, null, null, null, null),
-          new pbr::GeneratedClrTypeInfo(typeof(global::PullCRDTRequest), global::PullCRDTRequest.Parser, new[]{ "SceneId", "SceneNumber" }, null, null, null, null),
-          new pbr::GeneratedClrTypeInfo(typeof(global::CRDTStreamRequest), global::CRDTStreamRequest.Parser, null, null, null, null, null)
+          new pbr::GeneratedClrTypeInfo(typeof(global::PullCRDTRequest), global::PullCRDTRequest.Parser, new[]{ "SceneId", "SceneNumber" }, null, null, null, null)
         }));
   }
   #endregion
@@ -678,162 +675,6 @@ public sealed partial class PullCRDTRequest : pb::IMessage<PullCRDTRequest>
           SceneNumber = input.ReadInt32();
           break;
         }
-      }
-    }
-  }
-  #endif
-
-}
-
-/// <summary>
-/// CRDTStreamRequest is deprecated
-/// </summary>
-[global::System.ObsoleteAttribute]
-public sealed partial class CRDTStreamRequest : pb::IMessage<CRDTStreamRequest>
-#if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
-    , pb::IBufferMessage
-#endif
-{
-  private static readonly pb::MessageParser<CRDTStreamRequest> _parser = new pb::MessageParser<CRDTStreamRequest>(() => new CRDTStreamRequest());
-  private pb::UnknownFieldSet _unknownFields;
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public static pb::MessageParser<CRDTStreamRequest> Parser { get { return _parser; } }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public static pbr::MessageDescriptor Descriptor {
-    get { return global::ProtocolReflection.Descriptor.MessageTypes[3]; }
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  pbr::MessageDescriptor pb::IMessage.Descriptor {
-    get { return Descriptor; }
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public CRDTStreamRequest() {
-    OnConstruction();
-  }
-
-  partial void OnConstruction();
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public CRDTStreamRequest(CRDTStreamRequest other) : this() {
-    _unknownFields = pb::UnknownFieldSet.Clone(other._unknownFields);
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public CRDTStreamRequest Clone() {
-    return new CRDTStreamRequest(this);
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public override bool Equals(object other) {
-    return Equals(other as CRDTStreamRequest);
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public bool Equals(CRDTStreamRequest other) {
-    if (ReferenceEquals(other, null)) {
-      return false;
-    }
-    if (ReferenceEquals(other, this)) {
-      return true;
-    }
-    return Equals(_unknownFields, other._unknownFields);
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public override int GetHashCode() {
-    int hash = 1;
-    if (_unknownFields != null) {
-      hash ^= _unknownFields.GetHashCode();
-    }
-    return hash;
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public override string ToString() {
-    return pb::JsonFormatter.ToDiagnosticString(this);
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public void WriteTo(pb::CodedOutputStream output) {
-  #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
-    output.WriteRawMessage(this);
-  #else
-    if (_unknownFields != null) {
-      _unknownFields.WriteTo(output);
-    }
-  #endif
-  }
-
-  #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  void pb::IBufferMessage.InternalWriteTo(ref pb::WriteContext output) {
-    if (_unknownFields != null) {
-      _unknownFields.WriteTo(ref output);
-    }
-  }
-  #endif
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public int CalculateSize() {
-    int size = 0;
-    if (_unknownFields != null) {
-      size += _unknownFields.CalculateSize();
-    }
-    return size;
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public void MergeFrom(CRDTStreamRequest other) {
-    if (other == null) {
-      return;
-    }
-    _unknownFields = pb::UnknownFieldSet.MergeFrom(_unknownFields, other._unknownFields);
-  }
-
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  public void MergeFrom(pb::CodedInputStream input) {
-  #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
-    input.ReadRawMessage(this);
-  #else
-    uint tag;
-    while ((tag = input.ReadTag()) != 0) {
-      switch(tag) {
-        default:
-          _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, input);
-          break;
-      }
-    }
-  #endif
-  }
-
-  #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
-  [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-  [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
-  void pb::IBufferMessage.InternalMergeFrom(ref pb::ParseContext input) {
-    uint tag;
-    while ((tag = input.ReadTag()) != 0) {
-      switch(tag) {
-        default:
-          _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, ref input);
-          break;
       }
     }
   }
