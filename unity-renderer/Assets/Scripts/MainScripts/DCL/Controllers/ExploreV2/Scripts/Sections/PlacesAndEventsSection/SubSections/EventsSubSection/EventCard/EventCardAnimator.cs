0dde731909e9080d71e30c315a11fb51cc6c6889
Alejandro Jimenez <ajimenez@decentraland.org>
2022-10-31 13:20:28 +0100

    chore: link ondestroy calls

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExploreV2/Scripts/Sections/PlacesAndEventsSection/SubSections/EventsSubSection/EventCard/EventCardAnimator.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExploreV2/Scripts/Sections/PlacesAndEventsSection/SubSections/EventsSubSection/EventCard/EventCardAnimator.cs
@@ -9,16 +9,20 @@ public class EventCardAnimator : MonoBehaviour
     public void Focus()
     {
         cardShadow.DOFade(1, 0.1f)
-                  .SetEase(Ease.Linear);
+                  .SetEase(Ease.Linear)
+                  .SetLink(cardShadow.gameObject, LinkBehaviour.KillOnDestroy);
         infoTransform.DOAnchorPosY(5, 0.1f)
-                     .SetEase(Ease.InOutQuad);
+                     .SetEase(Ease.InOutQuad)
+                     .SetLink(infoTransform.gameObject, LinkBehaviour.KillOnDestroy);
     }
 
     public void Idle()
     {
         cardShadow.DOFade(0, 0.1f)
-                  .SetEase(Ease.Linear);
+                  .SetEase(Ease.Linear)
+                  .SetLink(cardShadow.gameObject, LinkBehaviour.KillOnDestroy);
         infoTransform.DOAnchorPosY(0, 0.1f)
-                     .SetEase(Ease.InOutQuad);
+                     .SetEase(Ease.InOutQuad)
+                     .SetLink(infoTransform.gameObject, LinkBehaviour.KillOnDestroy);
     }
 }
