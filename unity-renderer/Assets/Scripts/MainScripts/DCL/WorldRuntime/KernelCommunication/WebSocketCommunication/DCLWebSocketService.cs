2091d305d2ddef538f4e0f50d05e3db23dccdf6b
Kinerius <kinerius@gmail.com>
2022-10-31 18:55:32 -0300

    chore: change web socket message from Send to SendAsync

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/WebSocketCommunication/DCLWebSocketService.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/WebSocketCommunication/DCLWebSocketService.cs
@@ -33,8 +33,8 @@ public class DCLWebSocketService : WebSocketBehavior
         if (ConnectionState == WebSocketState.Open)
         {
             var serializeObject = JsonConvert.SerializeObject(x);
-            
-            Send(serializeObject);
+
+            SendAsync(serializeObject, b => { });
         
             if (VERBOSE)
             {
