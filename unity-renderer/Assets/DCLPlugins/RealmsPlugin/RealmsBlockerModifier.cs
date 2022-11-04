09afad82491802c7435fd76ce46a3c6374f081a7
Juan Molteni <juanignaciomolteni@gmail.com>
2022-11-04 11:14:06 -0300

    Added debug point

--- unity-renderer/Assets/DCLPlugins/RealmsPlugin/RealmsBlockerModifier.cs
+++ unity-renderer/Assets/DCLPlugins/RealmsPlugin/RealmsBlockerModifier.cs
@@ -1,13 +1,18 @@
 using DCL;
 using Decentraland.Bff;
+using UnityEngine;
 
 namespace DCLPlugins.RealmsPlugin
 {
     public class RealmsBlockerModifier : IRealmsModifier
     {
         
-        public void OnEnteredRealm(bool isCatalyst, AboutResponse realmConfiguration) =>
-            Environment.i.world.blockersController.SetEnabled(isCatalyst || ShouldGreenBlockersBeActive(realmConfiguration.Configurations.CityLoaderContentServer));    
+        public void OnEnteredRealm(bool isCatalyst, AboutResponse realmConfiguration)
+        { 
+            Environment.i.world.blockersController.SetEnabled(isCatalyst || ShouldGreenBlockersBeActive(realmConfiguration.Configurations.CityLoaderContentServer));
+            Debug.Log("AAAAAA " + isCatalyst);
+            Debug.Log("BBBBBB " + ShouldGreenBlockersBeActive(realmConfiguration.Configurations.CityLoaderContentServer));
+        }
      
         public void Dispose() { }
 
