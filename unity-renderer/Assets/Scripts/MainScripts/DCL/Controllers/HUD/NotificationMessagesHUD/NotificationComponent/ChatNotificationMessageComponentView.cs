44955837ebfa39c227fdf976a3bd6e8884f2ede0
Santi Andrade <sandrade@decentraland.org>
2022-11-09 19:26:37 -0300

    Update ChatNotificationMessageComponentView.cs

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NotificationMessagesHUD/NotificationComponent/ChatNotificationMessageComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NotificationMessagesHUD/NotificationComponent/ChatNotificationMessageComponentView.cs
@@ -106,7 +106,6 @@ public class ChatNotificationMessageComponentView : BaseComponentView, IChatNoti
     public void SetMessage(string message) 
     {
         model.message = notificationMessage.ReplaceUnsupportedCharacters(message, '?');
-        model.message = message;
         if (message.Length <= maxContentCharacters)
             notificationMessage.text = message;
         else
