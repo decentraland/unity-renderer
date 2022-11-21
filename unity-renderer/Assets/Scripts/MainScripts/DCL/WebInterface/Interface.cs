271590ed84b319445faf234cf0c9196e30190871
Kinerius <kinerius@gmail.com>
2022-11-21 17:03:25 -0300

    Removed unused function declaration

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WebInterface/Interface.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WebInterface/Interface.cs
@@ -864,7 +864,6 @@ namespace DCL.Interface
     [DllImport("__Internal")] public static extern void StartDecentraland();
     [DllImport("__Internal")] public static extern void MessageFromEngine(string type, string message);
     [DllImport("__Internal")] public static extern string GetGraphicCard();
-    [DllImport("__Internal")] public static extern void BinaryMessageFromEngine(int sceneNumber, byte[] bytes, int size);
 
     public static System.Action<string, string> OnMessageFromEngine;
 #else
