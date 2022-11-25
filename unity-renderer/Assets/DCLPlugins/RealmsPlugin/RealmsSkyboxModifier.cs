9e5dedc20e0a867b086d5ec7f06767f7698de5f3
Vitaly Popuzin <popuzin@gmail.com>
2022-11-25 16:48:27 +0100

    setup skybox hour based on realm values

--- unity-renderer/Assets/DCLPlugins/RealmsPlugin/RealmsSkyboxModifier.cs
+++ unity-renderer/Assets/DCLPlugins/RealmsPlugin/RealmsSkyboxModifier.cs
@@ -1,24 +1,23 @@
 ﻿using DCL;
 using Decentraland.Bff;
 using System;
-using UnityEngine;
 
 namespace DCLPlugins.RealmsPlugin
 {
     /// <summary>
     /// Align skybox with the realms settings. It allows the world creator to control skybox state.
     /// </summary>
-    public class RealmsSkyboxModifier: IRealmsModifier
+    public class RealmsSkyboxModifier : IRealmsModifier
     {
-        public void Dispose() { throw new NotImplementedException(); }
+        public void Dispose() { }
 
         public void OnEnteredRealm(bool isCatalyst, AboutResponse realmConfiguration)
         {
-
             if (realmConfiguration.Configurations.Skybox != null)
             {
-                TimeSpan time = TimeSpan.FromSeconds(realmConfiguration.Configurations.Skybox.FixedHour);
-                Debug.Log(time);                 // DataStore.i.skyboxConfig.fixedTime.Set(valueAsFloat);
+                DataStore.i.skyboxConfig.useDynamicSkybox.Set(false);
+                DataStore.i.skyboxConfig.fixedTime.Set(
+                    (float)TimeSpan.FromSeconds(realmConfiguration.Configurations.Skybox.FixedHour).TotalHours);
             }
         }
     }
