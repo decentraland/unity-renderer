49e9a30fdd8cf3ffa276f06dc7ada39e57e6bab2
Vitaly Popuzin <popuzin@gmail.com>
2022-11-25 13:54:56 +0100

    set SkyboxModifier plugin to read hour data from the realm

---
+++ unity-renderer/Assets/DCLPlugins/RealmsPlugin/RealmsSkyboxModifier.cs
@@ -0,0 +1,25 @@
+﻿using DCL;
+using Decentraland.Bff;
+using System;
+using UnityEngine;
+
+namespace DCLPlugins.RealmsPlugin
+{
+    /// <summary>
+    /// Align skybox with the realms settings. It allows the world creator to control skybox state.
+    /// </summary>
+    public class RealmsSkyboxModifier: IRealmsModifier
+    {
+        public void Dispose() { throw new NotImplementedException(); }
+
+        public void OnEnteredRealm(bool isCatalyst, AboutResponse realmConfiguration)
+        {
+
+            if (realmConfiguration.Configurations.Skybox != null)
+            {
+                TimeSpan time = TimeSpan.FromSeconds(realmConfiguration.Configurations.Skybox.FixedHour);
+                Debug.Log(time);                 // DataStore.i.skyboxConfig.fixedTime.Set(valueAsFloat);
+            }
+        }
+    }
+}
