7fb1de50bb1e3897d1613bc7fffa9a2100a8062f
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-11-28 18:02:12 -0300

    feat: ensure rpc

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/RPC.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/RPC.cs
@@ -1,6 +1,7 @@
 ﻿using Cysharp.Threading.Tasks;
 using DCL;
 using rpc_csharp;
+using System;
 
 namespace DCL
 {
@@ -8,15 +9,21 @@ namespace DCL
     {
         ClientEmotesKernelService IRPC.emotes { get; set; }
 
+        private readonly UniTaskCompletionSource modulesLoaded = new UniTaskCompletionSource();
+        public UniTask EnsureRpc() =>
+            modulesLoaded.Task;
+
         public static async UniTask LoadModules(RpcClientPort port, IRPC rpc)
         {
             rpc.emotes = new ClientEmotesKernelService(await port.LoadModule(EmotesKernelServiceCodeGen.ServiceName));
+
+            ((RPC)rpc).modulesLoaded.TrySetResult();
         }
 
         public void Initialize()
         {
         }
-        
+
         public void Dispose()
         {
         }
