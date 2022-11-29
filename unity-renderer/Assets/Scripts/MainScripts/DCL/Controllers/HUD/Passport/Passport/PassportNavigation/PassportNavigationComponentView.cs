a0eaef4a1857902c878a6c1391d4ed15dbca52d2
davidejensen <davidejensen@live.it>
2022-11-29 10:38:01 +0100

    Added tab system with standard section selector

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/PassportNavigationComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/PassportNavigationComponentView.cs
@@ -1,3 +1,4 @@
+using NSubstitute.ReturnsExtensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,30 @@ namespace DCL.Social.Passports
     public class PassportNavigationComponentView : BaseComponentView, IPassportNavigationComponentView
     {
         private const string GUEST_TEXT = "is a guest";
+        private const int ABOUT_SUB_SECTION_INDEX = 0;
+        private const int COLLECTIBLES_SUB_SECTION_INDEX = 1;
 
         [SerializeField] private GameObject aboutPanel;
         [SerializeField] private GameObject wearablesPanel;
+        [SerializeField] private SectionSelectorComponentView subSectionSelector;
         [SerializeField] private GameObject guestPanel;
         [SerializeField] private GameObject normalPanel;
         [SerializeField] private TextMeshProUGUI usernameText;
         [SerializeField] private TextMeshProUGUI descriptionText;
+
+        public override void Start()
+        {
+            subSectionSelector.GetSection(ABOUT_SUB_SECTION_INDEX).onSelect.RemoveAllListeners();
+            subSectionSelector.GetSection(COLLECTIBLES_SUB_SECTION_INDEX).onSelect.RemoveAllListeners();
+            subSectionSelector.GetSection(ABOUT_SUB_SECTION_INDEX).onSelect.AddListener((isActive) =>
+            {
+                aboutPanel.SetActive(isActive);
+            });
+            subSectionSelector.GetSection(COLLECTIBLES_SUB_SECTION_INDEX).onSelect.AddListener((isActive) =>
+            {
+                wearablesPanel.SetActive(isActive);
+            });
+        }
 
         public void SetGuestUser(bool isGuest)
         {
