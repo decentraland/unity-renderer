ea33071f69db92171e74cf0bb1e721ee3fe77ca2
Vitaly Popuzin <popuzin@gmail.com>
2022-11-29 14:32:33 +0100

    clean-up a code, simplified the logic

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SliderSettingsControlView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SliderSettingsControlView.cs
@@ -2,6 +2,7 @@ using System;
 using DCL.SettingsCommon.SettingsControllers.BaseControllers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace DCL.SettingsPanelHUD.Controls
@@ -23,6 +24,7 @@ namespace DCL.SettingsPanelHUD.Controls
         {
             sliderController = (SliderSettingsControlController)settingsControlController;
             sliderController.OnIndicatorLabelChange += OverrideIndicatorLabel;
+            sliderController.SliderValueChanged += OverrideSliderValue;
 
             this.sliderControlConfig = (SliderControlModel)controlConfig;
             slider.maxValue = this.sliderControlConfig.sliderMaxValue;
@@ -33,20 +35,34 @@ namespace DCL.SettingsPanelHUD.Controls
             OverrideIndicatorLabel(slider.value.ToString());
             sliderController.UpdateSetting(this.sliderControlConfig.storeValueAsNormalized ? RemapSliderValueTo01(slider.value) : slider.value);
 
-            slider.onValueChanged.AddListener(sliderValue =>
+            slider.onValueChanged.AddListener(OnSliderValueChanged());
+        }
+
+        private UnityAction<float> OnSliderValueChanged()
+        {
+            return sliderValue =>
             {
                 // https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
                 // FX = floating point with X precision digits
                 string stringFormat = sliderControlConfig.wholeNumbers ? "F0" : "F1";
 
-                OverrideIndicatorLabel( sliderValue.ToString(stringFormat) );
+                OverrideIndicatorLabel(sliderValue.ToString(stringFormat));
                 ApplySetting(this.sliderControlConfig.storeValueAsNormalized ? RemapSliderValueTo01(sliderValue) : sliderValue);
-            });
+            };
+        }
+
+        private void OverrideSliderValue(float newValue)
+        {
+            slider.onValueChanged.RemoveListener(OnSliderValueChanged());
+            slider.value = newValue;
+            slider.onValueChanged.AddListener(OnSliderValueChanged());
         }
 
         protected override void OnDestroy()
         {
             base.OnDestroy();
+
+            slider.onValueChanged.RemoveListener(OnSliderValueChanged());
 
             if (sliderController != null)
                 sliderController.OnIndicatorLabelChange -= OverrideIndicatorLabel;
@@ -56,13 +72,16 @@ namespace DCL.SettingsPanelHUD.Controls
         /// Overrides the text of the label associated to the slider.
         /// </summary>
         /// <param name="text">New label text.</param>
-        public void OverrideIndicatorLabel(string text) { indicatorLabel.text = text; }
+        public void OverrideIndicatorLabel(string text)
+        {
+            indicatorLabel.text = text;
+        }
 
         public override void RefreshControl()
         {
             base.RefreshControl();
 
-            float storedValue = Convert.ToSingle( sliderController.GetStoredValue() );
+            var storedValue = Convert.ToSingle(sliderController.GetStoredValue());
 
             float newValue = sliderControlConfig.storeValueAsNormalized ? RemapNormalizedValueToSlider(storedValue) : storedValue;
 
@@ -70,13 +89,12 @@ namespace DCL.SettingsPanelHUD.Controls
                 slider.value = newValue;
         }
 
-        private float RemapSliderValueTo01(float value)
-        {
-            return (value - slider.minValue)
-                / (slider.maxValue - slider.minValue)
-                * (1 - 0) + 0; //(value - from1) / (to1 - from1) * (to2 - from2) + from2
-        }
+        private float RemapSliderValueTo01(float value) =>
+            ((value - slider.minValue)
+             / (slider.maxValue - slider.minValue)
+             * (1 - 0)) + 0; //(value - from1) / (to1 - from1) * (to2 - from2) + from2
 
-        private float RemapNormalizedValueToSlider(float value) { return Mathf.Lerp(slider.minValue, slider.maxValue, value); }
+        private float RemapNormalizedValueToSlider(float value) =>
+            Mathf.Lerp(slider.minValue, slider.maxValue, value);
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/SkyboxTimeControlController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/SkyboxTimeControlController.cs
@@ -1,6 +1,4 @@
-using Cysharp.Threading.Tasks;
 using DCL.SettingsCommon.SettingsControllers.BaseControllers;
-using System.Threading;
 using UnityEngine;
 
 namespace DCL.SettingsControls
@@ -8,21 +6,10 @@ namespace DCL.SettingsControls
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Skybox Time", fileName = "SkyboxTime")]
     public class SkyboxTimeControlController : SliderSettingsControlController
     {
-        private const int CHECK_DELAY = 100; // in milliseconds
-
-        private CancellationTokenSource delayCancellation;
-
         public override void Initialize()
         {
             base.Initialize();
             UpdateSetting(currentGeneralSettings.skyboxTime);
-
-            if (DataStore.i.skyboxConfig.mode.Get() == SkyboxMode.Dynamic)
-            {
-                Debug.Log(0);
-                delayCancellation = new CancellationTokenSource();
-                CheckVirtualTimeAsync(CHECK_DELAY, delayCancellation.Token).Forget();
-            }
 
             DataStore.i.skyboxConfig.mode.OnChange += OnSkyboxModeChanged;
         }
@@ -31,39 +18,8 @@ namespace DCL.SettingsControls
         {
             if (current == previous) return;
 
-            switch (current)
-            {
-                case SkyboxMode.Dynamic:
-                    delayCancellation = new CancellationTokenSource();
-                    CheckVirtualTimeAsync(CHECK_DELAY, delayCancellation.Token).Forget();
-                    break;
-                case SkyboxMode.HoursFixedByWorld:
-                    delayCancellation.Cancel();
-                    delayCancellation.Dispose();
-                    RaiseSliderValueChanged(DataStore.i.skyboxConfig.fixedTime.Get());
-                    UpdateSetting(DataStore.i.skyboxConfig.fixedTime.Get());
-                    break;
-                case SkyboxMode.HoursFixedByUser:
-                    delayCancellation.Cancel();
-                    delayCancellation.Dispose();
-                    break;
-                default: break;
-            }
-        }
-
-        private async UniTask CheckVirtualTimeAsync(int delay, CancellationToken cancellationToken)
-        {
-            Debug.Log(1);
-
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                Debug.Log(2);
-                Debug.Log(DataStore.i.skyboxConfig.currentVirtualTime.Get());
-
-                RaiseSliderValueChanged(DataStore.i.skyboxConfig.fixedTime.Get());
-                UpdateSetting(DataStore.i.skyboxConfig.currentVirtualTime.Get());
-                await UniTask.Delay(delay, cancellationToken: cancellationToken);
-            }
+            RaiseSliderValueChanged(DataStore.i.skyboxConfig.fixedTime.Get());
+            UpdateSetting(DataStore.i.skyboxConfig.fixedTime.Get());
         }
 
         public override object GetStoredValue() =>
@@ -81,9 +37,6 @@ namespace DCL.SettingsControls
             var minuteSection = (int)((valueAsFloat - hourSection) * 60);
 
             RaiseOnIndicatorLabelChange($"{hourSection:00}:{minuteSection:00}");
-
-            Debug.Log(3);
-            Debug.Log(valueAsFloat);
 
             DataStore.i.skyboxConfig.fixedTime.Set(valueAsFloat);
         }
