a075b72e16fa39c857d50e0901c76c960aa9a115
Vitaly Popuzin <popuzin@gmail.com>
2022-11-30 13:09:11 +0100

    add validation test to QualitySettings being not modified

---
+++ unity-renderer/Assets/Scripts/Tests/ValidationTests/SettingAssetsValidationTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.ValidationTests
+{
+    public class SettingAssetsValidationTests
+    {
+        [Test]
+        public void ValidateAntiAliasingInQualitySettings() =>
+            Assert.That(QualitySettings.antiAliasing, Is.EqualTo(0));
+    }
+}
