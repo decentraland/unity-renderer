6724ca96f3021b603252e0756a846c57ade5bfb8
davidejensen <davidejensen@live.it>
2022-12-04 12:02:13 +0100

    Feat/passport about tab (#3614)
    * Feat: added about panel in new passport
    
    * Added tab system with standard section selector
    
    * Minor change
    
    * Minor changes and fixes
    
    * Added nft icon variation for equipped wearable
    
    * Added equipped wearables loading
    
    * Improvements to equipped wearables
    
    * Minor change
    
    * Minor improvements
    
    * Added wearable click to open marketplace event
    
    * Minor refactor based on code suggestions

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/IPassportNavigationComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/IPassportNavigationComponentView.cs
@@ -1,8 +1,14 @@
+using System;
+
 namespace DCL.Social.Passports
 {
     public interface IPassportNavigationComponentView
     {
+        event Action<string> OnClickBuyNft;
+        void InitializeView();
         void SetGuestUser(bool isGuest);
         void SetName(string username);
+        void SetDescription(string description);
+        void SetEquippedWearables(WearableItem[] wearables);
     }
 }
