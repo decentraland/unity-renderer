8488ab42db91d79d3cc02c2e0c1f30f67787cfaa
Nicolas Lorusso <nalorusso@gmail.com>
2022-12-07 12:57:48 -0300

    send friend request rpc

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/RPCFriendsApiBridge.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/RPCFriendsApiBridge.cs
@@ -16,46 +16,55 @@ namespace DCl.Social.Friends
             add => fallbackApiBridge.OnInitialized += value;
             remove => fallbackApiBridge.OnInitialized -= value;
         }
+
         public event Action<string> OnFriendNotFound
         {
             add => fallbackApiBridge.OnFriendNotFound += value;
             remove => fallbackApiBridge.OnFriendNotFound -= value;
         }
+
         public event Action<AddFriendsPayload> OnFriendsAdded
         {
             add => fallbackApiBridge.OnFriendsAdded += value;
             remove => fallbackApiBridge.OnFriendsAdded -= value;
         }
+
         public event Action<AddFriendRequestsPayload> OnFriendRequestsAdded
         {
             add => fallbackApiBridge.OnFriendRequestsAdded += value;
             remove => fallbackApiBridge.OnFriendRequestsAdded -= value;
         }
+
         public event Action<AddFriendsWithDirectMessagesPayload> OnFriendWithDirectMessagesAdded
         {
             add => fallbackApiBridge.OnFriendWithDirectMessagesAdded += value;
             remove => fallbackApiBridge.OnFriendWithDirectMessagesAdded -= value;
         }
+
         public event Action<UserStatus> OnUserPresenceUpdated
         {
             add => fallbackApiBridge.OnUserPresenceUpdated += value;
             remove => fallbackApiBridge.OnUserPresenceUpdated -= value;
         }
+
         public event Action<FriendshipUpdateStatusMessage> OnFriendshipStatusUpdated
         {
             add => fallbackApiBridge.OnFriendshipStatusUpdated += value;
             remove => fallbackApiBridge.OnFriendshipStatusUpdated -= value;
         }
+
         public event Action<UpdateTotalFriendRequestsPayload> OnTotalFriendRequestCountUpdated
         {
             add => fallbackApiBridge.OnTotalFriendRequestCountUpdated += value;
             remove => fallbackApiBridge.OnTotalFriendRequestCountUpdated -= value;
         }
+
         public event Action<UpdateTotalFriendsPayload> OnTotalFriendCountUpdated
         {
             add => fallbackApiBridge.OnTotalFriendCountUpdated += value;
             remove => fallbackApiBridge.OnTotalFriendCountUpdated -= value;
         }
+
         public event Action<FriendRequestPayload> OnFriendRequestAdded
         {
             add => fallbackApiBridge.OnFriendRequestAdded += value;
@@ -86,13 +95,13 @@ namespace DCl.Social.Friends
         public async UniTask<AddFriendRequestsV2Payload> GetFriendRequestsAsync(int sentLimit, int sentSkip, int receivedLimit, int receivedSkip)
         {
             GetFriendRequestsReply response = await rpc.FriendRequests()
-                .GetFriendRequests(new GetFriendRequestsPayload
-                {
-                    SentLimit = sentLimit,
-                    SentSkip = sentSkip,
-                    ReceivedLimit = receivedLimit,
-                    ReceivedSkip = receivedSkip
-                });
+                                                       .GetFriendRequests(new GetFriendRequestsPayload
+                                                        {
+                                                            SentLimit = sentLimit,
+                                                            SentSkip = sentSkip,
+                                                            ReceivedLimit = receivedLimit,
+                                                            ReceivedSkip = receivedSkip
+                                                        });
 
             return response.MessageCase == GetFriendRequestsReply.MessageOneofCase.Error
                 ? throw new FriendshipException(ToErrorCode(response.Error))
@@ -111,8 +120,22 @@ namespace DCl.Social.Friends
         public void RequestFriendship(string friendUserId) =>
             fallbackApiBridge.RequestFriendship(friendUserId);
 
-        public UniTask<RequestFriendshipConfirmationPayload> RequestFriendshipAsync(string userId, string messageBody) =>
-            fallbackApiBridge.RequestFriendshipAsync(userId, messageBody);
+        public async UniTask<RequestFriendshipConfirmationPayload> RequestFriendshipAsync(string userId, string messageBody)
+        {
+            SendFriendRequestReply reply = await rpc.FriendRequests()
+                                                    .SendFriendRequest(new SendFriendRequestPayload
+                                                     {
+                                                         MessageBody = messageBody,
+                                                         UserId = userId,
+                                                     });
+
+            return reply.MessageCase == SendFriendRequestReply.MessageOneofCase.Reply
+                ? new RequestFriendshipConfirmationPayload
+                {
+                    friendRequest = ToFriendRequestPayload(reply.Reply.FriendRequest),
+                }
+                : throw new FriendshipException(ToErrorCode(reply.Error));
+        }
 
         UniTask<CancelFriendshipConfirmationPayload> IFriendsApiBridge.CancelRequestAsync(string friendRequestId) =>
             fallbackApiBridge.CancelRequestAsync(friendRequestId);
@@ -133,7 +156,7 @@ namespace DCl.Social.Friends
             new FriendRequestPayload
             {
                 from = request.From,
-                timestamp = (long) request.Timestamp,
+                timestamp = (long)request.Timestamp,
                 to = request.To,
                 messageBody = request.MessageBody,
                 friendRequestId = request.FriendRequestId
