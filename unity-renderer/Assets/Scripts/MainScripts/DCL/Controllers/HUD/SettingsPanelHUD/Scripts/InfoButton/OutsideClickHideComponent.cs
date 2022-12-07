3418ccf3488bdffd2950ad9ab536e932662d8474
Vitaly Popuzin <popuzin@gmail.com>
2022-12-07 22:41:14 +0100

    created behaviour to hide on click outside

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/InfoButton/OutsideClickHideComponent.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DCL.Controllers.HUD.SettingsPanelHUD
+{
+    /// <summary>
+    /// Hide gameObject, when user clicks outside of it
+    /// </summary>
+    public class OutsideClickHideComponent : MonoBehaviour, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        private bool mouseIsOver;
+
+        private void OnEnable()
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            if (!mouseIsOver)
+                gameObject.SetActive(false);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            mouseIsOver = true;
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            mouseIsOver = false;
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
+}
