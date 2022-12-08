384bb98ecce55904f3b74f83eab14436cfe5ecb7
Vitaly Popuzin <popuzin@gmail.com>
2022-12-08 11:54:22 +0100

    add auto-size of tooltip background to text

--- unity-renderer/Assets/UIComponents/Scripts/Components/Tooltip/TooltipComponentView.cs
+++ unity-renderer/Assets/UIComponents/Scripts/Components/Tooltip/TooltipComponentView.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 namespace UIComponents.Scripts.Components.Tooltip
 {
     public class TooltipComponentView : BaseComponentView<TooltipComponentModel>
     {
+        private const int OFFSET = 16;
+
         [SerializeField] private TMP_Text text;
+
+        private RectTransform rectTransform;
+        private RectTransform rect => rectTransform ??= GetComponent<RectTransform>();
 
         public override void Show(bool instant = false)
         {
@@ -17,6 +21,13 @@ namespace UIComponents.Scripts.Components.Tooltip
         public override void RefreshControl()
         {
             SetText(model.message);
+            AdjustTooltipSizeToText();
+
+            void AdjustTooltipSizeToText()
+            {
+                rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, text.preferredWidth + (OFFSET * 2));
+                rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, text.preferredHeight + (OFFSET * 2));
+            }
         }
 
         private void SetText(string newText)
