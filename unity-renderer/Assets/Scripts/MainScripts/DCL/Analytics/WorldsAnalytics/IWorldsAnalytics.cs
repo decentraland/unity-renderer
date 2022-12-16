f9a1618fd470603a7642d5b0d698b595c64fe2fc
Juan Molteni <juanignaciomolteni@gmail.com>
2022-12-16 17:33:51 -0300

    Code cleaning

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Analytics/WorldsAnalytics/IWorldsAnalytics.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Analytics/WorldsAnalytics/IWorldsAnalytics.cs
@@ -2,12 +2,6 @@ namespace WorldsFeaturesAnalytics
 {
     public interface IWorldsAnalytics
     {
-        /*void SendPlayerEnteredWorld(string worldName, WorldAccessType accessType);
-
-        void SendPlayerLeavesWorld(string worldName, double sessionTimeInSeconds, ExitSourceType exitSourceType);
-
-        void SendPlayerUnableToAccessWorld(string worldName, UnableToAccessWorldReasonType unableToAccessReasonType);*/
-
         void OnEnteredRealm(bool isWorld, string newRealmName);
     }
 }
