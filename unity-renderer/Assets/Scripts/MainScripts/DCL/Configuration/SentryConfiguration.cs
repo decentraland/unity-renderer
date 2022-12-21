dde8294787c4bebe3a570ffdc231b54b2202fec7
Ashley Canning <ashley.canning@gmail.com>
2022-12-21 17:36:46 +0000

    - Add Env/Version/DSN

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Configuration/SentryConfiguration.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Configuration/SentryConfiguration.cs
@@ -6,5 +6,7 @@ namespace DCL.Configuration
     public static class SentryConfiguration
     {
         public static string Environment = "editor";
+        public static string Version = "";
+        public static string DSN = "";
     }
 }
