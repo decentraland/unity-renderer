a82df3b2415cc5975e8a1fa64b1a9ae81fdc3f0b
Ashley Canning <ashley.canning@gmail.com>
2022-12-30 10:46:38 +0000

    enable stack trace uploading

--- inject-params.js
+++ inject-params.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 if(!process.env.CIRCLE_BRANCH) throw new Error('env var CIRCLE_BRANCH not set!')
 if(!process.env.CIRCLE_SHA1) throw new Error('env var CIRCLE_SHA1 not set!')
 if(!process.env.SENTRY_DSN) throw new Error('env var SENTRY_DSN not set!')
+if(!process.env.SENTRY_AUTH_TOKEN) throw new Error('env var SENTRY_AUTH not set!')
+
+// Inject sentry environment parameters
 
 const fileToEdit = "unity-renderer/Assets/Resources/Sentry/SentryOptions.asset"
 const fileContents = fs.readFileSync(fileToEdit).toString()
@@ -14,8 +17,9 @@ if(process.env.CIRCLE_BRANCH == 'main') {
 } else if(process.env.CIRCLE_BRANCH == 'dev') {
   rendererEnvironment = 'development'
 } else {
-  rendererEnvironment = `branch/${process.env.CIRCLE_BRANCH || 'unknown'}`
+  rendererEnvironment = `branch`
 }
+
 
 const searchParams = {
   dsn: 'https://dsn.dsn/',
@@ -31,4 +35,21 @@ fs.writeFileSync(fileToEdit, fileContents
   .replace(searchParams.dsn, process.env.SENTRY_DSN)
   .replace(searchParams.env, rendererEnvironment)
   .replace(searchParams.release, process.env.CIRCLE_SHA1)
+)
+
+
+// Inject CLI parameters for sources/symbols
+const cliSearchParams = {
+  auth: '<AUTH_TOKEN>',
+}
+
+const cliFileToEdit = "unity-renderer/Assets/Plugins/Sentry/SentryCliOptions.asset"
+const cliFileContents = fs.readFileSync(cliFileToEdit).toString()
+
+for (const key in cliSearchParams) {
+  if (!cliFileContents.includes(cliSearchParams[key])) throw new Error(`the file ${cliFileToEdit} does not contain any ${cliSearchParams[key]} string to replace`)
+}
+
+fs.writeFileSync(cliFileToEdit, cliFileContents
+  .replace(cliSearchParams.auth, process.env.SENTRY_AUTH_TOKEN)
 )
