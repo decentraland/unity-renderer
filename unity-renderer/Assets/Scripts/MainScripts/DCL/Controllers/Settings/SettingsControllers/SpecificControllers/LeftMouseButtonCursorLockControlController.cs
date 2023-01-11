da6619ec7c20970c523ec81cd56221761295942c
Vitaly Popuzin <popuzin@gmail.com>
2023-01-11 15:44:17 +0100

    add UI in the settings section to switch between 2 CursorLock modes

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/SpecificControllers/LeftMouseButtonCursorLockControlController.cs
@@ -0,0 +1,19 @@
+﻿using DCL.SettingsCommon.SettingsControllers.BaseControllers;
+using UnityEngine;
+
+namespace DCL.SettingsCommon.SettingsControllers.SpecificControllers
+{
+    [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Left Mouse Button Cursor Lock", fileName = "LeftMouseButtonCursorLockControlController")]
+    public class LeftMouseButtonCursorLockControlController : ToggleSettingsControlController
+    {
+        public override object GetStoredValue() =>
+            currentGeneralSettings.leftMouseButtonCursorLock;
+
+        public override void UpdateSetting(object newValue)
+        {
+            currentGeneralSettings.leftMouseButtonCursorLock = (bool)newValue;
+            DataStore.i.camera.leftMouseButtonCursorLock.Set((bool)newValue);
+            ApplySettings();
+        }
+    }
+}
