33e387e0574dd711e5604358f80464d5610530ce
Leandro Mendoza <leandro@decentraland.org>
2023-01-12 09:49:22 -0300

    fix test

--- unity-renderer/Assets/DCLPlugins/ECS7/Systems/PointerInputSystem/Tests/ECSPointerInputSystemShould.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/Systems/PointerInputSystem/Tests/ECSPointerInputSystemShould.cs
@@ -275,8 +275,6 @@ namespace Tests
         [Test]
         public void ShowPointerDownHoverTooltip()
         {
-            dataStoreEcs7.buttonState[0] = true;
-
             dataStoreEcs7.lastPointerRayHit.didHit = true;
             dataStoreEcs7.lastPointerRayHit.hit.collider = colliderEntity1;
             dataStoreEcs7.lastPointerRayHit.hasValue = true;
