829857eb7be865f4abaac8561f74e1ad0d7f6f60
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2023-01-12 08:04:04 -0300

    hotfix: names & lands repetitive requests when the passport is opened (#3976)
    * fix names & lands repetitive requests: check equality before trigger the avatar profile update
    
    * fixed test

--- unity-renderer/Assets/Scripts/MainScripts/DCL/UserProfile/UserProfileModel.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/UserProfile/UserProfileModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 [System.Serializable]
 public class UserProfileModel
@@ -8,6 +9,14 @@ public class UserProfileModel
     {
         public string face256;
         public string body;
+
+        public bool Equals(Snapshots snapshots)
+        {
+            if (snapshots == null) return false;
+            if (snapshots.face256 != face256) return false;
+            if (snapshots.body != body) return false;
+            return true;
+        }
     }
 
     [System.Serializable]
@@ -22,6 +31,15 @@ public class UserProfileModel
         {
             OWNER = 0,
             OPERATOR = 1
+        }
+
+        public bool Equals(ParcelsWithAccess parcelsWithAccess)
+        {
+            if (parcelsWithAccess == null) return false;
+            if (parcelsWithAccess.x != x) return false;
+            if (parcelsWithAccess.y != y) return false;
+            if (parcelsWithAccess.landRole != landRole) return false;
+            return true;
         }
     }
 
@@ -47,6 +65,53 @@ public class UserProfileModel
     public int tutorialStep;
     public bool hasClaimedName = false;
 
+    public bool Equals(UserProfileModel model)
+    {
+        if (model == null) return false;
+        if (model.userId != userId) return false;
+        if (model.ethAddress != ethAddress) return false;
+        if (model.name != name) return false;
+        if (model.email != email) return false;
+        if (model.description != description) return false;
+        if (model.baseUrl != baseUrl) return false;
+        if (model.created_at != created_at) return false;
+        if (model.updated_at != updated_at) return false;
+        if (model.version != version) return false;
+        if (!model.avatar.Equals(avatar)) return false;
+
+        if (model.parcelsWithAccess != null)
+        {
+            if (!model.parcelsWithAccess.Equals(parcelsWithAccess))
+                return false;
+        }
+        else if (parcelsWithAccess != null)
+            return false;
+
+        if (model.hasConnectedWeb3 != hasConnectedWeb3) return false;
+        if (model.tutorialFlagsMask != tutorialFlagsMask) return false;
+        if (model.tutorialFlagsMask != tutorialFlagsMask) return false;
+
+        if (model.blocked != null && blocked != null)
+        {
+            if (!model.blocked.All(blocked.Contains) || model.blocked.Count != blocked.Count)
+                return false;
+        }
+        else if (model.blocked != null || blocked != null)
+            return false;
+
+        if (model.muted != null && muted != null)
+        {
+            if (!model.muted.All(muted.Contains) || model.muted.Count != muted.Count)
+                return false;
+        }
+        else if (model.muted != null || muted != null)
+            return false;
+
+        if (model.tutorialStep != tutorialStep) return false;
+        if (model.hasClaimedName != hasClaimedName) return false;
+
+        return true;
+    }
 
     public string ComposeCorrectUrl(string url)
     {
