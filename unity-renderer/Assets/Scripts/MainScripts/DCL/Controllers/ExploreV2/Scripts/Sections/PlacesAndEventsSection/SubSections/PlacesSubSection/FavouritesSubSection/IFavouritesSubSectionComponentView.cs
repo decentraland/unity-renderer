7524685f4636d07e06d4c152a714e11c8838a36e
davidejensen <davidejensen@live.it>
2023-01-20 11:02:45 +0100

    Feat: add new favourites section in explore

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExploreV2/Scripts/Sections/PlacesAndEventsSection/SubSections/PlacesSubSection/FavouritesSubSection/IFavouritesSubSectionComponentView.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFavouritesSubSectionComponentView : IPlacesAndEventsSubSectionComponentView
+{
+    /// <summary>
+    /// Colors used for the background of the friends heads.
+    /// </summary>
+    Color[] currentFriendColors { get; }
+
+    /// <summary>
+    /// Number of places per row that fit with the current places grid configuration.
+    /// </summary>
+    int currentPlacesPerRow { get; }
+
+    /// <summary>
+    /// It will be triggered when all the UI components have been fully initialized.
+    /// </summary>
+    event Action OnReady;
+
+    /// <summary>
+    /// It will be triggered when the info button is clicked.
+    /// </summary>
+    event Action<PlaceCardComponentModel> OnInfoClicked;
+
+    /// <summary>
+    /// It will be triggered when the JumpIn button is clicked.
+    /// </summary>
+    event Action<HotScenesController.HotSceneInfo> OnJumpInClicked;
+
+    /// <summary>
+    /// It will be triggered when a new friend handler is added by a place card.
+    /// </summary>
+    event Action<FriendsHandler> OnFriendHandlerAdded;
+
+    /// <summary>
+    /// It will be triggered each time the view is enabled.
+    /// </summary>
+    event Action OnFavouriteSubSectionEnable;
+
+    /// <summary>
+    /// It will be triggered when the "Show More" button is clicked.
+    /// </summary>
+    event Action OnShowMoreFavouritesClicked;
+
+    /// <summary>
+    /// Set the places component with a list of places.
+    /// </summary>
+    /// <param name="places">List of places (model) to be loaded.</param>
+    void SetFavourites(List<PlaceCardComponentModel> places);
+
+    /// <summary>
+    /// Add a list of places in the places component.
+    /// </summary>
+    /// <param name="places">List of places (model) to be added.</param>
+    void AddFavourites(List<PlaceCardComponentModel> places);
+
+    /// <summary>
+    /// Set the places component in loading mode.
+    /// </summary>
+    /// <param name="isVisible">True for activating the loading mode.</param>
+    void SetFavouritesAsLoading(bool isVisible);
+
+    /// <summary>
+    /// Activates/Deactivates the "Show More" button.
+    /// </summary>
+    /// <param name="isActive">True for activating it.</param>
+    void SetShowMoreFavouritesButtonActive(bool isActive);
+
+    /// <summary>
+    /// Shows the Place Card modal with the provided information.
+    /// </summary>
+    /// <param name="placeInfo">Place (model) to be loaded in the card.</param>
+    void ShowPlaceModal(PlaceCardComponentModel placeInfo);
+
+    /// <summary>
+    /// Hides the Place Card modal.
+    /// </summary>
+    void HidePlaceModal();
+
+    /// <summary>
+    /// Configure the needed pools for the places instantiation.
+    /// </summary>
+    void ConfigurePools();
+
+    void SetShowMoreButtonActive(bool isActive);
+}
