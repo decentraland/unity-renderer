7f21c85e03d3097efa91ef5414d50835378fe785
Juan Molteni <juanignaciomolteni@gmail.com>
2023-01-23 01:58:59 -0300

    Added build schema to addrassable

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Font/FontLoaderAddressable/FontLoaderAddressable.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Font/FontLoaderAddressable/FontLoaderAddressable.cs
@@ -8,28 +8,12 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class FontLoaderAddressable : MonoBehaviour
 {
 
+    public List<AssetReferenceT<TMP_FontAsset>> testList;
+
     private void Start()
     {
-        Addressables.InitializeAsync().Completed += AddressablesInitiated;
     }
 
-    private void AddressablesInitiated(AsyncOperationHandle<IResourceLocator> obj)
-    {
-        Addressables.LoadAssetsAsync<TMP_FontAsset>("fonts", FontLoaded);
-    }
-
-    private void FontLoaded(TMP_FontAsset obj)
-    {
-        List<TMP_FontAsset> fallbackFontAssets = TMP_Settings.fallbackFontAssets;
-
-        if (fallbackFontAssets == null) { fallbackFontAssets = new List<TMP_FontAsset>(); }
-
-        fallbackFontAssets.Add(obj);
-
-        TMP_Settings.fallbackFontAssets = fallbackFontAssets;
-
-        Debug.Log($"Font {obj.name} loaded succesfully");
-    }
 
 }
 
