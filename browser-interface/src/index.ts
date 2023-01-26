c3a30bc90c15df6d40fabcab281e67fd40228a19
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2023-01-26 12:37:27 -0300

    Rollback on index.ts
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- browser-interface/src/index.ts
+++ browser-interface/src/index.ts
@@ -94,7 +94,7 @@ export async function initializeWebRenderer(options: RendererOptions): Promise<D
     dataUrl: resolveWithBaseUrl(generatedFiles.dataUrl + postfix),
     frameworkUrl: resolveWithBaseUrl(generatedFiles.frameworkUrl + postfix),
     codeUrl: resolveWithBaseUrl(generatedFiles.codeUrl + postfix),
-    streamingAssetsUrl: "StreamingAssets",
+    streamingAssetsUrl: resolveWithBaseUrl("StreamingAssets"),
     companyName: "Decentraland",
     productName: "Decentraland World Client",
     productVersion: "0.1",
