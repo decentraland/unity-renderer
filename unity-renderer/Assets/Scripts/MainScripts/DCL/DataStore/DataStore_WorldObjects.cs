0ea8e87e1a05295dfffc655e08721ae478b9795f
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-01-26 11:08:37 +0000

    deleting useless comment

--- unity-renderer/Assets/Scripts/MainScripts/DCL/DataStore/DataStore_WorldObjects.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/DataStore/DataStore_WorldObjects.cs
@@ -12,7 +12,7 @@ namespace DCL
             public readonly BaseRefCounter<Material> materials = new BaseRefCounter<Material>();
             public readonly BaseRefCounter<Texture> textures = new BaseRefCounter<Texture>();
             public readonly BaseHashSet<long> owners = new BaseHashSet<long>();
-            public readonly BaseHashSet<Renderer> renderers = new BaseHashSet<Renderer>(); // FD:: test caching renderers
+            public readonly BaseHashSet<Renderer> renderers = new BaseHashSet<Renderer>();
             public readonly BaseHashSet<AudioClip> audioClips = new BaseHashSet<AudioClip>();
             public readonly BaseRefCounter<AnimationClip> animationClips = new BaseRefCounter<AnimationClip>();
 
