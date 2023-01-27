5c078722e9286cc3a61e274a634aeaa7b3f22f76
Nicolas Lorusso <nalorusso@gmail.com>
2023-01-27 17:58:37 -0300

    implemented test coverage

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ConfirmationPopup/ConfirmationPopupHUDComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ConfirmationPopup/ConfirmationPopupHUDComponentView.cs
@@ -7,12 +7,12 @@ namespace DCL.ConfirmationPopup
 {
     public class ConfirmationPopupHUDComponentView : BaseComponentView<ConfirmationPopupHUDViewModel>, IConfirmationPopupHUDView
     {
-        [SerializeField] private TMP_Text titleLabel;
-        [SerializeField] private TMP_Text bodyLabel;
-        [SerializeField] private TMP_Text confirmLabel;
-        [SerializeField] private TMP_Text cancelLabel;
-        [SerializeField] private ButtonComponentView confirmButton;
-        [SerializeField] private ButtonComponentView cancelButton;
+        [SerializeField] internal TMP_Text titleLabel;
+        [SerializeField] internal TMP_Text bodyLabel;
+        [SerializeField] internal TMP_Text confirmLabel;
+        [SerializeField] internal TMP_Text cancelLabel;
+        [SerializeField] internal ButtonComponentView confirmButton;
+        [SerializeField] internal ButtonComponentView cancelButton;
 
         public event Action OnConfirm;
         public event Action OnCancel;
