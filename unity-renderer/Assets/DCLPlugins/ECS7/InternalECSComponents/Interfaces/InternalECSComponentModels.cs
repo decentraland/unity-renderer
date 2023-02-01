30a5cb33b7c63423942bb10e18dbb2317d5da0ca
Leandro Mendoza <leandro@decentraland.org>
2023-02-01 09:23:14 -0300

    feat: (sdk7) move pointer event results into multiple entities

--- unity-renderer/Assets/DCLPlugins/ECS7/InternalECSComponents/Interfaces/InternalECSComponentModels.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/InternalECSComponents/Interfaces/InternalECSComponentModels.cs
@@ -51,6 +51,7 @@ namespace DCL.ECS7.InternalComponents
 
         public Queue<EventData> events;
         public int lastTimestamp;
+        public long lastEntity;
     }
 
     public class InternalUiContainer : InternalComponent
