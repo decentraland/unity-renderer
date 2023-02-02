7f25226d634d2b9a3d262630481291e0beab9fdd
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-02-02 20:30:33 -0300

    fix

--- unity-renderer/Assets/DCLPlugins/ECS7/Systems/VideoPlayerSystem/Tests/ECSVideoPlayerShould.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/Systems/VideoPlayerSystem/Tests/ECSVideoPlayerShould.cs
@@ -73,20 +73,20 @@ namespace Tests
 
             videoMaterialComponent.PutFor(scene0, entity00, new InternalVideoMaterial()
             {
-                material = new Material(Shader.Find("Standard")) { mainTexture = new Texture2D(1, 1), },
+                material = new Material(Shader.Find("DCL/Universal Render Pipeline/Lit")) { mainTexture = new Texture2D(1, 1), },
                 videoTextureDatas = new List<InternalVideoMaterial.VideoTextureData>() { new InternalVideoMaterial.VideoTextureData(100, ShaderUtils.BaseMap) },
             });
 
             videoMaterialComponent.PutFor(scene0, entity01, new InternalVideoMaterial()
             {
-                material = new Material(Shader.Find("Standard")) { mainTexture = new Texture2D(1, 1), },
+                material = new Material(Shader.Find("DCL/Universal Render Pipeline/Lit")) { mainTexture = new Texture2D(1, 1), },
                 videoTextureDatas = new List<InternalVideoMaterial.VideoTextureData>() { new InternalVideoMaterial.VideoTextureData(100, ShaderUtils.BaseMap) },
             });
 
             // Texture should not change on this
             videoMaterialComponent.PutFor(scene1, entity10, new InternalVideoMaterial()
             {
-                material = new Material(Shader.Find("Standard")) { mainTexture = new Texture2D(1, 1), },
+                material = new Material(Shader.Find("DCL/Universal Render Pipeline/Lit")) { mainTexture = new Texture2D(1, 1), },
                 videoTextureDatas = new List<InternalVideoMaterial.VideoTextureData>() { new InternalVideoMaterial.VideoTextureData(200, ShaderUtils.BaseMap) },
             });
 
