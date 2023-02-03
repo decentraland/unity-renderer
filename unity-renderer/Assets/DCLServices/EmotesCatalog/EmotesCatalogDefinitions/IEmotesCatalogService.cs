a991d2eaa53549978b052670a45b44d53b3704f7
Juan Molteni <juanignaciomolteni@gmail.com>
2023-02-03 15:02:29 -0300

    Code cleanup

--- unity-renderer/Assets/DCLServices/EmotesCatalog/EmotesCatalogDefinitions/IEmotesCatalogService.cs
+++ unity-renderer/Assets/DCLServices/EmotesCatalog/EmotesCatalogDefinitions/IEmotesCatalogService.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using DCL;
 using DCL.Emotes;
 using DCL.Helpers;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
 
 public interface IEmotesCatalogService : IService
 {
     bool TryGetLoadedEmote(string id, out WearableItem emote);
 
     Promise<WearableItem[]> RequestOwnedEmotes(string userId);
+
     UniTask<WearableItem[]> RequestOwnedEmotesAsync(string userId, CancellationToken ct = default);
 
     Promise<WearableItem> RequestEmote(string id);
+
     List<Promise<WearableItem>> RequestEmotes(IList<string> ids);
+
     UniTask<WearableItem> RequestEmoteAsync(string id, CancellationToken ct = default);
+
     UniTask<WearableItem[]> RequestEmotesAsync(IList<string> ids, CancellationToken ct = default);
 
     UniTask<EmbeddedEmotesSO> GetEmbeddedEmotes();
+
     void ForgetEmote(string id);
+
     void ForgetEmotes(IList<string> ids);
 }
