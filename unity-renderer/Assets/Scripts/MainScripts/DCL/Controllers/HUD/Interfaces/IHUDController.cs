c06381e6221198e52e75c4975b3d9c723a914a85
Vitaly Popuzin <popuzin@gmail.com>
2023-02-06 16:04:15 +0100

    fixed failing tests

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Interfaces/IHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Interfaces/IHUDController.cs
@@ -1,10 +1,10 @@
-using System;
+using Cysharp.Threading.Tasks;
 
 namespace DCL
 {
     public interface IHUDController : IService
     {
-        void ConfigureHUDElement(HUDElementID hudElementId, HUDConfiguration configuration, string extraPayload = null);
+        UniTask ConfigureHUDElement(HUDElementID hudElementId, HUDConfiguration configuration, string extraPayload = null);
         IHUD GetHUDElement(HUDElementID id);
         void Cleanup();
     }
