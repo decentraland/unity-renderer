3192815a9567e22d23a332a54951e25a9c14598a
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-02-07 18:52:56 +0000

    Fixed diffuse color

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/Defaults/PBMaterial_Defaults.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/Defaults/PBMaterial_Defaults.cs
@@ -27,7 +27,13 @@ namespace DCL.ECSComponents
             return true;
         }
 
-        public static Color4 GetDiffuseColor(this PBMaterial self) => GetAlbedoColor(self);
+        public static Color4 GetDiffuseColor(this PBMaterial self)
+        {
+            if (self.Unlit != null)
+                return self.Unlit.DiffuseColor ?? new Color4(Color_Defaults.color4White);
+
+            return new Color4(Color_Defaults.color4White);
+        }
 
         public static Color4 GetAlbedoColor(this PBMaterial self)
         {
