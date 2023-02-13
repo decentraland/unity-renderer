6ede6a0939ba6323d4e354008ac8d3bf6e825a9a
davidejensen <davidejensen@live.it>
2023-02-13 09:16:40 +0100

    Added pool for nft elements

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/ViewAllComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/ViewAllComponentView.cs
@@ -1,4 +1,6 @@
 using System;
+using DCL;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,14 +8,20 @@ using UnityEngine.UI;
 public class ViewAllComponentView : BaseComponentView, IViewAllComponentView
 {
     private const int ELEMENTS_PER_PAGE = 20;
+    private const string NFT_ELEMENTS_POOL_NAME_PREFIX = "NFTElementsEntriesPool_";
 
     [SerializeField] private TMP_Text sectionName;
     [SerializeField] private ButtonComponentView backButton;
     [SerializeField] private Transform itemsContainer;
     [SerializeField] private UIPageSelector pageSelector;
+    [SerializeField] private GameObject nftPageElement;
 
     public event Action OnBackFromViewAll;
     public event Action<int, int> OnRequestCollectibleElements;
+
+
+    private List<PoolableObject> nftElementsPoolableQueue = new List<PoolableObject>();
+    private Pool nftElementsEntryPool;
 
     public override void Start()
     {
@@ -25,6 +33,7 @@ public class ViewAllComponentView : BaseComponentView, IViewAllComponentView
     public void Initialize(int totalCollectiblesElements)
     {
         pageSelector.Setup(totalCollectiblesElements/ELEMENTS_PER_PAGE, false);
+        nftElementsEntryPool = GetNftElementsEntryPool();
     }
 
     private void RequestPage(int pageNumber)
@@ -44,5 +53,20 @@ public class ViewAllComponentView : BaseComponentView, IViewAllComponentView
     public void SetVisible(bool isVisible)
     {
         gameObject.SetActive(isVisible);
+    }
+
+    private Pool GetNftElementsEntryPool()
+    {
+        var pool = PoolManager.i.GetPool(NFT_ELEMENTS_POOL_NAME_PREFIX + name + GetInstanceID());
+        if (pool != null) return pool;
+
+        pool = PoolManager.i.AddPool(
+            NFT_ELEMENTS_POOL_NAME_PREFIX + name + GetInstanceID(),
+            Instantiate(nftPageElement).gameObject,
+            maxPrewarmCount: ELEMENTS_PER_PAGE,
+            isPersistent: true);
+
+        pool.ForcePrewarm();
+        return pool;
     }
 }
