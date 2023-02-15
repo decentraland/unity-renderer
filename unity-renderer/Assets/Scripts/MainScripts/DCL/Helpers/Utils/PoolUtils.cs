63e42e37234ff5684ea994d624e1142d0e94d76a
Alejandro Jimenez <ajimenez@decentraland.org>
2023-02-15 14:10:00 +0100

    feat: extension methods to prewarm pools

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Helpers/Utils/PoolUtils.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Helpers/Utils/PoolUtils.cs
@@ -25,6 +25,44 @@ namespace MainScripts.DCL.Helpers.Utils
             }
         }
 
+        /// <summary>
+        /// This method uses the Create function provided to the pool on construction
+        /// to generate instances. Therefore some allocation cannot be avoided.
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <param name="count"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Prewarm<T>(this ObjectPool<T> pool, int count) where T: class
+        {
+            T[] instances = new T[count];
+
+            for (int i = 0; i < count; i++) { instances[i] = pool.Get(); }
+
+            for (int i = 0; i < count; i++) { pool.Release(instances[i]); }
+        }
+
+        /// <summary>
+        /// By providing the Create function, this method can Prewarm a pool without extra allocation.
+        /// Watchout for inconsistencies between the pool's create method and the one provided
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <param name="builder"></param>
+        /// <param name="count"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Prewarm<T>(this ObjectPool<T> pool, Func<T> builder, int count) where T: class
+        {
+            for (int i = 0; i < count; i++) { pool.Release(builder()); }
+        }
+
+        public static ObjectPool<T> CreatePool<T>(int prewarmCount, Func<T> createFunc) where T: class
+        {
+            var pool = new ObjectPool<T>(createFunc, defaultCapacity: prewarmCount);
+
+            for (var i = 0; i < prewarmCount; i++) { pool.Release(createFunc()); }
+
+            return pool;
+        }
+
         public static ListPoolRent<T> RentList<T>() =>
             new ListPoolRent<T>(ListPool<T>.Get());
     }
