7505273446094ded03946e5baf725b6daa04d6d3
Esteban Ordano <esteban@decentraland.org>
2023-02-16 11:21:23 +0000

    chore: drop unused imports

--- browser-interface/packages/shared/loadingScreen/sagas.ts
+++ browser-interface/packages/shared/loadingScreen/sagas.ts
@@ -1,19 +1,10 @@
-﻿import { call, put, select, takeLatest } from 'redux-saga/effects'
-import { UPDATE_LOADING_SCREEN, updateLoadingScreen } from './actions'
-import { waitForRendererInstance } from '../renderer/sagas-helper'
-import { getParcelLoadingStarted, isLoadingScreenVisible } from './selectors'
-import { LoadingState } from '../loading/reducer'
-import { getLoadingState } from '../loading/selectors'
-import { RootState } from '../store/rootTypes'
-import { getUnityInstance } from 'unity-interface/IUnityInterface'
-import { AUTHENTICATE, CHANGE_LOGIN_STAGE, SIGNUP_SET_IS_SIGNUP } from '../session/actions'
-import { PARCEL_LOADING_STARTED, RENDERER_INITIALIZED_CORRECTLY } from '../renderer/types'
-import { PENDING_SCENES, SCENE_FAIL, SCENE_LOAD, SCENE_UNLOAD, UPDATE_STATUS_MESSAGE } from '../loading/actions'
+﻿import { PENDING_SCENES, SCENE_FAIL, SCENE_LOAD, SCENE_UNLOAD, UPDATE_STATUS_MESSAGE } from '../loading/actions'
 import { TELEPORT_TRIGGERED } from '../loading/types'
 import { SET_REALM_ADAPTER } from '../realm/actions'
+import { PARCEL_LOADING_STARTED, RENDERER_INITIALIZED_CORRECTLY } from '../renderer/types'
 import { POSITION_SETTLED, POSITION_UNSETTLED, SET_SCENE_LOADER } from '../scene-loader/actions'
+import { AUTHENTICATE, CHANGE_LOGIN_STAGE, SIGNUP_SET_IS_SIGNUP } from '../session/actions'
 import { RENDERING_ACTIVATED, RENDERING_BACKGROUND, RENDERING_DEACTIVATED, RENDERING_FOREGROUND } from './types'
-import { getFeatureFlagEnabled } from 'shared/meta/selectors'
 
 // The following actions may change the status of the loginVisible
 // Reaction on them will be ported to Renderer
