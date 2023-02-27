2ffbea4ccb9f6fed311c9bb9cbff10e170860ff8
Paul Dirac <paul.dirac@ioiltd.com>
2023-02-27 12:13:25 -0300

    feat: drop bouncy castle

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/WebSocketCommunication/WebSocketCommunication.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/WebSocketCommunication/WebSocketCommunication.cs
@@ -59,11 +59,12 @@ public class WebSocketCommunication : IKernelCommunication
             if (withSSL)
             {
                 wssServerUrl = $"wss://localhost:{port}/";
+                string cert = "";
                 ws = new WebSocketServer(wssServerUrl)
                 {
                     SslConfiguration =
                     {
-                        ServerCertificate = CertificateUtils.CreateSelfSignedCert(),
+                        ServerCertificate = buildSelfSignedServerCertificate(),
                         ClientCertificateRequired = false,
                         CheckCertificateRevocation = false,
                         ClientCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true,
@@ -108,6 +109,37 @@ public class WebSocketCommunication : IKernelCommunication
 
         string wssUrl = wssServerUrl + wssServiceId;
         return wssUrl;
+    }
+    
+    private X509Certificate2 buildSelfSignedServerCertificate()
+    {
+        SubjectAlternativeNameBuilder sanBuilder = new SubjectAlternativeNameBuilder();
+        sanBuilder.AddIpAddress(IPAddress.Loopback);
+        sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
+        sanBuilder.AddDnsName("localhost");
+        sanBuilder.AddDnsName(Environment.MachineName);
+
+        X500DistinguishedName distinguishedName = new X500DistinguishedName($"CN={CertificateName}");
+
+        using (RSA rsa = RSA.Create(2048))
+        {
+            var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256,RSASignaturePadding.Pkcs1);
+
+            request.CertificateExtensions.Add(
+                new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature , false));
+
+
+            request.CertificateExtensions.Add(
+               new X509EnhancedKeyUsageExtension(
+                   new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+            request.CertificateExtensions.Add(sanBuilder.Build());
+
+            var certificate= request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+            certificate.FriendlyName = CertificateName;
+
+            return new X509Certificate2(certificate.Export(X509ContentType.Pfx, "WeNeedASaf3rPassword"), "WeNeedASaf3rPassword", X509KeyStorageFlags.MachineKeySet);
+        }
     }
     private void OnWebSocketLog(LogData logData, string message)
     {
