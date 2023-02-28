4192d82904d549ed10189eed810fdd787c614597
Esteban Ordano <esteban@decentraland.org>
2023-02-28 04:14:03 +0100

    Update ensureFriendProfile.ts
    Signed-off-by: Esteban Ordano <esteban@decentraland.org>

--- browser-interface/packages/shared/friends/ensureFriendProfile.ts
+++ browser-interface/packages/shared/friends/ensureFriendProfile.ts
@@ -1,4 +1,4 @@
-import { Avatar } from '@dcl/schemas'
+import type { Avatar } from '@dcl/schemas'
 import { sendProfileToRenderer } from 'shared/profiles/actions'
 import { retrieveProfileFromCatalyst } from 'shared/profiles/retrieveProfile'
 import { store } from 'shared/store/isolatedStore'
