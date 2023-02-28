f1681d2000f451c179a6b629d27f283529426f7e
Esteban Ordano <esteban@decentraland.org>
2023-02-28 04:13:16 +0100

    Update ensureFriendProfile.ts
    Signed-off-by: Esteban Ordano <esteban@decentraland.org>

--- browser-interface/packages/shared/friends/ensureFriendProfile.ts
+++ browser-interface/packages/shared/friends/ensureFriendProfile.ts
@@ -5,6 +5,7 @@ import { store } from 'shared/store/isolatedStore'
 
 export async function ensureFriendProfile(userId: string): Promise<Avatar> {
   const profile = await retrieveProfileFromCatalyst(userId, undefined)
+  store.dispatch(profileSuccess(profile))
   store.dispatch(sendProfileToRenderer(profile.userId))
 
   return profile
