c8fb017cd37463440a3139e9579ad2e43211bc0c
Esteban Ordano <esteban@decentraland.org>
2023-02-28 14:55:35 +0100

    revert: shared/comms/selectors.ts
    Signed-off-by: Esteban Ordano <esteban@decentraland.org>

--- browser-interface/packages/shared/comms/selectors.ts
+++ browser-interface/packages/shared/comms/selectors.ts
@@ -8,7 +8,7 @@ import { RoomConnection } from './interface'
 import { RootCommsState } from './types'
 
 export const getCommsIsland = (store: RootCommsState): string | undefined => store.comms.island
-export const getCommsRoom = (state: RootCommsState): RoomConnection | undefined => state.comms?.context
+export const getCommsRoom = (state: RootCommsState): RoomConnection | undefined => state.comms.context
 
 export function reconnectionState(state: RootState): {
   commsConnection: RoomConnection | undefined
