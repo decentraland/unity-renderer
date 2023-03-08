f71dde3be2d8c2a901096bd4b187a3bcda6bf5c5
Alejandro Jimenez <ajimenez@decentraland.org>
2023-03-08 10:33:14 +0100

    chore: remove airdropping hud

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/HUDAssetPath.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/HUDAssetPath.cs
@@ -4,7 +4,6 @@
     {
         public const string SIGNUP_HUD = "SignupHUD";
         public const string LOADING_HUD = "LoadingHUD";
-        public const string AIRDROPPING_HUD = "AirdroppingHUD";
         public const string QUESTS_TRACKER_HUD = "QuestsTrackerHUD";
     }
 }
