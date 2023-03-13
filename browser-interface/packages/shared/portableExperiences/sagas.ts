442812bccf38b9c188ae64790c1e435e95094eaf
Esteban Ordano <esteban@decentraland.org>
2023-03-13 14:27:28 -0300

    fix: race condition on PX (#4611)
    * fix: race condition on wearables
    
    * fix: onLoginCompleted -> waitForRenderer
    
    * fix: tests to check dummy rpcClient
    
    * fix: linter
    
    * refactor: move initialize PX to shared/PX
    
    * fix: document why debouncing
    
    * add empty parcels copy to build process
    
    * restore export for disconnectRoom
    
    ---------
    
    Co-authored-by: menduz <github@menduz.com>

--- browser-interface/packages/shared/portableExperiences/sagas.ts
+++ browser-interface/packages/shared/portableExperiences/sagas.ts
@@ -1,39 +1,87 @@
 import { PORTABLE_EXPERIENCES_DEBOUNCE_DELAY } from 'config'
-import { call, takeEvery, debounce, select, put, delay } from 'redux-saga/effects'
+import { call, debounce, delay, fork, put, select, takeEvery } from 'redux-saga/effects'
+import { trackEvent } from 'shared/analytics/trackEvent'
+import { waitForMetaConfigurationInitialization } from 'shared/meta/sagas'
+import { getFeatureFlagVariantValue } from 'shared/meta/selectors'
+import { waitForRendererRpcConnection } from 'shared/renderer/sagas-helper'
 import { LoadableScene } from 'shared/types'
 import {
   ADD_DESIRED_PORTABLE_EXPERIENCE,
   REMOVE_DESIRED_PORTABLE_EXPERIENCE
 } from 'shared/wearablesPortableExperience/actions'
-import { declareWantedPortableExperiences } from 'unity-interface/portableExperiencesUtils'
 import {
+  declareWantedPortableExperiences,
+  getPortableExperienceFromUrn
+} from 'unity-interface/portableExperiencesUtils'
+import {
+  ACTIVATE_ALL_PORTABLE_EXPERIENCES,
+  addKernelPortableExperience,
+  ADD_KERNEL_PX,
   ADD_SCENE_PX,
   DENY_PORTABLE_EXPERIENCES,
   ReloadScenePortableExperienceAction,
   RELOAD_SCENE_PX,
   REMOVE_SCENE_PX,
+  SHUTDOWN_ALL_PORTABLE_EXPERIENCES,
   updateEnginePortableExperiences,
   UpdateEnginePortableExperiencesAction,
-  UPDATE_ENGINE_PX,
-  SHUTDOWN_ALL_PORTABLE_EXPERIENCES,
-  ACTIVATE_ALL_PORTABLE_EXPERIENCES,
-  ADD_KERNEL_PX
+  UPDATE_ENGINE_PX
 } from './actions'
 import { getDesiredPortableExperiences } from './selectors'
 
 export function* portableExperienceSaga(): any {
-  yield takeEvery([
-    REMOVE_DESIRED_PORTABLE_EXPERIENCE,
-    ADD_DESIRED_PORTABLE_EXPERIENCE,
-    SHUTDOWN_ALL_PORTABLE_EXPERIENCES,
-    ACTIVATE_ALL_PORTABLE_EXPERIENCES,
-    DENY_PORTABLE_EXPERIENCES,
-    ADD_SCENE_PX,
-    ADD_KERNEL_PX,
-    REMOVE_SCENE_PX
-  ], handlePortableExperienceChanges)
+  // Ensure we have a connection to the renderer
+  yield call(waitForRendererRpcConnection)
+
+  // List the actions that might trigger a portable experience change
+  yield takeEvery(
+    [
+      REMOVE_DESIRED_PORTABLE_EXPERIENCE,
+      ADD_DESIRED_PORTABLE_EXPERIENCE,
+      SHUTDOWN_ALL_PORTABLE_EXPERIENCES,
+      ACTIVATE_ALL_PORTABLE_EXPERIENCES,
+      DENY_PORTABLE_EXPERIENCES,
+      ADD_SCENE_PX,
+      ADD_KERNEL_PX,
+      REMOVE_SCENE_PX
+    ],
+    handlePortableExperienceChanges
+  )
   yield takeEvery(RELOAD_SCENE_PX, reloadPortableExperienceChanges)
+
+  // Many actions can trigger a rebuild of the portable experience "desired list", for example, by adding wearables one
+  // by one in some async `for` loop that awaits for each experience.
+  // Thus, we reactions to UPDATE_ENGINE_PX, so we only alert the renderer once, and avoid unnecessary processing costs.
   yield debounce(PORTABLE_EXPERIENCES_DEBOUNCE_DELAY(), UPDATE_ENGINE_PX, handlePortableExperienceChangesEffect)
+
+  // Finally, initialize the portable experiences
+  yield fork(fetchInitialPortableExperiences)
+}
+
+export function* fetchInitialPortableExperiences() {
+  yield waitForMetaConfigurationInitialization()
+
+  const qs = new URLSearchParams(globalThis.location.search)
+
+  const globalPortableExperiences: string[] = qs.has('GLOBAL_PX')
+    ? qs.getAll('GLOBAL_PX')
+    : yield select(getFeatureFlagVariantValue, 'initial_portable_experiences')
+
+  if (Array.isArray(globalPortableExperiences)) {
+    for (const id of globalPortableExperiences) {
+      try {
+        const px: LoadableScene = yield call(getPortableExperienceFromUrn, id)
+        yield put(addKernelPortableExperience(px))
+      } catch (err: any) {
+        console.error(err)
+        trackEvent('error', {
+          context: 'fetchInitialPortableExperiences',
+          message: err.message,
+          stack: err.stack
+        })
+      }
+    }
+  }
 }
 
 // every time the desired portable experiences change, the action `updateEnginePortableExperiences` should be dispatched
