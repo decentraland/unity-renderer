75f1d3f52db946c2c1731a71d3d239f8ad70436c
Nicolas Lorusso <nalorusso@gmail.com>
2023-03-15 11:22:38 -0300

    added test coverage for max amount of results on MemoryChatMentionSuggestionProvider

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ChatController/Tests/MemoryChatMentionSuggestionProviderShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ChatController/Tests/MemoryChatMentionSuggestionProviderShould.cs
@@ -18,21 +18,25 @@ namespace DCL.Social.Chat.Mentions
         public void SetUp()
         {
             userProfileController = new GameObject("UserProfileController").AddComponent<UserProfileController>();
+
             userProfileController.AddUserProfileToCatalog(new UserProfileModel
             {
                 userId = "user1",
                 name = "lucky",
             });
+
             userProfileController.AddUserProfileToCatalog(new UserProfileModel
             {
                 userId = "user2",
                 name = "womo",
             });
+
             userProfileController.AddUserProfileToCatalog(new UserProfileModel
             {
                 userId = "user3",
                 name = "heyun",
             });
+
             userProfileController.AddUserProfileToCatalog(new UserProfileModel
             {
                 userId = "user4",
@@ -46,53 +50,108 @@ namespace DCL.Social.Chat.Mentions
         [TearDown]
         public void TearDown()
         {
+            dataStore.channels.availableMembersByChannel.Clear();
+            dataStore.player.otherPlayers.Clear();
             Object.Destroy(userProfileController.gameObject);
         }
 
         [UnityTest]
-        public IEnumerator GetProfilesFromUserProfileController() => UniTask.ToCoroutine(async () =>
-        {
-            List<UserProfile> profiles = await mentionSuggestionProvider.GetProfilesStartingWith("hey", 5, default(CancellationToken));
+        public IEnumerator GetAllProfilesStartingWith() =>
+            UniTask.ToCoroutine(async () =>
+            {
+                List<UserProfile> profiles = await mentionSuggestionProvider.GetProfilesStartingWith("h", 5, default(CancellationToken));
 
-            Assert.AreEqual("user3", profiles[0].userId);
-            Assert.AreEqual(1, profiles.Count);
-        });
+                Assert.AreEqual("user3", profiles[0].userId);
+                Assert.AreEqual("user4", profiles[1].userId);
+                Assert.AreEqual(2, profiles.Count);
+            });
 
         [UnityTest]
-        public IEnumerator GetNearbyProfiles() => UniTask.ToCoroutine(async () =>
-        {
-            dataStore.player.otherPlayers.Add("user3", new Player
+        public IEnumerator GetMaxAmountOfProfilesStartingWith() =>
+            UniTask.ToCoroutine(async () =>
             {
-                id = "user3",
-                name = "heyun",
-            });
-            dataStore.player.otherPlayers.Add("user4", new Player
-            {
-                id = "user4",
-                name = "holy",
-            });
+                List<UserProfile> profiles = await mentionSuggestionProvider.GetProfilesStartingWith("h", 1, default(CancellationToken));
 
-            List<UserProfile> profiles = await mentionSuggestionProvider.GetNearbyProfilesStartingWith("h", 5, default(CancellationToken));
-
-            Assert.AreEqual("user3", profiles[0].userId);
-            Assert.AreEqual("user4", profiles[1].userId);
-            Assert.AreEqual(2, profiles.Count);
-        });
+                Assert.AreEqual("user3", profiles[0].userId);
+                Assert.AreEqual(1, profiles.Count);
+            });
 
         [UnityTest]
-        public IEnumerator GetProfilesFromChannels() => UniTask.ToCoroutine(async () =>
-        {
-            dataStore.channels.availableMembersByChannel.Add("channelId", new HashSet<string>
+        public IEnumerator GetNearbyProfiles() =>
+            UniTask.ToCoroutine(async () =>
             {
-                "user1",
-                "user2",
-                "user3",
+                dataStore.player.otherPlayers.Add("user3", new Player
+                {
+                    id = "user3",
+                    name = "heyun",
+                });
+
+                dataStore.player.otherPlayers.Add("user4", new Player
+                {
+                    id = "user4",
+                    name = "holy",
+                });
+
+                List<UserProfile> profiles = await mentionSuggestionProvider.GetNearbyProfilesStartingWith("h", 5, default(CancellationToken));
+
+                Assert.AreEqual("user3", profiles[0].userId);
+                Assert.AreEqual("user4", profiles[1].userId);
+                Assert.AreEqual(2, profiles.Count);
             });
 
-            List<UserProfile> profiles = await mentionSuggestionProvider.GetProfilesFromChatChannelsStartingWith("hey", "channelId", 5, default(CancellationToken));
+        [UnityTest]
+        public IEnumerator GetMaxAmountOfNearbyProfiles() =>
+            UniTask.ToCoroutine(async () =>
+            {
+                dataStore.player.otherPlayers.Add("user3", new Player
+                {
+                    id = "user3",
+                    name = "heyun",
+                });
 
-            Assert.AreEqual("user3", profiles[0].userId);
-            Assert.AreEqual(1, profiles.Count);
-        });
+                dataStore.player.otherPlayers.Add("user4", new Player
+                {
+                    id = "user4",
+                    name = "holy",
+                });
+
+                List<UserProfile> profiles = await mentionSuggestionProvider.GetNearbyProfilesStartingWith("h", 1, default(CancellationToken));
+
+                Assert.AreEqual("user3", profiles[0].userId);
+                Assert.AreEqual(1, profiles.Count);
+            });
+
+        [UnityTest]
+        public IEnumerator GetProfilesFromChannels() =>
+            UniTask.ToCoroutine(async () =>
+            {
+                dataStore.channels.availableMembersByChannel.Add("channelId", new HashSet<string>
+                {
+                    "user1",
+                    "user2",
+                    "user3",
+                });
+
+                List<UserProfile> profiles = await mentionSuggestionProvider.GetProfilesFromChatChannelsStartingWith("hey", "channelId", 5, default(CancellationToken));
+
+                Assert.AreEqual("user3", profiles[0].userId);
+                Assert.AreEqual(1, profiles.Count);
+            });
+
+        [UnityTest]
+        public IEnumerator GetMaxAmountOfProfilesFromChannels() =>
+            UniTask.ToCoroutine(async () =>
+            {
+                dataStore.channels.availableMembersByChannel.Add("channelId", new HashSet<string>
+                {
+                    "user3",
+                    "user4",
+                });
+
+                List<UserProfile> profiles = await mentionSuggestionProvider.GetProfilesFromChatChannelsStartingWith("h", "channelId", 1, default(CancellationToken));
+
+                Assert.AreEqual("user3", profiles[0].userId);
+                Assert.AreEqual(1, profiles.Count);
+            });
     }
 }
