d18c7ecf30b8a3afddaf3949c45976590389fc7a
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2023-03-16 13:45:47 -0300

    Set to 120
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- browser-interface/packages/shared/world/SceneWorker.ts
+++ browser-interface/packages/shared/world/SceneWorker.ts
@@ -324,7 +324,7 @@ export class SceneWorker {
 
     sceneEvents.emit(SCENE_LOAD, signalSceneLoad(this.loadableScene))
 
-    const WORKER_TIMEOUT = 180_000
+    const WORKER_TIMEOUT = 120_000
     setTimeout(() => this.onLoadTimeout(), WORKER_TIMEOUT)
   }
 
