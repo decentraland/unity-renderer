df2fda5d9e534e95d3ede3a73798afcab6fe7d1b
davidejensen <davidejensen@live.it>
2023-03-24 09:38:38 +0100

    Revert "Merge pull request #4713 from decentraland/hotfix/nearby-chat-sequence"
    This reverts commit 0f438daa0f73f65f8d34a926bd93d67f29cf8a6e, reversing
    changes made to 7a2352c290fbd89641c55d4e7062966f113e2712.

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ChatWidgetHUD/ChatEntrySortingSequentally.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace DCL.Social.Chat
-{
-    public class ChatEntrySortingSequentially : IComparer<ChatEntryModel>
-    {
-        public int Compare(ChatEntryModel x, ChatEntryModel y) => 0;
-    }
-}
