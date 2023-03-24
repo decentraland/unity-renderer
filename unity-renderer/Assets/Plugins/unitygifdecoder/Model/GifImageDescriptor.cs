1b7578d98aa36be58b85be94c3c8e71142b228de
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-03-24 18:29:43 +0000

    Temporarily added Unity Gif Decoder as a plugin and removed from package manager

---
+++ unity-renderer/Assets/Plugins/unitygifdecoder/Model/GifImageDescriptor.cs
@@ -0,0 +1,40 @@
+﻿namespace ThreeDISevenZeroR.UnityGifDecoder.Model
+{
+    public struct GifImageDescriptor
+    {
+        public int left;
+        public int top;
+
+        public bool isInterlaced;
+        public bool hasLocalColorTable;
+        public int localColorTableSize;
+
+        private int width;
+        private int height;
+        public int Width
+        {
+            get
+            {
+                if (width % 4 != 0)
+                {
+                    return ((width / 4) + 1) * 4;
+                }
+                return width;
+            }
+            set => width = value;
+        }
+
+        public int Height
+        {
+            get
+            {
+                if (height % 4 != 0)
+                {
+                    return ((height / 4) + 1) * 4;
+                }
+                return height;
+            }
+            set => height = value;
+        }
+    }
+}
---
+++ unity-renderer/Assets/Plugins/unitygifdecoder/Model/GifPlainText.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace ThreeDISevenZeroR.UnityGifDecoder.Model
+{
+    public struct GifPlainText
+    {
+        public int left;
+        public int top;
+        // public int width;
+        // public int height;
+        public int charWidth;
+        public int charHeight;
+        public Color32 backgroundColor;
+        public Color32 foregroundColor;
+        public string text;
+        public Color32[] colors;
+
+        private int width;
+        private int height;
+        public int Width
+        {
+            get
+            {
+                if (width % 4 != 0)
+                {
+                    return (width / 4) * 4;
+                }
+                return width;
+            }
+            set => width = value;
+        }
+
+        public int Height
+        {
+            get
+            {
+                if (height % 4 != 0)
+                {
+                    return (height / 4) * 4;
+                }
+                return height;
+            }
+            set => height = value;
+        }
+    }
+}
