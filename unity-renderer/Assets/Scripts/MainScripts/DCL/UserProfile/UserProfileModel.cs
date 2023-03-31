4f718c38f8ae09d0f0963933dcb9e2f12eb601fc
Santi Andrade <sandrade@decentraland.org>
2023-03-31 14:48:06 +0200

    Compare only the lenght of the mutes/blocked lists

--- unity-renderer/Assets/Scripts/MainScripts/DCL/UserProfile/UserProfileModel.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/UserProfile/UserProfileModel.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 
 [System.Serializable]
 public class UserProfileModel
@@ -93,9 +92,9 @@ public class UserProfileModel
 
         if (model.blocked != null && blocked != null)
         {
-            if (model.blocked.Count != blocked.Count ||
-                model.blocked.Except(blocked).Any() ||
-                blocked.Except(model.blocked).Any())
+            // We're comparing only the lenght of the 'blocked' list because we found several cases where it is too big and it would cause huge hiccups.
+            // Also, taking into account that this list should be removed from the profile data in the catalysts, we decided to nor compare the content.
+            if (model.blocked.Count != blocked.Count)
                 return false;
         }
         else if (model.blocked != null || blocked != null)
@@ -103,9 +102,9 @@ public class UserProfileModel
 
         if (model.muted != null && muted != null)
         {
-            if (model.muted.Count != muted.Count ||
-                model.muted.Except(muted).Any() ||
-                muted.Except(model.muted).Any())
+            // We're comparing only the lenght of the 'muted' list because we found several cases where it is too big and it would cause huge hiccups.
+            // Also, taking into account that this list should be removed from the profile data in the catalysts, we decided to nor compare the content.
+            if (model.muted.Count != muted.Count)
                 return false;
         }
         else if (model.muted != null || muted != null)
