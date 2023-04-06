2e0bb5b08d76b74ae4f4584a5a3998e294030321
davidejensen <davidejensen@live.it>
2023-04-06 10:24:38 +0200

    chore: passport performance improvements (#4705)
    * chore: passport performance improvements
    
    * Minor improvements in passport opening
    
    * Minor prefab changes
    
    * Minor change

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/PassportNavigationComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Passport/Passport/PassportNavigation/PassportNavigationComponentView.cs
@@ -129,6 +129,11 @@ namespace DCL.Social.Passports
             viewAllLANDs.onClick.AddListener(()=>OpenViewAllSection(PassportSection.Lands));
 
             descriptionText.SubscribeToClickEvents(OnDescriptionClicked);
+            nftWearablesPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Wearables");
+            nftEmotesPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Emotes");
+            nftNamesPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Names");
+            nftLandsPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Lands");
+            nftIconsEntryPool = GetNftIconEntryPool();
         }
 
         private void OpenViewAllSection(PassportSection section)
@@ -174,11 +179,6 @@ namespace DCL.Social.Passports
 
         public void InitializeView()
         {
-            nftWearablesPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Wearables");
-            nftEmotesPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Emotes");
-            nftNamesPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Names");
-            nftLandsPagesEntryPool = GetNftPagesEntryPool(NFT_PAGES_POOL_NAME_PREFIX + "Lands");
-            nftIconsEntryPool = GetNftIconEntryPool();
             ReleaseAllNftPagesPoolObjects();
             ReleaseAllNftIconsPoolObjects();
             SetInitialPage();
