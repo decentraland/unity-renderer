3c6f674e1925995fd3a09ca7d1068e0713b80974
Pato <pato@decentraland.org>
2023-04-07 14:49:41 -0300

    remove testing code

--- unity-renderer/Assets/DCLPlugins/ECS7/Systems/BillboardSystem/ECSBillboardSystem.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/Systems/BillboardSystem/ECSBillboardSystem.cs
@@ -82,23 +82,5 @@ namespace ECSSystems.BillboardSystem
                 billboardT.LookAt(billboardPos + forward, up);
             }
         }
-
-        public static Quaternion GetXAxisRotation(Quaternion quaternion)
-        {
-            float a = Mathf.Sqrt((quaternion.w * quaternion.w) + (quaternion.x * quaternion.x));
-            return new Quaternion(x: quaternion.x, y: 0, z: 0, w: quaternion.w / a);
-        }
-
-        public static Quaternion GetYAxisRotation(Quaternion quaternion)
-        {
-            float a = Mathf.Sqrt((quaternion.w * quaternion.w) + (quaternion.y * quaternion.y));
-            return new Quaternion(x: 0, y: quaternion.y, z: 0, w: quaternion.w / a);
-        }
-
-        public static Quaternion GetZAxisRotation(Quaternion quaternion)
-        {
-            float a = Mathf.Sqrt((quaternion.w * quaternion.w) + (quaternion.z * quaternion.z));
-            return new Quaternion(x: 0, y: 0, z: quaternion.z, w: quaternion.w / a);
-        }
     }
 }
