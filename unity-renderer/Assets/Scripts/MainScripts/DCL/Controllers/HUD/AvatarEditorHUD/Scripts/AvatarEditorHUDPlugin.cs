1f66cfe13dbbaa02799217ca8a67d57b92d4dfa5
Nicolas Lorusso <nalorusso@gmail.com>
2023-04-11 15:24:22 -0300

    removed feature flag binding

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDPlugin.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDPlugin.cs
@@ -5,34 +5,23 @@ namespace DCL.AvatarEditor
     public class AvatarEditorHUDPlugin : IPlugin
     {
         private readonly AvatarEditorHUDController hudController;
-        private readonly DataStore_FeatureFlag featureFlags;
 
         public AvatarEditorHUDPlugin()
         {
-            featureFlags = DataStore.i.featureFlags;
-
             hudController = new AvatarEditorHUDController(
-                featureFlags,
+                DataStore.i.featureFlags,
                 Environment.i.platform.serviceProviders.analytics,
                 Environment.i.serviceLocator.Get<IWearablesCatalogService>(),
                 new UserProfileWebInterfaceBridge());
 
-            // there is a race condition going on if we initialize the avatar editor before the feature flags are set
-            // WearablesCatalogServiceProxy.wearablesCatalogServiceInUse is not defined which is needed to retrieve the wearable catalog
-            featureFlags.flags.OnChange += InitializeHUD;
+            // there could be a race condition going on if we initialize the avatar editor before the feature flags are set
+            // WearablesCatalogServiceProxy.wearablesCatalogServiceInUse is not defined when is needed to retrieve the wearable catalog
+            hudController.Initialize();
         }
 
         public void Dispose()
         {
-            featureFlags.flags.OnChange -= InitializeHUD;
             hudController.Dispose();
-        }
-
-        private void InitializeHUD(FeatureFlag current, FeatureFlag previous)
-        {
-            featureFlags.flags.OnChange -= InitializeHUD;
-
-            hudController.Initialize();
         }
     }
 }
