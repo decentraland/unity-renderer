38a4deb74a4fbe577db7ec2268fef598c2c8a88d
Nicolas Lorusso <nalorusso@gmail.com>
2023-04-11 14:50:47 -0300

    moved the initialization of avatar editor after feature flags are set

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDController.cs
@@ -1,20 +1,19 @@
+using Cysharp.Threading.Tasks;
 using DCL;
+using DCL.Emotes;
 using DCL.EmotesCustomization;
 using DCL.Helpers;
 using DCL.Interface;
 using DCL.NotificationModel;
+using DCL.Tasks;
+using DCLServices.WearablesCatalogService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using Cysharp.Threading.Tasks;
-using DCL.Emotes;
-using DCL.Tasks;
-using DCLServices.WearablesCatalogService;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-using Categories = WearableLiterals.Categories;
 using Environment = DCL.Environment;
 using Random = UnityEngine.Random;
 using Type = DCL.NotificationModel.Type;
@@ -29,8 +28,8 @@ public class AvatarEditorHUDController : IHUD
     private const string THIRD_PARTY_COLLECTIONS_FEATURE_FLAG = "third_party_collections";
     private const int REQUESTS_COOLDOWN_TIME = 60;
     internal const string EQUIP_WEARABLE_METRIC = "equip_wearable";
-    protected static readonly string[] categoriesThatMustHaveSelection = { Categories.BODY_SHAPE, Categories.UPPER_BODY, Categories.LOWER_BODY, Categories.FEET, Categories.EYES, Categories.EYEBROWS, Categories.MOUTH };
-    protected static readonly string[] categoriesToRandomize = { Categories.HAIR, Categories.EYES, Categories.EYEBROWS, Categories.MOUTH, Categories.FACIAL, Categories.HAIR, Categories.UPPER_BODY, Categories.LOWER_BODY, Categories.FEET };
+    protected static readonly string[] categoriesThatMustHaveSelection = { WearableLiterals.Categories.BODY_SHAPE, WearableLiterals.Categories.UPPER_BODY, WearableLiterals.Categories.LOWER_BODY, WearableLiterals.Categories.FEET, WearableLiterals.Categories.EYES, WearableLiterals.Categories.EYEBROWS, WearableLiterals.Categories.MOUTH };
+    protected static readonly string[] categoriesToRandomize = { WearableLiterals.Categories.HAIR, WearableLiterals.Categories.EYES, WearableLiterals.Categories.EYEBROWS, WearableLiterals.Categories.MOUTH, WearableLiterals.Categories.FACIAL, WearableLiterals.Categories.HAIR, WearableLiterals.Categories.UPPER_BODY, WearableLiterals.Categories.LOWER_BODY, WearableLiterals.Categories.FEET };
 
     [NonSerialized]
     public bool bypassUpdateAvatarPreview = false;
@@ -118,15 +117,7 @@ public class AvatarEditorHUDController : IHUD
         eyeColorList = Resources.Load<ColorList>("EyeColor");
         view.SetColors(skinColorList.colors, hairColorList.colors, eyeColorList.colors);
 
-        async UniTask InitializeWearableCatalog()
-        {
-            // there is a race condition due feature flags..
-            // wearablesCatalogService.WearablesCatalog is null until WearablesCatalogServiceProxy.wearablesCatalogServiceInUse is valid
-            await UniTask.WaitUntil(() => wearablesCatalogService.WearablesCatalog != null);
-            SetCatalog(wearablesCatalogService.WearablesCatalog);
-        }
-
-        InitializeWearableCatalog().Forget();
+        SetCatalog(wearablesCatalogService.WearablesCatalog);
 
         this.userProfile.OnUpdate += LoadUserProfile;
 
@@ -402,7 +393,7 @@ public class AvatarEditorHUDController : IHUD
 
         view.SetIsWeb3(userProfile.hasConnectedWeb3);
 
-        ProcessCatalog(this.catalog);
+        ProcessCatalog(catalog);
 
         if (avatarIsDirty)
             return;
@@ -450,7 +441,7 @@ public class AvatarEditorHUDController : IHUD
         for (var i = 0; i < categoriesThatMustHaveSelection.Length; i++)
         {
             var category = categoriesThatMustHaveSelection[i];
-            if (category != Categories.BODY_SHAPE && !(categoriesInUse.Contains(category)))
+            if (category != WearableLiterals.Categories.BODY_SHAPE && !(categoriesInUse.Contains(category)))
             {
                 WearableItem wearable;
                 var defaultItemId = WearableLiterals.DefaultWearables.GetDefaultWearable(model.bodyShape.id, category);
@@ -476,7 +467,7 @@ public class AvatarEditorHUDController : IHUD
         wearablesCatalogService.WearablesCatalog.TryGetValue(wearableId, out var wearable);
         if (wearable == null) return;
 
-        if (wearable.data.category == Categories.BODY_SHAPE)
+        if (wearable.data.category == WearableLiterals.Categories.BODY_SHAPE)
         {
             if (wearable.id == model.bodyShape.id)
                 return;
@@ -498,7 +489,7 @@ public class AvatarEditorHUDController : IHUD
             else
             {
                 if (IsTryingToReplaceSkin(wearable))
-                    UnequipWearable(model.GetWearable(Categories.SKIN));
+                    UnequipWearable(model.GetWearable(WearableLiterals.Categories.SKIN));
 
                 var sameCategoryEquipped = model.GetWearable(wearable.data.category);
                 if (sameCategoryEquipped != null)
@@ -571,7 +562,7 @@ public class AvatarEditorHUDController : IHUD
 
     private void EquipBodyShape(WearableItem bodyShape)
     {
-        if (bodyShape.data.category != Categories.BODY_SHAPE)
+        if (bodyShape.data.category != WearableLiterals.Categories.BODY_SHAPE)
         {
             Debug.LogError($"Item ({bodyShape.id} is not a body shape");
             return;
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDPlugin.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/AvatarEditorHUD/Scripts/AvatarEditorHUDPlugin.cs
@@ -5,20 +5,33 @@ namespace DCL.AvatarEditor
     public class AvatarEditorHUDPlugin : IPlugin
     {
         private readonly AvatarEditorHUDController hudController;
+        private readonly DataStore_FeatureFlag featureFlags;
 
         public AvatarEditorHUDPlugin()
         {
+            featureFlags = DataStore.i.featureFlags;
+
             hudController = new AvatarEditorHUDController(
-                DataStore.i.featureFlags,
+                featureFlags,
                 Environment.i.platform.serviceProviders.analytics,
                 Environment.i.serviceLocator.Get<IWearablesCatalogService>(),
                 new UserProfileWebInterfaceBridge());
-            hudController.Initialize();
+
+            // there is a race condition going on if we initialize the avatar editor before the feature flags are set
+            // WearablesCatalogServiceProxy.wearablesCatalogServiceInUse is not defined which is needed to retrieve the wearable catalog
+            featureFlags.flags.OnChange += InitializeHUD;
         }
 
         public void Dispose()
         {
             hudController.Dispose();
+        }
+
+        private void InitializeHUD(FeatureFlag current, FeatureFlag previous)
+        {
+            featureFlags.flags.OnChange -= InitializeHUD;
+
+            hudController.Initialize();
         }
     }
 }
