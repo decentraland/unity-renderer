eec9ee00e7d8ee867871809a885d11a0cb20f0ad
Kinerius <kinerius@gmail.com>
2023-04-12 12:02:51 -0300

    Added test to check if the avatar was loaded correctly

---
+++ unity-renderer/Assets/Rendering/LoadingAvatar/Editor/BaseAvatarTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rendering.LoadingAvatar.Editor
+{
+    public class BaseAvatarTest
+    {
+        private const string assetPath = "Assets/Rendering/LoadingAvatar/CrossSection/Avatar_Male_SingleMesh.glb";
+
+        [Test]
+        public void BaseAvatarIsImportedCorrectly()
+        {
+            GameObject importedGameObject = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+            Assert.IsNotNull(importedGameObject);
+        }
+    }
+}
