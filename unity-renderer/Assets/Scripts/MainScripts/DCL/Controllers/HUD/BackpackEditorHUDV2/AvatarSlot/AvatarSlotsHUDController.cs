3696ab2bc17ba047197b751d003c9eb0866397fb
davidejensen <davidejensen@live.it>
2023-04-14 09:21:05 +0200

    Slots definition changes

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BackpackEditorHUDV2/AvatarSlot/AvatarSlotsHUDController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BackpackEditorHUDV2/AvatarSlot/AvatarSlotsHUDController.cs
@@ -1,7 +1,28 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class AvatarSlotsHUDController : IHUD
 {
+    private AvatarSlotsDefinitionSO avatarSlotsDefinition;
+
+    public AvatarSlotsHUDController()
+    {
+        avatarSlotsDefinition = Resources.Load<AvatarSlotsDefinitionSO>("AvatarSlotsDefinition");
+    }
+
+    private void GenerateSlots()
+    {
+        foreach (SerializableKeyValuePair<string, List<string>> section in avatarSlotsDefinition.slotsDefinition)
+        {
+            foreach (string avatarSlotSection in section.value)
+            {
+                //generate elements in section
+            }
+        }
+    }
+
     public void Dispose()
     {
         throw new NotImplementedException();
