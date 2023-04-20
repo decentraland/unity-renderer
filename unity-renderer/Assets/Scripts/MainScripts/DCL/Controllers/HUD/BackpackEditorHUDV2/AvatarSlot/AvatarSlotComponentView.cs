1dfccf638f3866a29cfe2c23ad5d9c80febc39e5
davidejensen <davidejensen@live.it>
2023-04-20 11:37:51 +0200

    Feat: add avatar equip slots animations for interactions

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BackpackEditorHUDV2/AvatarSlot/AvatarSlotComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BackpackEditorHUDV2/AvatarSlot/AvatarSlotComponentView.cs
@@ -2,17 +2,21 @@ using System;
 using TMPro;
 using UIComponents.Scripts.Components;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using DG.Tweening;
 using UnityEngine.UI;
 
 namespace DCL.Backpack
 {
     public class AvatarSlotComponentView : BaseComponentView<AvatarSlotComponentModel>, IAvatarSlotComponentView
     {
+        private const float ANIMATION_TIME = 0.2f;
+        private const float SHAKE_ANIMATION_TIME = 0.75f;
+
         [Header("Configuration")]
         [SerializeField] internal AvatarSlotComponentModel model;
 
         [SerializeField] internal NftTypeIconSO typeIcons;
+        [SerializeField] internal Transform nftContainer;
         [SerializeField] internal NftRarityBackgroundSO rarityBackgrounds;
         [SerializeField] internal Image typeImage;
         [SerializeField] internal ImageComponentView nftImage;
@@ -51,9 +55,9 @@ namespace DCL.Backpack
             model.isHidden = isHidden;
             model.hiddenBy = hiddenBy;
             hiddenSlot.SetActive(isHidden);
-
             if (isHidden)
             {
+                nftContainer.DOShakePosition(SHAKE_ANIMATION_TIME, 4);
                 emptySlot.SetActive(false);
                 tooltipHiddenText.gameObject.SetActive(true);
                 tooltipHiddenText.text = $"Hidden by: {hiddenBy}";
@@ -96,6 +100,8 @@ namespace DCL.Backpack
         {
             focusedImage.enabled = true;
             tooltipContainer.SetActive(true);
+            focusedImage.transform.localScale = new Vector3(0, 0, 0);
+            focusedImage.transform.DOScale(1, ANIMATION_TIME).SetEase(Ease.OutBack);
         }
 
         public override void OnLoseFocus()
@@ -107,7 +113,21 @@ namespace DCL.Backpack
         public void OnSlotClick()
         {
             isSelected = !isSelected;
-            selectedImage.enabled = isSelected;
+
+            if (isSelected)
+            {
+                selectedImage.enabled = true;
+                selectedImage.transform.localScale = new Vector3(0, 0, 0);
+                selectedImage.transform.DOScale(1, ANIMATION_TIME).SetEase(Ease.OutBack);
+            }
+            else
+            {
+                selectedImage.transform.DOScale(0, ANIMATION_TIME).SetEase(Ease.OutBack).OnComplete(() =>
+                {
+                    selectedImage.enabled = false;
+                    selectedImage.transform.localScale = new Vector3(1,1,1);
+                });
+            }
 
             OnSelectAvatarSlot?.Invoke(model.category, isSelected);
         }
