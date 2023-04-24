cd02893f18fa5f39767af3c4fa88691ebf32840b
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2023-04-24 12:02:44 -0300

    fix: jump home button not clickable on start (#5038)

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Common/ShowHideUIByTriggerJumpToHome.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Common/ShowHideUIByTriggerJumpToHome.cs
@@ -7,6 +7,8 @@ public class ShowHideUIByTriggerJumpToHome : ShowHideUIByTrigger
 
     protected override void SetUIVisibility(bool isVisible)
     {
-        base.SetUIVisibility(isVisible && DataStore.i.HUDs.jumpHomeButtonVisible.Get());
+        if (!DataStore.i.HUDs.jumpHomeButtonVisible.Get())
+            return;
+        base.SetUIVisibility(isVisible);
     }
 }
