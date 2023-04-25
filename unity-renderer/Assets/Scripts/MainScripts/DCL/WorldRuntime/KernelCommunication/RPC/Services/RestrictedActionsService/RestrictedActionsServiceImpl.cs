a635ca0d4795b7ab84d2c09755aabf8a56bb09de
Pato <pato@decentraland.org>
2023-04-25 17:40:18 -0300

    implement `OpenNftDialog` service method

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/Services/RestrictedActionsService/RestrictedActionsServiceImpl.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/Services/RestrictedActionsService/RestrictedActionsServiceImpl.cs
@@ -45,12 +45,21 @@ namespace RPC.Services
         public async UniTask<SuccessResponse> OpenNftDialog(OpenNftDialogRequest request, RPCContext context, CancellationToken ct)
         {
             await UniTask.SwitchToMainThread(ct);
+            RestrictedActionsContext restrictedActions = context.restrictedActions;
+
             bool success = false;
+            int currentFrameCount = restrictedActions.GetCurrentFrameCount?.Invoke() ?? GetCurrentFrameCount();
 
             try
             {
-                context.restrictedActions.OpenNftPrompt?.Invoke("", "");
-                success = true;
+                if ((currentFrameCount - restrictedActions.LastFrameWithInput) <= MAX_ELAPSED_FRAMES_SINCE_INPUT)
+                {
+                    if (TryParseUrn(request.Urn, out string contractAddress, out string tokenId))
+                    {
+                        restrictedActions.OpenNftPrompt?.Invoke(contractAddress, tokenId);
+                        success = true;
+                    }
+                }
             }
             catch (Exception _)
             { // ignored
@@ -62,6 +71,50 @@ namespace RPC.Services
         private static int GetCurrentFrameCount()
         {
             return Time.frameCount;
+        }
+
+        // TODO: change this when support for wearables/emotes/etc urn is added
+        // urn format urn:<CHAIN>:<CONTRACT_STANDARD>:<CONTRACT>:<TOKEN_ID> i.e: urn:ethereum:erc721:0x00...000:123
+        internal static bool TryParseUrn(string urn, out string contractAddress, out string tokenId)
+        {
+            const char SEPARATOR = ':';
+            const string CHAIN_ETHEREUM = "ethereum";
+
+            contractAddress = string.Empty;
+            tokenId = string.Empty;
+
+            try
+            {
+                var urnSpan = urn.AsSpan();
+
+                if (!urnSpan.Slice(0, 3).Equals("urn", StringComparison.Ordinal))
+                    return false;
+
+                urnSpan = urnSpan.Slice(4);
+
+                var chainSpan = urnSpan.Slice(0, CHAIN_ETHEREUM.Length);
+                urnSpan = urnSpan.Slice(chainSpan.Length + 1);
+
+                if (!chainSpan.Equals(CHAIN_ETHEREUM, StringComparison.Ordinal))
+                    return false;
+
+                var contractStandardSpan = urnSpan.Slice(0, urnSpan.IndexOf(SEPARATOR));
+                urnSpan = urnSpan.Slice(contractStandardSpan.Length + 1);
+
+                var contractAddressSpan = urnSpan.Slice(0, urnSpan.IndexOf(SEPARATOR));
+                urnSpan = urnSpan.Slice(contractAddressSpan.Length + 1);
+
+                var tokenIdSpan = urnSpan;
+
+                contractAddress = contractAddressSpan.ToString();
+                tokenId = tokenIdSpan.ToString();
+                return true;
+            }
+            catch (Exception _)
+            { // ignored
+            }
+
+            return false;
         }
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/Services/RestrictedActionsService/Tests/RestrictedActionsServiceImplShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/Services/RestrictedActionsService/Tests/RestrictedActionsServiceImplShould.cs
@@ -114,5 +114,154 @@ namespace Tests
                 Assert.IsFalse(requested);
             });
         }
+
+        [Test]
+        public void ParseUrnCorrectly()
+        {
+            const string CONTRACT_ADDRESS = "0x0123512313223123123123123";
+            const string TOKEN_ID = "645433";
+
+            Assert.IsTrue(RestrictedActionsServiceImpl.TryParseUrn(
+                $"urn:ethereum:erc1150:{CONTRACT_ADDRESS}:{TOKEN_ID}",
+                out string contractAddress,
+                out string tokenId));
+
+            Assert.AreEqual(CONTRACT_ADDRESS, contractAddress);
+            Assert.AreEqual(TOKEN_ID, tokenId);
+        }
+
+        [Test]
+        public void ParseUrnFailsCorrectly()
+        {
+            string contractAddress;
+            string tokenId;
+
+            // "urn" missing
+            Assert.IsFalse(RestrictedActionsServiceImpl.TryParseUrn(
+                $"ethereum:erc1150:0x0000temptation:666",
+                out contractAddress,
+                out tokenId));
+
+            // "chain" missing
+            Assert.IsFalse(RestrictedActionsServiceImpl.TryParseUrn(
+                $"urn:erc1150:0x0000temptation:666",
+                out contractAddress,
+                out tokenId));
+
+            // "chain" invalid
+            Assert.IsFalse(RestrictedActionsServiceImpl.TryParseUrn(
+                $"urn:some-chain:erc1150:0x0000temptation:666",
+                out contractAddress,
+                out tokenId));
+
+            // contract address missing
+            Assert.IsFalse(RestrictedActionsServiceImpl.TryParseUrn(
+                $"urn:ethereum:erc1150:666",
+                out contractAddress,
+                out tokenId));
+
+            // token id missing
+            Assert.IsFalse(RestrictedActionsServiceImpl.TryParseUrn(
+                $"urn:ethereum:erc1150:0x0000temptation",
+                out contractAddress,
+                out tokenId));
+
+            // empty string
+            Assert.IsFalse(RestrictedActionsServiceImpl.TryParseUrn(
+                "",
+                out contractAddress,
+                out tokenId));
+        }
+
+        [UnityTest]
+        public IEnumerator NftPromptRequestSuccessCorrectly()
+        {
+            yield return UniTask.ToCoroutine(async () =>
+            {
+                const string EXPECTED_CONTRACT_ADDRESS = "0x0temptation";
+                const string EXPECTED_TOKEN_ID = "1234";
+
+                bool requested = false;
+
+                string contractAddress = string.Empty;
+                string tokenId = string.Empty;
+
+                restrictedActions.OpenNftPrompt = (contract, token) =>
+                {
+                    requested = true;
+                    contractAddress = contract;
+                    tokenId = token;
+                };
+
+                restrictedActions.GetCurrentFrameCount = () => 0;
+                restrictedActions.LastFrameWithInput = 0;
+
+                var result = await rpcClient.OpenNftDialog(
+                    new OpenNftDialogRequest()
+                    {
+                        Urn = $"urn:ethereum:erc1150:{EXPECTED_CONTRACT_ADDRESS}:{EXPECTED_TOKEN_ID}"
+                    });
+
+                Assert.IsTrue(result.Success);
+                Assert.IsTrue(requested);
+                Assert.AreEqual(contractAddress, EXPECTED_CONTRACT_ADDRESS);
+                Assert.AreEqual(tokenId, EXPECTED_TOKEN_ID);
+            });
+        }
+
+        [UnityTest]
+        public IEnumerator NftPromptRequestFailDueToFrameRequestDifference()
+        {
+            yield return UniTask.ToCoroutine(async () =>
+            {
+                const string EXPECTED_CONTRACT_ADDRESS = "0x0temptation";
+                const string EXPECTED_TOKEN_ID = "1234";
+
+                bool requested = false;
+
+                restrictedActions.OpenNftPrompt = (contract, token) =>
+                {
+                    requested = true;
+                };
+
+                restrictedActions.GetCurrentFrameCount = () => RestrictedActionsServiceImpl.MAX_ELAPSED_FRAMES_SINCE_INPUT + 1;
+                restrictedActions.LastFrameWithInput = 0;
+
+                var result = await rpcClient.OpenNftDialog(
+                    new OpenNftDialogRequest()
+                    {
+                        Urn = $"urn:ethereum:erc1150:{EXPECTED_CONTRACT_ADDRESS}:{EXPECTED_TOKEN_ID}"
+                    });
+
+                Assert.IsFalse(result.Success);
+                Assert.IsFalse(requested);
+            });
+        }
+
+        [UnityTest]
+        public IEnumerator NftPromptRequestFailDueToBadUrn()
+        {
+            yield return UniTask.ToCoroutine(async () =>
+            {
+                bool requested = false;
+
+                restrictedActions.OpenNftPrompt = (contract, token) =>
+                {
+                    requested = true;
+                };
+
+                restrictedActions.GetCurrentFrameCount = () => 0;
+                restrictedActions.LastFrameWithInput = 0;
+
+                var result = await rpcClient.OpenNftDialog(
+                    new OpenNftDialogRequest()
+                    {
+                        Urn = string.Empty
+                    });
+
+                Assert.IsFalse(result.Success);
+                Assert.IsFalse(requested);
+            });
+        }
     }
 }
