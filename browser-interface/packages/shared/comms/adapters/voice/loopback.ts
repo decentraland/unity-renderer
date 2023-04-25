da5524b6874d437d70217cbfda29eec826f8a024
Hugo Arregui <hugo@decentraland.org>
2023-04-25 09:13:18 -0300

    chore: refactor livekit voice handler, reuse loopback interface across handlers (#5014)

--- browser-interface/packages/shared/comms/adapters/voice/loopback.ts
+++ browser-interface/packages/shared/comms/adapters/voice/loopback.ts
@@ -1,3 +1,70 @@
+import Html from './Html'
+import { isChrome } from 'lib/browser/isChrome'
+
+export type GlobalAudioStream = {
+  setGainVolume(volume: number): void
+  getDestinationStream(): MediaStream
+  getAudioContext(): AudioContext
+  getGainNode(): GainNode
+  play(): void
+}
+
+let globalAudioStream: undefined | GlobalAudioStream = undefined
+
+export async function getGlobalAudioStream() {
+  if (!globalAudioStream) {
+    globalAudioStream = await createAudioStream()
+  }
+
+  return globalAudioStream
+}
+
+export async function createAudioStream(): Promise<GlobalAudioStream> {
+  const parentElement = Html.loopbackAudioElement()
+
+  const audioContext = new AudioContext()
+  const destination = audioContext.createMediaStreamDestination()
+  const destinationStream = isChrome() ? await startLoopback(destination.stream) : destination.stream
+
+  const gainNode = audioContext.createGain()
+  gainNode.connect(destination)
+  gainNode.gain.value = 1
+
+  if (!parentElement) {
+    throw new Error('Cannot create global audio stream: no parent element')
+  }
+
+  parentElement.srcObject = destinationStream
+
+  function getGainNode() {
+    return gainNode
+  }
+
+  function setGainVolume(volume: number) {
+    gainNode.gain.value = volume
+  }
+
+  function getDestinationStream() {
+    return destinationStream
+  }
+
+  function getAudioContext() {
+    return audioContext
+  }
+
+  function play() {
+    parentElement!.play().catch(console.error)
+  }
+
+  return {
+    getGainNode,
+    setGainVolume,
+    getDestinationStream,
+    getAudioContext,
+    play
+  }
+}
+
 const offerOptions = {
   offerVideo: false,
   offerAudio: true,
