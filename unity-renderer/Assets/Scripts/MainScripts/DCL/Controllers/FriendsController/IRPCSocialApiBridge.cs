0b84f0c28be11b5f3ac3afe29e44e78797c089c2
jmoguilevsky <jmoguilevsky@gmail.com>
2023-04-26 11:46:52 -0300

    pr review

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/IRPCSocialApiBridge.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/IRPCSocialApiBridge.cs
@@ -20,11 +20,7 @@ namespace DCL.Social.Friends
 
         UniTaskVoid InitializeClient(CancellationToken cancellationToken = default);
 
-        UniTaskVoid InitializeSubscriptions(CancellationToken cancellationToken = default);
-
         UniTask<FriendshipInitializationMessage> InitializeFriendshipsInformation(CancellationToken cancellationToken = default);
-
-        UniTask ListenToFriendEvents(CancellationToken cancellationToken = default);
 
         UniTask RejectFriendship(string friendRequestId, CancellationToken cancellationToken = default);
 
@@ -32,27 +28,17 @@ namespace DCL.Social.Friends
 
         UniTask UpdateFriendship(UpdateFriendshipPayload updateFriendshipPayload, CancellationToken cancellationToken = default);
 
-        void RemoveFriend(string userId);
-
         UniTask<AddFriendsPayload> GetFriendsAsync(int limit, int skip, CancellationToken cancellationToken = default);
 
         UniTask<AddFriendsPayload> GetFriendsAsync(string usernameOrId, int limit, CancellationToken cancellationToken = default);
 
-        void GetFriendRequests(int sentLimit, int sentSkip, int receivedLimit, int receivedSkip);
-
         UniTask<AddFriendRequestsV2Payload> GetFriendRequestsAsync(int sentLimit, int sentSkip, int receivedLimit, int receivedSkip, CancellationToken cancellationToken = default);
-
-        void GetFriendsWithDirectMessages(string usernameOrId, int limit, int skip);
 
         UniTask<RequestFriendshipConfirmationPayload> RequestFriendshipAsync(string userId, string messageBody, CancellationToken cancellationToken = default);
 
         UniTask<CancelFriendshipConfirmationPayload> CancelRequestAsync(string friendRequestId, CancellationToken cancellationToken = default);
 
         UniTask CancelRequestByUserIdAsync(string userId, CancellationToken cancellationToken = default);
-
-        void CancelRequestByUserId(string userId);
-
-        void AcceptFriendship(string userId);
 
         UniTask<AcceptFriendshipPayload> AcceptFriendshipAsync(string friendRequestId, CancellationToken cancellationToken = default);
 
