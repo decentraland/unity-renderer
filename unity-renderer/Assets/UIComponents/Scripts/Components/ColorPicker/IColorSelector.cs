bb816f67111db496558ab7f7d33ad7fa27f59bb6
Santi Andrade <sandrade@decentraland.org>
2023-04-27 17:33:06 +0200

    feat: adapt color picker tool into the new backpack (#5028)
    * Create ColorPicker UI Component
    
    * Update ColorPickerComponentView.cs
    
    * Integrate color picker into the backpack prefab
    
    * Load/save selected colors into the profile
    
    * Fix tests
    
    * Minor fixes
    
    * Apply feedback
    
    * Fix missing conflicts
    
    * Fix up/down arrows in the color picker component
    
    * Apply feedback: Renamings in NftTypeColorSupportingSO
    
    * Apply feedback: Clean ColorToggle class
    
    * Apply feedback: Create ToggleAvatarSlotDelegate
    
    * Apply feedback: Fix BackpackEditorHUDControllerShould

--- unity-renderer/Assets/UIComponents/Scripts/Components/ColorPicker/IColorSelector.cs
+++ unity-renderer/Assets/UIComponents/Scripts/Components/ColorPicker/IColorSelector.cs
@@ -1,14 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public interface IColorSelector
 {
     public event System.Action<Color> OnColorSelectorChange;
-
     public void Populate(IReadOnlyList<Color> colors);
-
     public void Cleanup();
-
     public void Select(Color color);
+    void SelectRandom();
 }
