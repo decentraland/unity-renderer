39f21e8e073d4a924dcc7a8706d68aead12bcaf3
davidejensen <davidejensen@live.it>
2023-05-03 12:24:07 +0200

    Added rewards handling

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestCompletedHUD/QuestCompletedComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestCompletedHUD/QuestCompletedComponentView.cs
@@ -1,3 +1,4 @@
+using MainScripts.DCL.Helpers.Utils;
 using System.Collections.Generic;
 using TMPro;
 using UIComponents.Scripts.Components;
@@ -7,7 +8,22 @@ namespace DCL.Quest
 {
     public class QuestCompletedComponentView : BaseComponentView<QuestCompletedComponentModel>, IQuestCompletedComponentView
     {
+        private const int MAX_REWARDS_COUNT = 3;
+
+        [SerializeField] internal GameObject rewardsSection;
         [SerializeField] private TMP_Text questTitle;
+        [SerializeField] internal Transform rewardsContainer;
+
+        [SerializeField] internal QuestRewardComponentView rewardPrefab;
+
+        private UnityObjectPool<QuestRewardComponentView> rewardsPool;
+        private List<QuestRewardComponentView> usedRewards = new ();
+
+        public override void Awake()
+        {
+            rewardsPool = new UnityObjectPool<QuestRewardComponentView>(rewardPrefab, rewardsContainer);
+            rewardsPool.Prewarm(MAX_REWARDS_COUNT);
+        }
 
         public override void RefreshControl()
         {
@@ -21,10 +37,30 @@ namespace DCL.Quest
             questTitle.text = title;
         }
 
-        public void SetRewards(List<QuestRewardComponentModel> rewards)
+        public void SetRewards(List<QuestRewardComponentModel> rewardsList)
         {
-            model.rewards = rewards;
+            model.rewards = rewardsList;
 
+            for (var i = 0; i < rewardsContainer.childCount; i++)
+                Destroy(rewardsContainer.GetChild(i));
+
+            if (rewardsList == null || rewardsList.Count == 0)
+            {
+                rewardsSection.SetActive(false);
+                return;
+            }
+
+            rewardsSection.SetActive(true);
+
+            foreach (var pooledReward in usedRewards)
+                rewardsPool.Release(pooledReward);
+
+            foreach (var rewardModel in rewardsList)
+            {
+                QuestRewardComponentView pooledReward = rewardsPool.Get();
+                pooledReward.SetModel(rewardModel);
+                usedRewards.Add(pooledReward);
+            }
         }
     }
 }
