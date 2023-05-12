8bbf6a5211dbe8fdb138397f6c606884c593c2a8
jmoguilevsky <jmoguilevsky@gmail.com>
2023-05-12 12:12:57 -0300

    add tests, add equality comparisson for friend request

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/Tests/RPCSocialApiBridgeShould.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/FriendsController/Tests/RPCSocialApiBridgeShould.cs
@@ -1,6 +1,8 @@
 using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
 using Decentraland.Social.Friendships;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
 using MainScripts.DCL.Controllers.FriendsController;
 using NSubstitute;
 using NUnit.Framework;
@@ -9,7 +11,10 @@ using rpc_csharp.transport;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 using System.Threading;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -27,6 +32,7 @@ namespace DCL.Social.Friends
         private RPCSocialApiBridge rpcSocialApiBridge;
         private MockSocialServerContext context;
         private IUserProfileBridge userProfileBridge;
+        private IFriendshipsService<MockSocialServerContext> friendshipsService;
 
         private const string OWN_ID = "My custom id";
         private const string ACCESS_TOKEN = "Token";
@@ -42,7 +48,7 @@ namespace DCL.Social.Friends
 
             (var client, var server) = MemoryTransport.Create();
 
-            var friendshipsService = Substitute.For<IFriendshipsService<MockSocialServerContext>>();
+            friendshipsService = Substitute.For<IFriendshipsService<MockSocialServerContext>>();
 
             friendshipsService.GetFriends(Arg.Any<Payload>(), Arg.Any<MockSocialServerContext>())
                               .Returns(callInfo =>
@@ -128,9 +134,52 @@ namespace DCL.Social.Friends
                 userProfileBridge.RequestFullUserProfileAsync(Arg.Any<string>())
                                  .Returns(new UniTask<UserProfile>(userProfile));
 
-                await rpcSocialApiBridge.GetInitializationInformationAsync(cancellationToken);
+                var incomingResult = new List<FriendRequest>();
+                var outgoingResult = new List<FriendRequest>();
+
+                rpcSocialApiBridge.OnIncomingFriendRequestAdded += friendRequest => { incomingResult.Add(friendRequest); };
+                rpcSocialApiBridge.OnOutgoingFriendRequestAdded += friendRequest => { outgoingResult.Add(friendRequest); };
+
+                var incomingRequests = new[]
+                {
+                    NewRequest("a Message 1", "firstUser", 1),
+                    NewRequest("a Message 2", "secondUser", 2),
+                    NewRequest("a Message 3", "thirdUser", 3),
+                };
+
+                var outgoingRequests = new[]
+                {
+                    NewRequest("a Message 4", "fourthUser", 4),
+                    NewRequest("a Message 5", "fifthUser", 5),
+                    NewRequest("a Message 6", "sixthUser", 6),
+                    NewRequest("a Message 7", "seventhUser", 7),
+                };
+
+                friendshipsService.GetRequestEvents(Arg.Any<Payload>(), Arg.Any<MockSocialServerContext>(), Arg.Any<CancellationToken>())
+                                  .Returns(UniTask.FromResult(new RequestEvents()
+                                   {
+                                       Incoming = new Requests()
+                                       {
+                                           Items = { incomingRequests },
+                                           Total = incomingRequests.Length,
+                                       },
+                                       Outgoing = new Requests()
+                                       {
+                                           Items = { outgoingRequests },
+                                           Total = outgoingRequests.Length,
+                                       }
+                                   }));
+
+                var response = await rpcSocialApiBridge.GetInitializationInformationAsync(cancellationToken);
 
                 Assert.AreEqual(4, friends.Count);
+                Assert.AreEqual(incomingRequests.Length, response.totalReceivedRequests);
+
+                var expectedIncoming = incomingRequests.Select(IncomingFriendRequestFromResponse).ToList();
+                var expectedOutgoing = outgoingRequests.Select(OutgoingFriendRequestFromResponse).ToList();
+
+                CollectionAssert.AreEqual(expectedIncoming.ToList(), incomingResult);
+                CollectionAssert.AreEqual(expectedOutgoing.ToList(), outgoingResult);
             });
         }
 
@@ -212,5 +261,32 @@ namespace DCL.Social.Friends
                 }
             };
         }
+
+        private static RequestResponse NewRequest(string message, string address, long createdAt) =>
+            new ()
+            {
+                Message = message,
+                User = new User()
+                {
+                    Address = address,
+                },
+                CreatedAt = createdAt,
+            };
+
+        private static FriendRequest IncomingFriendRequestFromResponse(RequestResponse response) =>
+            new FriendRequest(
+                $"{response.User.Address}-{response.CreatedAt}",
+                response.CreatedAt,
+                response.User.Address,
+                OWN_ID,
+                response.Message);
+
+        private static FriendRequest OutgoingFriendRequestFromResponse(RequestResponse response) =>
+            new FriendRequest(
+                $"{response.User.Address}-{response.CreatedAt}",
+                response.CreatedAt,
+                OWN_ID,
+                response.User.Address,
+                response.Message);
     }
 }
