ec87fb286488b747a8de9388056405ec9a35efb8
davidejensen <davidejensen@live.it>
2023-05-13 17:00:36 +0200

    Additions to prefab and component view

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestDetails/IQuestDetailsComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestDetails/IQuestDetailsComponentView.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace DCL.Quests
 {
     public interface IQuestDetailsComponentView
     {
+        public event Action<string, bool> OnPinChange;
+        public event Action<string> OnQuestAbandon;
 
+        public void SetQuestName(string nameText);
+        public void SetQuestCreator(string questCreator);
+        public void SetQuestDescription(string questDescription);
+        public void SetQuestId(string questId);
+        public void SetCoordinates(Vector2Int coordinates);
+        public void SetQuestSteps(List<QuestStepComponentModel> questSteps);
+        public void SetQuestRewards(List<QuestRewardComponentModel> questRewards);
     }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestDetails/QuestDetailsComponentModel.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestDetails/QuestDetailsComponentModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace DCL.Quests
 {
@@ -6,6 +8,12 @@ namespace DCL.Quests
     public record QuestDetailsComponentModel
     {
         public string questName;
+        public string questCreator;
         public string questDescription;
+        public string questId;
+        public bool isPinned = false;
+        public Vector2Int coordinates;
+        public List<QuestStepComponentModel> questSteps;
+        public List<QuestRewardComponentModel> questRewards;
     }
 }
