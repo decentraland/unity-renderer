8ac847d47eed657fd8c0e8996a5c55d437ef20a5
davidejensen <davidejensen@live.it>
2023-05-15 11:32:00 +0200

    Wired UI's with test scene with mock quest service

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestsController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestsController.cs
@@ -4,69 +4,85 @@ using DCLServices.QuestsService;
 using Decentraland.Quests;
 using System;
 using System.Collections.Generic;
-using System.Threading;
 
-public class QuestsController : IDisposable
+namespace DCL.Quests
 {
-    private readonly IQuestsService questsService;
-    private readonly IUserProfileBridge userProfileBridge;
-    private readonly IQuestTrackerComponentView questTrackerComponentView;
-    private readonly IQuestCompletedComponentView questCompletedComponentView;
-    private readonly IQuestOfferComponentView questOfferComponentView;
-    private UserProfile ownUserProfile => userProfileBridge.GetOwn();
-
-    public QuestsController(
-        IQuestsService questsService,
-        IUserProfileBridge userProfileBridge,
-        IQuestTrackerComponentView questTrackerComponentView,
-        IQuestCompletedComponentView questCompletedComponentView,
-        IQuestOfferComponentView questOfferComponentView)
+    public class QuestsController : IDisposable
     {
-        this.questsService = questsService;
-        this.userProfileBridge = userProfileBridge;
-        this.questTrackerComponentView = questTrackerComponentView;
-        this.questCompletedComponentView = questCompletedComponentView;
-        this.questOfferComponentView = questOfferComponentView;
+        private readonly IQuestsService questsService;
+        private readonly IUserProfileBridge userProfileBridge;
+        private readonly IQuestTrackerComponentView questTrackerComponentView;
+        private readonly IQuestCompletedComponentView questCompletedComponentView;
+        private readonly IQuestOfferComponentView questOfferComponentView;
+        private UserProfile ownUserProfile => userProfileBridge.GetOwn();
 
-        questsService.SetUserId(ownUserProfile.userId);
-        questsService.OnQuestUpdated += UpdateQuestTracker;
-    }
-
-    private void ShowQuestOffer(QuestStateUpdate quest)
-    {
-        questOfferComponentView.SetQuestTitle(quest.Name);
-        questOfferComponentView.SetQuestDescription(quest.Description);
-        questOfferComponentView.SetIsGuest(ownUserProfile.isGuest);
-    }
-
-    private void UpdateQuestTracker(QuestStateUpdate questStateUpdate)
-    {
-        //if(questStateUpdate.QuestState.StepsLeft == 0 && questStateUpdate.QuestState.CurrentSteps.Values)
-
-        List<QuestStepComponentModel> questSteps = new List<QuestStepComponentModel>();
-        foreach (var step in questStateUpdate.QuestState.CurrentSteps)
+        public QuestsController(
+            IQuestsService questsService,
+            IUserProfileBridge userProfileBridge,
+            IQuestTrackerComponentView questTrackerComponentView,
+            IQuestCompletedComponentView questCompletedComponentView,
+            IQuestOfferComponentView questOfferComponentView)
         {
-            foreach (Task task in step.Value.TasksCompleted)
-                questSteps.Add(new QuestStepComponentModel {isCompleted = true, text = task.Description});
+            this.questsService = questsService;
+            this.userProfileBridge = userProfileBridge;
+            this.questTrackerComponentView = questTrackerComponentView;
+            this.questCompletedComponentView = questCompletedComponentView;
+            this.questOfferComponentView = questOfferComponentView;
 
-            foreach (Task task in step.Value.ToDos)
-                questSteps.Add(new QuestStepComponentModel {isCompleted = false, text = task.Description});
+            questsService.SetUserId(ownUserProfile.userId);
+            questsService.OnQuestUpdated += UpdateQuestTracker;
         }
-        questTrackerComponentView.SetQuestTitle(questStateUpdate.Name);
-        questTrackerComponentView.SetQuestSteps(questSteps);
-    }
 
-    private void AccceptQuest(string questId)
-    {
-        questsService.StartQuest(questId).Forget();
-    }
+        public QuestsController(
+            IQuestsService questsService,
+            IQuestTrackerComponentView questTrackerComponentView,
+            IQuestCompletedComponentView questCompletedComponentView,
+            IQuestOfferComponentView questOfferComponentView)
+        {
+            this.questsService = questsService;
+            this.questTrackerComponentView = questTrackerComponentView;
+            this.questCompletedComponentView = questCompletedComponentView;
+            this.questOfferComponentView = questOfferComponentView;
 
-    private void AbortQuest(string questId)
-    {
-        questsService.AbortQuest(questId).Forget();
-    }
+            questsService.OnQuestUpdated += UpdateQuestTracker;
+        }
 
-    public void Dispose()
-    {
+        private void ShowQuestOffer(QuestStateUpdate quest)
+        {
+            questOfferComponentView.SetQuestTitle(quest.Name);
+            questOfferComponentView.SetQuestDescription(quest.Description);
+            questOfferComponentView.SetIsGuest(ownUserProfile.isGuest);
+        }
+
+        private void UpdateQuestTracker(QuestStateUpdate questStateUpdate)
+        {
+            //if(questStateUpdate.QuestState.StepsLeft == 0 && questStateUpdate.QuestState.CurrentSteps.Values)
+
+            List<QuestStepComponentModel> questSteps = new List<QuestStepComponentModel>();
+
+            foreach (var step in questStateUpdate.QuestState.CurrentSteps)
+            {
+                foreach (Task task in step.Value.TasksCompleted)
+                    questSteps.Add(new QuestStepComponentModel { isCompleted = true, text = task.Id });
+
+                foreach (Task task in step.Value.ToDos)
+                    questSteps.Add(new QuestStepComponentModel { isCompleted = false, text = task.Id });
+            }
+
+            questTrackerComponentView.SetQuestTitle(questStateUpdate.Name);
+            questTrackerComponentView.SetQuestSteps(questSteps);
+        }
+
+        private void AccceptQuest(string questId)
+        {
+            questsService.StartQuest(questId).Forget();
+        }
+
+        private void AbortQuest(string questId)
+        {
+            questsService.AbortQuest(questId).Forget();
+        }
+
+        public void Dispose() { }
     }
 }
