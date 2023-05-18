501c915e1238b0ab4618bdef046515e481b7eaf1
Patricio Guerra <kinerius@gmail.com>
2023-05-18 12:02:11 -0300

    Added test scene

---
+++ unity-renderer/Assets/DCLServices/StableDiffusionService/StableDiffusionTest.cs
@@ -0,0 +1,28 @@
+﻿using DCL;
+using Newtonsoft.Json.Linq;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DCLServices.StableDiffusionService
+{
+    public class StableDiffusionTest : MonoBehaviour
+    {
+        [SerializeField] private TextAsset jsonFile;
+        [SerializeField] private RawImage targetImage;
+        private StableDiffusionService service;
+
+        private void Start()
+        {
+            service = new StableDiffusionService(WebRequestController.Create(), JObject.Parse(jsonFile.text), null);
+        }
+
+        [ContextMenu("Generate")]
+        private async void Generate()
+        {
+            service = new StableDiffusionService(WebRequestController.Create(), JObject.Parse(jsonFile.text), null);
+            var result = await service.GetTexture(new TextToImageConfig());
+            targetImage.texture = result;
+        }
+    }
+}
