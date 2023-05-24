c806c0369a0c807a14a6fa827f8c8f0e73384cfe
Vitaly Popuzin <popuzin@gmail.com>
2023-05-24 21:39:43 +0200

    moved embeded textures out to addressables

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Texture/Providers/EmbeddedTextureProvider.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Texture/Providers/EmbeddedTextureProvider.cs
@@ -1,21 +1,28 @@
 ﻿using Cysharp.Threading.Tasks;
-using MainScripts.DCL.AssetsEmbedment.Runtime;
+using DCL.Providers;
 using System.Threading;
 using UnityEngine;
 
 namespace DCL
 {
     /// <summary>
-    /// Provides textures from "Resources"
+    /// Provides embeded textures
     /// </summary>
     public class EmbeddedTextureProvider : ITextureAssetProvider
     {
+        private readonly IAddressableResourceProvider resourceProvider;
+
+        public EmbeddedTextureProvider(IAddressableResourceProvider resourceProvider)
+        {
+            this.resourceProvider = resourceProvider;
+        }
+
         public async UniTask<Texture2D> GetTextureAsync(string url, CancellationToken cancellationToken = default)
         {
-            var lastSlash = url.LastIndexOf('/');
-            var hash = lastSlash > -1 ? url.Remove(0, lastSlash + 1) : url;
-            var result = await Resources.LoadAsync<Texture2D>(EmbeddedTextureResourcesPath.VALUE + "/" + hash).WithCancellation(cancellationToken);
-            return (Texture2D)result;
+            int lastSlash = url.LastIndexOf('/');
+            string hash = lastSlash > -1 ? url.Remove(0, lastSlash + 1) : url;
+
+            return await resourceProvider.GetAddressable<Texture2D>(hash, cancellationToken);
         }
     }
 }
