ad2ed196335ab365baac63f712d1a293ffc8f339
Pravus <pravusjif@gmail.com>
2023-05-25 14:48:53 +0200

    added NewTest with commented-out test-related tags

---
+++ unity-renderer/Assets/Scripts/NewTest/NewTestShould.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    // TODO: RE-ENABLE TESTS AFTER INSTANT STEPS FIX
+    public class NewTestShould
+    {
+        // [Test]
+        public void Test1()
+        {
+        }
+    }
+}
