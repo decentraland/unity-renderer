190904495aaabfd7a34e9c7651355fdeab40fe8d
davidejensen <davidejensen@live.it>
2023-05-25 10:20:39 +0200

    Additional changes to quest log

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/IQuestLogComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/IQuestLogComponentView.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IQuestLogComponentView
+namespace DCL.Quests
 {
+    public interface IQuestLogComponentView
+    {
 
+    }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestLogComponentView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Quest_V2/QuestLogComponentView.cs
@@ -5,42 +5,45 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class QuestLogComponentView : BaseComponentView, IQuestLogComponentView
+namespace DCL.Quests
 {
-    private const string IN_PROGRESS_TITLE = "Active quests";
-    private const string COMPLETED_TITLE = "Completed quests";
-    private const int IN_PROGRESS_SECTION_INDEX = 0;
-    private const int COMPLETED_SECTION_INDEX = 1;
-
-    [SerializeField] internal SectionSelectorComponentView sectionSelector;
-    [SerializeField] internal QuestDetailsComponentView questDetailsComponentView;
-    [SerializeField] internal TMP_Text headerText;
-    [SerializeField] internal GameObject emptyState;
-    [SerializeField] internal GameObject emptyActiveState;
-    [SerializeField] internal GameObject emptyCompletedState;
-
-    public override void Awake()
+    public class QuestLogComponentView : BaseComponentView, IQuestLogComponentView
     {
-        sectionSelector.GetSection(IN_PROGRESS_SECTION_INDEX).onSelect.RemoveAllListeners();
-        sectionSelector.GetSection(COMPLETED_SECTION_INDEX).onSelect.RemoveAllListeners();
-        sectionSelector.GetSection(IN_PROGRESS_SECTION_INDEX).onSelect.AddListener((isSelected) =>
-        {
-            if (isSelected)
-            {
-                headerText.text = IN_PROGRESS_TITLE;
-            }
-        });
-        sectionSelector.GetSection(COMPLETED_SECTION_INDEX).onSelect.AddListener((isSelected) =>
-        {
-            if (isSelected)
-            {
-                headerText.text = COMPLETED_TITLE;
-            }
-        });
-    }
+        private const string IN_PROGRESS_TITLE = "Active quests";
+        private const string COMPLETED_TITLE = "Completed quests";
+        private const int IN_PROGRESS_SECTION_INDEX = 0;
+        private const int COMPLETED_SECTION_INDEX = 1;
 
-    public override void RefreshControl()
-    {
-        throw new NotImplementedException();
+        [SerializeField] internal SectionSelectorComponentView sectionSelector;
+        [SerializeField] internal QuestDetailsComponentView questDetailsComponentView;
+        [SerializeField] internal TMP_Text headerText;
+        [SerializeField] internal GameObject emptyState;
+        [SerializeField] internal GameObject emptyActiveState;
+        [SerializeField] internal GameObject emptyCompletedState;
+
+        public override void Awake()
+        {
+            sectionSelector.GetSection(IN_PROGRESS_SECTION_INDEX).onSelect.RemoveAllListeners();
+            sectionSelector.GetSection(COMPLETED_SECTION_INDEX).onSelect.RemoveAllListeners();
+            sectionSelector.GetSection(IN_PROGRESS_SECTION_INDEX).onSelect.AddListener((isSelected) =>
+            {
+                if (isSelected)
+                {
+                    headerText.text = IN_PROGRESS_TITLE;
+                }
+            });
+            sectionSelector.GetSection(COMPLETED_SECTION_INDEX).onSelect.AddListener((isSelected) =>
+            {
+                if (isSelected)
+                {
+                    headerText.text = COMPLETED_TITLE;
+                }
+            });
+        }
+
+        public override void RefreshControl()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
