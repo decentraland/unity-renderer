80c8316b26db340ae1f0a47e73f800ee8dbcbbe7
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-05-26 15:35:15 +0100

    Dummy Tests for asmdef problem

---
+++ unity-renderer/Assets/Scripts/_DummyTests/Tests02/DummyTestClass02.cs
@@ -0,0 +1,38 @@
+using Cysharp.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using DCL.Helpers;
+using DCL.Providers;
+using DCLPlugins.LoadingScreenPlugin;
+using Decentraland.Bff;
+using MainScripts.DCL.Controllers.ShaderPrewarm;
+using System;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.TestTools;
+
+namespace DCL.DummyTest.Test
+{
+    public class DummyTestClass02
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        // [Category("EditModeCI")]
+        [Test]
+        public void DummyTest02()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+    }
+}
