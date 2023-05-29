0d95299515930b83b7946fcbec1c04324bf2b213
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-05-29 11:56:01 +0100

    Fixed all errors modifying AssemblyInfo to include main DCL.PlaymodeTests for "internal" visibility

--- unity-renderer/Assets/UIComponents/AssemblyInfo.cs
+++ unity-renderer/Assets/UIComponents/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("DCL.PlaymodeTests")]
 [assembly: InternalsVisibleTo("UIComponentsTests")]
 [assembly: InternalsVisibleTo("ExploreV2Tests")]
 [assembly: InternalsVisibleTo("ExperiencesViewerTests")]
