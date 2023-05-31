e4ffc18e9584ef254173b80609b485afb91a01b4
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-05-31 10:15:01 +0100

    updated AssmblyInfo

--- unity-renderer/Assets/UIComponents/AssemblyInfo.cs
+++ unity-renderer/Assets/UIComponents/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("DCL.PlaymodeTests")]
-[assembly: InternalsVisibleTo("UIComponentsTests")]
 [assembly: InternalsVisibleTo("ExploreV2Tests")]
 [assembly: InternalsVisibleTo("ExperiencesViewerTests")]
 [assembly: InternalsVisibleTo("VoiceChatHUDTests")]
