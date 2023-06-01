048136b315a69d0e40ad9003b8d324e1868b4e30
Patricio Guerra <kinerius@decentraland.org>
2023-06-01 15:46:52 -0300

    chore: Remove old GLTF importer (#5209)
    * Removed GLTF importer and replaced all of its usages with the new one
    
    * Fixed local file urls not working with gltfast
    
    * Fixed more tests
    
    * Fixed more tests
    
    * Fixed even more tests
    
    * Fixed more tests v3v1.5vFinalPostav2.jpg
    
    * Fixed AvatarShape tests, they are now super fast and wont fail.
    
    * Fixed tests getting stuck in batchmode
    
    * removed cache
    
    * Removed more cache
    
    * Added cache back
    
    * Material caching gone
    
    * Added back material optimization to ABs
    
    * Removed all gltfast feature flag remainders
    
    * Fixed white avatars
    
    * Rollback unintentional deletion of folder
    
    * Suggestion
    
    * More suggestions
    
    * removed permitted sources for gltf textures
    
    * Added back the support for old gltf hierarchy
    
    * Fixed conflicts after rebase
    
    * Fixed GltfContainerCollidersHandler not supporting skinned mesh renderers
    
    * Changed the example glb and rolled back the GltfContainerCollidersHandler change
    
    * Added test case to comply with the ADR-215 specification to not support skinned meshes
    
    * Fixed null reference and invalid wearable on test

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarShape.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarShape.cs
@@ -206,18 +206,19 @@ namespace DCL
                     playerName.Show(true);
                 }
 
-                avatar.Load(wearableItems, emotes.ToList(), new AvatarSettings
+                var forceRender = new HashSet<string>(userProfileBridge.Get(model.id)?.avatar.forceRender ?? new HashSet<string>());
+
+                var avatarSettings = new AvatarSettings
                 {
                     playerName = model.name,
                     bodyshapeId = model.bodyShape,
                     eyesColor = model.eyeColor,
                     skinColor = model.skinColor,
                     hairColor = model.hairColor,
-                    forceRender = new HashSet<string>(userProfileBridge.Get(model.id).avatar.forceRender ?? new HashSet<string>()),
-                }, loadingCts.Token);
+                    forceRender = forceRender,
+                };
 
-                // Yielding a UniTask doesn't do anything, we manually wait until the avatar is ready
-                yield return new WaitUntil(() => avatar.status == IAvatar.Status.Loaded);
+                yield return avatar.Load(wearableItems, emotes.ToList(), avatarSettings, loadingCts.Token).ToCoroutine(Debug.LogException);
             }
 
             avatar.PlayEmote(model.expressionTriggerId, model.expressionTriggerTimestamp);
@@ -246,7 +247,7 @@ namespace DCL
                 entity, player
             );
 
-            outlineOnHover.Initialize(new OnPointerDown.Model(), entity, player.avatar);
+            outlineOnHover.Initialize(new OnPointerDown.Model(), entity, player?.avatar);
 
             avatarCollider.gameObject.SetActive(true);
 
