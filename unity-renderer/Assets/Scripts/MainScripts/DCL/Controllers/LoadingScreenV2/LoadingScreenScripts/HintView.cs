763714f48112de0201d9881392096f0b5c285b5d
D-Fabio <10808443+0xD-Fabio@users.noreply.github.com>
2023-06-02 17:31:24 +0100

    First draft LoadingScreenHintsController classes

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/LoadingScreenV2/LoadingScreenScripts/HintView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/LoadingScreenV2/LoadingScreenScripts/HintView.cs
@@ -1,8 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
-public class HintView
+namespace DCL.Controllers.LoadingScreenV2
 {
+    /// <summary>
+    /// View responsible of showing the corresponding provided Hint by LoadingScreenHintsController
+    /// </summary>
+    public class HintView : MonoBehaviour
+    {
+        [SerializeField] internal TMP_Text hintText;
+        [SerializeField] internal Image hintImage;
 
+        public void Initialize(Hint hint, Texture2D texture)
+        {
+            hintText.text = hint.Title;
+            hintImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            HideHint();
+        }
+
+        public void ShowHint()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void HideHint()
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
 }
--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/LoadingScreenV2/LoadingScreenScripts/LoadingScreenHintsController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/LoadingScreenV2/LoadingScreenScripts/LoadingScreenHintsController.cs
@@ -1,4 +1,9 @@
+using Cysharp.Threading.Tasks;
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace DCL.Controllers.LoadingScreenV2
 {
@@ -16,7 +21,102 @@ namespace DCL.Controllers.LoadingScreenV2
     /// </summary>
     public class LoadingScreenHintsController
     {
+        private readonly TimeSpan SHOWING_TIME_HINTS = TimeSpan.FromSeconds(5);
+        private const int MAX_HINTS = 15;
 
+        private readonly HintView hintViewPrefab;
+        private readonly HintRequestService hintRequestService;
+        private readonly List<HintView> hintViewPool;
+
+        private Dictionary<int, Tuple<Hint, Texture2D>> hintsDictionary;
+        private int currentHintIndex = 0;
+
+        private CancellationTokenSource disposeCts;
+
+        public LoadingScreenHintsController(HintView hintViewPrefab, HintRequestService hintRequestService)
+        {
+            this.hintViewPrefab = hintViewPrefab;
+            this.hintRequestService = hintRequestService;
+
+            hintsDictionary = new Dictionary<int, Tuple<Hint, Texture2D>>();
+            hintViewPool = new List<HintView>();
+        }
+
+        public async UniTask RequestHints(CancellationToken ctx)
+        {
+            hintViewPool.ForEach(hintView => hintView.HideHint());
+            hintsDictionary.Clear();
+
+            Dictionary<Hint, Texture2D> hintsResult = await hintRequestService.RequestHintsFromSources(ctx, MAX_HINTS);
+            int index = 0;
+            foreach (var hintResult in hintsResult)
+            {
+                hintsDictionary.Add(index++, new Tuple<Hint, Texture2D>(hintResult.Key, hintResult.Value));
+            }
+
+            if (hintsDictionary.Count == 0)
+                return;
+
+            StartHintsCarousel();
+        }
+
+        private async UniTask IterateHintsAsync()
+        {
+            while (!disposeCts.Token.IsCancellationRequested)
+            {
+                CarouselNextHint();
+                await UniTask.Delay(SHOWING_TIME_HINTS, cancellationToken: disposeCts.Token);
+            }
+        }
+
+        public void StopHintsCarousel()
+        {
+            if (disposeCts ==  null) return;
+
+            disposeCts.Cancel();
+            disposeCts.Dispose();
+            disposeCts = null;
+        }
+
+        public void StartHintsCarousel()
+        {
+            if (disposeCts != null) return;
+
+            disposeCts = new CancellationTokenSource();
+            IterateHintsAsync().Forget();
+        }
+
+        public void CarouselNextHint()
+        {
+            hintViewPool[currentHintIndex].HideHint();
+            currentHintIndex = (currentHintIndex + 1) % hintsDictionary.Count;
+            UpdateHintView();
+        }
+
+        private void UpdateHintView()
+        {
+            var hintTuple = hintsDictionary[currentHintIndex];
+            HintView hintView = hintViewPool[currentHintIndex];
+            hintView.Initialize(hintTuple.Item1, hintTuple.Item2);
+            hintView.ShowHint();
+        }
+
+        public void Dispose()
+        {
+            StopHintsCarousel();
+
+            foreach (var hintKvp in hintsDictionary)
+            {
+                Object.Destroy(hintKvp.Value.Item2);
+            }
+
+            hintsDictionary.Clear();
+            hintViewPool.ForEach(hintView => hintView.HideHint());
+        }
     }
+
 }
+
+
+
 
