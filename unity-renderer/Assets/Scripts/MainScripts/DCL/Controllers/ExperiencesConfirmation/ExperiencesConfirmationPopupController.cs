755a13285cd821236d13494583706ef37fa1c529
Nicolas Lorusso <nalorusso@gmail.com>
2023-07-12 17:40:26 -0300

    refactor user input into async task

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExperiencesConfirmation/ExperiencesConfirmationPopupController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExperiencesConfirmation/ExperiencesConfirmationPopupController.cs
@@ -1,20 +1,32 @@
+using Cysharp.Threading.Tasks;
+using DCL.Tasks;
 using DCL.World.PortableExperiences;
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
 
 namespace DCL.PortableExperiences.Confirmation
 {
     public class ExperiencesConfirmationPopupController
     {
+        private enum InputType
+        {
+            Accept,
+            Reject,
+            Cancel,
+        }
+
         private readonly IExperiencesConfirmationPopupView view;
         private readonly DataStore dataStore;
+        private readonly IConfirmedExperiencesRepository confirmedExperiencesRepository;
         private readonly IUserProfileBridge userProfileBridge;
         private readonly List<string> descriptionBuffer = new ();
 
-        private Action acceptCallback;
-        private Action rejectCallback;
         private string experienceId;
         private bool dontShowAnymore;
+        private CancellationTokenSource openProcessCancellationToken = new ();
+        private UniTaskCompletionSource<InputType> userInputTask = new ();
 
         public ExperiencesConfirmationPopupController(IExperiencesConfirmationPopupView view,
             DataStore dataStore,
@@ -23,36 +35,14 @@ namespace DCL.PortableExperiences.Confirmation
         {
             this.view = view;
             this.dataStore = dataStore;
+            this.confirmedExperiencesRepository = confirmedExperiencesRepository;
             this.userProfileBridge = userProfileBridge;
 
             view.Hide(true);
 
-            view.OnAccepted += () =>
-            {
-                view.Hide();
-
-                if (dontShowAnymore)
-                    confirmedExperiencesRepository.Set(experienceId, true);
-
-                acceptCallback?.Invoke();
-            };
-
-            view.OnRejected += () =>
-            {
-                view.Hide();
-
-                if (dontShowAnymore)
-                    confirmedExperiencesRepository.Set(experienceId, false);
-
-                rejectCallback?.Invoke();
-            };
-
-            view.OnCancelled += () =>
-            {
-                view.Hide();
-                rejectCallback?.Invoke();
-            };
-
+            view.OnAccepted += () => userInputTask.TrySetResult(InputType.Accept);
+            view.OnRejected += () => userInputTask.TrySetResult(InputType.Reject);
+            view.OnCancelled += () => userInputTask.TrySetResult(InputType.Cancel);
             view.OnDontShowAnymore += () => dontShowAnymore = true;
             view.OnKeepShowing += () => dontShowAnymore = false;
 
@@ -62,38 +52,76 @@ namespace DCL.PortableExperiences.Confirmation
         public void Dispose()
         {
             view.Dispose();
+            openProcessCancellationToken.SafeCancelAndDispose();
 
             dataStore.world.portableExperiencePendingToConfirm.OnChange -= OnConfirmRequested;
         }
 
         private void OnConfirmRequested(ExperiencesConfirmationData current, ExperiencesConfirmationData previous)
         {
-            string pxId = current.Experience.ExperienceId;
-
-            ExperiencesConfirmationData.ExperienceMetadata metadata = current.Experience;
-
-            experienceId = pxId;
-            acceptCallback = current.OnAcceptCallback;
-            rejectCallback = current.OnRejectCallback;
-
-            descriptionBuffer.Clear();
-
-            if (metadata.Permissions != null)
+            async UniTaskVoid ShowAndWaitForUserInputThenHide(
+                ExperiencesConfirmationData confirmationData,
+                CancellationToken cancellationToken)
             {
-                foreach (string permission in metadata.Permissions)
-                    descriptionBuffer.Add(ConvertPermissionIdToDescription(permission));
+                string pxId = confirmationData.Experience.ExperienceId;
+
+                ExperiencesConfirmationData.ExperienceMetadata metadata = confirmationData.Experience;
+
+                experienceId = pxId;
+
+                descriptionBuffer.Clear();
+
+                if (metadata.Permissions != null)
+                {
+                    foreach (string permission in metadata.Permissions)
+                        descriptionBuffer.Add(ConvertPermissionIdToDescription(permission));
+                }
+
+                view.Show();
+
+                view.SetModel(new ExperiencesConfirmationViewModel
+                {
+                    Name = metadata.ExperienceName,
+                    IconUrl = metadata.IconUrl,
+                    Permissions = descriptionBuffer,
+                    Description = metadata.Description,
+                    IsSmartWearable = userProfileBridge.GetOwn().avatar.wearables.Contains(pxId),
+                });
+
+                try
+                {
+                    InputType inputType = await userInputTask.Task.AttachExternalCancellation(cancellationToken);
+
+                    switch (inputType)
+                    {
+                        case InputType.Accept:
+                            if (dontShowAnymore)
+                                confirmedExperiencesRepository.Set(experienceId, true);
+
+                            confirmationData.OnAcceptCallback?.Invoke();
+                            break;
+                        case InputType.Reject:
+                            if (dontShowAnymore)
+                                confirmedExperiencesRepository.Set(experienceId, false);
+
+                            confirmationData.OnRejectCallback?.Invoke();
+                            break;
+                        case InputType.Cancel:
+                            confirmationData.OnRejectCallback?.Invoke();
+                            break;
+                    }
+
+                    view.Hide();
+                }
+                catch (OperationCanceledException) { }
+                catch (Exception e) { Debug.LogException(e); }
             }
 
-            view.Show();
-
-            view.SetModel(new ExperiencesConfirmationViewModel
-            {
-                Name = metadata.ExperienceName,
-                IconUrl = metadata.IconUrl,
-                Permissions = descriptionBuffer,
-                Description = metadata.Description,
-                IsSmartWearable = userProfileBridge.GetOwn().avatar.wearables.Contains(pxId),
-            });
+            openProcessCancellationToken = openProcessCancellationToken.SafeRestart();
+            openProcessCancellationToken = new CancellationTokenSource();
+            userInputTask.TrySetCanceled();
+            userInputTask = new UniTaskCompletionSource<InputType>();
+            ShowAndWaitForUserInputThenHide(current, openProcessCancellationToken.Token).Forget();
         }
 
         private string ConvertPermissionIdToDescription(string permissionId)
