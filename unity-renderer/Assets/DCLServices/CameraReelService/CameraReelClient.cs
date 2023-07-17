8e2332b80aa997e5076d592688a9dd0165c286ac
Vitaly Popuzin <popuzin@gmail.com>
2023-07-17 18:16:36 +0200

    added mock for the Service

---
+++ unity-renderer/Assets/DCLServices/CameraReelService/CameraReelClient.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using DCL;
+using System;
+using System.Threading;
+
+namespace DCLServices.CameraReelService
+{
+    public interface ICameraReelClient
+    {
+        UniTask GetScreenshot(string placeUUID, CancellationToken ct);
+
+        UniTask PutScreenshot(string placeUUID, CancellationToken ct);
+    }
+
+    public class CameraReelClient : ICameraReelClient
+    {
+        private const string BASE_URL = "https://places.decentraland.org/api/places";
+
+        private readonly IWebRequestController webRequestController;
+
+        public CameraReelClient(IWebRequestController webRequestController)
+        {
+            this.webRequestController = webRequestController;
+        }
+
+        public UniTask GetScreenshot(string placeUUID, CancellationToken ct) =>
+            throw new NotImplementedException();
+
+        public UniTask PutScreenshot(string placeUUID, CancellationToken ct) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ unity-renderer/Assets/DCLServices/CameraReelService/CameraReelNetworkService.cs
@@ -0,0 +1,34 @@
+﻿using Cysharp.Threading.Tasks;
+using DCL;
+using System;
+using System.Threading;
+
+namespace DCLServices.CameraReelService
+{
+    public interface ICameraReelNetworkService : IService
+    {
+        UniTask GetScreenshot(string placeUUID, CancellationToken ct, bool renewCache = false);
+
+        UniTask PutScreenshot(string placeUUID, CancellationToken ct, bool renewCache = false);
+    }
+
+    public class CameraReelNetworkService : ICameraReelNetworkService
+    {
+        private readonly ICameraReelClient client;
+
+        public CameraReelNetworkService(ICameraReelClient client)
+        {
+            this.client = client;
+        }
+
+        public void Initialize() { }
+
+        public void Dispose() { }
+
+        public UniTask GetScreenshot(string placeUUID, CancellationToken ct, bool renewCache = false) =>
+            throw new NotImplementedException();
+
+        public UniTask PutScreenshot(string placeUUID, CancellationToken ct, bool renewCache = false) =>
+            throw new NotImplementedException();
+    }
+}
