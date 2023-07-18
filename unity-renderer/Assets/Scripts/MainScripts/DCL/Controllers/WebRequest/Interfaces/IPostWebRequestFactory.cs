2d1cca3e02cd123c184077df3663c3e6192601f4
Vitaly Popuzin <popuzin@gmail.com>
2023-07-18 20:20:42 +0200

    added multiPart to the Web Post request

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/WebRequest/Interfaces/IPostWebRequestFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/WebRequest/Interfaces/IPostWebRequestFactory.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
 namespace DCL
 {
     public interface IPostWebRequestFactory : IWebRequestFactory
     {
         void SetBody(string postData);
+
+        public void SetBody(List<IMultipartFormSection> data);
     }
 }
