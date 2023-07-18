8ef902e7c582cceed69a2dce65624f6eef786c13
Vitaly Popuzin <popuzin@gmail.com>
2023-07-18 20:20:42 +0200

    added multiPart to the Web Post request

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/WebRequest/PostWebRequestFactory.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/WebRequest/PostWebRequestFactory.cs
@@ -1,21 +1,38 @@
+using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace DCL
 {
     public class PostWebRequestFactory : IPostWebRequestFactory
     {
-        private string postData;
+        private byte[] postData;
+        private string contentType;
 
-        public void SetBody(string data) =>
-            this.postData = data;
+        public void SetBody(string data)
+        {
+            this.postData = Encoding.UTF8.GetBytes(data);
+            this.contentType = "application/x-www-form-urlencoded";
+        }
+
+        public void SetBody(List<IMultipartFormSection> data)
+        {
+            WWWForm form = new WWWForm();
+            foreach (var section in data)
+                form.AddBinaryData(section.sectionName, section.sectionData, section.fileName, section.contentType);
+
+            this.postData = form.data;
+            this.contentType = "multipart/form-data";
+        }
 
         public UnityWebRequest CreateWebRequest(string url)
         {
-            var unityWebRequest = UnityWebRequest.Post(url, postData);
-            byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
-            unityWebRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            UnityWebRequest unityWebRequest = new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST);
+            unityWebRequest.uploadHandler = new UploadHandlerRaw(postData);
             unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+            unityWebRequest.SetRequestHeader("Content-Type", contentType);
+
             return unityWebRequest;
         }
     }
