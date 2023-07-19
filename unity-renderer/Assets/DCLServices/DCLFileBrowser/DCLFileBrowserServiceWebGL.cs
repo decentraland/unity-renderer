9c9124e1a73e0795ec02015688384722e54da3e4
Alejandro Jimenez <ajimenez@decentraland.org>
2023-07-19 15:23:51 +0200

    fix: extension issue

--- unity-renderer/Assets/DCLServices/DCLFileBrowser/DCLFileBrowserServiceWebGL.cs
+++ unity-renderer/Assets/DCLServices/DCLFileBrowser/DCLFileBrowserServiceWebGL.cs
@@ -18,20 +18,38 @@ namespace DCLServices.DCLFileBrowser
 
         public void SaveFile(string title, string directory, string defaultName, byte[] content, params ExtensionFilter[] extensions)
         {
-            SaveFile(defaultName, content);
+            SaveFile(defaultName, content, extensions);
         }
 
         public UniTask SaveFileAsync(string title, string directory, string defaultName, byte[] content, params ExtensionFilter[] extensions)
         {
-            SaveFile(defaultName, content);
+            SaveFile(defaultName, content, extensions);
             return UniTask.CompletedTask;
         }
 
-        private void SaveFile(string fileName, byte[] content)
+        private void SaveFile(string fileName, byte[] content, ExtensionFilter[] extensionsFilter)
         {
+            if (extensionsFilter.Length > 0 && extensionsFilter[0].Extensions.Length > 0  && !FileNameContainsAnyExtension(fileName, extensionsFilter))
+            {
+                fileName += $".{extensionsFilter[0].Extensions[0]}";
+            }
+
             if (!WebGLFileSaver.IsSavingSupported())
                 throw new Exception("File saving is not supported in the browser");
             WebGLFileSaver.SaveFile(content, fileName);
+        }
+
+        private bool FileNameContainsAnyExtension(string fileName, ExtensionFilter[] extensionsFilter)
+        {
+            foreach (ExtensionFilter extensionFilter in extensionsFilter)
+            {
+                foreach (string extension in extensionFilter.Extensions)
+                {
+                    if (fileName.EndsWith(extension))
+                        return true;
+                }
+            }
+            return false;
         }
     }
 }
