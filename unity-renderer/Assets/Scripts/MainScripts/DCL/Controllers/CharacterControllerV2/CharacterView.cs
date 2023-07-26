14998249b9d07a202a86499ab8b351506ce4b619
Patricio Guerra <kinerius@gmail.com>
2023-07-26 12:21:34 -0300

    Added a nice debug UI to modify speed values

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/CharacterControllerV2/CharacterView.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/CharacterControllerV2/CharacterView.cs
@@ -4,7 +4,10 @@ using DCL.Helpers;
 using JetBrains.Annotations;
 using MainScripts.DCL.Controllers.CharacterController;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
 using Environment = DCL.Environment;
 
 namespace MainScripts.DCL.Controllers.CharacterControllerV2
@@ -46,6 +49,29 @@ namespace MainScripts.DCL.Controllers.CharacterControllerV2
             var worldData = DataStore.i.Get<DataStore_World>();
             worldData.avatarTransform.Set(avatarGameObject.transform);
             worldData.fpsTransform.Set(firstPersonCameraGameObject.transform);
+
+            // REMOVE THIS AFTER WE ARE DONE EDITING VALUES
+            var mouseCatcher = new GameObject("DebugMouseCatcher");
+            var panel = new GameObject("Panel");
+            panel.transform.SetParent(mouseCatcher.transform);
+            mouseCatcher.AddComponent<RectTransform>();
+
+            var background = panel.AddComponent<Image>();
+            Color backgroundColor = Color.gray;
+            backgroundColor.a = 0.15f;
+            background.color = backgroundColor;
+
+            var rectTransform = panel.GetComponent<RectTransform>();
+            rectTransform.pivot = new Vector2(0, 1);
+            rectTransform.anchorMin = new Vector2(0, 1);
+            rectTransform.anchorMax = new Vector2(0, 1);
+            rectTransform.anchoredPosition = new Vector2(300, -10f);
+            rectTransform.sizeDelta = new Vector2(75+30+300, (7 * 40) + 14);
+
+            var cv = mouseCatcher.AddComponent<Canvas>();
+            cv.renderMode = RenderMode.ScreenSpaceOverlay;
+            var rc = mouseCatcher.AddComponent<GraphicRaycaster>();
+            rc.blockingMask = Physics.AllLayers;
         }
 
         [Obsolete]
@@ -101,6 +127,46 @@ namespace MainScripts.DCL.Controllers.CharacterControllerV2
             CommonScriptableObjects.characterForward.Set(forward);
             CommonScriptableObjects.playerUnityEulerAngles.Set(transform.eulerAngles);
         }
+
+        private int accumulation = 0;
+        private Dictionary<string, string> valuesTemp = new Dictionary<string, string>();
+
+        private void OnGUI()
+        {
+            accumulation = 0;
+            var baseWidth = 300;
+            var separation1 = 200;
+
+            GUI.skin.label.fontSize = 30;
+            GUI.skin.textField.fontSize = 30;
+            data.walkSpeed = DrawFloatSlider(baseWidth, separation1, data.walkSpeed, "walkSpeed");
+            data.jogSpeed = DrawFloatSlider(baseWidth, separation1, data.jogSpeed, "jogSpeed");
+            data.runSpeed = DrawFloatSlider(baseWidth, separation1, data.runSpeed, "runSpeed");
+            data.acceleration = DrawFloatSlider(baseWidth, separation1, data.acceleration, "acceleration");
+            data.airAcceleration = DrawFloatSlider(baseWidth, separation1, data.airAcceleration, "airAcceleration");
+            data.gravity = DrawFloatSlider(baseWidth, separation1, data.gravity, "gravity");
+            data.jumpHeight = DrawFloatSlider(baseWidth, separation1, data.jumpHeight, "jumpHeight");
+        }
+
+
+        private float DrawFloatSlider(int baseWidth, int separation1, float value, string label)
+        {
+            if (!valuesTemp.ContainsKey(label))
+                valuesTemp.Add(label, value.ToString());
+
+            var fieldHeight = 40;
+            GUI.Label(new Rect(Width(baseWidth + fieldHeight), Height(fieldHeight + accumulation), Width(200), Height(fieldHeight)), label);
+            string result = GUI.TextField(new Rect(Width(baseWidth + fieldHeight + separation1), Height(fieldHeight + accumulation), Width(100), Height(fieldHeight)), valuesTemp[label]);
+            valuesTemp[label] = result;
+            accumulation += fieldHeight + 2;
+            return float.TryParse(result, out float newNumber) ? newNumber : value;
+        }
+
+        private float Width(float value) =>
+            value * Screen.width / 1920f;
+
+        private float Height(float value) =>
+            value * Screen.height / 1080f;
     }
 
     public interface ICharacterView
