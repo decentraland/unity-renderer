5a6c619b07299d839f4d2068265ef6ba9ee5373c
Pato <pato@decentraland.org>
2023-07-26 13:37:37 -0300

    disabled api call outside worlds

--- browser-interface/packages/shared/apis/host/CommsAPI.ts
+++ browser-interface/packages/shared/apis/host/CommsAPI.ts
@@ -3,10 +3,21 @@ import * as codegen from '@dcl/rpc/dist/codegen'
 import type { PortContext } from './context'
 import { VideoTracksActiveStreamsRequest, VideoTracksActiveStreamsData, CommsApiServiceDefinition } from 'shared/protocol/decentraland/kernel/apis/comms_api.gen'
 import { RemoteParticipant } from 'livekit-client'
+import { getRealmAdapter } from 'shared/realm/selectors'
+import { store } from 'shared/store/isolatedStore'
+import { dclWorldUrl, isDclEns } from 'shared/realm/resolver'
 
 export function registerCommsApiServiceServerImplementation(port: RpcServerPort<PortContext>) {
     codegen.registerService(port, CommsApiServiceDefinition, async () => ({
         async getActiveVideoStreams(req: VideoTracksActiveStreamsRequest, ctx: PortContext) {
+
+            const realmAdapter = getRealmAdapter(store.getState())
+            const realmName = realmAdapter?.about.configurations?.realmName
+            const isWorld = isDclEns(realmName) && realmAdapter?.baseUrl === dclWorldUrl(realmName)
+            if (!isWorld){
+                ctx.logger.error('API only available for Worlds')
+                return { streams: [] }
+            }
 
             // TODO: scene permissions
 
