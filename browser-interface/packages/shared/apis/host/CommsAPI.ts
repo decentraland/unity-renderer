656e056150ef58d4450e45ce1c4950f9d73ad258
Pato <pato@decentraland.org>
2023-07-27 17:27:00 -0300

    fix world check

--- browser-interface/packages/shared/apis/host/CommsAPI.ts
+++ browser-interface/packages/shared/apis/host/CommsAPI.ts
@@ -3,18 +3,17 @@ import * as codegen from '@dcl/rpc/dist/codegen'
 import type { PortContext } from './context'
 import { VideoTracksActiveStreamsRequest, VideoTracksActiveStreamsData, CommsApiServiceDefinition } from 'shared/protocol/decentraland/kernel/apis/comms_api.gen'
 import { RemoteParticipant } from 'livekit-client'
-import { getRealmAdapter } from 'shared/realm/selectors'
+import { isWorldLoaderActive } from 'shared/realm/selectors'
 import { store } from 'shared/store/isolatedStore'
-import { dclWorldUrl, isDclEns } from 'shared/realm/resolver'
 import { getLivekitParticipants } from 'shared/comms/selectors'
+import { ensureRealmAdapter } from 'shared/realm/ensureRealmAdapter'
 
 export function registerCommsApiServiceServerImplementation(port: RpcServerPort<PortContext>) {
     codegen.registerService(port, CommsApiServiceDefinition, async () => ({
         async getActiveVideoStreams(req: VideoTracksActiveStreamsRequest, ctx: PortContext) {
 
-            const realmAdapter = getRealmAdapter(store.getState())
-            const realmName = realmAdapter?.about.configurations?.realmName
-            const isWorld = isDclEns(realmName) && realmAdapter?.baseUrl === dclWorldUrl(realmName)
+            const realmAdapter = await ensureRealmAdapter()
+            const isWorld = isWorldLoaderActive(realmAdapter)
             if (!isWorld){
                 ctx.logger.error('API only available for Worlds')
                 return { streams: [] }
