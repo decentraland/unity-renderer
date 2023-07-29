bcf62ed83386ae76a654b7c64f56b1488998d7bd
Vitaly Popuzin <popuzin@gmail.com>
2023-07-29 16:21:17 +0200

    split Gallery and Section mediator

---
+++ unity-renderer/Assets/Features/CameraReel/Gallery/Scripts/CameraReelGalleryView.cs
@@ -0,0 +1,80 @@
+﻿using DCL;
+using DCLServices.CameraReelService;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Environment = DCL.Environment;
+
+namespace CameraReel.Gallery
+{
+    public class CameraReelGalleryView : MonoBehaviour
+    {
+        private const int LIMIT = 20;
+        private readonly LinkedList<CameraReelResponse> reels = new ();
+
+        [SerializeField] internal GridContainerComponentView monthGridContainer;
+        [SerializeField] internal Image thumbnailPrefab;
+        [SerializeField] private Button showMore;
+
+        private int offset;
+
+        private ICameraReelNetworkService cameraReelNetworkServiceLazy;
+        private ICameraReelNetworkService cameraReelNetworkService => cameraReelNetworkServiceLazy ??= Environment.i.serviceLocator.Get<ICameraReelNetworkService>();
+
+        public event Action<CameraReelResponse> ScreenshotThumbnailClicked;
+        public event Action<(int current, int max)> ScreenshotsLoaded;
+
+        private void OnEnable()
+        {
+            showMore.onClick.AddListener(LoadImages);
+        }
+
+        private void OnDisable()
+        {
+            showMore.onClick.RemoveAllListeners();
+        }
+
+        private async void LoadImages()
+        {
+            CameraReelResponses reelImages = await cameraReelNetworkService.GetScreenshotGallery(DataStore.i.player.ownPlayer.Get().id, LIMIT, offset);
+            offset += LIMIT;
+
+            ScreenshotsLoaded?.Invoke((reelImages.currentImages, reelImages.maxImages));
+            StartCoroutine(DownloadImageAndCreateObject(reelImages.images));
+        }
+
+        private IEnumerator DownloadImageAndCreateObject(List<CameraReelResponse> reelImages)
+        {
+            foreach (CameraReelResponse reel in reelImages)
+            {
+                reels.AddLast(reel);
+
+                UnityWebRequest request = UnityWebRequestTexture.GetTexture(reel.thumbnailUrl);
+                yield return request.SendWebRequest();
+
+                if (request.result != UnityWebRequest.Result.Success)
+                    Debug.Log(request.error);
+                else
+                {
+                    Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+
+                    Image image = Instantiate(thumbnailPrefab, monthGridContainer.transform);
+                    image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    image.gameObject.SetActive(true);
+
+                    Button button = image.GetComponent<Button>();
+                    button.onClick.AddListener( () => ScreenshotThumbnailClicked?.Invoke(reel));
+                }
+            }
+        }
+
+        public CameraReelResponse GetPreviousScreenshot(CameraReelResponse current) =>
+            reels.Find(current)?.Previous?.Value;
+
+        public CameraReelResponse GetNextScreenshot(CameraReelResponse current) =>
+            reels.Find(current)?.Next?.Value;
+    }
+}
--- unity-renderer/Assets/Features/CameraReel/Gallery/Scripts/CameraReelSectionView.cs
+++ unity-renderer/Assets/Features/CameraReel/Gallery/Scripts/CameraReelSectionView.cs
@@ -1,98 +1,61 @@
+using CameraReel.Gallery;
 using CameraReel.ScreenshotViewer;
-using DCL;
 using DCLServices.CameraReelService;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class CameraReelSectionView : MonoBehaviour
 {
-    private const int LIMIT = 20;
-
-    [SerializeField] internal GridContainerComponentView gridContrainer;
-    [SerializeField] internal Image prefab;
-
     [SerializeField] private Slider storageBar;
-    [SerializeField] private Button showMore;
 
-    [Space(10)]
+    [SerializeField] private CameraReelGalleryView galleryView;
     [SerializeField] private ScreenshotViewerHUDView screenshotViewer;
 
-    private int offset;
-    private readonly LinkedList<CameraReelResponse> reels = new ();
+    private void Awake()
+    {
+        storageBar.gameObject.SetActive(false);
+    }
 
     private void OnEnable()
     {
-        showMore.onClick.AddListener(LoadImages);
-        screenshotViewer.PrevScreenshotClicked += LoadPrevScreenshot;
-        screenshotViewer.NextScreenshotClicked += LoadNextScreenshot;
+        galleryView.ScreenshotsLoaded += UpdateStorageBar;
+        galleryView.ScreenshotThumbnailClicked += ShowScreenshotWithMetadata;
+        screenshotViewer.PrevScreenshotClicked += ShowPrevScreenshot;
+        screenshotViewer.NextScreenshotClicked += ShowNextScreenshot;
     }
 
     private void OnDisable()
     {
-        showMore.onClick.RemoveAllListeners();
-        screenshotViewer.PrevScreenshotClicked -= LoadPrevScreenshot;
-        screenshotViewer.NextScreenshotClicked -= LoadNextScreenshot;
+        galleryView.ScreenshotsLoaded -= UpdateStorageBar;
+        galleryView.ScreenshotThumbnailClicked -= ShowScreenshotWithMetadata;
+        screenshotViewer.PrevScreenshotClicked -= ShowPrevScreenshot;
+        screenshotViewer.NextScreenshotClicked -= ShowNextScreenshot;
     }
 
-    private void LoadNextScreenshot(CameraReelResponse current)
+    private void UpdateStorageBar((int current, int max) storage)
     {
-        CameraReelResponse next = reels.Find(current)?.Next?.Value;
-
-        if (next != null)
-            ShowScreenshotWithMetadata(next);
-    }
-
-    private void LoadPrevScreenshot(CameraReelResponse current)
-    {
-        CameraReelResponse prev = reels.Find(current)?.Previous?.Value;
-
-        if (prev != null)
-            ShowScreenshotWithMetadata(prev);
-    }
-
-    private async void LoadImages()
-    {
-        ICameraReelNetworkService cameraReelNetworkService = Environment.i.serviceLocator.Get<ICameraReelNetworkService>();
-        CameraReelResponses reelImages = await cameraReelNetworkService.GetScreenshotGallery(DataStore.i.player.ownPlayer.Get().id, LIMIT, offset);
-
-        storageBar.value = reelImages.currentImages;
-        storageBar.maxValue = reelImages.maxImages;
-
-        offset += LIMIT;
-
-        StartCoroutine(DownloadImageAndCreateObject(reelImages.images));
-    }
-
-    private IEnumerator DownloadImageAndCreateObject(List<CameraReelResponse> reelImages)
-    {
-        foreach (CameraReelResponse reel in reelImages)
-        {
-            reels.AddLast(reel);
-
-            UnityWebRequest request = UnityWebRequestTexture.GetTexture(reel.thumbnailUrl);
-            yield return request.SendWebRequest();
-
-            if (request.result != UnityWebRequest.Result.Success)
-                Debug.Log(request.error);
-            else
-            {
-                Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-
-                Image image = Instantiate(prefab, gridContrainer.transform);
-                image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-                image.gameObject.SetActive(true);
-
-                Button button = image.GetComponent<Button>();
-                button.onClick.AddListener(() => ShowScreenshotWithMetadata(reel));
-            }
-        }
+        storageBar.value = storage.current;
+        storageBar.maxValue = storage.max;;
     }
 
     private void ShowScreenshotWithMetadata(CameraReelResponse reel)
     {
         screenshotViewer.Show(reel);
+    }
+
+    private void ShowNextScreenshot(CameraReelResponse current)
+    {
+        CameraReelResponse next = galleryView.GetNextScreenshot(current);
+
+        if (next != null)
+            ShowScreenshotWithMetadata(next);
+    }
+
+    private void ShowPrevScreenshot(CameraReelResponse current)
+    {
+        CameraReelResponse prev = galleryView.GetPreviousScreenshot(current);
+
+        if (prev != null)
+            ShowScreenshotWithMetadata(prev);
     }
 }
