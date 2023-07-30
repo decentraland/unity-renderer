831075a82e2cd820be273bb510a175336a5f8118
Vitaly Popuzin <popuzin@gmail.com>
2023-07-30 12:28:37 +0200

    missed changes

--- unity-renderer/Assets/Features/CameraReel/Gallery/Scripts/CameraReelGalleryView.cs
+++ unity-renderer/Assets/Features/CameraReel/Gallery/Scripts/CameraReelGalleryView.cs
@@ -1,8 +1,9 @@
-﻿using DCL;
+﻿using Cysharp.Threading.Tasks;
+using DCL;
 using DCLServices.CameraReelService;
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -15,10 +16,16 @@ namespace CameraReel.Gallery
         private const int LIMIT = 20;
         private readonly LinkedList<CameraReelResponse> reels = new ();
 
-        [SerializeField] internal GridContainerComponentView monthGridContainer;
+        [SerializeField] internal GameObject monthSeparatorTemplate;
+        [SerializeField] internal GridContainerComponentView monthGridContainerTemplate;
         [SerializeField] internal Image thumbnailPrefab;
+        [SerializeField] internal Transform container;
+
         [SerializeField] private Button showMore;
 
+        private readonly Dictionary<int, GridContainerComponentView> monthContainers = new ();
+
+        internal GridContainerComponentView currentMonthGridContainer;
         private int offset;
 
         private ICameraReelNetworkService cameraReelNetworkServiceLazy;
@@ -39,35 +46,60 @@ namespace CameraReel.Gallery
 
         private async void LoadImages()
         {
-            CameraReelResponses reelImages = await cameraReelNetworkService.GetScreenshotGallery(DataStore.i.player.ownPlayer.Get().id, LIMIT, offset);
-            offset += LIMIT;
+            CameraReelResponses reelImages = await cameraReelNetworkService.GetScreenshotGallery(
+                DataStore.i.player.ownPlayer.Get().id, LIMIT, offset);
 
             ScreenshotsLoaded?.Invoke((reelImages.currentImages, reelImages.maxImages));
-            StartCoroutine(DownloadImageAndCreateObject(reelImages.images));
+
+            offset += LIMIT;
+
+            DownloadImageAndCreateObject(reelImages.images);
         }
 
-        private IEnumerator DownloadImageAndCreateObject(List<CameraReelResponse> reelImages)
+        private void DownloadImageAndCreateObject(List<CameraReelResponse> reelImages)
         {
             foreach (CameraReelResponse reel in reelImages)
             {
                 reels.AddLast(reel);
 
-                UnityWebRequest request = UnityWebRequestTexture.GetTexture(reel.thumbnailUrl);
-                yield return request.SendWebRequest();
+                int month = reel.metadata.GetLocalizedDateTime().Month;
 
-                if (request.result != UnityWebRequest.Result.Success)
-                    Debug.Log(request.error);
+                GridContainerComponentView gridContainer;
+
+                if (!monthContainers.ContainsKey(month))
+                {
+                    var separator = Instantiate(monthSeparatorTemplate, container);
+                    separator.gameObject.SetActive(true);
+                    gridContainer = Instantiate(monthGridContainerTemplate, container);
+                    gridContainer.gameObject.SetActive(true);
+
+                    monthContainers.Add(month, gridContainer);
+                }
                 else
                 {
-                    Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-
-                    Image image = Instantiate(thumbnailPrefab, monthGridContainer.transform);
-                    image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-                    image.gameObject.SetActive(true);
-
-                    Button button = image.GetComponent<Button>();
-                    button.onClick.AddListener( () => ScreenshotThumbnailClicked?.Invoke(reel));
+                    gridContainer = monthContainers[month];
                 }
+
+                Image image = Instantiate(thumbnailPrefab, gridContainer.transform);
+                image.gameObject.SetActive(true);
+
+                Button button = image.GetComponent<Button>();
+                button.onClick.AddListener( () => ScreenshotThumbnailClicked?.Invoke(reel));
+
+                SetThumbnailFromWebAsync(reel, image);
+            }
+        }
+
+        private static async Task SetThumbnailFromWebAsync(CameraReelResponse reel, Image image)
+        {
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(reel.thumbnailUrl);
+            await request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+                Debug.Log(request.error);
+            else
+            {
+                Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
             }
         }
 
