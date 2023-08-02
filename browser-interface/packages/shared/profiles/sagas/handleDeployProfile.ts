825a9684f3201016657b9891a4eb68a75390f731
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2023-08-02 16:00:57 -0300

    WIP: add logs

--- browser-interface/packages/shared/profiles/sagas/handleDeployProfile.ts
+++ browser-interface/packages/shared/profiles/sagas/handleDeployProfile.ts
@@ -28,6 +28,7 @@ export function* handleDeployProfile(deployProfileAction: DeployProfile) {
   const identity: ExplorerIdentity = yield select(getCurrentIdentity)
   const userId: string = yield select(getCurrentUserId)
   const profile: Avatar = deployProfileAction.payload.profile
+  defaultLogger.info('about to deploy profile', profile)
   try {
     yield call(deployAvatar, {
       url: profileServiceUrl,
