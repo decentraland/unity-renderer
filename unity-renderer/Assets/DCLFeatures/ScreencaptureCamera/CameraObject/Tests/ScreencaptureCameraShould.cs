81d38e306d8ab4fd7eaa427d244d08e116066031
Vitaly Popuzin <popuzin@gmail.com>
2023-08-07 20:09:27 +0200

    reverted from changing Resolution to CaptureScreenshot

--- unity-renderer/Assets/DCLFeatures/ScreencaptureCamera/CameraObject/Tests/ScreencaptureCameraShould.cs
+++ unity-renderer/Assets/DCLFeatures/ScreencaptureCamera/CameraObject/Tests/ScreencaptureCameraShould.cs
@@ -140,7 +140,7 @@ namespace DCLFeatures.ScreencaptureCamera.Tests
     {
         public ScreenRecorderDummy() : base(null, null, null) { }
 
-        public override Texture2D CaptureScreenshot(int targetScreenWidth, int targetScreenHeight) =>
+        public override Texture2D CaptureScreenshot() =>
             new (1, 1);
     }
 
