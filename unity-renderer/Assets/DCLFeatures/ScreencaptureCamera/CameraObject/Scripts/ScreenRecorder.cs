99a0410aa468fb7600bac704b33def5542c07fba
Vitaly Popuzin <popuzin@gmail.com>
2023-08-07 12:48:37 +0200

    wip on taking screenshot with bloom

--- unity-renderer/Assets/DCLFeatures/ScreencaptureCamera/CameraObject/Scripts/ScreenRecorder.cs
+++ unity-renderer/Assets/DCLFeatures/ScreencaptureCamera/CameraObject/Scripts/ScreenRecorder.cs
@@ -8,74 +8,62 @@ namespace DCLFeatures.ScreencaptureCamera
         private const int FRAME_WIDTH = 1920;
         private const int FRAME_HEIGHT = 1080;
 
-        private readonly Camera screenshotCamera;
         private readonly Sprite sprite;
         private readonly RectTransform imageRectTransform;
         private readonly RectTransform canvasRectTransform;
 
         private (float width, float height) spriteRect;
 
-        public ScreenRecorder(Camera screenshotCamera, RectTransform canvasRectTransform, Sprite sprite, RectTransform spriteRectTransform)
+        public ScreenRecorder(RectTransform canvasRectTransform, Sprite sprite, RectTransform spriteRectTransform)
         {
             this.sprite = sprite;
             imageRectTransform = spriteRectTransform;
 
-            this.screenshotCamera = screenshotCamera;
             this.canvasRectTransform = canvasRectTransform;
         }
 
+        private readonly Texture2D finalTexture = new (FRAME_WIDTH, FRAME_HEIGHT, TextureFormat.RGB24, false);
+
         public virtual Texture2D CaptureScreenshot()
         {
-            Debug.Log($"Screen res: {Screen.width}:{Screen.height}");
-            Debug.Log($"Sprite frame res: {spriteRect.width}:{spriteRect.height}");
-
+            Debug.Log($"golden rule rect {imageRectTransform.rect.width}:{imageRectTransform.rect.height}");
+            Debug.Log($"sprite bounds {sprite.bounds.size.x}:{sprite.bounds.size.y}");
             spriteRect = GetCurrentSpriteResolution(imageRectTransform.rect, sprite.bounds);
+            Debug.Log($"sprite bounds {spriteRect.width}:{spriteRect.height}");
 
             float scaleFactorW = FRAME_WIDTH / spriteRect.width;
             float scaleFactorH = FRAME_HEIGHT / spriteRect.height;
-            Debug.Log($"Frame scale factor: {scaleFactorW}:{scaleFactorH}");
-            Debug.Log($"Needed new res: {Screen.width * scaleFactorW}:{Screen.height * scaleFactorH}");
 
             int upscalingFactor = Mathf.CeilToInt(Mathf.Max(scaleFactorW, scaleFactorH));
-            Debug.Log($"Upscaling factor rounded: {upscalingFactor}");
+            Debug.Log($"Scale factor {scaleFactorW}:{scaleFactorH} = {upscalingFactor}");
 
             Texture2D screenshotTexture = ScreenCapture.CaptureScreenshotAsTexture(upscalingFactor);
-            string path = Application.dataPath + "/_UpscaledScreenshot1.png";
+            var path = Application.dataPath + "/_UpscaledScreenshot1.png";
             File.WriteAllBytes(path, screenshotTexture.EncodeToJPG());
             Debug.Log($"Upscaled res: {Screen.width * upscalingFactor}:{Screen.height * upscalingFactor}");
             Debug.Log($"Saved to {path}");
 
+            // TODO(Vit): optimize this by removing intermediate texture creation. Read/Write bilinear of cropped frame
+            // Scaling to have 1920x1080 in center
             int imageToCropWidth = Mathf.RoundToInt(canvasRectTransform.rect.width * scaleFactorW);
             int imageToCropHeight = Mathf.RoundToInt(canvasRectTransform.rect.height * scaleFactorH);
             Texture2D rescaledScreenshot = ScaleTexture(screenshotTexture, imageToCropWidth, imageToCropHeight);
-            path = Application.dataPath + "/_RescaledScreenshot1.png";
-            File.WriteAllBytes(path, rescaledScreenshot.EncodeToJPG());
-            Debug.Log($"Rescaled to: {imageToCropWidth}:{imageToCropHeight}");
-            Debug.Log($"Saved to {path}");
+            Debug.Log($"Image to crop {imageToCropWidth}:{imageToCropHeight}");
 
+            // Cropping 1920x1080 central part
             int cornerX = Mathf.RoundToInt((imageToCropWidth - FRAME_WIDTH) / 2f);
             int cornerY = Mathf.RoundToInt((imageToCropHeight - FRAME_HEIGHT) / 2f);
+            Debug.Log($"Coreners {cornerX}:{cornerY}");
 
             Color[] pixels = rescaledScreenshot.GetPixels(cornerX, cornerY, Mathf.RoundToInt(FRAME_WIDTH), Mathf.RoundToInt(FRAME_HEIGHT));
-            var texture = new Texture2D(FRAME_WIDTH, FRAME_HEIGHT, TextureFormat.RGB24, false);
-            texture.SetPixels(pixels);
-            texture.Apply();
+            finalTexture.SetPixels(pixels);
+            finalTexture.Apply();
 
-            path = Application.dataPath + "/_FinalScreenshot1.png";
-            File.WriteAllBytes(path, texture.EncodeToJPG());
+             path = Application.dataPath + "/_FinalScreenshot1.png";
+            File.WriteAllBytes(path, finalTexture.EncodeToJPG());
             Debug.Log($"Saved to {path}");
 
-            // // int newFrameWidth = Mathf.RoundToInt(spriteRect.width * scaleFactorW);
-            // // int newFrameHeight = Mathf.RoundToInt(spriteRect.height * scaleFactorH);
-            // // Debug.Log($"New Sprite frame res: {newFrameWidth}:{newFrameHeight}");
-
-            // // // Clean up the screenshot texture
-            // // // Object.Destroy(screenshotTexture);
-
-            // // // Check for the HDR being saved correctly, but being lost when converting to JPG
-            // // // File.WriteAllBytes("Assets/screenshot1.png", texture.EncodeToEXR(Texture2D.EXRFlags.CompressZIP));
-
-            return texture;
+            return finalTexture;
         }
 
         private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
