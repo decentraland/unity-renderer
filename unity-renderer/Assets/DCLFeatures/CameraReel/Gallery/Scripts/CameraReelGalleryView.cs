2b90629710663bf9ad9a1cb89e7adfdb88b3f65f
Nicolas Lorusso <nalorusso@gmail.com>
2023-08-09 17:35:06 -0300

    ignore localization in month names

--- unity-renderer/Assets/DCLFeatures/CameraReel/Gallery/Scripts/CameraReelGalleryView.cs
+++ unity-renderer/Assets/DCLFeatures/CameraReel/Gallery/Scripts/CameraReelGalleryView.cs
@@ -146,7 +146,52 @@ namespace DCLFeatures.CameraReel.Gallery
             monthContainers.Remove(date);
         }
 
-        private string GetMonthName(DateTime date) =>
-            date.ToString("MMMM yyyy", CultureInfo.InvariantCulture);
+        private string GetMonthName(DateTime date)
+        {
+            var month = "";
+
+            switch (date.Month)
+            {
+                case 1:
+                    month = "January";
+                    break;
+                case 2:
+                    month = "February";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "September";
+                    break;
+                case 10:
+                    month = "October";
+                    break;
+                case 11:
+                    month = "November";
+                    break;
+                case 12:
+                    month = "December";
+                    break;
+            }
+
+            return $"{month} {date.Year}";
+        }
+
     }
 }
