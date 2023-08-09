83a9f40e73c37edbc0419e6396b2bafdf4b79be3
Santi Andrade <sandrade@decentraland.org>
2023-08-09 14:31:32 +0200

    Revert "Re-design event modal"
    This reverts commit c1f41b5c7b863eeb8a8d92b9a1954d0c21aa6112.

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExploreV2/Tests/EventCardComponentViewTests.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/ExploreV2/Tests/EventCardComponentViewTests.cs
@@ -151,8 +151,7 @@ public class EventCardComponentViewTests
         // Assert
         Assert.AreEqual(isLive, eventCardComponent.model.isLive, "The event card isLive does not match in the model.");
         Assert.AreEqual(isLive, eventCardComponent.liveTag.gameObject.activeSelf);
-        if (!isEventCardModal)
-            Assert.AreEqual(!isLive, eventCardComponent.eventDateText.gameObject.activeSelf);
+        Assert.AreEqual(!isLive, eventCardComponent.eventDateText.gameObject.activeSelf);
         Assert.AreEqual(isEventCardModal || isLive, eventCardComponent.jumpinButton.gameObject.activeSelf);
         Assert.AreEqual(!isLive && !eventCardComponent.model.eventFromAPIInfo.attending, eventCardComponent.subscribeEventButton.gameObject.activeSelf);
         Assert.AreEqual(!isLive && eventCardComponent.model.eventFromAPIInfo.attending, eventCardComponent.unsubscribeEventButton.gameObject.activeSelf);
@@ -313,6 +312,7 @@ public class EventCardComponentViewTests
 
         // Assert
         Assert.AreEqual(testCoords, eventCardModalComponent.model.coords, "The event card coords does not match in the model.");
+        Assert.AreEqual($"{testCoords.x},{testCoords.y}", eventCardModalComponent.jumpinButton.model.text);
     }
 
     [Test]
