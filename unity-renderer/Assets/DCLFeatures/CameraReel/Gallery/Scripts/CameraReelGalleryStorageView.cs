83698dced68860615dd018b0c8414cdadebd2ff3
Nicolas Lorusso <nalorusso@gmail.com>
2023-08-16 16:48:19 -0300

    hide storage header until the loading is complete & fixed the storage bar displaying

--- unity-renderer/Assets/DCLFeatures/CameraReel/Gallery/Scripts/CameraReelGalleryStorageView.cs
+++ unity-renderer/Assets/DCLFeatures/CameraReel/Gallery/Scripts/CameraReelGalleryStorageView.cs
@@ -10,12 +10,6 @@ namespace DCLFeatures.CameraReel.Gallery
         [SerializeField] private TMP_Text storageText;
         [SerializeField] private GameObject storageFullMessage;
 
-        private void Awake()
-        {
-            storageBar.gameObject.SetActive(false);
-            storageText.gameObject.SetActive(false);
-        }
-
         public void UpdateStorageBar(int current, int max)
         {
             storageText.text = $"Storage: {current}/{max} photo taken";
@@ -26,5 +20,8 @@ namespace DCLFeatures.CameraReel.Gallery
             storageBar.value = current;
             storageBar.gameObject.SetActive(true);
         }
+
+        public void Show() =>
+            gameObject.SetActive(true);
     }
 }
