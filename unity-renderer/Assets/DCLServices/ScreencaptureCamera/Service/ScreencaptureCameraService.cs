5237e2315787d2391619c8bd095c2d78090b3736
Vitaly Popuzin <popuzin@gmail.com>
2023-08-17 20:32:52 +0200

    WIP on converting to Service and Addressables

---
+++ unity-renderer/Assets/DCLServices/ScreencaptureCamera/Service/ScreencaptureCameraService.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using DCL.Providers;
+using DCLFeatures.ScreencaptureCamera.CameraObject;
+using System.Threading;
+
+namespace DCLServices.ScreencaptureCamera.Service
+{
+    public class ScreencaptureCameraService : IScreencaptureCameraService
+    {
+        private readonly IAddressableResourceProvider resourceProvider;
+        private ScreencaptureCameraBehaviour cameraBehaviour;
+
+        public ScreencaptureCameraService(IAddressableResourceProvider resourceProvider)
+        {
+            this.resourceProvider = resourceProvider;
+        }
+
+        public void Initialize() { }
+
+        public async UniTask InitializeAsync(CancellationToken cancellationToken)
+        {
+            cameraBehaviour = await resourceProvider.Instantiate<ScreencaptureCameraBehaviour>("ScreencaptureCameraController", cancellationToken: cancellationToken);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
