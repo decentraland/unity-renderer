68758f4148773e883fd0d784dea3bd66193f652d
Santi Andrade <sandrade@decentraland.org>
2023-08-28 12:04:37 +0200

    feat: host events in worlds (part 1) (#5621)
    * Merge the [live] tag with the users in event cards
    
    * Add new fields in EventFromAPIModel
    
    * Update EventsSubSectionComponentController.cs
    
    * fix
    
    * Clean up unnecessary code
    
    * Show world address in the JumpIn button
    
    * Polish event card
    
    * Polish event pop up
    
    * Polish pop up. Update textures.
    
    * Show world icon in world’s events
    
    * Fix the number of users for world’s cards
    
    * Add place name into the hover area of the cards
    
    ---------
    
    Co-authored-by: Mateo Kuruk Miccino <mateomiccino@gmail.com>
    Co-authored-by: Romina Marchetti <rma.marchetti@gmail.com>

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/Interfaces/ITeleportController.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/Interfaces/ITeleportController.cs
@@ -3,7 +3,7 @@ using DCL;
 public interface ITeleportController : IService
 {
     void Teleport(int x, int y);
-    void JumpIn(int coordsX, int coordsY, string serverName, string layerName);
+    void JumpIn(int coordsX, int coordsY, string serverName, string layerName = null);
     void GoToCrowd();
     void GoToMagic();
 }
