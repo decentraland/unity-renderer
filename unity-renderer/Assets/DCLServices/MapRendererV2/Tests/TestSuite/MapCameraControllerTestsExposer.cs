139a791e6d8c9632b24d3c4defdb299822cc6ff1
Vitaly Popuzin <popuzin@gmail.com>
2023-09-20 17:39:41 +0200

    fixed Parcel selector

--- unity-renderer/Assets/DCLServices/MapRendererV2/Tests/TestSuite/MapCameraControllerTestsExposer.cs
+++ unity-renderer/Assets/DCLServices/MapRendererV2/Tests/TestSuite/MapCameraControllerTestsExposer.cs
@@ -5,12 +5,11 @@ namespace DCLServices.MapRendererV2.MapCameraController
 {
     internal partial class MapCameraController
     {
-#if UNITY_EDITOR
         internal float CAMERA_HEIGHT_EXPOSED => CAMERA_HEIGHT;
+
         internal ICoordsUtils CoordUtils => coordsUtils;
         internal MapCameraObject MapCameraObject => mapCameraObject;
         internal RenderTexture RenderTexture => renderTexture;
-        internal Vector2Int zoom => zoomValues;
-#endif
+        internal Vector2Int ZoomValues => zoomValues;
     }
 }
