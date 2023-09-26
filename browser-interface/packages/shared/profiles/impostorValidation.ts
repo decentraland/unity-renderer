53ffcedc0c341ae858412d5c33218b4bfc7de8aa
Nicolas Lorusso <nalorusso@gmail.com>
2023-09-26 14:43:40 -0300

    use of recoverAddressFromEthSignature to get signer

--- browser-interface/packages/shared/profiles/impostorValidation.ts
+++ browser-interface/packages/shared/profiles/impostorValidation.ts
@@ -1,16 +1,19 @@
 import {RemoteProfileWithHash} from "./types";
 import {hashV1} from "@dcl/hashing";
 import {Avatar} from "@dcl/schemas";
-import {hash, recover} from "eth-crypto";
+import {recoverAddressFromEthSignature} from "@dcl/crypto/dist/crypto";
 
-export async function isImpostor(profile: RemoteProfileWithHash, catalystPublicKey: string | undefined): Promise<boolean> {
-  let profileChecksum = await getProfileChecksum(profile.profile.avatars[0]);
-  let signature = recover(profile.signedHash, hash.keccak256(profile.hash));
-  return profileChecksum != profile.hash || signature != catalystPublicKey
+export async function isImpostor(profile: RemoteProfileWithHash, signer: string | undefined): Promise<boolean> {
+  let checksum = await getProfileChecksum(profile.profile.avatars[0]);
+  return checksum != profile.hash || getSigner(profile) != signer
 }
 
 async function getProfileChecksum(avatar: Avatar) {
   const encoder = new TextEncoder()
   const payload = JSON.stringify([avatar.name, avatar.hasClaimedName, ...avatar.avatar.wearables])
   return await hashV1(encoder.encode(payload));
+}
+
+function getSigner(profile: RemoteProfileWithHash): string {
+  return recoverAddressFromEthSignature(profile.signedHash, profile.hash)
 }
