4f83b010c9e4bb2620ea687e22ad9b8672c88a1f
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-09-26 10:51:17 -0300

    change about usage from renderer-protocol

--- browser-interface/packages/shared/dao/index.ts
+++ browser-interface/packages/shared/dao/index.ts
@@ -5,7 +5,7 @@ import { fetchCatalystNodesFromContract } from 'lib/web3/fetchCatalystNodesFromC
 import { CatalystNode } from 'lib/web3/fetchCatalystNodesFromContract'
 import { commsLogger } from 'shared/comms/logger'
 import { getDisabledCatalystConfig } from 'shared/meta/selectors'
-import { AboutResponse } from 'shared/protocol/decentraland/realm/about.gen'
+import { AboutResponse } from 'shared/protocol/decentraland/renderer/about.gen'
 import { setRealmAdapter } from 'shared/realm/actions'
 import {
   adapterForRealmConfig,
