abcc155b286b0e7e74fb62ba2bc8b651131844a7
Vitaly Popuzin <popuzin@gmail.com>
2023-09-27 20:01:44 +0200

    add feature flag

--- unity-renderer/Assets/DCLServices/MapRendererV2/MapLayers/PlayerMarker/PlayerMarkerObject.cs
+++ unity-renderer/Assets/DCLServices/MapRendererV2/MapLayers/PlayerMarker/PlayerMarkerObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DCL;
+using UnityEngine;
 
 namespace DCLServices.MapRendererV2.MapLayers.PlayerMarker
 {
@@ -6,6 +7,12 @@ namespace DCLServices.MapRendererV2.MapLayers.PlayerMarker
     {
         [SerializeField] private SpriteRenderer compass;
         [SerializeField] private SpriteRenderer circle;
+
+        private void Start()
+        {
+            if (!DataStore.i.featureFlags.flags.Get().IsFeatureEnabled("map_focus_home_or_user"))
+                circle.enabled = false;
+        }
 
         public void SetSortingOrder(int sortingOrder)
         {
