59a004e885e93b16a4041468ad470f38082b669b
Vitaly Popuzin <popuzin@gmail.com>
2023-10-04 20:15:30 +0200

    text adjustment

--- unity-renderer/Assets/DCLServices/MapRendererV2/MapLayers/PointsOfInterest/SceneOfInterestMarkerObject.cs
+++ unity-renderer/Assets/DCLServices/MapRendererV2/MapLayers/PointsOfInterest/SceneOfInterestMarkerObject.cs
@@ -8,11 +8,15 @@ namespace DCLServices.MapRendererV2.MapLayers.PointsOfInterest
         [field: SerializeField] internal TextMeshPro title { get; set; }
         [field: SerializeField] internal SpriteRenderer[] renderers { get; private set; }
 
-        private float baseTextScale;
+        private float titleBaseScale;
+        private Vector3 titleBasePosition;
+
+        private const float Y_POSITION_OFFSET = -0.2f;
 
         private void Awake()
         {
-            baseTextScale = title.transform.localScale.x;
+            titleBaseScale = title.transform.localScale.x;
+            titleBasePosition = title.transform.localPosition;
         }
 
         public void SetScale(float baseScale, float newScale)
@@ -20,9 +24,17 @@ namespace DCLServices.MapRendererV2.MapLayers.PointsOfInterest
             transform.localScale = new Vector3(newScale, newScale, 1f);
 
             // Apply inverse scaling to the text object
-            float textScaleFactor = baseScale / newScale; // Calculate the inverse scale factor
-            title.transform.localScale = new Vector3(baseTextScale * textScaleFactor, baseTextScale * textScaleFactor, 1f);
+            float positionFactor = newScale / baseScale;
+            float yOffset = (1 - positionFactor) * Y_POSITION_OFFSET;
 
+            float yValue = yOffset < 0.9f
+                ? titleBasePosition.y + yOffset
+                : titleBasePosition.y / positionFactor;
+
+            title.transform.localPosition =  new Vector3(titleBasePosition.x, yValue, titleBasePosition.z);
+
+            float textScaleFactor = baseScale / newScale; // Calculate the inverse scale factor
+            title.transform.localScale = new Vector3(titleBaseScale * textScaleFactor, titleBaseScale * textScaleFactor, 1f);
         }
     }
 }
