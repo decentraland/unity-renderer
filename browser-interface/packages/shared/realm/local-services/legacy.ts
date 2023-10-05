2a3616f527d293413147ec8539fbf8781b1fd17e
Hugo Arregui <hugo@decentraland.org>
2023-10-05 12:42:46 -0300

    Save Thu Oct  5 12:42:45 PM -03 2023

--- browser-interface/packages/shared/realm/local-services/legacy.ts
+++ browser-interface/packages/shared/realm/local-services/legacy.ts
@@ -1,15 +1,12 @@
 import { ETHEREUM_NETWORK } from 'config'
 import { AboutResponse } from 'shared/protocol/decentraland/renderer/about.gen'
 import { LegacyServices } from '../types'
-import { store } from 'shared/store/isolatedStore'
-import { getSelectedNetwork } from 'shared/dao/selectors'
 
-export function legacyServices(baseUrl: string, about: AboutResponse): LegacyServices {
+export function legacyServices(network: ETHEREUM_NETWORK, baseUrl: string, about: AboutResponse): LegacyServices {
   const contentServer = about.content?.publicUrl || baseUrl + '/content'
   const lambdasServer = about.lambdas?.publicUrl || baseUrl + '/lambdas'
 
-  const state = store.getState()
-  const tld = getSelectedNetwork(state) === ETHEREUM_NETWORK.MAINNET ? 'org' : 'zone'
+  const tld = network === ETHEREUM_NETWORK.MAINNET ? 'org' : 'zone'
   const realmProviderUrl = `https://realm-provider.decentraland.${tld}`
 
   return {
