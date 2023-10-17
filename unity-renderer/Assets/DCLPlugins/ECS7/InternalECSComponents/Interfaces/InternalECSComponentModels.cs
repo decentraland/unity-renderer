80d8b959db59b9570ee3ed3b970fe01fde87c963
Pravus <pravusjif@gmail.com>
2023-10-17 12:55:33 +0200

    added some tests

--- unity-renderer/Assets/DCLPlugins/ECS7/InternalECSComponents/Interfaces/InternalECSComponentModels.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/InternalECSComponents/Interfaces/InternalECSComponentModels.cs
@@ -192,7 +192,7 @@ namespace DCL.ECS7.InternalComponents
             this.shouldSort = false;
 
             this.rootElement = new VisualElement();
-            this.rootElement.pickingMode = PickingMode.Ignore; // ignore mouse by default
+            this.rootElement.pickingMode = PickingMode.Ignore; // ignore pointer by default
             rootElement.name += $"(Id: {entityId})";
         }
     }
