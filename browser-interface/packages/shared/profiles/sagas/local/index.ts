edf7cf0eb0bfa7aa0fe877f523a7c9284cc01581
Nicolas Lorusso <nalorusso@gmail.com>
2023-11-10 16:12:52 -0300

    Revert "Revert "fix: crossplatform profile (#5910)""
    This reverts commit d4e84f4e9fe456233a768e33ca6039c99eb20736.

--- browser-interface/packages/shared/profiles/sagas/local/index.ts
+++ browser-interface/packages/shared/profiles/sagas/local/index.ts
@@ -1,12 +1,11 @@
 import type { Avatar } from '@dcl/schemas'
 import type { ETHEREUM_NETWORK } from 'config'
 import type { ExplorerIdentity } from 'shared/session/types'
-import { apply, put, select } from 'redux-saga/effects'
+import { apply, select } from 'redux-saga/effects'
 import { ensureAvatarCompatibilityFormat } from 'lib/decentraland/profiles/transformations/profileToServerFormat'
 import { localProfilesRepo } from './localProfilesRepo'
 import { getCurrentIdentity, getCurrentNetwork } from 'shared/session/selectors'
 import defaultLogger from 'lib/logger'
-import { profileSuccess } from 'shared/profiles/actions'
 
 export function* fetchLocalProfile() {
   const network: ETHEREUM_NETWORK = yield select(getCurrentNetwork)
@@ -14,8 +13,7 @@ export function* fetchLocalProfile() {
   const profile = (yield apply(localProfilesRepo, localProfilesRepo.get, [identity.address, network])) as Avatar | null
   if (profile && profile.userId === identity.address) {
     try {
-      const finalProfile = ensureAvatarCompatibilityFormat(profile)
-      yield put(profileSuccess(finalProfile))
+      return ensureAvatarCompatibilityFormat(profile)
     } catch (error) {
       defaultLogger.log(`Invalid profile stored: ${JSON.stringify(profile, null, 2)}`)
       return null
