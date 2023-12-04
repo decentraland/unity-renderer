78a3a2c93d4bfa73be2db9f7bb7c6b88f86bb284
Hugo Arregui <hugo@decentraland.org>
2023-12-04 16:06:55 -0300

    Save Mon Dec  4 04:06:55 PM -03 2023

--- browser-interface/packages/lib/web3/getEthereumNetworkFromProvider.ts
+++ browser-interface/packages/lib/web3/getEthereumNetworkFromProvider.ts
@@ -2,8 +2,12 @@ import { ETHEREUM_NETWORK } from 'config'
 import { requestManager } from './provider'
 
 export async function getEthereumNetworkFromProvider(): Promise<ETHEREUM_NETWORK> {
-  const web3Network = await requestManager.net_version()
-  const chainId = parseInt(web3Network, 10)
-  const web3net = chainId === 1 ? ETHEREUM_NETWORK.MAINNET : ETHEREUM_NETWORK.SEPOLIA
-  return web3net
+  try {
+    const web3Network = await requestManager.net_version()
+    const chainId = parseInt(web3Network, 10)
+    const web3net = chainId === 1 ? ETHEREUM_NETWORK.MAINNET : ETHEREUM_NETWORK.SEPOLIA
+    return web3net
+  } catch (err) {
+    return ETHEREUM_NETWORK.MAINNET
+  }
 }
