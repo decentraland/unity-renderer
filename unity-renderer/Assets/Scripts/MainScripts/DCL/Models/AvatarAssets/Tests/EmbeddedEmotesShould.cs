776923afbbc971bb7f32ee370014331565373c7a
Agnieszka Besz <aga@besz.me>
2024-01-17 15:36:36 +0000

    Add test to prevent underscore in embedded emotes id

---
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Models/AvatarAssets/Tests/EmbeddedEmotesShould.cs
@@ -0,0 +1,29 @@
+﻿using DCL.Emotes;
+using NUnit.Framework;
+using UnityEditor;
+
+namespace AvatarAssets.Tests
+{
+    [Category("EditModeCI")]
+    public class EmbeddedEmotesShould
+    {
+        [Test]
+        public void ValidateEmbeddedEmotesData()
+        {
+            var assetPath = "Assets/DCLServices/EmotesService/Addressables/EmbeddedEmotes.asset";
+            var asset = AssetDatabase.LoadAssetAtPath<EmbeddedEmotesSO>(assetPath);
+
+            Assert.NotNull(asset);
+
+            EmbeddedEmote[] embeddedEmotes = asset.GetEmbeddedEmotes();
+
+            // Check the count of embedded emotes
+            Assert.AreEqual(embeddedEmotes.Length, 33);
+
+            // Ensure that no emote ID contains an underscore
+            // validation on catalyst to save the profile will reject the request
+            foreach (var emote in embeddedEmotes)
+                Assert.IsFalse(emote.id.Contains("_"), $"Emote ID '{emote.id}' should not contain an underscore.");
+        }
+    }
+}
