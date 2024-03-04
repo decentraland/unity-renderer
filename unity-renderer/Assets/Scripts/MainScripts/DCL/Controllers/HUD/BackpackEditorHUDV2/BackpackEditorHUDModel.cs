852b6183a0c77723571ceb2be8a412879e11363c
Nicolas Lorusso <nalorusso@gmail.com>
2024-03-04 17:10:20 -0300

    add extended urns for saving outfits

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BackpackEditorHUDV2/BackpackEditorHUDModel.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BackpackEditorHUDV2/BackpackEditorHUDModel.cs
@@ -13,12 +13,25 @@ namespace DCL.Backpack
         public Color eyesColor;
         public HashSet<string> forceRender = new ();
 
-        public AvatarModel ToAvatarModel()
+        public AvatarModel ToAvatarModel(Dictionary<string, string> extendedWearableUrns = null)
         {
+            List<string> wearables;
+
+            if (extendedWearableUrns != null)
+            {
+                wearables = new List<string>();
+
+                foreach (string w in this.wearables.Keys)
+                    wearables.Add(extendedWearableUrns.TryGetValue(w, out string extendedUrn)
+                        ? extendedUrn : w);
+            }
+            else
+                wearables = this.wearables.Keys.ToList();
+
             return new AvatarModel
             {
                 bodyShape = bodyShape.id,
-                wearables = wearables.Keys.ToList(),
+                wearables = wearables,
                 hairColor = hairColor,
                 skinColor = skinColor,
                 eyeColor = eyesColor,
