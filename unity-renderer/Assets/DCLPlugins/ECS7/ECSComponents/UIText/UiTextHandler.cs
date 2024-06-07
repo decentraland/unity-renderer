eea3a1c3d02889299035ce74dd5366db3156c4c0
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-06-07 13:24:34 -0300

    Support for new property TextWrap in UIText

--- unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/UIText/UiTextHandler.cs
+++ unity-renderer/Assets/DCLPlugins/ECS7/ECSComponents/UIText/UiTextHandler.cs
@@ -24,7 +24,6 @@ namespace DCL.ECSComponents.UIText
         public void OnComponentCreated(IParcelScene scene, IDCLEntity entity)
         {
             uiElement = new Label { text = string.Empty };
-            uiElement.style.whiteSpace = WhiteSpace.Normal;
             fontUpdater = new UIFontUpdater(uiElement, fontPromiseKeeper);
             UiElementUtils.SetElementDefaultStyle(uiElement.style);
             AddElementToRoot(scene, entity, uiElement);
@@ -40,6 +39,12 @@ namespace DCL.ECSComponents.UIText
         public void OnComponentModelUpdated(IParcelScene scene, IDCLEntity entity, PBUiText model)
         {
             uiElement.text = model.Value;
+
+            var wrapMode = WhiteSpace.Normal; // Default mode is Wrap enabled
+            if (model.HasTextWrap && model.TextWrap == TextWrap.TwNoWrap)
+                wrapMode = WhiteSpace.NoWrap;
+
+            uiElement.style.whiteSpace = wrapMode;
             uiElement.style.color = model.GetColor().ToUnityColor();
             uiElement.style.fontSize = model.GetFontSize();
             uiElement.style.unityTextAlign = model.GetTextAlign().ToUnityTextAlign();
