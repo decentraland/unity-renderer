651497d33fa84dc43b3f5ddac03e6e02d1866056
Nicolas Lorusso <nalorusso@gmail.com>
2024-07-17 12:33:19 -0300

    dont consider extended urns for collections-linked-wearables

--- unity-renderer/Assets/DCLServices/WearablesCatalogService/ExtendedUrnParser.cs
+++ unity-renderer/Assets/DCLServices/WearablesCatalogService/ExtendedUrnParser.cs
@@ -3,7 +3,8 @@
     public static class ExtendedUrnParser
     {
         private const int QUANTITY_OF_PARTS_ON_SHORTENED_ITEMS_URN = 6;
-        private const string COLLECTIONS_THIRDPARTY = "collections-thirdparty";
+        private const string COLLECTIONS_THIRD_PARTY = "collections-thirdparty";
+        private const string COLLECTIONS_LINKED_WEARABLES = "collections-linked-wearables";
 
         public static string GetShortenedUrn(string urnReceived)
         {
@@ -15,7 +16,8 @@
         }
 
         public static bool IsExtendedUrn(string urn) =>
-            urn.Split(':').Length > QUANTITY_OF_PARTS_ON_SHORTENED_ITEMS_URN && !urn.Contains(COLLECTIONS_THIRDPARTY);
+            urn.Split(':').Length > QUANTITY_OF_PARTS_ON_SHORTENED_ITEMS_URN
+            && !urn.Contains(COLLECTIONS_THIRD_PARTY)
+            && !urn.Contains(COLLECTIONS_LINKED_WEARABLES);
     }
-
 }
