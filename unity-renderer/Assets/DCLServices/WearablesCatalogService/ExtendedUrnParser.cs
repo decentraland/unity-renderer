ebc38416659c16be6c9f9961d032404338b69e34
Nicolas Lorusso <nalorusso@gmail.com>
2024-08-06 12:40:18 -0300

    removed collections-linked-wearables urn checks

--- unity-renderer/Assets/DCLServices/WearablesCatalogService/ExtendedUrnParser.cs
+++ unity-renderer/Assets/DCLServices/WearablesCatalogService/ExtendedUrnParser.cs
@@ -3,9 +3,7 @@
     public static class ExtendedUrnParser
     {
         private const int REGULAR_NFTS_SHORT_PARTS = 6;
-        private const int LINKED_NFTS_SHORT_PARTS = 8;
         private const string COLLECTIONS_THIRD_PARTY = "collections-thirdparty";
-        private const string COLLECTIONS_LINKED_WEARABLES = "collections-linked-wearables";
 
         public static string GetShortenedUrn(string urnReceived)
         {
@@ -22,9 +20,6 @@
         {
             if (urn.Contains(COLLECTIONS_THIRD_PARTY))
                 return false;
-
-            if (urn.Contains(COLLECTIONS_LINKED_WEARABLES))
-                return CountParts(urn) > LINKED_NFTS_SHORT_PARTS;
 
             return CountParts(urn) > REGULAR_NFTS_SHORT_PARTS;
         }
