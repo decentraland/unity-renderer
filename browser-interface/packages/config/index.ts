85fec7a1518c58425f75ddedb22201639c619620
Gabriel Díaz <gabrieldiaz31@gmail.com>
2025-01-24 14:32:55 -0300

    Merge pull request #6262 from decentraland/fix/catalog-using-peer-testing
    fix: Load items from builder-server using peer-testing

--- browser-interface/packages/config/index.ts
+++ browser-interface/packages/config/index.ts
@@ -107,6 +107,8 @@ export const BYPASS_CONTENT_ALLOWLIST = qs.has('BYPASS_CONTENT_ALLOWLIST')
   ? qs.get('BYPASS_CONTENT_ALLOWLIST') === 'true'
   : PIN_CATALYST || globalThis.location.hostname !== 'play.decentraland.org'
 
+export const PEER_TESTING_CATALYST = 'https://peer-testing.decentraland.org'
+
 const META_CONFIG_URL = ensureSingleString(qs.get('META_CONFIG_URL'))
 
 export const CHANNEL_TO_JOIN_CONFIG_URL = ensureSingleString(qs.get('CHANNEL'))
